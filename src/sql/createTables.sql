597b22e5046c5ca314488032b14ab66accd671ad
Alexander Gabriel <alex.barbalex@gmail.com>
2023-11-24 20:18:52 +0100

    work on sql

--- src/sql/createTables.sql
+++ src/sql/createTables.sql
@@ -54,6 +54,7 @@ DROP TABLE IF EXISTS projects CASCADE;
 
 CREATE TABLE projects(
   project_id uuid PRIMARY KEY DEFAULT public.uuid_generate_v7(),
+  account_id uuid DEFAULT NULL REFERENCES accounts(account_id) ON DELETE NO action ON UPDATE NO action,
   name text DEFAULT NULL,
   type text DEFAULT NULL,
   subproject_name_singular text DEFAULT NULL,
@@ -67,7 +68,46 @@ CREATE TABLE projects(
 
 CREATE INDEX ON projects USING btree(project_id);
 
+CREATE INDEX ON projects USING btree(account_id);
+
 CREATE INDEX ON projects USING btree(name);
 
 CREATE INDEX ON projects USING btree(deleted);
+
+COMMENT ON COLUMN projects.type IS '"species" or "biotope", preset: "species"';
+
+COMMENT ON COLUMN projects.subproject_name_singular IS 'Preset: "Art"';
+
+COMMENT ON COLUMN projects.subproject_name_plural IS 'Preset: "Arten"';
+
+COMMENT ON COLUMN projects.values_on_multiple_levels IS 'One of: "use first", "use second", "use all". Preset: "use first"';
+
+COMMENT ON COLUMN projects.multiple_action_values_on_same_level IS 'One of: "use all", "use last". Preset: "use all"';
+
+COMMENT ON COLUMN projects.multiple_check_values_on_same_level IS 'One of: "use all", "use last". Preset: "use last"';
+
+---------------------------------------------
+-- place_levels
+--
+DROP TABLE IF EXISTS place_levels CASCADE;
+
+CREATE TABLE place_levels(
+  place_level_id uuid PRIMARY KEY DEFAULT public.uuid_generate_v7(),
+  project_id uuid DEFAULT NULL REFERENCES projects(project_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  level integer DEFAULT NULL,
+  name_singular text DEFAULT NULL,
+  name_plural text DEFAULT NULL,
+  name_short text DEFAULT NULL,
+  order_by text DEFAULT NULL,
+  reports boolean DEFAULT FALSE,
+  report_values boolean DEFAULT FALSE,
+  actions boolean DEFAULT FALSE,
+  action_values boolean DEFAULT FALSE,
+  action_reports boolean DEFAULT FALSE,
+  checks boolean DEFAULT FALSE,
+  check_values boolean DEFAULT FALSE,
+  check_taxons boolean DEFAULT FALSE,
+  observation_references boolean DEFAULT FALSE,
+  deleted boolean DEFAULT FALSE
+);
 
