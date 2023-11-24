fb262d597698357b57a60b28bf594607a3785969
Alexander Gabriel <alex.barbalex@gmail.com>
2023-11-24 18:09:58 +0100

    work on sql

--- src/sql/createTables.sql
+++ src/sql/createTables.sql
@@ -1,5 +1,8 @@
 CREATE EXTENSION IF NOT EXISTS postgis;
 
+---------------------------------------------
+-- accounts
+--
 DROP TABLE IF EXISTS accounts CASCADE;
 
 CREATE TABLE accounts(
@@ -15,17 +18,15 @@ CREATE INDEX ON accounts USING btree(account_id);
 CREATE INDEX ON accounts USING gist(period);
 
 ---------------------------------------------
+-- users
+--
 DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users(
   user_id uuid PRIMARY KEY DEFAULT public.uuid_generate_v7(),
-  -- email needs to be unique
-  -- project manager can list project user by email without knowing if this user already exists
-  -- then user can create a login (= row in user table) and work in the project
-  email text UNIQUE DEFAULT NULL,
+  email text UNIQUE DEFAULT NULL, -- TODO: email needs to be unique
   person_id uuid DEFAULT NULL,
   account_id uuid DEFAULT NULL REFERENCES accounts(account_id) ON DELETE NO action ON UPDATE NO action,
-  -- auth_id uuid DEFAULT NULL REFERENCES auth.user (id) ON DELETE NO action ON UPDATE CASCADE,
   auth_id uuid DEFAULT NULL,
   deleted boolean DEFAULT FALSE
 );
