d6e6eea40aaf958e676cf69a5d85e1abb3fded70
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2023-11-25 15:57:40 +0100

    add sql for tables

--- src/sql/createTables.sql
+++ src/sql/createTables.sql
@@ -465,3 +465,44 @@ COMMENT ON COLUMN units.name IS 'Name of unit, like "Anzahl"';
 
 COMMENT ON COLUMN units.summable IS 'Whether values of this unit can be summed. Else: distribution of count per value. Preset: false';
 
+---------------------------------------------
+-- places
+--
+DROP TABLE IF EXISTS places CASCADE;
+
+CREATE TABLE places(
+  place_id uuid PRIMARY KEY DEFAULT public.uuid_generate_v7(),
+  subproject_id uuid DEFAULT NULL REFERENCES subprojects(subproject_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  parent_id uuid DEFAULT NULL REFERENCES places(place_id) ON DELETE NO action ON UPDATE CASCADE,
+  level integer DEFAULT 1,
+  data jsonb DEFAULT NULL,
+  geometry geometry(GeometryCollection, 4326) DEFAULT NULL,
+  deleted boolean DEFAULT FALSE
+);
+
+CREATE INDEX ON places USING btree(place_id);
+
+CREATE INDEX ON places USING btree(subproject_id);
+
+CREATE INDEX ON places USING btree(parent_id);
+
+CREATE INDEX ON places USING btree(level);
+
+CREATE INDEX ON places USING gin(data);
+
+CREATE INDEX ON places USING gist(geometry);
+
+CREATE INDEX ON places((1))
+WHERE
+  deleted;
+
+COMMENT ON COLUMN places.subproject_id IS 'always set to optimize queries';
+
+COMMENT ON COLUMN places.parent_id IS 'parent place. null for places of level 1';
+
+COMMENT ON COLUMN places.level IS 'level of place: 1, 2';
+
+COMMENT ON COLUMN places.data IS 'Room for place specific data, defined in "fields" table';
+
+COMMENT ON COLUMN places.geometry IS 'geometry of place';
+
