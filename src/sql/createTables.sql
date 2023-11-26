dfb86dd1a36471831efee6bfbf345e76e13b2657
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2023-11-26 12:18:36 +0100

    add sql for tables

--- src/sql/createTables.sql
+++ src/sql/createTables.sql
@@ -921,8 +921,14 @@ CREATE TABLE user_messages(
   user_message_id uuid PRIMARY KEY DEFAULT public.uuid_generate_v7(),
   user_id uuid DEFAULT NULL REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
   message_id uuid DEFAULT NULL REFERENCES message(message_id) ON DELETE CASCADE ON UPDATE CASCADE,
-  read boolean DEFAULT FALSE,
-  deleted boolean DEFAULT FALSE
+  read boolean DEFAULT FALSE
 );
+
+CREATE INDEX ON user_messages USING btree(user_message_id);
+
+CREATE INDEX ON user_messages USING btree(user_id);
+
+CREATE INDEX ON user_messages USING btree(message_id);
+
 
 
