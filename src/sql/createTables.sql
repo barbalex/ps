c8774695fd1b94b4e18f5cdbb892127a5807d9b0
Alexander Gabriel <alex.barbalex@gmail.com>
2023-11-27 16:22:44 +0100

    build tables

--- src/sql/createTables.sql
+++ src/sql/createTables.sql
@@ -1383,3 +1383,32 @@ COMMENT ON COLUMN files.file IS 'file content';
 
 COMMENT ON COLUMN files.url IS 'URL of file, if it is saved on a web service';
 
+---------------------------------------------
+-- persons
+--
+DROP TABLE IF EXISTS persons CASCADE;
+
+CREATE TABLE persons(
+  person_id uuid PRIMARY KEY DEFAULT public.uuid_generate_v7(),
+  account_id uuid DEFAULT NULL REFERENCES accounts(account_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  data jsonb DEFAULT NULL,
+  order_by text DEFAULT NULL,
+  deleted boolean DEFAULT FALSE
+);
+
+CREATE INDEX ON persons USING btree(person_id);
+
+CREATE INDEX ON persons USING btree(account_id);
+
+CREATE INDEX ON persons((1))
+WHERE
+  deleted;
+
+COMMENT ON TABLE persons IS 'Persons are used to assign actions and checks to';
+
+COMMENT ON COLUMN persons.account_id IS 'redundant account_id enhances data safety';
+
+COMMENT ON COLUMN persons.data IS 'Room for person specific data, defined in "fields" table';
+
+COMMENT ON COLUMN persons.order_by IS 'Used to order persons in lists. Contains a field included in the data. Can be a comma separated list of fields. TODO: One or multiple comma separated virtual fields will be added and indexed in sqlite and postgresql.';
+
