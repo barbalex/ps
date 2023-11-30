6a02eca7e6fdb041f92fd699a05dacb4b577ec04
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2023-11-30 19:18:20 +0100

    try to electrify users

--- src/sql/createTables.sql
+++ src/sql/createTables.sql
@@ -6,11 +6,12 @@ CREATE EXTENSION IF NOT EXISTS postgis;
 DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users(
-  user_id uuid PRIMARY KEY DEFAULT gen_random_uuid() -- public.uuid_generate_v7() not supported by electric-sql
+  user_id uuid PRIMARY KEY DEFAULT public.uuid_generate_v7(),
+  -- gen_random_uuid() -- public.uuid_generate_v7() not supported by electric-sql
   -- no accout_id as users are app level
   email text DEFAULT NULL, -- TODO: email needs to be unique per account. But: not possible in electric-sql
   auth_id uuid DEFAULT NULL,
-  deleted boolean DEFAULT NULL -- electric does not support boolean default false
+  deleted boolean DEFAULT FALSE -- electric does not support boolean default false
 );
 
 CREATE INDEX ON users USING btree(user_id);
