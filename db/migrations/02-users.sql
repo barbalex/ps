438256eb48fcd35539b915b95a0752f9338dd4e1
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2023-11-30 19:10:16 +0100

    try to electrify users

---
+++ db/migrations/02-users.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users(
+  user_id uuid PRIMARY KEY DEFAULT NULL -- public.uuid_generate_v7() not supported by electric-sql
+  -- no accout_id as users are app level
+  email text DEFAULT NULL, -- TODO: email needs to be unique per account. But: not possible in electric-sql
+  auth_id uuid DEFAULT NULL,
+  deleted boolean DEFAULT NULL -- electric does not support boolean default false
+);
+
+CREATE INDEX ON users USING btree(user_id);
+
+CREATE INDEX ON users USING btree(email);
+
+CREATE INDEX ON users((1))
+WHERE
+  deleted;
+
+COMMENT ON COLUMN users.email IS 'email needs to be unique. project manager can list project user by email before this user creates an own login (thus has no user_id yet)';
+
+COMMENT ON TABLE users IS 'Goal: manage users and authorize them';
+
