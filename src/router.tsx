83ffd0bd6f077ad5eb5e20c8b687bfc8136f570d
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-06 15:17:33 +0100

    refactor routing

--- src/router.tsx
+++ src/router.tsx
@@ -9,7 +9,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        lazy: () => import('./routes/home'),
+        element: null,
         errorElement: <ErrorPage />,
         handle: {
           crumb: () => <Link to="/">Home</Link>,
@@ -27,185 +27,205 @@ export const router = createBrowserRouter([
             </>
           ),
         },
-      },
-      {
-        path: 'users',
-        lazy: () => import('./routes/users'),
-        handle: {
-          crumb: () => (
-            <>
-              <Link to="/">Home</Link>
-              <div>&rArr;</div>
-              <Link to="/users">Users</Link>
-            </>
-          ),
-        },
-        to: () => (
-          <>
-            <Link to="/">Home</Link>
-            <Link to="/users">Users</Link>
-          </>
-        ),
-      },
-      {
-        path: 'users/:user_id',
-        lazy: () => import('./routes/user'),
-        handle: {
-          crumb: () => (
-            <>
-              <Link to="/">Home</Link>
-              <div>&rArr;</div>
-              <Link to="/users">Users</Link>
-              <div>&rArr;</div>
-              <Link to="/users/:user_id">User</Link>
-            </>
-          ),
-        },
-      },
-      {
-        path: 'accounts',
-        lazy: () => import('./routes/accounts'),
-        handle: {
-          crumb: () => (
-            <>
-              <Link to="/">Home</Link>
-              <div>&rArr;</div>
-              <Link to="/accounts">Accounts</Link>
-            </>
-          ),
-        },
-      },
-      {
-        path: 'projects',
-        lazy: () => import('./routes/Projects'),
-        handle: {
-          crumb: () => (
-            <>
-              <Link to="/">Home</Link>
-              <div>&rArr;</div>
-              <Link to="/projects">Projects</Link>
-            </>
-          ),
-        },
-      },
-      {
-        path: 'projects/:project_id',
-        lazy: () => import('./routes/project'),
-        handle: {
-          crumb: (match) => (
-            <>
-              <Link to="/">Home</Link>
-              <div>&rArr;</div>
-              <Link to="/projects">Projects</Link>
-              <div>&rArr;</div>
-              <Link to={`/projects/${match.params.project_id}`}>Project</Link>
-            </>
-          ),
-          to: (match) => (
-            <>
-              <Link to={`/projects/${match.params.project_id}/subprojects`}>
-                Subprojects
-              </Link>
-            </>
-          ),
-        },
-      },
-      {
-        path: 'projects/:project_id/subprojects',
-        lazy: () => import('./routes/subprojects'),
-        handle: {
-          crumb: (match) => (
-            <>
-              <Link to="/">Home</Link>
-              <div>&rArr;</div>
-              <Link to="/projects">Projects</Link>
-              <div>&rArr;</div>
-              <Link to={`/projects/${match.params.project_id}`}>Project</Link>
-              <div>&rArr;</div>
-              <Link to={`/projects/${match.params.project_id}/subprojects`}>
-                Subprojects
-              </Link>
-            </>
-          ),
-        },
-      },
-      {
-        path: 'field-types',
-        lazy: () => import('./routes/fieldTypes'),
-        handle: {
-          crumb: () => (
-            <>
-              <Link to="/">Home</Link>
-              <div>&rArr;</div>
-              <Link to="/field-types">Field Types</Link>
-            </>
-          ),
-        },
-      },
-      {
-        path: 'widget-types',
-        lazy: () => import('./routes/widgetTypes'),
-        handle: {
-          crumb: () => (
-            <>
-              <Link to="/">Home</Link>
-              <div>&rArr;</div>
-              <Link to="/widget-types">Widget Types</Link>
-            </>
-          ),
-        },
-      },
-      {
-        path: 'widgets-for-fields',
-        lazy: () => import('./routes/widgetsForFields'),
-        handle: {
-          crumb: () => (
-            <>
-              <Link to="/">Home</Link>
-              <div>&rArr;</div>
-              <Link to="/widgets-for-fields">Widgets For Fields</Link>
-            </>
-          ),
-        },
-      },
-      {
-        path: 'files',
-        lazy: () => import('./routes/files'),
-        handle: {
-          crumb: () => (
-            <>
-              <Link to="/">Home</Link>
-              <div>&rArr;</div>
-              <Link to="/files">Files</Link>{' '}
-            </>
-          ),
-        },
-      },
-      {
-        path: 'messages',
-        lazy: () => import('./routes/messages'),
-        handle: {
-          crumb: () => (
-            <>
-              <Link to="/">Home</Link>
-              <div>&rArr;</div>
-              <Link to="/messages">Messages</Link>
-            </>
-          ),
-        },
-      },
-      {
-        path: 'docs',
-        lazy: () => import('./routes/docs'),
-        handle: {
-          crumb: () => (
-            <>
-              <Link to="/">Home</Link>
-              <div>&rArr;</div>
-              <Link to="/docs">Docs</Link>
-            </>
-          ),
-        },
+        children: [
+          {
+            index: true,
+            lazy: () => import('./routes/home'),
+          },
+          {
+            path: 'users',
+            element: null,
+            handle: {
+              crumb: () => (
+                <>
+                  <Link to="/">Home</Link>
+                  <div>&rArr;</div>
+                  <Link to="/users">Users</Link>
+                </>
+              ),
+            },
+            children: [
+              {
+                index: true,
+                lazy: () => import('./routes/users'),
+              },
+              {
+                path: ':user_id',
+                lazy: () => import('./routes/user'),
+                handle: {
+                  crumb: () => (
+                    <>
+                      <Link to="/">Home</Link>
+                      <div>&rArr;</div>
+                      <Link to="/users">Users</Link>
+                      <div>&rArr;</div>
+                      <Link to="/users/:user_id">User</Link>
+                    </>
+                  ),
+                },
+              },
+            ],
+          },
+          {
+            path: 'accounts',
+            lazy: () => import('./routes/accounts'),
+            handle: {
+              crumb: () => (
+                <>
+                  <Link to="/">Home</Link>
+                  <div>&rArr;</div>
+                  <Link to="/accounts">Accounts</Link>
+                </>
+              ),
+            },
+          },
+          {
+            path: 'projects',
+            element: null,
+            handle: {
+              crumb: () => (
+                <>
+                  <Link to="/">Home</Link>
+                  <div>&rArr;</div>
+                  <Link to="/projects">Projects</Link>
+                </>
+              ),
+            },
+            children: [
+              { index: true, lazy: () => import('./routes/projects') },
+              {
+                path: ':project_id',
+                element: null,
+                handle: {
+                  crumb: (match) => (
+                    <>
+                      <Link to="/">Home</Link>
+                      <div>&rArr;</div>
+                      <Link to="/projects">Projects</Link>
+                      <div>&rArr;</div>
+                      <Link to={`/projects/${match.params.project_id}`}>
+                        Project
+                      </Link>
+                    </>
+                  ),
+                  to: (match) => (
+                    <>
+                      <Link
+                        to={`/projects/${match.params.project_id}/subprojects`}
+                      >
+                        Subprojects
+                      </Link>
+                    </>
+                  ),
+                },
+                children: [
+                  { index: true, lazy: () => import('./routes/subprojects') },
+                  {
+                    path: 'projects/:project_id/subprojects',
+                    lazy: () => import('./routes/subprojects'),
+                    handle: {
+                      crumb: (match) => (
+                        <>
+                          <Link to="/">Home</Link>
+                          <div>&rArr;</div>
+                          <Link to="/projects">Projects</Link>
+                          <div>&rArr;</div>
+                          <Link to={`/projects/${match.params.project_id}`}>
+                            Project
+                          </Link>
+                          <div>&rArr;</div>
+                          <Link
+                            to={`/projects/${match.params.project_id}/subprojects`}
+                          >
+                            Subprojects
+                          </Link>
+                        </>
+                      ),
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            path: 'field-types',
+            lazy: () => import('./routes/fieldTypes'),
+            handle: {
+              crumb: () => (
+                <>
+                  <Link to="/">Home</Link>
+                  <div>&rArr;</div>
+                  <Link to="/field-types">Field Types</Link>
+                </>
+              ),
+            },
+          },
+          {
+            path: 'widget-types',
+            lazy: () => import('./routes/widgetTypes'),
+            handle: {
+              crumb: () => (
+                <>
+                  <Link to="/">Home</Link>
+                  <div>&rArr;</div>
+                  <Link to="/widget-types">Widget Types</Link>
+                </>
+              ),
+            },
+          },
+          {
+            path: 'widgets-for-fields',
+            lazy: () => import('./routes/widgetsForFields'),
+            handle: {
+              crumb: () => (
+                <>
+                  <Link to="/">Home</Link>
+                  <div>&rArr;</div>
+                  <Link to="/widgets-for-fields">Widgets For Fields</Link>
+                </>
+              ),
+            },
+          },
+          {
+            path: 'files',
+            lazy: () => import('./routes/files'),
+            handle: {
+              crumb: () => (
+                <>
+                  <Link to="/">Home</Link>
+                  <div>&rArr;</div>
+                  <Link to="/files">Files</Link>{' '}
+                </>
+              ),
+            },
+          },
+          {
+            path: 'messages',
+            lazy: () => import('./routes/messages'),
+            handle: {
+              crumb: () => (
+                <>
+                  <Link to="/">Home</Link>
+                  <div>&rArr;</div>
+                  <Link to="/messages">Messages</Link>
+                </>
+              ),
+            },
+          },
+          {
+            path: 'docs',
+            lazy: () => import('./routes/docs'),
+            handle: {
+              crumb: () => (
+                <>
+                  <Link to="/">Home</Link>
+                  <div>&rArr;</div>
+                  <Link to="/docs">Docs</Link>
+                </>
+              ),
+            },
+          },
+        ],
       },
     ],
   },
