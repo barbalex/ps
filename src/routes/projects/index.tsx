88305d1ef0261d01001725464349bbddc6714cd1
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-06 10:47:36 +0100

    add projects

---
+++ src/routes/projects/index.tsx
@@ -0,0 +1,46 @@
+import { useLiveQuery } from 'electric-sql/react'
+import { uuidv7 } from '@kripod/uuidv7'
+import { Link } from 'react-router-dom'
+
+import { Projects as Project } from '../../../generated/client'
+import { useElectric } from '../../ElectricProvider'
+import '../../User.css'
+
+export const Projects = () => {
+  const { db } = useElectric()!
+  const { results } = useLiveQuery(db.projects.liveMany())
+
+  const add = async () => {
+    await db.projects.create({
+      data: {
+        project_id: uuidv7(),
+      },
+    })
+  }
+
+  const clear = async () => {
+    await db.projects.deleteMany()
+  }
+
+  const projects: Project[] = results ?? []
+
+  return (
+    <div>
+      <div className="controls">
+        <button className="button" onClick={add}>
+          Add
+        </button>
+        <button className="button" onClick={clear}>
+          Clear
+        </button>
+      </div>
+      {projects.map((project: Project, index: number) => (
+        <p key={index} className="item">
+          <Link to={`/projects/${project.project_id}`}>
+            {project.project_id}
+          </Link>
+        </p>
+      ))}
+    </div>
+  )
+}
