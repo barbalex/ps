0910c4861d52844d222ef7ce0bca1eee8c02992c
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-08 18:09:53 +0100

    comment

--- src/components/Navs.tsx
+++ src/components/Navs.tsx
@@ -2,12 +2,16 @@ import { useMatches, useLocation } from 'react-router-dom'
 
 // new idea
 // get all matches for next level down
+// But: does not seem possible with react-router
+// Hard part would be to navigate through the route object using the location
+// Plus: Adding other stuff is probably easier with the current approach
+// like: inserting, deleting etc.
 
 export const Navs = () => {
   const location = useLocation()
   const matches = useMatches()
 
-  console.log('Navs, matches', matches)
+  // console.log('Navs, matches', matches)
 
   const tos = matches
     .filter((match) => match.pathname === location.pathname)
