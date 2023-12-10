4abfdbf5f8d1f088d3a6e78c0dbe5bab9d916f46
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-10 22:44:36 +0100

    begin breadcrumb menu

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,13 +1,20 @@
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 import { useMatches, useNavigate } from 'react-router-dom'
 import { BsCaretDown } from 'react-icons/bs'
 import IconButton from '@mui/material/IconButton'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
 
 import './breadcrumbs.css'
 
 export const Breadcrumbs = () => {
   const matches = useMatches()
   const navigate = useNavigate()
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const openMenu = Boolean(anchorEl)
+
+  const closeMenu = useCallback(() => setAnchorEl(null), [])
 
   const filteredMatches = matches.filter((match) => match.handle?.crumb)
 
@@ -17,34 +24,49 @@ export const Breadcrumbs = () => {
   const onClick = useCallback(({ e, table, folder, match }) => {
     e.stopPropagation()
     console.log('clicked', { match, table, folder })
+    setAnchorEl(e.currentTarget)
   }, [])
 
   return (
-    <nav className="breadcrumbs">
-      {filteredMatches.map((match, index) => {
-        const { text, table, folder } =
-          match?.handle?.crumb?.(match) ?? {}
-        const className =
-          location.pathname === match.pathname
-            ? 'breadcrumbs__crumb is-active'
-            : 'breadcrumbs__crumb link'
+    <>
+      <nav className="breadcrumbs">
+        {filteredMatches.map((match, index) => {
+          const { text, table, folder } = match?.handle?.crumb?.(match) ?? {}
+          const className =
+            location.pathname === match.pathname
+              ? 'breadcrumbs__crumb is-active'
+              : 'breadcrumbs__crumb link'
 
-        return (
-          <div
-            className={className}
-            key={index}
-            onClick={() => navigate(match.pathname)}
-          >
-            {text}
-            <IconButton
-              onClick={(e) => onClick({ e, table, folder, match })}
-              className="icon"
+          return (
+            <div
+              className={className}
+              key={index}
+              onClick={() => navigate(match.pathname)}
             >
-              <BsCaretDown />
-            </IconButton>
-          </div>
-        )
-      })}
-    </nav>
+              {text}
+              <IconButton
+                onClick={(e) => onClick({ e, table, folder, match })}
+                className="icon"
+              >
+                <BsCaretDown />
+              </IconButton>
+            </div>
+          )
+        })}
+      </nav>{' '}
+      <Menu
+        id="breadcrumb-menu"
+        anchorEl={anchorEl}
+        open={openMenu}
+        onClose={closeMenu}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={closeMenu}>Profile</MenuItem>
+        <MenuItem onClick={closeMenu}>My account</MenuItem>
+        <MenuItem onClick={closeMenu}>Logout</MenuItem>
+      </Menu>
+    </>
   )
 }
