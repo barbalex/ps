a7a736fbabb996ea4a2c724a2f8bd7f69e01565c
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2023-12-12 15:10:47 +0100

    clean

--- src/routes/project.tsx
+++ src/routes/project.tsx
@@ -1,8 +1,11 @@
+import { useCallback } from 'react'
 import { useLiveQuery } from 'electric-sql/react'
 import { uuidv7 } from '@kripod/uuidv7'
-import { useParams } from 'react-router-dom'
+import { Form, useParams } from 'react-router-dom'
+import FormControl from '@mui/material/FormControl'
 
 import { Projects as Project } from '../../../generated/client'
+import { TextField } from '../components/shared/TextField'
 
 import '../User.css'
 
@@ -15,7 +18,7 @@ export const Component = () => {
     db.projects.liveUnique({ where: { project_id } }),
   )
 
-  const addItem = async () => {
+  const addRow = async () => {
     await db.projects.create({
       data: {
         project_id: uuidv7(),
@@ -23,23 +26,38 @@ export const Component = () => {
     })
   }
 
-  const clearItems = async () => {
-    await db.projects.deleteMany()
+  const deleteRow = async () => {
+    await db.projects.delete({
+      where: {
+        project_id,
+      },
+    })
   }
 
-  const project: Project = results
+  const row: Project = results
+
+  const onBlur = useCallback(
+    (event) => {
+      db.projects.update({
+        where: { project_id },
+        data: { [event.target.name]: event.target.value },
+      })
+    },
+    [db.projects, project_id],
+  )
 
   return (
     <div>
       <div className="controls">
-        <button className="button" onClick={addItem}>
+        <button className="button" onClick={addRow}>
           Add
         </button>
-        <button className="button" onClick={clearItems}>
-          Clear
+        <button className="button" onClick={deleteRow}>
+          Delete
         </button>
       </div>
-      <div>{`Project with id ${project?.project_id ?? ''}`}</div>
+      <div>{`Project with id ${row?.project_id ?? ''}`}</div>
+      {/* <TextField name="name" label="Name" value={row.name} onBlur={onBlur} /> */}
     </div>
   )
 }
