f63792e90c988074994e5b791d9b5c2c2debc366
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-13 13:46:41 +0100

    rename owner_user_id

--- db/migrations/02-accounts.sql
+++ db/migrations/02-accounts.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS accounts CASCADE;
 
 CREATE TABLE accounts(
   account_id uuid PRIMARY KEY DEFAULT NULL, -- public.uuid_generate_v7(),
-  owning_user_id uuid DEFAULT NULL REFERENCES users(user_id) ON DELETE NO action ON UPDATE NO action,
+  user_id uuid DEFAULT NULL REFERENCES users(user_id) ON DELETE NO action ON UPDATE NO action,
   type text DEFAULT NULL,
   period_start date DEFAULT NULL,
   period_end date DEFAULT NULL
@@ -12,7 +12,7 @@ CREATE TABLE accounts(
 -- where period @> '2023-11-01'::date
 CREATE INDEX ON accounts USING btree(account_id);
 
-CREATE INDEX ON accounts USING btree(owning_user_id);
+CREATE INDEX ON accounts USING btree(user_id);
 
 CREATE INDEX ON accounts USING btree(period_start);
 
@@ -20,7 +20,7 @@ CREATE INDEX ON accounts USING btree(period_end);
 
 COMMENT ON TABLE accounts IS 'Goal: earn money';
 
-COMMENT ON COLUMN accounts.owning_user_id IS 'user that owns the account. null for accounts that are not owned by a user';
+COMMENT ON COLUMN accounts.user_id IS 'user that owns the account. null for accounts that are not owned by a user';
 
 COMMENT ON COLUMN accounts.type IS 'type of account: "free", "basic", "premium"? (TODO: needs to be defined)';
 
