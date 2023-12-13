9b6b9e9fb4ad0498a0195c8e318454d373043706
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-13 14:09:23 +0100

    solve issue

--- db/migrations/39-fields.sql
+++ db/migrations/39-fields.sql
@@ -3,8 +3,8 @@ CREATE TABLE fields(
   project_id uuid DEFAULT NULL REFERENCES projects(project_id) ON DELETE CASCADE ON UPDATE CASCADE,
   account_id uuid DEFAULT NULL REFERENCES accounts(account_id) ON DELETE CASCADE ON UPDATE CASCADE,
   tble text DEFAULT NULL,
-  type text DEFAULT NULL REFERENCES field_types(field_type) ON DELETE CASCADE ON UPDATE CASCADE,
-  widget text DEFAULT NULL REFERENCES widget_types(widget_type) ON DELETE CASCADE ON UPDATE CASCADE,
+  field_type text DEFAULT NULL REFERENCES field_types(field_type) ON DELETE CASCADE ON UPDATE CASCADE,
+  widget_type text DEFAULT NULL REFERENCES widget_types(widget_type) ON DELETE CASCADE ON UPDATE CASCADE,
   label text DEFAULT NULL,
   list_id uuid DEFAULT NULL REFERENCES lists(list_id) ON DELETE NO action ON UPDATE CASCADE,
   preset text DEFAULT NULL,
@@ -20,9 +20,9 @@ CREATE INDEX ON fields USING btree(account_id);
 
 CREATE INDEX ON fields USING btree(tble);
 
-CREATE INDEX ON fields USING btree(type);
+CREATE INDEX ON fields USING btree(field_type);
 
-CREATE INDEX ON fields USING btree(widget);
+CREATE INDEX ON fields USING btree(widget_type);
 
 CREATE INDEX ON fields USING btree(label);
 
--- src/sql/createTables.sql
+++ src/sql/createTables.sql
@@ -34,7 +34,7 @@ DROP TABLE IF EXISTS accounts CASCADE;
 
 CREATE TABLE accounts(
   account_id uuid PRIMARY KEY DEFAULT public.uuid_generate_v7(),
-  owning_user_id uuid DEFAULT NULL REFERENCES users(user_id) ON DELETE NO action ON UPDATE NO action,
+  user_id uuid DEFAULT NULL REFERENCES users(user_id) ON DELETE NO action ON UPDATE NO action,
   type text DEFAULT NULL,
   -- period daterange DEFAULT NULL -- not supported by electric-sql
   period_start date DEFAULT NULL,
@@ -45,7 +45,7 @@ CREATE TABLE accounts(
 -- where period @> '2023-11-01'::date
 CREATE INDEX ON accounts USING btree(account_id);
 
-CREATE INDEX ON accounts USING btree(owning_user_id);
+CREATE INDEX ON accounts USING btree(user_id);
 
 -- CREATE INDEX ON accounts USING gist(period);
 CREATE INDEX ON accounts USING btree(period_start);
@@ -54,7 +54,7 @@ CREATE INDEX ON accounts USING btree(period_end);
 
 COMMENT ON TABLE accounts IS 'Goal: earn money';
 
-COMMENT ON COLUMN accounts.owning_user_id IS 'user that owns the account. null for accounts that are not owned by a user';
+COMMENT ON COLUMN accounts.user_id IS 'user that owns the account. null for accounts that are not owned by a user';
 
 COMMENT ON COLUMN accounts.type IS 'type of account: "free", "basic", "premium"? (TODO: needs to be defined)';
 
