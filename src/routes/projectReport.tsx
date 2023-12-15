54a855cd1c48d4ef5cef5b6368679958188b746f
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-15 19:25:24 +0100

    padd only form containers

--- src/routes/projectReport.tsx
+++ src/routes/projectReport.tsx
@@ -32,7 +32,7 @@ export const Component = () => {
   const projectReport: ProjectReport = results
 
   return (
-    <div>
+    <div className="form-container">
       <div className="controls">
         <button className="button" onClick={addItem}>
           Add
