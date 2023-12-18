676908b629272c87af8243ff35f7825965a5352b
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-18 18:04:56 +0100

    outsource

---
+++ src/modules/idFieldFromTable.ts
@@ -0,0 +1,12 @@
+export const idFieldFromTable = (table: string): string =>
+  table.endsWith('taxa')
+    ? `${table.slice(0, -1)}on_id`
+    : table === 'taxonomies'
+    ? 'taxonomy'
+    : table === 'field_types'
+    ? 'field_type'
+    : table === 'widget_types'
+    ? 'widget_type'
+    : table === 'widgets_for_fields'
+    ? 'widget_for_field_id'
+    : `${table.slice(0, -1)}_id`
