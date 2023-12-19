035bc854777b9d261f084be82bbb5e93d0259b36
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2023-12-19 20:19:38 +0100

    Add support for subproject users in labelFromData function

--- src/modules/labelFromData.ts
+++ src/modules/labelFromData.ts
@@ -85,6 +85,9 @@ export const labelFromData = ({ data, table }) => {
     case 'goal_report_values':
       return data.goal_report_value_id
       break
+    case 'subproject_users':
+      return data.subproject_user_id
+      break
     default:
       return undefined
   }
