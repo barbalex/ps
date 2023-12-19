64233e52d1d1068512521c3f064dd4f3cf8e14fd
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2023-12-19 20:16:52 +0100

    Add new cases to labelFromData function

--- src/modules/labelFromData.ts
+++ src/modules/labelFromData.ts
@@ -67,6 +67,24 @@ export const labelFromData = ({ data, table }) => {
       // TODO: build virtual field from data.label, return that here
       return data.place_id
       break
+    case 'place_users':
+      return data.place_user_id
+      break
+    case 'subproject_taxa':
+      return data.subproject_taxon_id
+      break
+    case 'subproject_reports':
+      return data.year ?? data.subproject_report_id
+      break
+    case 'goals':
+      return data.goal_id
+      break
+    case 'goal_reports':
+      return data.goal_report_id
+      break
+    case 'goal_report_values':
+      return data.goal_report_value_id
+      break
     default:
       return undefined
   }
