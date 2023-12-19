4ff0217189e733561936d079ef3dce752d9ecf6c
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-19 12:32:48 +0100

    Add subproject form functionality

--- src/routes/subproject.tsx
+++ src/routes/subproject.tsx
@@ -1,47 +1,125 @@
+import { useCallback } from 'react'
 import { useLiveQuery } from 'electric-sql/react'
 import { uuidv7 } from '@kripod/uuidv7'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
+import { FaPlus, FaMinus } from 'react-icons/fa'
+import { Button, Switch } from '@fluentui/react-components'
 
 import { Subprojects as Subproject } from '../../../generated/client'
+import { subproject as createSubprojectPreset } from '../modules/dataPresets'
 
 import '../form.css'
 
 import { useElectric } from '../ElectricProvider'
+import { TextField } from '../components/shared/TextField'
+import { TextFieldInactive } from '../components/shared/TextFieldInactive'
 
 export const Component = () => {
-  const { db } = useElectric()!
   const { project_id, subproject_id } = useParams()
+  const navigate = useNavigate()
+
+  const { db } = useElectric()
   const { results } = useLiveQuery(
     db.subprojects.liveUnique({ where: { subproject_id } }),
   )
 
-  const addItem = async () => {
+  const addRow = async () => {
+    const newSubproject = createSubprojectPreset()
     await db.subprojects.create({
       data: {
-        subproject_id: uuidv7(),
+        ...newSubproject,
         project_id,
         // TODO: add account_id
       },
     })
+    navigate(
+      `/projects/${project_id}/subprojects/${newSubproject.subproject_id}`,
+    )
   }
 
-  const clearItems = async () => {
-    await db.subprojects.deleteMany()
+  const deleteRow = async () => {
+    await db.subprojects.delete({
+      where: {
+        subproject_id,
+      },
+    })
+    navigate(`/projects/${project_id}/subprojects`)
   }
 
-  const subproject: Subproject = results
+  const row: Subproject = results
+
+  const onChange = useCallback(
+    (e, data) => {
+      const targetType = e.target.type
+      const value =
+        targetType === 'checkbox'
+          ? data.checked
+          : targetType === 'number'
+          ? e.target.valueAsNumber ?? null
+          : e.target.value ?? null // targetType === 'checkbox' ? data.checked :
+      const name = e.target.name
+      // console.log('onChange', {
+      //   name,
+      //   targetType,
+      //   value,
+      // })
+      db.subprojects.update({
+        where: { subproject_id },
+        data: { [name]: value },
+      })
+    },
+    [db.subprojects, subproject_id],
+  )
+
+  if (!row) {
+    return <div>Loading...</div>
+  }
 
   return (
     <div className="form-container">
       <div className="controls">
-        <button className="button" onClick={addItem}>
-          Add
-        </button>
-        <button className="button" onClick={clearItems}>
-          Clear
-        </button>
+        <Button
+          size="large"
+          icon={<FaPlus />}
+          onClick={addRow}
+          title="Add new subproject" // TODO: use subproject_name_singular
+        />
+        <Button
+          size="large"
+          icon={<FaMinus />}
+          onClick={deleteRow}
+          title="Delete subproject" // TODO: use subproject_name_singular
+        />
       </div>
-      <div>{`Subproject with id ${subproject?.subproject_id ?? ''}`}</div>
+      <TextFieldInactive
+        label="Account"
+        name="account_id"
+        value={row.account_id ?? ''}
+      />
+      <TextFieldInactive
+        label="Project"
+        name="project_id"
+        value={row.project_id ?? ''}
+      />
+      <TextField
+        label="Name"
+        name="name"
+        value={row.name ?? ''}
+        onChange={onChange}
+      />
+      <TextField
+        label="Since year"
+        name="since_year"
+        value={row.since_year ?? ''}
+        type="number"
+        onChange={onChange}
+      />
+      <Switch
+        label="Enable uploading files to subprojects"
+        name="files_active"
+        checked={row.files_active ?? false}
+        onChange={onChange}
+      />
     </div>
   )
 }
