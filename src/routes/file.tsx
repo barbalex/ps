cd71fe7d596b0c8d4397d3bb899099b4966f542b
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2023-12-19 15:33:18 +0100

    Refactor file.tsx: Add useNavigate hook and createFilePreset function

--- src/routes/file.tsx
+++ src/routes/file.tsx
@@ -1,28 +1,31 @@
 import { useLiveQuery } from 'electric-sql/react'
-import { uuidv7 } from '@kripod/uuidv7'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 import { Files as File } from '../../../generated/client'
+import { file as createFilePreset } from '../modules/dataPresets'
 
 import '../form.css'
 
 import { useElectric } from '../ElectricProvider'
 
 export const Component = () => {
-  const { db } = useElectric()!
+  const navigate = useNavigate()
+
+  const { db } = useElectric()
   const { file_id } = useParams()
   const { results } = useLiveQuery(db.files.liveUnique({ where: { file_id } }))
 
   const addItem = async () => {
+    const newFile = createFilePreset()
     await db.files.create({
-      data: {
-        file_id: uuidv7(),
-      },
+      data: newFile,
     })
+    navigate(`/files/${newFile.file_id}`)
   }
 
   const clearItems = async () => {
     await db.files.deleteMany()
+    Navigate(`/files`)
   }
 
   const file: File = results
