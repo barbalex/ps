d2cfc5a46b5ef71e05996aa307cab69c95c34998
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-22 10:35:31 +0100

    Update table_name field in fields table

--- src/sql/createTables.sql
+++ src/sql/createTables.sql
@@ -1560,7 +1560,7 @@ CREATE TABLE fields(
   field_id uuid PRIMARY KEY DEFAULT public.uuid_generate_v7(),
   project_id uuid DEFAULT NULL REFERENCES projects(project_id) ON DELETE CASCADE ON UPDATE CASCADE,
   account_id uuid DEFAULT NULL REFERENCES accounts(account_id) ON DELETE CASCADE ON UPDATE CASCADE,
-  tble text DEFAULT NULL,
+  table_name text DEFAULT NULL,
   field_type_id text DEFAULT NULL REFERENCES field_types(field_type_id) ON DELETE CASCADE ON UPDATE CASCADE,
   widget_type_id text DEFAULT NULL REFERENCES widget_types(widget_type_id) ON DELETE CASCADE ON UPDATE CASCADE,
   label text DEFAULT NULL,
@@ -1576,7 +1576,7 @@ CREATE INDEX ON fields USING btree(project_id);
 
 CREATE INDEX ON fields USING btree(account_id);
 
-CREATE INDEX ON fields USING btree(tble);
+CREATE INDEX ON fields USING btree(table_name);
 
 CREATE INDEX ON fields USING btree(field_type_id);
 
@@ -1598,7 +1598,7 @@ COMMENT ON TABLE fields IS 'Fields are used to define the data structure of data
 
 COMMENT ON COLUMN fields.account_id IS 'redundant account_id enhances data safety';
 
-COMMENT ON COLUMN fields.tble IS 'table, on which this field is used inside the jsob field "data"';
+COMMENT ON COLUMN fields.table_name IS 'table, on which this field is used inside the jsob field "data"';
 
 ---------------------------------------------
 -- ui
