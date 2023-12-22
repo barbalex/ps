941670e5624e0c537255ddc558e76e9ffc64fc5b
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-22 17:42:36 +0100

    comment

--- src/components/LabelGenerator/goalReportValues.ts
+++ src/components/LabelGenerator/goalReportValues.ts
@@ -6,6 +6,8 @@ export const generateGoalReportValueLabel = async (db) => {
   if (!hasLabel) {
     await db.raw({
       sql: `ALTER TABLE goal_report_values ADD COLUMN label text GENERATED ALWAYS AS (goal_report_value_id)`,
+      // subqueries are not possible in generated columns...
+      // sql: `ALTER TABLE goal_report_values ADD COLUMN label text GENERATED ALWAYS AS (iif(coalesce(unit_id, value_integer, value_numeric, value_text) is not null, select label from units u where u.unit_id = unit_id || ':' || coalesce(value_integer, value_numeric, value_text), goal_report_value_id))`,
     })
     await db.raw({
       sql: 'CREATE INDEX IF NOT EXISTS goal_report_values_label_idx ON goal_report_values(label)',
