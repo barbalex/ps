ed489a026756a161e8693ab0cd165029f5f3f0a3
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-22 20:47:40 +0100

    Add FormMenu component

---
+++ src/components/FormMenu/index.tsx
@@ -0,0 +1,22 @@
+import { memo } from 'react'
+import { Button } from '@fluentui/react-components'
+import { FaPlus, FaMinus } from 'react-icons/fa'
+
+export const FormMenu = memo(({ addRow, deleteRow, tableName }) => {
+  return (
+    <div className="controls">
+      <Button
+        size="large"
+        icon={<FaPlus />}
+        onClick={addRow}
+        title={`Add new ${tableName}`}
+      />
+      <Button
+        size="large"
+        icon={<FaMinus />}
+        onClick={deleteRow}
+        title={`Delete ${tableName}`}
+      />
+    </div>
+  )
+})
