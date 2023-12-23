87fab863624eb6de7625aa806db5be5c578dcd15
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-23 18:44:01 +0100

    Refactor TextField and TextFieldInactive components

--- src/components/shared/TextField.tsx
+++ src/components/shared/TextField.tsx
@@ -1,38 +1,16 @@
 import { memo } from 'react'
 import {
-  makeStyles,
-  shorthands,
-  useId,
   Input,
-  Label,
+  Field,
 } from '@fluentui/react-components'
 import type { InputProps } from '@fluentui/react-components'
 
-const useStyles = makeStyles({
-  root: {
-    // Stack the label above the field
-    display: 'flex',
-    flexDirection: 'column',
-    // Use 2px gap below the label (per the design system)
-    ...shorthands.gap('2px'),
-  },
-})
-
 export const TextField = memo((props: InputProps) => {
-  const inputId = useId('input')
-  const styles = useStyles()
-  const { size, disabled, label } = props
+  const { label } = props
 
   return (
-    <div className={styles.root}>
-      <Label
-        htmlFor={inputId}
-        size={size ?? 'medium'}
-        disabled={disabled ?? false}
-      >
-        {label ?? '(no label provided)'}
-      </Label>
-      <Input id={inputId} {...props} />
-    </div>
+    <Field label={label ?? '(no label provided)'} >
+      <Input {...props} appearance="underline" />
+    </Field>
   )
 })
--- src/components/shared/TextFieldInactive.tsx
+++ src/components/shared/TextFieldInactive.tsx
@@ -2,51 +2,29 @@ import { memo } from 'react'
 import { useState } from 'react'
 import {
   makeStyles,
-  shorthands,
-  useId,
   Input,
-  Label,
   Body1,
+  Field,
 } from '@fluentui/react-components'
 import type { InputProps } from '@fluentui/react-components'
 
 const useStyles = makeStyles({
-  root: {
-    // Stack the label above the field
-    display: 'flex',
-    flexDirection: 'column',
-    // Use 2px gap below the label (per the design system)
-    ...shorthands.gap('2px'),
-  },
   body: {
     color: 'grey',
   },
 })
 
 export const TextFieldInactive = memo((props: InputProps) => {
-  const inputId = useId('input')
   const styles = useStyles()
 
   const [changed, setChanged] = useState(false)
 
   return (
-    <div className={styles.root}>
-      <Label
-        htmlFor={inputId}
-        size={props.size ?? 'medium'}
-        disabled={props.disabled ?? false}
-      >
-        {props.label ?? '(no label provided)'}
-      </Label>
+    <Field label={props.label ?? '(no label provided)'}>
       <Input
-        id={inputId}
+        appearance="underline"
         {...props}
         onChange={(e) => {
-          console.log('TextFieldInactive onChange', {
-            value: e.target.value,
-            props,
-            changed,
-          })
           if (!changed && e.target.value !== props.value) {
             setChanged(true)
           }
@@ -60,6 +38,6 @@ export const TextFieldInactive = memo((props: InputProps) => {
           className={styles.body}
         >{`${props.label} can't be changed`}</Body1>
       )}
-    </div>
+    </Field>
   )
 })
