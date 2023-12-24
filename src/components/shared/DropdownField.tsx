b31783e2cc4ef59930829cf312aa50420763544e
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-24 23:39:07 +0100

    Refactor DropdownField component to use useMemo hook

--- src/components/shared/DropdownField.tsx
+++ src/components/shared/DropdownField.tsx
@@ -1,11 +1,13 @@
-import { memo } from 'react'
+import { memo, useMemo } from 'react'
 import { Dropdown, Field, Option } from '@fluentui/react-components'
 import type { InputProps } from '@fluentui/react-components'
 
 export const DropdownField = memo((props: InputProps) => {
   const { name, label, options, value, onChange } = props
-  const selectedOptions = options.filter(({ value: v }) => v === value)
-  console.log('DropdownField', { props, name, value, options, selectedOptions })
+  const selectedOptions = useMemo(
+    () => options.filter(({ value: v }) => v === value),
+    [options, value],
+  )
 
   return (
     <Field label={label ?? '(no label provided)'}>
