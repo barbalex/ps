e7efa602b06f11f467b19a07331be0e179a80cbd
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-25 16:51:12 +0100

    Add DropdownFieldSimpleOptions component

---
+++ src/components/shared/DropdownFieldSimpleOptions.tsx
@@ -0,0 +1,29 @@
+import { memo, useMemo } from 'react'
+import { Dropdown, Field, Option } from '@fluentui/react-components'
+import type { InputProps } from '@fluentui/react-components'
+
+export const DropdownFieldSimpleOptions = memo((props: InputProps) => {
+  const { name, label, options = [], value, onChange } = props
+  const selectedOptions = useMemo(
+    () => options.filter((option) => option === value),
+    [options, value],
+  )
+
+  return (
+    <Field label={label ?? '(no label provided)'}>
+      <Dropdown
+        name={name}
+        value={selectedOptions?.[0] ?? ''}
+        selectedOptions={selectedOptions}
+        onOptionSelect={(e, data) =>
+          onChange({ target: { name, value: data.optionValue } })
+        }
+        appearance="underline"
+      >
+        {options.map((option) => {
+          return <Option key={option}>{option}</Option>
+        })}
+      </Dropdown>
+    </Field>
+  )
+})
