f471a03735e0df92c3c1e122fdeb9dcdc07c3ae7
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-25 23:52:18 +0100

    comment

--- src/components/shared/Jsonb.tsx
+++ src/components/shared/Jsonb.tsx
@@ -1,8 +1,10 @@
-// This is the component to render a jsonb field
+// This is the component to render data contained in jsonb
 // There will need to be a way to add/remove fields to the jsonb object
 // 1. get fields list for this table
-// 2. loop through fields
-// 3. build input depending on field properties
+// 2. build own onChange to pass to the fields?
+// 3. loop through fields
+// 4. build input depending on field properties
+
 export const Jsonb = memo(({ table, data, onChange }) => {
   return null
 })
