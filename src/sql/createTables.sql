514e9e138c12d2b5031eb71b551f9f60c1a252df
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-25 16:51:33 +0100

    Update field_types and widget_types tables

--- src/sql/createTables.sql
+++ src/sql/createTables.sql
@@ -1438,18 +1438,16 @@ CREATE INDEX ON field_types((1))
 WHERE
   deleted;
 
-INSERT INTO field_types(name, sort, comment)
-  VALUES ('text', 1, 'Example: text'),
-('boolean', 2, 'true or false'),
-('integer', 3, 'Example: 1'),
-('decimal', 4, 'Example: 1.1'),
-('date', 5, 'Example: 2021-03-08'),
-('date-time', 6, 'Timestamp with time zone. Example: 2021-03-08 10:23:54+01'),
-('time', 7, 'Time of day. Example: 10:23'),
-('file-reference', 8, 'the id of the file')
+INSERT INTO field_types(field_type_id, name, sort, comment)
+  VALUES ('018ca19e-7a23-7bf4-8523-ff41e3b60807', 'text', 1, 'Example: text'),
+('018ca19f-2923-7ae5-9ae6-a5c81ab65042', 'boolean', 2, 'true or false'),
+('018ca19f-3ec9-7dab-b77a-bb20ea7d188b', 'integer', 3, 'Example: 1'),
+('018ca19f-51ef-7c43-bc3f-e87e259b742b', 'decimal', 4, 'Example: 1.1'),
+('018ca19f-6638-77cf-98e8-38e601af97a1', 'date', 5, 'Example: 2021-03-08'),
+('018ca19f-787d-78f6-ac72-01f1e7f53d4f', 'date-time', 6, 'Timestamp with time zone. Example: 2021-03-08 10:23:54+01'),
+('018ca19f-8b79-7194-b59b-7075bb5b550a', 'time', 7, 'Time of day. Example: 10:23')
 ON CONFLICT ON CONSTRAINT field_types_pkey
-  DO UPDATE SET
-    comment = excluded.comment;
+  DO NOTHING;
 
 ---------------------------------------------
 -- widget_types
@@ -1477,23 +1475,21 @@ CREATE INDEX ON widget_types((1))
 WHERE
   deleted;
 
-INSERT INTO widget_types(name, needs_list, sort, comment)
-  VALUES ('text', 0, 1, 'Short field accepting text'),
-('textarea', 0, 2, 'Field accepting text, lines can break'),
-('markdown', 0, 3, 'Field accepting text, expressing markdown'),
-('options-2', 0, 4, 'single boolean field showing one option for true (active) and false (not active)'),
-('options-3', 0, 5, 'single boolean field showing true, false and null'),
-('options-few', 1, 7, 'short list, showing every entry'),
-('options-many', 1, 8, 'long dropdown-list'),
-('datepicker', 0, 9, 'enables choosing a date'),
-('filepicker', 0, 10, 'enables choosing a file'),
-('jes-no', 0, 6, 'boolean field presenting one option for true and false each'),
-('datetimepicker', 0, 10, 'enables choosing a date-time'),
-('timepicker', 0, 11, 'enables choosing time of day'),
-('rich-text', 0, 12, 'enables rich formatting of text')
+INSERT INTO widget_types(widget_type_id, name, needs_list, sort, comment)
+  VALUES ('018ca1a0-f187-7fdf-955b-4eaadaa92553', 'text', 0, 1, 'Short field accepting text'),
+('018ca1a1-0868-7f1e-80aa-119fa3932538', 'textarea', 0, 2, 'Field accepting text, lines can break'),
+('018ca1a1-2e50-7426-9199-1cf37717aef8', 'markdown', 0, 3, 'Field accepting text, expressing markdown'),
+('018ca1a1-466c-7445-aee7-437ae82561af', 'options-2', 0, 4, 'single boolean field showing one option for true (active) and false (not active)'),
+('018ca1a1-5a58-70df-af5b-dfb41dc84fdd', 'options-3', 0, 5, 'single boolean field showing true, false and null'),
+('018ca1a1-9ea1-77a0-a89e-e7dfa92e2cfe', 'options-few', 1, 7, 'short list, showing every entry'),
+('018ca1a1-c94b-7d29-b21c-42053ade0411', 'options-many', 1, 8, 'long dropdown-list'),
+('018ca1a1-dd0d-717c-b6ee-733418ebd871', 'datepicker', 0, 9, 'enables choosing a date'),
+('018ca1a1-f095-7fa2-8935-3abe52ee718d', 'jes-no', 0, 6, 'boolean field presenting one option for true and false each'),
+('018ca1a2-058b-78b3-a078-0558dcef75cb', 'datetimepicker', 0, 10, 'enables choosing a date-time'),
+('018ca1a2-1a76-7218-8289-44688fd14101', 'timepicker', 0, 11, 'enables choosing time of day'),
+('018ca1a2-2e2a-7fd6-8c57-92654c3201a5', 'rich-text', 0, 12, 'enables rich formatting of text')
 ON CONFLICT ON CONSTRAINT widget_types_value_key
-  DO UPDATE SET
-    comment = excluded.comment, sort = excluded.sort, needs_list = excluded.needs_list;
+  DO NOTHING;
 
 ---------------------------------------------
 -- widgets_for_fields
@@ -1518,26 +1514,25 @@ CREATE INDEX ON widgets_for_fields((1))
 WHERE
   deleted;
 
-INSERT INTO widgets_for_fields(field_value, widget_value)
-  VALUES ('text', 'text'),
-('text', 'markdown'),
-('boolean', 'options-2'),
-('boolean', 'options-3'),
-('integer', 'text'),
-('decimal', 'text'),
-('decimal', 'options-few'),
-('decimal', 'options-many'),
-('text', 'options-many'),
-('integer', 'options-many'),
-('text', 'options-few'),
-('integer', 'options-few'),
-('date', 'datepicker'),
-('text', 'textarea'),
-('file-reference', 'filepicker'),
-('boolean', 'jes-no'),
-('date-time', 'datetimepicker'),
-('time', 'timepicker'),
-('text', 'rich-text')
+INSERT INTO widgets_for_fields(widget_for_field_id, field_value, widget_value)
+  VALUES ('018ca1aa-6fa6-7be5-b5f8-5caca1565687', '018ca19e-7a23-7bf4-8523-ff41e3b60807', '018ca1a0-f187-7fdf-955b-4eaadaa92553'),
+('018ca1aa-898a-7120-9dbd-a7cd5e0c436a', '018ca19e-7a23-7bf4-8523-ff41e3b60807', '018ca1a1-2e50-7426-9199-1cf37717aef8'),
+('018ca1aa-9c7c-799f-87c6-5a68767be443', '018ca19f-2923-7ae5-9ae6-a5c81ab65042', '018ca1a1-466c-7445-aee7-437ae82561af'),
+('018ca1aa-af33-735e-b25d-df9681fa7758', '018ca19f-2923-7ae5-9ae6-a5c81ab65042', '018ca1a1-5a58-70df-af5b-dfb41dc84fdd'),
+('018ca1aa-cb58-705e-bc1f-d856b63b81dc', '018ca19f-3ec9-7dab-b77a-bb20ea7d188b', '018ca1a0-f187-7fdf-955b-4eaadaa92553'),
+('018ca1aa-dfe9-7ec0-b2d0-b396be3c063a', '018ca19f-51ef-7c43-bc3f-e87e259b742b', '018ca1a0-f187-7fdf-955b-4eaadaa92553'),
+('018ca1aa-f2e4-70a0-be53-225dceee7306', '018ca19f-51ef-7c43-bc3f-e87e259b742b', '018ca1a1-9ea1-77a0-a89e-e7dfa92e2cfe'),
+('018ca1ab-0887-7388-85b1-62f0b45f6151', '018ca19f-51ef-7c43-bc3f-e87e259b742b', '018ca1a1-c94b-7d29-b21c-42053ade0411'),
+('018ca1ab-195a-74df-a3fd-5f0000e6d244', '018ca19e-7a23-7bf4-8523-ff41e3b60807', '018ca1a1-c94b-7d29-b21c-42053ade0411'),
+('018ca1ab-2af1-7803-8d90-006da2cff9dc', '018ca19f-3ec9-7dab-b77a-bb20ea7d188b', '018ca1a1-c94b-7d29-b21c-42053ade0411'),
+('018ca1ab-4523-755f-99f7-89ce44fe96bb', '018ca19e-7a23-7bf4-8523-ff41e3b60807', '018ca1a1-9ea1-77a0-a89e-e7dfa92e2cfe'),
+('018ca1ab-569d-787b-ad22-f6a6685b700d', '018ca19f-3ec9-7dab-b77a-bb20ea7d188b', '018ca1a1-9ea1-77a0-a89e-e7dfa92e2cfe'),
+('018ca1ab-67df-7e2d-b96c-2c790961447d', '018ca19f-6638-77cf-98e8-38e601af97a1', '018ca1a1-dd0d-717c-b6ee-733418ebd871'),
+('018ca1ab-78c5-719a-8bd6-3c9a1093544c', '018ca19e-7a23-7bf4-8523-ff41e3b60807', '018ca1a1-0868-7f1e-80aa-119fa3932538'),
+('018ca1ab-8ac9-7845-90d8-5fd0b2a20d89', '018ca19f-2923-7ae5-9ae6-a5c81ab65042', '018ca1a1-f095-7fa2-8935-3abe52ee718d'),
+('018ca1ab-9db6-7ddd-9d5c-c1b4ea8e808d', '018ca19f-787d-78f6-ac72-01f1e7f53d4f', '018ca1a2-058b-78b3-a078-0558dcef75cb'),
+('018ca1ab-b0ae-732b-a9f2-50589d2e0508', '018ca19f-8b79-7194-b59b-7075bb5b550a', '018ca1a2-1a76-7218-8289-44688fd14101'),
+('018ca1ab-c323-7d01-995b-9759ae9a3eb8', '018ca19e-7a23-7bf4-8523-ff41e3b60807', '018ca1a2-2e2a-7fd6-8c57-92654c3201a5')
 ON CONFLICT ON CONSTRAINT widgets_for_fields_field_value_widget_value_key
   DO NOTHING;
 
