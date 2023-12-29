eb3bce0ff73322e69a992a0e4f7ba9ed5712d414
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-29 12:59:53 +0100

    Refactor DateField component

--- src/components/shared/DateField.tsx
+++ src/components/shared/DateField.tsx
@@ -1,28 +1,28 @@
 import { memo } from 'react'
-import type { InputProps } from '@fluentui/react-components'
 import { Field } from '@fluentui/react-components'
 import { DatePicker } from '@fluentui/react-datepicker-compat'
 
-export const DateField = memo((props: InputProps) => {
-  const { label, value, name, onChange, validationMessage, validationState } =
-    props
+export const DateField = memo(
+  ({ label, value, name, onChange, validationMessage, validationState }) => {
+    // console.log('DateField', { value, label, name })
 
-  return (
-    <Field
-      label={label}
-      validationMessage={validationMessage}
-      validationState={validationState}
-    >
-      <DatePicker
-        placeholder="Select a date or click to write..."
-        name={name}
-        value={value}
-        onChange={onChange}
-        onSelectDate={(date) => onChange({ target: { name, value: date } })}
-        firstDayOfWeek={1}
-        allowTextInput
-        formatDate={(date) => (!date ? '' : date.toLocaleDateString('de-CH'))}
-      />
-    </Field>
-  )
-})
+    return (
+      <Field
+        label={label}
+        validationMessage={validationMessage}
+        validationState={validationState}
+      >
+        <DatePicker
+          placeholder="Select a date or click to write..."
+          name={name}
+          value={value}
+          onChange={onChange}
+          onSelectDate={(date) => onChange({ target: { name, value: date } })}
+          firstDayOfWeek={1}
+          allowTextInput
+          formatDate={(date) => (!date ? '' : date.toLocaleDateString('de-CH'))}
+        />
+      </Field>
+    )
+  },
+)
