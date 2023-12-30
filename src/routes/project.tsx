41e6b72009a8e7b7cc3ebaf2122b9b845d75b5ce
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-30 22:15:45 +0100

    Refactor project configuration form

--- src/routes/project.tsx
+++ src/routes/project.tsx
@@ -1,20 +1,15 @@
 import { useCallback } from 'react'
 import { useLiveQuery } from 'electric-sql/react'
 import { useParams, useNavigate } from 'react-router-dom'
-import {
-  Field,
-  RadioGroup,
-  Radio,
-  Label,
-  Checkbox,
-  Divider,
-} from '@fluentui/react-components'
+import { Label, Divider } from '@fluentui/react-components'
 
 import { Projects as Project } from '../../../generated/client'
 import { project as createProjectPreset } from '../modules/dataPresets'
 import { useElectric } from '../ElectricProvider'
 import { TextField } from '../components/shared/TextField'
 import { TextFieldInactive } from '../components/shared/TextFieldInactive'
+import { RadioGroupField } from '../components/shared/RadioGroupField'
+import { CheckboxField } from '../components/shared/CheckboxField'
 import { Jsonb } from '../components/shared/Jsonb'
 import { getValueFromChange } from '../modules/getValueFromChange'
 import { FormMenu } from '../components/FormMenu'
@@ -85,17 +80,13 @@ export const Component = () => {
       />
       <Divider />
       <Label>Project configuration</Label>
-      <Field label="Type">
-        <RadioGroup
-          layout="horizontal"
-          value={row.type ?? ''}
-          name="type"
-          onChange={onChange}
-        >
-          <Radio label="Species" value="species" />
-          <Radio label="Biotope" value="biotope" />
-        </RadioGroup>
-      </Field>
+      <RadioGroupField
+        label="Type"
+        name="type"
+        list={['species', 'biotope']}
+        value={row.type ?? ''}
+        onChange={onChange}
+      />
       <TextField
         label="Name of subproject (singular)"
         name="subproject_name_singular"
@@ -117,90 +108,75 @@ export const Component = () => {
       />
       <Divider />
       <Label>{`Value(s) to use in reports when:`}</Label>
-      <Field label="...values exist on multiple place levels">
-        <RadioGroup
-          layout="horizontal"
-          value={row.values_on_multiple_levels ?? ''}
-          name="values_on_multiple_levels"
-          onChange={onChange}
-        >
-          <Radio label="first level" value="first" />
-          <Radio label="second level" value="second" />
-          <Radio label="all levels" value="all" />
-        </RadioGroup>
-      </Field>
-      <Field label="...multiple action values exist on the same place level">
-        <RadioGroup
-          layout="horizontal"
-          value={row.multiple_action_values_on_same_level ?? ''}
-          name="multiple_action_values_on_same_level"
-          onChange={onChange}
-        >
-          <Radio label="first" value="first" />
-          <Radio label="last" value="last" />
-          <Radio label="all" value="all" />
-        </RadioGroup>
-      </Field>
-      <Field label="...multiple check Values exist on the same place level">
-        <RadioGroup
-          layout="horizontal"
-          value={row.multiple_check_values_on_same_level ?? ''}
-          name="multiple_check_values_on_same_level"
-          onChange={onChange}
-        >
-          <Radio label="first" value="first" />
-          <Radio label="last" value="last" />
-          <Radio label="all" value="all" />
-        </RadioGroup>
-      </Field>
+      <RadioGroupField
+        label="...values exist on multiple place levels"
+        name="values_on_multiple_levels"
+        list={['first', 'second', 'all']}
+        value={row.values_on_multiple_levels ?? ''}
+        onChange={onChange}
+      />
+      <RadioGroupField
+        label="...multiple action values exist on the same place level"
+        name="multiple_action_values_on_same_level"
+        list={['first', 'last', 'all']}
+        value={row.multiple_action_values_on_same_level ?? ''}
+        onChange={onChange}
+      />
+      <RadioGroupField
+        label="...multiple check Values exist on the same place level"
+        name="multiple_check_values_on_same_level"
+        list={['first', 'last', 'all']}
+        value={row.multiple_check_values_on_same_level ?? ''}
+        onChange={onChange}
+      />
       <Divider />
       <Label>Enable uploading files to:</Label>
-      <Checkbox
+      <CheckboxField
         label="Projects"
         name="files_active_projects"
-        checked={row.files_active_projects ?? false}
+        value={row.files_active_projects ?? false}
         onChange={onChange}
       />
-      <Checkbox
+      <CheckboxField
         label="Project reports"
         name="files_active_projects_reports"
-        checked={row.files_active_projects_reports ?? false}
+        value={row.files_active_projects_reports ?? false}
         onChange={onChange}
       />
-      <Checkbox
+      <CheckboxField
         label="Subprojects"
         name="files_active_subprojects"
-        checked={row.files_active_subprojects ?? false}
+        value={row.files_active_subprojects ?? false}
         onChange={onChange}
       />
-      <Checkbox
+      <CheckboxField
         label="Subproject reports"
         name="files_active_subproject_reports"
-        checked={row.files_active_subproject_reports ?? false}
+        value={row.files_active_subproject_reports ?? false}
         onChange={onChange}
       />
-      <Checkbox
+      <CheckboxField
         label="Places"
         name="files_active_places"
-        checked={row.files_active_places ?? false}
+        value={row.files_active_places ?? false}
         onChange={onChange}
       />
-      <Checkbox
+      <CheckboxField
         label="Actions"
         name="files_active_actions"
-        checked={row.files_active_actions ?? false}
+        value={row.files_active_actions ?? false}
         onChange={onChange}
       />
-      <Checkbox
+      <CheckboxField
         label="Checks"
         name="files_active_checks"
-        checked={row.files_active_checks ?? false}
+        value={row.files_active_checks ?? false}
         onChange={onChange}
       />
-      <Checkbox
+      <CheckboxField
         label="Check reports"
         name="files_active_check_reports"
-        checked={row.files_active_check_reports ?? false}
+        value={row.files_active_check_reports ?? false}
         onChange={onChange}
       />
     </div>
