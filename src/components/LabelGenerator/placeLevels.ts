f3450efcce2b63f26a42adb534818d42c55c9672
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-31 11:00:26 +0100

    alter label of placeLevels

--- src/components/LabelGenerator/placeLevels.ts
+++ src/components/LabelGenerator/placeLevels.ts
@@ -5,14 +5,13 @@ export const generatePlaceLevelLabel = async (db) => {
   const hasLabel = columns.some((column) => column.name === 'label')
   if (!hasLabel) {
     await db.raw({
-      sql: `ALTER TABLE place_levels ADD COLUMN label text GENERATED ALWAYS AS (coalesce(CAST(level as VARCHAR) || ': ' || name_short, place_level_id))`,
+      sql: `ALTER TABLE place_levels ADD COLUMN label text GENERATED ALWAYS AS (coalesce(CAST(level as VARCHAR) || '.' || coalesce(name_short, name_plural, place_level_id), place_level_id))`,
     })
+    // await db.raw({
+    //   sql: `ALTER TABLE place_levels ADD COLUMN label text GENERATED ALWAYS AS (coalesce(name_plural, place_level_id))`,
+    // })
     await db.raw({
       sql: 'CREATE INDEX IF NOT EXISTS place_levels_label_idx ON place_levels(label)',
     })
   }
-  // console.log('LabelGenerator, place_levels:', {
-  //   columns,
-  //   hasLabel,
-  // })
 }
