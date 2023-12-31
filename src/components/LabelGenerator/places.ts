ac2b712f67cd1d468912721c126028327ded9c4c
Alexander Gabriel <alex.barbalex@gmail.com>
2023-12-31 16:25:52 +0100

    work on places label

--- src/components/LabelGenerator/places.ts
+++ src/components/LabelGenerator/places.ts
@@ -3,6 +3,7 @@ export const generatePlaceLabel = async (db) => {
     sql: 'PRAGMA table_xinfo(places)',
   })
   const hasLabel = columns.some((column) => column.name === 'label')
+
   if (!hasLabel) {
     // TODO: build virtual field from projects.places_label_by, return that here
     // But: not possible because generated columns can only fetch from the same row/table
@@ -11,24 +12,24 @@ export const generatePlaceLabel = async (db) => {
     await db.raw({
       sql: 'ALTER TABLE places ADD COLUMN label text GENERATED ALWAYS AS (place_id)',
     })
-    await db.raw({
+    const result = await db.raw({
       sql: `
       CREATE TRIGGER if not exists places_label_trigger
       AFTER UPDATE of data ON places
       BEGIN
-        UPDATE places SET label=NEW.data->>projects.places_label_by
-         FROM places 
-          inner join subprojects on places.subproject_id=subprojects.subproject_id 
-          inner join projects on subprojects.project_id=projects.project_id
-         WHERE place_id=NEW.place_id;
+        UPDATE places SET label_triggered = NEW.data->>projects.places_label_by
+        FROM (
+          SELECT places_label_by from projects 
+          where project_id = (select project_id from subprojects where subproject_id = NEW.subproject_id)
+        ) as projects
+         WHERE place_id = NEW.place_id;
       END`,
     })
+    console.log('LabelGenerator, places:', {
+      result,
+    })
     await db.raw({
-      sql: 'CREATE INDEX IF NOT EXISTS places_label_idx ON places(label)',
+      sql: 'CREATE INDEX IF NOT EXISTS places_label_idx ON places(label_triggered)',
     })
   }
-  // console.log('LabelGenerator, places:', {
-  //   columns,
-  //   hasLabel,
-  // })
 }
