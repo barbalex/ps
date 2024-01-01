abb3bd96102e65568e693f38a979be31847e34a8
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2024-01-01 18:19:10 +0100

    comment

--- src/components/LabelGenerator/places.ts
+++ src/components/LabelGenerator/places.ts
@@ -34,25 +34,24 @@ export const generatePlaceLabel = async (db) => {
   }
 }
 
-
 // CREATE TRIGGER if not exists places_label_trigger
 // AFTER UPDATE of data ON places
 // BEGIN
 //   UPDATE places SET label_triggered = json_extract(NEW.data, projects.places_label_by)
 //   FROM (
-//     SELECT places_label_by from projects 
+//     SELECT places_label_by from projects
 //     where project_id = (select project_id from subprojects where subproject_id = NEW.subproject_id)
 //   ) as projects
 //     WHERE places.place_id = NEW.place_id;
 // END
 // try with multiple columns:
-CREATE TRIGGER if not exists places_label_trigger
-AFTER UPDATE of data ON places
-BEGIN
-  UPDATE places SET label = json_extract(NEW.data, json_extract(projects.places_label_by, '$'))
-  FROM (
-    SELECT places_label_by from projects 
-    where project_id = (select project_id from subprojects where subproject_id = NEW.subproject_id)
-  ) as projects
-    WHERE places.place_id = NEW.place_id;
-END
+// CREATE TRIGGER if not exists places_label_trigger
+// AFTER UPDATE of data ON places
+// BEGIN
+//   UPDATE places SET label = json_extract(NEW.data, json_extract(projects.places_label_by, '$'))
+//   FROM (
+//     SELECT places_label_by from projects
+//     where project_id = (select project_id from subprojects where subproject_id = NEW.subproject_id)
+//   ) as projects
+//     WHERE places.place_id = NEW.place_id;
+// END
