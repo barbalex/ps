f6e43693ecc97fe37a508c8680a4cdc47c9c9b64
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-07 18:55:31 +0100

    add passed in validation

--- src/components/shared/DropdownField.tsx
+++ src/components/shared/DropdownField.tsx
@@ -1,12 +1,11 @@
 import { memo, useMemo } from 'react'
 import { Dropdown, Field, Option } from '@fluentui/react-components'
-import type { InputProps } from '@fluentui/react-components'
 import { useLiveQuery } from 'electric-sql/react'
 
 import { useElectric } from '../../ElectricProvider'
 
-export const DropdownField = memo((props: InputProps) => {
-  const {
+export const DropdownField = memo(
+  ({
     name,
     label,
     table,
@@ -16,68 +15,77 @@ export const DropdownField = memo((props: InputProps) => {
     value,
     onChange,
     autoFocus,
-  } = props
+    validationMessageIn,
+    validationStateIn,
+  }) => {
+    const { db } = useElectric()
+    const { results = [] } = useLiveQuery(
+      db[table]?.liveMany({
+        where,
+        orderBy,
+      }),
+    )
+    const options = results.map((o) => ({
+      text: o.label,
+      value: o[idField ?? name],
+    }))
+    const selectedOptions = useMemo(
+      () => options.filter(({ value: v }) => v === value),
+      [options, value],
+    )
 
-  const { db } = useElectric()
-  const { results = [] } = useLiveQuery(
-    db[table]?.liveMany({
-      where,
-      orderBy,
-    }),
-  )
-  const options = results.map((o) => ({
-    text: o.label,
-    value: o[idField ?? name],
-  }))
-  const selectedOptions = useMemo(
-    () => options.filter(({ value: v }) => v === value),
-    [options, value],
-  )
-
-  const validationState = !results?.length && !!value ? 'warning' : 'none'
-  const validationMessage =
-    !results?.length && !!value
+    const validationState = validationStateIn
+      ? validationStateIn
+      : !results?.length && !!value
+      ? 'warning'
+      : 'none'
+    const validationMessage = validationMessageIn
+      ? validationMessageIn
+      : !results?.length && !!value
       ? `No ${table} found. Please add one first.`
       : undefined
 
-  // console.log('DropdownField', {
-  //   name,
-  //   label,
-  //   table,
-  //   value,
-  //   options,
-  //   selectedOptions,
-  //   validationState,
-  //   validationMessage,
-  //   results,
-  // })
+    // console.log('DropdownField', {
+    //   name,
+    //   label,
+    //   table,
+    //   value,
+    //   options,
+    //   selectedOptions,
+    //   validationState,
+    //   validationMessage,
+    //   results,
+    //   validationMessageIn,
+    //   validationStateIn,
+    // })
 
-  return (
-    <Field
-      label={label ?? '(no label provided)'}
-      validationMessage={validationMessage}
-      validationState={validationState}
-    >
-      <Dropdown
-        name={name}
-        value={selectedOptions?.[0]?.text ?? ''}
-        selectedOptions={selectedOptions}
-        onOptionSelect={(e, data) =>
-          onChange({ target: { name, value: data.optionValue } })
-        }
-        appearance="underline"
-        autoFocus={autoFocus}
+    return (
+      <Field
+        label={label ?? '(no label provided)'}
+        validationMessage={validationMessage}
+        validationState={validationState}
       >
-        {options.map((params) => {
-          const { text, value } = params
+        <Dropdown
+          name={name}
+          value={selectedOptions?.[0]?.text ?? ''}
+          selectedOptions={selectedOptions}
+          onOptionSelect={(e, data) =>
+            onChange({ target: { name, value: data.optionValue } })
+          }
+          appearance="underline"
+          autoFocus={autoFocus}
+        >
+          {options.map((params) => {
+            const { text, value } = params
 
-          return (
-            <Option key={value} value={value}>
-              {text}
-            </Option>
-          )
-        })}
-      </Dropdown>
-    </Field>
-  )
-})
+            return (
+              <Option key={value} value={value}>
+                {text}
+              </Option>
+            )
+          })}
+        </Dropdown>
+      </Field>
+    )
+  },
+)
