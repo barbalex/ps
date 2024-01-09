de6523120eed897e41a48d36bdb296ade63873c7
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-09 13:28:41 +0100

    filter correctly

--- src/components/Navs/DataNavs.tsx
+++ src/components/Navs/DataNavs.tsx
@@ -42,6 +42,7 @@ export const DataNavs = ({ matches }) => {
     if (table === 'places' && placesCountInPath === 2) {
       filterParams.parent_id = dataMatch?.params?.place_id
     } else if (table === 'places') {
+      filterParams[parentIdName] = parentId
       filterParams.parent_id = null
     } else {
       filterParams[parentIdName] = parentId
