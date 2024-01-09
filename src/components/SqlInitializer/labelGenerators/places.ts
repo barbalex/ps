498fd1efd6975143bb7a9c546d122f637b26a6f5
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-09 13:39:14 +0100

    set label on places with trigger

--- src/components/SqlInitializer/labelGenerators/places.ts
+++ src/components/SqlInitializer/labelGenerators/places.ts
@@ -9,15 +9,12 @@ export const generatePlaceLabel = async (db) => {
     // But: not possible because generated columns can only fetch from the same row/table
     // Alternatives:
     // - use a trigger to update the label field
-    await db.raw({
-      sql: 'ALTER TABLE places ADD COLUMN label text GENERATED ALWAYS AS (place_id)',
-    })
     const result = await db.raw({
       sql: `
       CREATE TRIGGER if not exists places_label_trigger
       AFTER UPDATE of data ON places
       BEGIN
-        UPDATE places SET label_triggered = json_extract(NEW.data, projects.places_label_by)
+        UPDATE places SET label = json_extract(NEW.data, '$.' || projects.places_label_by)
         FROM (
           SELECT places_label_by from projects 
           where project_id = (select project_id from subprojects where subproject_id = NEW.subproject_id)
@@ -29,7 +26,7 @@ export const generatePlaceLabel = async (db) => {
       result,
     })
     await db.raw({
-      sql: 'CREATE INDEX IF NOT EXISTS places_label_idx ON places(label_triggered)',
+      sql: 'CREATE INDEX IF NOT EXISTS places_label_idx ON places(label)',
     })
   }
 }
