6f2cbfc0a3d3155773a615d7bf45d8bde550f527
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-09 10:59:10 +0100

    clean

--- src/components/FormMenu/index.tsx
+++ src/components/FormMenu/index.tsx
@@ -3,46 +3,36 @@ import { Button } from '@fluentui/react-components'
 import { FaPlus, FaMinus, FaChevronRight, FaChevronLeft } from 'react-icons/fa'
 
 export const FormMenu = memo(
-  ({ addRow, deleteRow, toNext, toPrevious, tableName }) => {
-    console.log('FormMenu ', {
-      tableName,
-      addRow,
-      deleteRow,
-      toNext,
-      toPrevious,
-    })
-
-    return (
-      <div className="controls">
-        {toPrevious && (
-          <Button
-            size="medium"
-            icon={<FaChevronLeft />}
-            onClick={toPrevious}
-            title={`Previous ${tableName}`}
-          />
-        )}
+  ({ addRow, deleteRow, toNext, toPrevious, tableName }) => (
+    <div className="controls">
+      {toPrevious && (
         <Button
           size="medium"
-          icon={<FaPlus />}
-          onClick={addRow}
-          title={`New ${tableName}`}
+          icon={<FaChevronLeft />}
+          onClick={toPrevious}
+          title={`Previous ${tableName}`}
         />
+      )}
+      <Button
+        size="medium"
+        icon={<FaPlus />}
+        onClick={addRow}
+        title={`New ${tableName}`}
+      />
+      <Button
+        size="medium"
+        icon={<FaMinus />}
+        onClick={deleteRow}
+        title={`Delete ${tableName}`}
+      />
+      {toNext && (
         <Button
           size="medium"
-          icon={<FaMinus />}
-          onClick={deleteRow}
-          title={`Delete ${tableName}`}
+          icon={<FaChevronRight />}
+          onClick={toNext}
+          title={`Next ${tableName}`}
         />
-        {toNext && (
-          <Button
-            size="medium"
-            icon={<FaChevronRight />}
-            onClick={toNext}
-            title={`Next ${tableName}`}
-          />
-        )}
-      </div>
-    )
-  },
+      )}
+    </div>
+  ),
 )
