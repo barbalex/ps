4e144bbe3acbd7ac0449bca649182f595d91a7ff
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-11 12:13:59 +0100

    add trigger

--- src/components/SqlInitializer/labelGenerators/projects.ts
+++ src/components/SqlInitializer/labelGenerators/projects.ts
@@ -2,11 +2,9 @@ export const generateProjectLabel = async (db) => {
   const columns = await db.raw({
     sql: 'PRAGMA table_xinfo(projects)',
   })
-  // const indexes = await db.raw({
-  //   sql: 'PRAGMA index_list(projects)',
-  // })
-  const hasLabel = columns.some((column) => column.name === 'label')
-  if (!hasLabel) {
+
+  const labelExists = columns.some((column) => column.name === 'label')
+  if (!labelExists) {
     await db.raw({
       sql: 'ALTER TABLE projects ADD COLUMN label text GENERATED ALWAYS AS (coalesce(name, project_id))',
     })
@@ -14,9 +12,23 @@ export const generateProjectLabel = async (db) => {
       sql: 'CREATE INDEX IF NOT EXISTS projects_label_idx ON projects(label)',
     })
   }
-  // console.log('LabelGenerator, projects:', {
-  //   columns,
-  //   hasLabel,
-  //   indexes,
-  // })
+
+  const triggers = await db.raw({
+    sql: `select name from sqlite_master where type = 'trigger';`,
+  })
+  const triggerExists = triggers.some(
+    (column) => column.name === 'projects_places_label_trigger',
+  )
+  if (!triggerExists) {
+    const result = await db.raw({
+      sql: `
+      CREATE TRIGGER IF NOT EXISTS projects_places_label_trigger
+        AFTER UPDATE OF places_label_by ON projects
+      BEGIN
+        UPDATE places SET label = json_extract(data, '$.' || NEW.places_label_by);
+      END;
+`,
+    })
+    console.log('TriggerGenerator, projects, result:', result)
+  }
 }
