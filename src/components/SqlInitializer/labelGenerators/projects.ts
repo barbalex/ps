9566ac702285f3a8620fce5ea27d9e3dd817a2d7
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-11 18:18:44 +0100

    Refactor project label generation and add trigger for updating labels

--- src/components/SqlInitializer/labelGenerators/projects.ts
+++ src/components/SqlInitializer/labelGenerators/projects.ts
@@ -1,18 +1,17 @@
 export const generateProjectLabel = async (db) => {
-  const columns = await db.raw({
-    sql: 'PRAGMA table_xinfo(projects)',
-  })
-
-  const labelExists = columns.some((column) => column.name === 'label')
-  if (!labelExists) {
-    await db.raw({
-      sql: 'ALTER TABLE projects ADD COLUMN label text GENERATED ALWAYS AS (coalesce(name, project_id))',
-    })
-    await db.raw({
-      sql: 'CREATE INDEX IF NOT EXISTS projects_label_idx ON projects(label)',
-    })
-  }
-
+  // const columns = await db.raw({
+  //   sql: 'PRAGMA table_xinfo(projects)',
+  // })
+  // const labelExists = columns.some((column) => column.name === 'label')
+  // if (!labelExists) {
+  //   await db.raw({
+  //     sql: 'ALTER TABLE projects ADD COLUMN label text GENERATED ALWAYS AS (coalesce(name, project_id))',
+  //   })
+  //   await db.raw({
+  //     sql: 'CREATE INDEX IF NOT EXISTS projects_label_idx ON projects(label)',
+  //   })
+  // }
+  // if places_label_by is changed, need to update all labels of places
   const triggers = await db.raw({
     sql: `select name from sqlite_master where type = 'trigger';`,
   })
@@ -22,12 +21,27 @@ export const generateProjectLabel = async (db) => {
   if (!triggerExists) {
     const result = await db.raw({
       sql: `
-      CREATE TRIGGER IF NOT EXISTS projects_places_label_trigger
-        AFTER UPDATE OF places_label_by ON projects
+      CREATE TRIGGER IF NOT EXISTS projects_label_trigger
+        AFTER UPDATE ON projects
       BEGIN
-        UPDATE places SET label = json_extract(data, '$.' || NEW.places_label_by);
-      END;
-`,
+        UPDATE projects SET label = CASE
+        WHEN accounts.projects_label_by IS NULL THEN
+          name
+          WHEN accounts.projects_label_by = 'name' THEN
+          name
+        ELSE
+          json_extract(NEW.data, '$.' || accounts.projects_label_by)
+        END
+      FROM(
+      SELECT
+        projects_label_by
+      FROM
+        accounts
+      WHERE
+        account_id = NEW.account_id) AS accounts
+      WHERE
+        projects.project_id = NEW.project_id;
+      END;`,
     })
     console.log('TriggerGenerator, projects, result:', result)
   }
