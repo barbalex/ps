63758070f1ff716298d3bc2b7e1d7ebc74925240
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-12 20:10:38 +0100

    label subproject_taxa

--- src/components/SqlInitializer/labelGenerators/subprojectTaxa.ts
+++ src/components/SqlInitializer/labelGenerators/subprojectTaxa.ts
@@ -1,18 +1,37 @@
 export const generateSubprojectTaxonLabel = async (db) => {
-  const columns = await db.raw({
-    sql: 'PRAGMA table_xinfo(subproject_taxa)',
+  // if taxon_id is changed, update the label
+  const triggers = await db.raw({
+    sql: `select name from sqlite_master where type = 'trigger';`,
   })
-  const hasLabel = columns.some((column) => column.name === 'label')
-  if (!hasLabel) {
-    await db.raw({
-      sql: 'ALTER TABLE subproject_taxa ADD COLUMN label text GENERATED ALWAYS AS (subproject_taxon_id)',
+  const subprojectTaxonLabelTriggerExists = triggers.some(
+    (column) => column.name === 'subproject_taxon_label_trigger',
+  )
+  if (!subprojectTaxonLabelTriggerExists) {
+    const result = await db.raw({
+      sql: `
+      CREATE TRIGGER IF NOT EXISTS subproject_taxon_label_trigger
+        AFTER UPDATE OF taxon_id ON subproject_taxa
+      BEGIN
+        UPDATE subproject_taxa SET label = concat(
+          (SELECT name FROM taxonomies where taxonomy_id = (select taxonomy_id from taxa where taxon_id = NEW.taxon_id)),
+          ': ',
+          (SELECT name FROM taxa WHERE taxon_id = NEW.taxon_id)
+        );
+      END;`,
     })
+    console.log('TriggerGenerator, subproject_taxa, result:', result)
+    // same on insert
     await db.raw({
-      sql: 'CREATE INDEX IF NOT EXISTS subproject_taxa_label_idx ON subproject_taxa(label)',
+      sql: `
+      CREATE TRIGGER IF NOT EXISTS subproject_taxon_label_trigger_insert
+        AFTER INSERT ON subproject_taxa
+      BEGIN
+        UPDATE subproject_taxa SET label = concat(
+          (SELECT name FROM taxonomies where taxonomy_id = (select taxonomy_id from taxa where taxon_id = NEW.taxon_id)),
+          ': ',
+          (SELECT name FROM taxa WHERE taxon_id = NEW.taxon_id)
+        );
+      END;`,
     })
   }
-  // console.log('LabelGenerator, subproject_taxa:', {
-  //   columns,
-  //   hasLabel,
-  // })
 }
