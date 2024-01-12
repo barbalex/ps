c1c61d0813de400e889912481669d76d9a07f513
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-12 11:10:05 +0100

    Update project label generation triggers

--- src/components/SqlInitializer/labelGenerators/projects.ts
+++ src/components/SqlInitializer/labelGenerators/projects.ts
@@ -22,18 +22,6 @@ export const generateProjectLabel = async (db) => {
       'TriggerGenerator, projects_places_label_trigger, result:',
       result,
     )
-    // same on insert
-    await db.raw({
-      sql: `CREATE TRIGGER IF NOT EXISTS projects_places_label_trigger_insert
-              AFTER INSERT ON projects
-            BEGIN
-              UPDATE places SET label = case
-                when NEW.places_label_by = 'id' then place_id
-                when NEW.places_label_by = 'level' then level
-                else json_extract(data, '$.' || NEW.places_label_by)
-              end;
-            END;`,
-    })
   }
 
   //if anything in projects is changed, update its label
@@ -66,5 +54,30 @@ export const generateProjectLabel = async (db) => {
       END;`,
     })
     console.log('TriggerGenerator, projects_label_trigger, result:', result)
+    // same on insert
+    await db.raw({
+      sql: `
+      CREATE TRIGGER IF NOT EXISTS projects_label_trigger_insert
+        AFTER INSERT ON projects
+      BEGIN
+        UPDATE projects SET label = CASE
+        WHEN accounts.projects_label_by IS NULL THEN
+          name
+          WHEN accounts.projects_label_by = 'name' THEN
+          name
+        ELSE
+          json_extract(NEW.data, '$.' || accounts.projects_label_by)
+        END
+      FROM(
+      SELECT
+        projects_label_by
+      FROM
+        accounts
+      WHERE
+        account_id = NEW.account_id) AS accounts
+      WHERE
+        projects.project_id = NEW.project_id;
+      END;`,
+    })
   }
 }
