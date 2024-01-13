ba7e19bed88e4bb9d5c7e748605caca857630697
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-13 13:07:41 +0100

    refactor

--- src/components/ListViewHeader/Menu.tsx
+++ src/components/ListViewHeader/Menu.tsx
@@ -2,7 +2,7 @@ import { memo } from 'react'
 import { Button } from '@fluentui/react-components'
 import { FaPlus } from 'react-icons/fa'
 
-export const ListViewMenu = memo(({ addRow, tableName }) => {
+export const Menu = memo(({ addRow, tableName }) => {
   // console.log('render FormMenu, table: ', tableName)
 
   return (
--- src/components/ListViewHeader/index.tsx
+++ src/components/ListViewHeader/index.tsx
@@ -1,11 +1,11 @@
 import { memo } from 'react'
-import { ListViewMenu } from './ListViewMenu'
+import { Menu } from './Menu'
 
 export const ListViewHeader = memo(({ title, addRow, tableName }) => {
   return (
     <div className="list-view-header">
       <h1>{title}</h1>
-      <ListViewMenu addRow={addRow} tableName={tableName} />
+      <Menu addRow={addRow} tableName={tableName} />
     </div>
   )
 })
