03f248441d3c490a32e0b1a889097a6fbf6ed4da
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-15 19:52:15 +0100

    simplify

--- src/components/BreadcrumbsOverflowing/Matches.tsx
+++ src/components/BreadcrumbsOverflowing/Matches.tsx
@@ -6,21 +6,17 @@ import './breadcrumbs.css'
 
 // problem: menu is not rendered after width changes
 // solution: rerender after width changes
-export const Matches = ({ rerenderInteger, matches, width }) => {
-  console.log('Matches, width:', width)
+export const Matches = ({ rerenderInteger, matches }) =>
+  matches.map((match) => {
+    const { table, folder } = match?.handle?.crumb?.(match) ?? {}
 
-  return ( matches.map((match) => {
-        const { table, folder } = match?.handle?.crumb?.(match) ?? {}
-
-        return (
-          <OverflowItem key={`${match.id}/${rerenderInteger}`} id={match.id}>
-            {table === 'root' || folder === false ? (
-              <BreadcrumbForFolder match={match} />
-            ) : (
-              <BreadcrumbForData match={match} />
-            )}
-          </OverflowItem>
-        )
-      }
-  ))
-}
+    return (
+      <OverflowItem key={`${match.id}/${rerenderInteger}`} id={match.id}>
+        {table === 'root' || folder === false ? (
+          <BreadcrumbForFolder match={match} />
+        ) : (
+          <BreadcrumbForData match={match} />
+        )}
+      </OverflowItem>
+    )
+  })
