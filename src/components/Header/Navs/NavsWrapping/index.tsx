5ec9e2328aab12d68c1f7d6b67b3cd6355be07b2
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-16 16:52:41 +0100

    initiate

---
+++ src/components/Header/Navs/NavsWrapping/index.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react'
+import { useMatches, useLocation, Link } from 'react-router-dom'
+
+import { DataNavs } from './DataNavs'
+
+export const NavsWrapping = () => {
+  const location = useLocation()
+  const matches = useMatches()
+
+  const thisPathsMatches = matches.filter(
+    (match) => match.pathname === location.pathname && match.handle,
+  )
+
+  const [tos, setTos] = useState([])
+  useEffect(() => {
+    const fetch = async () => {
+      const tos = []
+      for (const match of thisPathsMatches) {
+        const to = await match?.handle?.to?.(match)
+        if (!to) continue
+        tos.push(to)
+      }
+
+      return setTos(tos.filter((to) => Boolean(to)))
+    }
+    fetch()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.pathname])
+
+  // console.log('Navs', {
+  //   matches,
+  //   tos,
+  //   thisPathsMatches,
+  //   pathname: location.pathname,
+  // })
+
+  // hide this area of there are no tos
+  if (!tos?.length) return <DataNavs matches={thisPathsMatches} />
+
+  return (
+    <nav className="navs">
+      {(tos[0] ?? []).map(({ path, text }) => (
+        <Link key={path} to={path}>
+          {text}
+        </Link>
+      ))}
+    </nav>
+  )
+}
