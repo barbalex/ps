8ac210d89915df51c7470f21a293188dc2d753aa
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-16 15:53:46 +0100

    navigate back from options

--- src/components/Header/TopHeader/Menu.tsx
+++ src/components/Header/TopHeader/Menu.tsx
@@ -1,19 +1,25 @@
-import { memo } from 'react'
+import { memo, useCallback } from 'react'
 import { Button } from '@fluentui/react-components'
 import { FaCog } from 'react-icons/fa'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 import { user_id } from '../../SqlInitializer'
 
 export const Menu = memo(() => {
   const navigate = useNavigate()
+  const params = useParams()
+
+  const onClick = useCallback(() => {
+    if (params.user_id) return navigate(-1)
+    navigate(`/options/${user_id}`)
+  }, [navigate, params])
 
   return (
     <div className="controls">
       <Button
         size="medium"
         icon={<FaCog />}
-        onClick={() => navigate(`/options/${user_id}`)}
+        onClick={onClick}
         title="Options"
       />
     </div>
