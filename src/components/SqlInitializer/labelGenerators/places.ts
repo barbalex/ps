ac5ac32101d6b64dc41b630ab9f4561aa3f20e0d
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-16 16:42:03 +0100

    add insert trigger

--- src/components/SqlInitializer/labelGenerators/places.ts
+++ src/components/SqlInitializer/labelGenerators/places.ts
@@ -30,6 +30,26 @@ export const generatePlaceLabel = async (db) => {
       END`,
     })
     console.log('LabelGenerator, places, result:', result)
+    // now to same on insert
+    const resultInsert = await db.raw({
+      sql: `
+      CREATE TRIGGER if not exists places_label_insert_trigger
+      AFTER INSERT ON places
+      BEGIN
+        UPDATE places SET label = case 
+        when projects.places_label_by is null then place_id 
+        when projects.places_label_by = 'id' then place_id 
+        when projects.places_label_by = 'level' then level 
+        else json_extract(NEW.data, '$.' || projects.places_label_by) 
+        end
+        FROM (
+          SELECT places_label_by from projects 
+          where project_id = (select project_id from subprojects where subproject_id = NEW.subproject_id)
+        ) as projects
+         WHERE places.place_id = NEW.place_id;
+      END`,
+    })
+    console.log('LabelGenerator, places, resultInsert:', resultInsert)
     // const resultArray = await db.raw({
     //   sql: `
     //   CREATE TRIGGER if not exists places_label_array_trigger
