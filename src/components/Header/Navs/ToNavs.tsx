1ec8329d2c38c814f65a6adf46030aa297c5f6d7
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-18 12:15:45 +0100

    memoize

--- src/components/Header/Navs/ToNavs.tsx
+++ src/components/Header/Navs/ToNavs.tsx
@@ -1,9 +1,10 @@
+import { memo } from 'react'
 import { Nav } from './Nav'
 
-export const ToNavs = ({ tos }) => (
+export const ToNavs = memo(({ tos }) => (
   <nav className="navs">
     {tos.map((to) => (
       <Nav key={to.path} to={to.path} label={to.text} />
     ))}
   </nav>
-)
+))
