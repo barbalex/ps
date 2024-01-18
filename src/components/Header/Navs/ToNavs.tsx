b0ef3f59d83241f7a75e63e5c7a7344460d11d81
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-18 15:25:30 +0100

    simplify

--- src/components/Header/Navs/ToNavs.tsx
+++ src/components/Header/Navs/ToNavs.tsx
@@ -1,11 +1,12 @@
-// import { memo } from 'react'
+import { memo } from 'react'
 import { OverflowItem } from '@fluentui/react-components'
 
 import { Nav } from './Nav'
 
-export const ToNavs = ({ tos }) =>
+export const ToNavs = memo(({ tos }) =>
   tos.map((to) => (
-    <OverflowItem key={to.path} id={`${to.path}/nav`}>
+    <OverflowItem key={to.path} id={to.path}>
       <Nav to={to.path} label={to.text} />
     </OverflowItem>
-  ))
+  )),
+)
