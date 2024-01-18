b5c5a4f3fa4ade3ee8de4b5baac2634214f0fb53
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-18 20:22:49 +0100

    configure css hooks

--- src/css.ts
+++ src/css.ts
@@ -1,5 +1,22 @@
-import { createHooks } from "@css-hooks/react";
-    
-export const [hooks, css] = createHooks({
-  "&:hover": "&:hover",
-});
+import { createHooks } from '@css-hooks/react'
+import { recommended } from '@css-hooks/recommended'
+
+export const [hooks, css] = createHooks(
+  recommended({
+    // breakpoints: ["500px", "1000px"],
+    // colorSchemes: ["dark", "light"],
+    pseudoClasses: [':hover', ':focus', ':active', ':disabled'],
+  }),
+)
+
+/* Hooks created:
+ - @media (width < 500px)
+ - @media (500px <= width < 1000px)
+ - @media (1000px <= width)
+ - @media (prefers-color-scheme: dark)
+ - @media (prefers-color-scheme: light)
+ - &:hover
+ - &:focus
+ - &:disabled
+ - &:active
+*/
