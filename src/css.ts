3b837b9283e6c4412d34742be1a42f57b864c73c
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-18 20:33:32 +0100

    comment

--- src/css.ts
+++ src/css.ts
@@ -2,6 +2,7 @@ import { createHooks } from '@css-hooks/react'
 import { recommended } from '@css-hooks/recommended'
 
 export const [hooks, css] = createHooks(
+  // implement breakpoints and color schemes later
   recommended({
     // breakpoints: ["500px", "1000px"],
     // colorSchemes: ["dark", "light"],
