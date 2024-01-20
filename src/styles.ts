9e66eb038209901647e1be6eca9ae7b981e0696d
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-20 12:10:29 +0100

    style

--- src/styles.ts
+++ src/styles.ts
@@ -5,3 +5,13 @@ export const controls = {
   justifyContent: 'center',
   columnGap: 10,
 }
+
+export const controlsButton = {
+  backgroundColor: 'rgba(38, 82, 37, 0)',
+  border: 'none',
+  color: 'white',
+  '&:hover': {
+    backgroundColor: 'rgba(38, 82, 37, 0.8)',
+    color: 'white',
+  },
+}
