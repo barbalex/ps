52f834dc8135d21fa7f1f591f2cadb9c85dde15b
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-20 14:06:45 +0100

    update tabs

--- src/components/Layout/Header/Menu.tsx
+++ src/components/Layout/Header/Menu.tsx
@@ -3,7 +3,6 @@ import {
   Button,
   Toolbar,
   ToolbarToggleButton,
-  ToolbarProps,
 } from '@fluentui/react-components'
 import { FaCog } from 'react-icons/fa'
 import { useNavigate, useParams } from 'react-router-dom'
@@ -36,8 +35,25 @@ export const Menu = memo(() => {
     db.ui_options.liveUnique({ where: { user_id } }),
   )
 
-  const tabs = results?.tabs
+  const tabs = results?.tabs ?? []
   console.log('Menu, tabs:', tabs)
+  const onChangeTabs = useCallback(
+    (e, { name, checkedItems }) => {
+      console.log('onChangeTabs', { e, name, checkedItems })
+      // if checkedItems[0] is not in tabs, add it
+      // if checkedItems[0] is in tabs, remove it
+      const checkedItem = checkedItems[0]
+      const newTabs = tabs.includes(checkedItem)
+        ? tabs.filter((tab) => tab !== checkedItem)
+        : [...tabs, checkedItem]
+      // then update ui_options.tabs
+      db.ui_options.update({
+        where: { user_id },
+        data: { tabs: newTabs },
+      })
+    },
+    [db.ui_options, tabs],
+  )
 
   const onClick = useCallback(() => {
     if (params.user_id) return navigate(-1)
@@ -46,6 +62,21 @@ export const Menu = memo(() => {
 
   return (
     <div style={controls}>
+      <Toolbar
+        aria-label="with controlled Toggle Button"
+        checkedValues={tabs}
+        onCheckedValueChange={onChangeTabs}
+      >
+        <ToolbarToggleButton aria-label="Bold" name="tabs" value="tree">
+          Tree
+        </ToolbarToggleButton>
+        <ToolbarToggleButton aria-label="Italic" name="tabs" value="data">
+          Data
+        </ToolbarToggleButton>
+        <ToolbarToggleButton aria-label="Underline" name="tabs" value="map">
+          Map
+        </ToolbarToggleButton>
+      </Toolbar>
       <Button
         size="medium"
         icon={<FaCog />}
