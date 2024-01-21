a831196cef93ca205897e78fb145a0c180806a37
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-21 10:11:09 +0100

    work on tree

---
+++ src/components/Layout/Main/Row.tsx
@@ -0,0 +1,28 @@
+const labelStyle = {
+  fontSize: '1em',
+  flexGrow: 1,
+  paddingLeft: 5,
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  '&:hover': {
+    backgroundColor: rgba(55, 118, 28, 0.05),
+    cursor: 'pointer',
+  },
+}
+
+export const Row = ({ inActiveNodeArray, active, level, node }) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        fontWeight: inActiveNodeArray ? 'bold' : 'normal',
+        ...(active && { color: 'red' }),
+        marginLeft: level * 28,
+      }}
+    >
+      <div style={labelStyle}>{node.label}</div>
+    </div>
+  )
+}
