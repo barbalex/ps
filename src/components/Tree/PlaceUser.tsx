809d42c5169a8c347a240e41b68c416f83877289
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-23 11:17:55 +0100

    add nodes

--- src/components/Tree/PlaceUser.tsx
+++ src/components/Tree/PlaceUser.tsx
@@ -2,51 +2,55 @@ import { useCallback } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 
 import { Node } from './Node'
-import { PlaceUsers as PlaceUser } from '../../../generated/client'
+import {
+  PlaceUsers as PlaceUser,
+  Places as Place,
+} from '../../../generated/client'
 
 export const PlaceUserNode = ({
   project_id,
   subproject_id,
   place_id,
   placeUser,
+  place,
   level = 8,
 }: {
   project_id: string
   subproject_id: string
   placeUser: PlaceUser
+  place: Place
   level: number
 }) => {
   const location = useLocation()
   const navigate = useNavigate()
 
   const urlPath = location.pathname.split('/').filter((p) => p !== '')
-  const isOpen =
+  const isOpenBase =
     urlPath[0] === 'projects' &&
     urlPath[1] === project_id &&
     urlPath[2] === 'subprojects' &&
     urlPath[3] === subproject_id &&
     urlPath[4] === 'places' &&
-    urlPath[5] === place_id &&
-    urlPath[6] === 'users' &&
-    urlPath[7] === placeUser.place_user_id
+    urlPath[5] === (place_id ?? place.place_id)
+  const isOpen = place_id
+    ? isOpenBase &&
+      urlPath[6] === 'places' &&
+      urlPath[7] === place.place_id &&
+      urlPath[8] === 'users' &&
+      urlPath[9] === placeUser.place_user_id
+    : isOpenBase &&
+      urlPath[6] === 'users' &&
+      urlPath[7] === placeUser.place_user_id
   const isActive = isOpen && urlPath.length === level
 
+  const baseUrl = `/projects/${project_id}/subprojects/${subproject_id}/places/${
+    place_id ?? place.place_id
+  }${place_id ? `/places/${place.place_id}` : ''}/users`
+
   const onClickButton = useCallback(() => {
-    if (isOpen)
-      return navigate(
-        `/projects/${project_id}/subprojects/${subproject_id}/places/${place_id}/users`,
-      )
-    navigate(
-      `/projects/${project_id}/subprojects/${subproject_id}/places/${place_id}/users/${placeUser.place_user_id}`,
-    )
-  }, [
-    isOpen,
-    navigate,
-    placeUser.place_user_id,
-    place_id,
-    project_id,
-    subproject_id,
-  ])
+    if (isOpen) return navigate(baseUrl)
+    navigate(`${baseUrl}/${placeUser.place_user_id}`)
+  }, [baseUrl, isOpen, navigate, placeUser.place_user_id])
 
   return (
     <Node
@@ -56,7 +60,7 @@ export const PlaceUserNode = ({
       isInActiveNodeArray={isOpen}
       isActive={isActive}
       childrenCount={10}
-      to={`/projects/${project_id}/subprojects/${subproject_id}/places/${place_id}/users/${placeUser.place_user_id}`}
+      to={`${baseUrl}/${placeUser.place_user_id}`}
       onClickButton={onClickButton}
     />
   )
--- src/components/Tree/PlaceUsers.tsx
+++ src/components/Tree/PlaceUsers.tsx
@@ -11,6 +11,7 @@ export const PlaceUsersNode = ({
   project_id,
   subproject_id,
   place_id,
+  place,
   level = 7,
 }) => {
   const location = useLocation()
@@ -19,13 +20,12 @@ export const PlaceUsersNode = ({
   const { db } = useElectric()!
   const { results } = useLiveQuery(
     db.place_users.liveMany({
-      where: { deleted: false, place_id },
+      where: { deleted: false, place_id: place.place_id },
       orderBy: { label: 'asc' },
     }),
   )
   const placeUsers: PlaceUser[] = results ?? []
 
-  // TODO: get name by place_level
   const placeUsersNode = useMemo(
     () => ({
       label: `Users (${placeUsers.length})`,
@@ -34,25 +34,29 @@ export const PlaceUsersNode = ({
   )
 
   const urlPath = location.pathname.split('/').filter((p) => p !== '')
-  const isOpen =
+  const isOpenBase =
     urlPath[0] === 'projects' &&
     urlPath[1] === project_id &&
     urlPath[2] === 'subprojects' &&
     urlPath[3] === subproject_id &&
     urlPath[4] === 'places' &&
-    urlPath[5] === place_id &&
-    urlPath[6] === 'users'
+    urlPath[5] === (place_id ?? place.place_id)
+  const isOpen = place_id
+    ? isOpenBase &&
+      urlPath[6] === 'places' &&
+      urlPath[7] === place.place_id &&
+      urlPath[8] === 'users'
+    : isOpenBase && urlPath[6] === 'users'
   const isActive = isOpen && urlPath.length === level
 
+  const baseUrl = `/projects/${project_id}/subprojects/${subproject_id}/places/${
+    place_id ?? place.place_id
+  }${place_id ? `/places/${place.place_id}` : ''}`
+
   const onClickButton = useCallback(() => {
-    if (isOpen)
-      return navigate(
-        `/projects/${project_id}/subprojects/${subproject_id}/places/${place_id}`,
-      )
-    navigate(
-      `/projects/${project_id}/subprojects/${subproject_id}/places/${place_id}/users`,
-    )
-  }, [isOpen, navigate, place_id, project_id, subproject_id])
+    if (isOpen) return navigate(baseUrl)
+    navigate(`${baseUrl}/users`)
+  }, [baseUrl, isOpen, navigate])
 
   return (
     <>
@@ -63,7 +67,7 @@ export const PlaceUsersNode = ({
         isInActiveNodeArray={isOpen}
         isActive={isActive}
         childrenCount={placeUsers.length}
-        to={`/projects/${project_id}/subprojects/${subproject_id}/places/${place_id}/users`}
+        to={`${baseUrl}/users`}
         onClickButton={onClickButton}
       />
       {isOpen &&
@@ -73,7 +77,9 @@ export const PlaceUsersNode = ({
             project_id={project_id}
             subproject_id={subproject_id}
             place_id={place_id}
+            place={place}
             placeUser={placeUser}
+            level={level + 1}
           />
         ))}
     </>
