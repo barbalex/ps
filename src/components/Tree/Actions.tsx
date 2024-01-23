4d112256be72c869b385d0b4ea1c343d1b2592e9
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-23 11:17:35 +0100

    add nodes

--- src/components/Tree/Actions.tsx
+++ src/components/Tree/Actions.tsx
@@ -11,6 +11,7 @@ export const ActionsNode = ({
   project_id,
   subproject_id,
   place_id,
+  place,
   level = 7,
 }) => {
   const location = useLocation()
@@ -19,13 +20,12 @@ export const ActionsNode = ({
   const { db } = useElectric()!
   const { results } = useLiveQuery(
     db.actions.liveMany({
-      where: { deleted: false, place_id },
+      where: { deleted: false, place_id: place.place_id },
       orderBy: { label: 'asc' },
     }),
   )
   const actions: Action[] = results ?? []
 
-  // TODO: get name by place_level
   const actionsNode = useMemo(
     () => ({
       label: `Actions (${actions.length})`,
@@ -34,25 +34,29 @@ export const ActionsNode = ({
   )
 
   const urlPath = location.pathname.split('/').filter((p) => p !== '')
-  const isOpen =
+  const isOpenBase =
     urlPath[0] === 'projects' &&
     urlPath[1] === project_id &&
     urlPath[2] === 'subprojects' &&
     urlPath[3] === subproject_id &&
     urlPath[4] === 'places' &&
-    urlPath[5] === place_id &&
-    urlPath[6] === 'actions'
+    urlPath[5] === (place_id ?? place.place_id)
+  const isOpen = place_id
+    ? isOpenBase &&
+      urlPath[6] === 'places' &&
+      urlPath[7] === place.place_id &&
+      urlPath[8] === 'actions'
+    : isOpenBase && urlPath[6] === 'actions'
   const isActive = isOpen && urlPath.length === level
 
+  const baseUrl = `/projects/${project_id}/subprojects/${subproject_id}/places/${
+    place_id ?? place.place_id
+  }${place_id ? `/places/${place.place_id}` : ''}`
+
   const onClickButton = useCallback(() => {
-    if (isOpen)
-      return navigate(
-        `/projects/${project_id}/subprojects/${subproject_id}/places/${place_id}`,
-      )
-    navigate(
-      `/projects/${project_id}/subprojects/${subproject_id}/places/${place_id}/actions`,
-    )
-  }, [isOpen, navigate, place_id, project_id, subproject_id])
+    if (isOpen) return navigate(baseUrl)
+    navigate(`${baseUrl}/actions`)
+  }, [baseUrl, isOpen, navigate])
 
   return (
     <>
@@ -63,7 +67,7 @@ export const ActionsNode = ({
         isInActiveNodeArray={isOpen}
         isActive={isActive}
         childrenCount={actions.length}
-        to={`/projects/${project_id}/subprojects/${subproject_id}/places/${place_id}/actions`}
+        to={`${baseUrl}/actions`}
         onClickButton={onClickButton}
       />
       {isOpen &&
@@ -73,7 +77,9 @@ export const ActionsNode = ({
             project_id={project_id}
             subproject_id={subproject_id}
             place_id={place_id}
+            place={place}
             action={action}
+            level={level + 1}
           />
         ))}
     </>
