85689357cad1e81a8d1f8c6daf7b80db24d1f3ea
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-24 16:31:58 +0100

    try working with gbif

--- src/gbif.sh
+++ src/gbif.sh
@@ -14,14 +14,19 @@ curl -X 'GET' \
   -H 'accept: application/json' 
 
 # does not work
+# https://discourse.gbif.org/t/species-search-by-datasetkey-not-working/4263
 curl -X 'GET' \
   'https://api.gbif.org/v1/species/suggest?datasetKey=83fdfd3d-3a25-4705-9fbe-3db1d1892b13' \
   -H 'accept: application/json' 
+
+https://api.gbif.org/v1/species/search?datasetKey=83fdfd3d-3a25-4705-9fbe-3db1d1892b13
+https://api.gbif.org/v1/species/search?datasetKey=2d59e5db-57ad-41ff-97d6-11f5fb264527
 
 # does not work
 curl -X 'GET' \
   'https://api.gbif.org/v1/species/search?datasetKey=83fdfd3d-3a25-4705-9fbe-3db1d1892b13&q=veronica&limit=3' \
   -H 'accept: application/json'
+
 
 # does not work
 curl -X 'GET' \
@@ -58,6 +63,15 @@ curl -X 'GET' \
   'https://api.gbif.org/v1/occurrence/search?datasetKey=83fdfd3d-3a25-4705-9fbe-3db1d1892b13&speciesKey=3172062&limit=3&offset=0' \
   -H 'accept: application/json'
 
+# works: search by taxonID (url-encode the taxonID: infospecies.ch:infoflora:59100)
+curl -X 'GET' \
+  'https://api.gbif.org/v1/occurrence/search?datasetKey=83fdfd3d-3a25-4705-9fbe-3db1d1892b13&taxonID=infospecies.ch%3Ainfoflora%3A59100&limit=3&offset=0' \
+  -H 'accept: application/json'
+# BUT: how to get a list of speciesKeys for a dataset?
+
 curl -X 'GET' \
   'https://api.gbif.org/v1/occurrence/search?speciesKey=3172062&&limit=3&offset=0' \
   -H 'accept: application/json'
+
+  # species download from the occurrence dataset: GBIF.org (24 January 2024) GBIF Occurrence Download  https://doi.org/10.15468/dl.ctsxky
+  # https://discourse.gbif.org/t/species-search-by-datasetkey-not-working/4263/2
