732494d92d4962a6e584fe3950548ba72f143c8c
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-25 10:46:58 +0100

    prepare migration to electric-sql v0.9

--- src/components/SqlInitializer/labelGenerators/subprojects.ts
+++ src/components/SqlInitializer/labelGenerators/subprojects.ts
@@ -1,3 +1,7 @@
+// TODO: db.raw is deprecated in v0.9
+// https://electric-sql.com/docs/usage/data-access/queries#raw-sql
+// try db.rawQuery instead for reading data
+// alternatively use db.unsafeExec(sql): https://electric-sql.com/docs/api/clients/typescript#instantiation
 export const generateSubprojectLabel = async (db) => {
   const columns = await db.raw({
     sql: 'PRAGMA table_xinfo(subprojects)',
