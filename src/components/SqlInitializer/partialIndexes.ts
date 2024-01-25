b79d69750f104f49702fddcf33849a8cd37de388
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-25 10:47:34 +0100

    prepare migration to electric-sql v0.9

--- src/components/SqlInitializer/partialIndexes.ts
+++ src/components/SqlInitializer/partialIndexes.ts
@@ -1,5 +1,9 @@
 import sql from '../../sql/createPartialIndexes.txt'
 
+// TODO: db.raw is deprecated in v0.9
+// https://electric-sql.com/docs/usage/data-access/queries#raw-sql
+// try db.rawQuery instead for reading data
+// alternatively use db.unsafeExec(sql): https://electric-sql.com/docs/api/clients/typescript#instantiation
 export const generatePartialIndexes = async (db) => {
   await db.raw({
     sql,
