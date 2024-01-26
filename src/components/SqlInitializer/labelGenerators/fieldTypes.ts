a19785074ca989b77543ce7885619b11a0d07419
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-26 16:40:41 +0100

    use new methods

--- src/components/SqlInitializer/labelGenerators/fieldTypes.ts
+++ src/components/SqlInitializer/labelGenerators/fieldTypes.ts
@@ -1,22 +1,14 @@
-// TODO: db.raw is deprecated in v0.9
-// https://electric-sql.com/docs/usage/data-access/queries#raw-sql
-// try db.rawQuery instead for reading data
-// alternatively use db.unsafeExec(sql): https://electric-sql.com/docs/api/clients/typescript#instantiation
 export const generateFieldTypeLabel = async (db) => {
-  const columns = await db.raw({
+  const columns = await db.rawQuery({
     sql: 'PRAGMA table_xinfo(field_types)',
   })
   const hasLabel = columns.some((column) => column.name === 'label')
   if (!hasLabel) {
-    await db.raw({
+    await db.unsafeExec({
       sql: 'ALTER TABLE field_types ADD COLUMN label text GENERATED ALWAYS AS (coalesce(name, field_type_id))',
     })
-    await db.raw({
+    await db.unsafeExec({
       sql: 'CREATE INDEX IF NOT EXISTS field_types_label_idx ON field_types(label)',
     })
   }
-  // console.log('LabelGenerator, field_types:', {
-  //   columns,
-  //   hasLabel,
-  // })
 }
--- src/components/SqlInitializer/labelGenerators/fields.ts
+++ src/components/SqlInitializer/labelGenerators/fields.ts
@@ -1,22 +1,14 @@
-// TODO: db.raw is deprecated in v0.9
-// https://electric-sql.com/docs/usage/data-access/queries#raw-sql
-// try db.rawQuery instead for reading data
-// alternatively use db.unsafeExec(sql): https://electric-sql.com/docs/api/clients/typescript#instantiation
 export const generateFieldLabel = async (db) => {
-  const columns = await db.raw({
+  const columns = await db.rawQuery({
     sql: 'PRAGMA table_xinfo(fields)',
   })
   const hasLabel = columns.some((column) => column.name === 'label')
   if (!hasLabel) {
-    await db.raw({
+    await db.unsafeExec({
       sql: `ALTER TABLE fields ADD COLUMN label text GENERATED ALWAYS AS (iif(coalesce(table_name, name) is not null, table_name || '.' || name, field_id))`,
     })
-    await db.raw({
+    await db.unsafeExec({
       sql: 'CREATE INDEX IF NOT EXISTS fields_label_idx ON fields(label)',
     })
   }
-  // console.log('LabelGenerator, fields:', {
-  //   columns,
-  //   hasLabel,
-  // })
 }
