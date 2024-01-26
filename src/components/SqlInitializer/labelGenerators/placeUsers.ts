471020478b783f367fecaa46818e61336db501c4
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-26 16:49:05 +0100

    use new methods

--- src/components/SqlInitializer/labelGenerators/placeUsers.ts
+++ src/components/SqlInitializer/labelGenerators/placeUsers.ts
@@ -1,17 +1,13 @@
-// TODO: db.raw is deprecated in v0.9
-// https://electric-sql.com/docs/usage/data-access/queries#raw-sql
-// try db.rawQuery instead for reading data
-// alternatively use db.unsafeExec(sql): https://electric-sql.com/docs/api/clients/typescript#instantiation
 export const generatePlaceUserLabel = async (db) => {
   // if user_id or role is changed, update label with email from users and with role
-  const triggers = await db.raw({
+  const triggers = await db.rawQuery({
     sql: `select name from sqlite_master where type = 'trigger';`,
   })
   const placeUsersLabelTriggerExists = triggers.some(
     (column) => column.name === 'place_users_label_trigger',
   )
   if (!placeUsersLabelTriggerExists) {
-    const result = await db.raw({
+    const result = await db.unsafeExec({
       sql: `
       CREATE TRIGGER IF NOT EXISTS place_users_label_trigger
         AFTER UPDATE OF user_id, role ON place_users
@@ -21,7 +17,7 @@ export const generatePlaceUserLabel = async (db) => {
     })
     console.log('TriggerGenerator, place_users, result:', result)
     // same on insert
-    await db.raw({
+    await db.unsafeExec({
       sql: `
       CREATE TRIGGER IF NOT EXISTS place_users_label_trigger_insert
         AFTER INSERT ON place_users
--- src/components/SqlInitializer/labelGenerators/projectReports.ts
+++ src/components/SqlInitializer/labelGenerators/projectReports.ts
@@ -1,22 +1,14 @@
-// TODO: db.raw is deprecated in v0.9
-// https://electric-sql.com/docs/usage/data-access/queries#raw-sql
-// try db.rawQuery instead for reading data
-// alternatively use db.unsafeExec(sql): https://electric-sql.com/docs/api/clients/typescript#instantiation
 export const generateProjectReportLabel = async (db) => {
-  const columns = await db.raw({
+  const columns = await db.rawQuery({
     sql: 'PRAGMA table_xinfo(project_reports)',
   })
   const hasLabel = columns.some((column) => column.name === 'label')
   if (!hasLabel) {
-    await db.raw({
+    await db.unsafeExec({
       sql: 'ALTER TABLE project_reports ADD COLUMN label text GENERATED ALWAYS AS (coalesce(year, project_report_id))',
     })
-    await db.raw({
+    await db.unsafeExec({
       sql: 'CREATE INDEX IF NOT EXISTS project_reports_label_idx ON project_reports(label)',
     })
   }
-  // console.log('LabelGenerator, project_reports:', {
-  //   columns,
-  //   hasLabel,
-  // })
 }
