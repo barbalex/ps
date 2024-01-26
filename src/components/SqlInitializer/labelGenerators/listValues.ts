11b47912c342e9bcca9534ae8996999e36f3b834
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-26 16:44:03 +0100

    use new methods

--- src/components/SqlInitializer/labelGenerators/listValues.ts
+++ src/components/SqlInitializer/labelGenerators/listValues.ts
@@ -1,22 +1,14 @@
-// TODO: db.raw is deprecated in v0.9
-// https://electric-sql.com/docs/usage/data-access/queries#raw-sql
-// try db.rawQuery instead for reading data
-// alternatively use db.unsafeExec(sql): https://electric-sql.com/docs/api/clients/typescript#instantiation
 export const generateListValueLabel = async (db) => {
-  const columns = await db.raw({
+  const columns = await db.rawQuery({
     sql: 'PRAGMA table_xinfo(list_values)',
   })
   const hasLabel = columns.some((column) => column.name === 'label')
   if (!hasLabel) {
-    await db.raw({
+    await db.unsafeExec({
       sql: 'ALTER TABLE list_values ADD COLUMN label text GENERATED ALWAYS AS (coalesce(value, list_value_id))',
     })
-    await db.raw({
+    await db.unsafeExec({
       sql: 'CREATE INDEX IF NOT EXISTS list_values_label_idx ON list_values(label)',
     })
   }
-  // console.log('LabelGenerator, list_values:', {
-  //   columns,
-  //   hasLabel,
-  // })
 }
--- src/components/SqlInitializer/labelGenerators/lists.ts
+++ src/components/SqlInitializer/labelGenerators/lists.ts
@@ -1,22 +1,14 @@
-// TODO: db.raw is deprecated in v0.9
-// https://electric-sql.com/docs/usage/data-access/queries#raw-sql
-// try db.rawQuery instead for reading data
-// alternatively use db.unsafeExec(sql): https://electric-sql.com/docs/api/clients/typescript#instantiation
 export const generateListLabel = async (db) => {
-  const columns = await db.raw({
+  const columns = await db.rawQuery({
     sql: 'PRAGMA table_xinfo(lists)',
   })
   const hasLabel = columns.some((column) => column.name === 'label')
   if (!hasLabel) {
-    await db.raw({
+    await db.unsafeExec({
       sql: 'ALTER TABLE lists ADD COLUMN label text GENERATED ALWAYS AS (coalesce(name, list_id))',
     })
-    await db.raw({
+    await db.unsafeExec({
       sql: 'CREATE INDEX IF NOT EXISTS lists_label_idx ON lists(label)',
     })
   }
-  // console.log('LabelGenerator, lists:', {
-  //   columns,
-  //   hasLabel,
-  // })
 }
