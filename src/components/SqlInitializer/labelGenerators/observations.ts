ff95f11271e1e922502fe1ca0d3bb799380cfc1b
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-26 16:44:57 +0100

    use new methods

--- src/components/SqlInitializer/labelGenerators/observations.ts
+++ src/components/SqlInitializer/labelGenerators/observations.ts
@@ -1,17 +1,13 @@
-// TODO: db.raw is deprecated in v0.9
-// https://electric-sql.com/docs/usage/data-access/queries#raw-sql
-// try db.rawQuery instead for reading data
-// alternatively use db.unsafeExec(sql): https://electric-sql.com/docs/api/clients/typescript#instantiation
 export const generateObservationLabel = async (db) => {
-  const columns = await db.raw({
+  const columns = await db.rawQuery({
     sql: 'PRAGMA table_xinfo(observations)',
   })
   const hasLabel = columns.some((column) => column.name === 'label')
   if (!hasLabel) {
-    const result = await db.raw({
+    const result = await db.unsafeExec({
       sql: `ALTER TABLE observations ADD COLUMN label text GENERATED ALWAYS AS (coalesce(concat(date, ': ', author), observation_id))`,
     })
-    await db.raw({
+    await db.unsafeExec({
       sql: 'CREATE INDEX IF NOT EXISTS observations_label_idx ON observations(label)',
     })
     console.log('LabelGenerator, observations, result:', result)
