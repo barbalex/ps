7004ce948480533e9a4dea2bce6467c4368ae304
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-26 16:50:49 +0100

    use new methods

--- src/components/SqlInitializer/labelGenerators/subprojects.ts
+++ src/components/SqlInitializer/labelGenerators/subprojects.ts
@@ -1,22 +1,14 @@
-// TODO: db.raw is deprecated in v0.9
-// https://electric-sql.com/docs/usage/data-access/queries#raw-sql
-// try db.rawQuery instead for reading data
-// alternatively use db.unsafeExec(sql): https://electric-sql.com/docs/api/clients/typescript#instantiation
 export const generateSubprojectLabel = async (db) => {
-  const columns = await db.raw({
+  const columns = await db.rawQuery({
     sql: 'PRAGMA table_xinfo(subprojects)',
   })
   const hasLabel = columns.some((column) => column.name === 'label')
   if (!hasLabel) {
-    await db.raw({
+    await db.unsafeExec({
       sql: 'ALTER TABLE subprojects ADD COLUMN label text GENERATED ALWAYS AS (coalesce(name, subproject_id))',
     })
-    await db.raw({
+    await db.unsafeExec({
       sql: 'CREATE INDEX IF NOT EXISTS subprojects_label_idx ON subprojects(label)',
     })
   }
-  // console.log('LabelGenerator, subprojects:', {
-  //   columns,
-  //   hasLabel,
-  // })
 }
