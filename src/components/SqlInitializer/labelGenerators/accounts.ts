1144095ee8fec5b6dc83796c6b4e8fd8b976cbd5
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-26 16:35:53 +0100

    use new methods

--- src/components/SqlInitializer/labelGenerators/accounts.ts
+++ src/components/SqlInitializer/labelGenerators/accounts.ts
@@ -1,30 +1,26 @@
-// TODO: db.raw is deprecated in v0.9
-// https://electric-sql.com/docs/usage/data-access/queries#raw-sql
-// try db.rawQuery instead for reading data
-// alternatively use db.unsafeExec(sql): https://electric-sql.com/docs/api/clients/typescript#instantiation
 export const generateAccountLabel = async (db) => {
-  const columns = await db.raw({
+  const columns = await db.rawQuery({
     sql: 'PRAGMA table_xinfo(accounts)',
   })
   const hasLabel = columns.some((column) => column.name === 'label')
   if (!hasLabel) {
-    await db.raw({
+    await db.unsafeExec({
       sql: 'ALTER TABLE accounts ADD COLUMN label text GENERATED ALWAYS AS (account_id)',
     })
-    await db.raw({
+    await db.unsafeExec({
       sql: 'CREATE INDEX IF NOT EXISTS accounts_label_idx ON accounts(label)',
     })
   }
 
   // if projects_label_by is changed, need to update all labels of projects
-  const triggers = await db.raw({
+  const triggers = await db.rawQuery({
     sql: `select name from sqlite_master where type = 'trigger';`,
   })
   const accountsProjectsLabeltriggerExists = triggers.some(
     (column) => column.name === 'accounts_projects_label_trigger',
   )
   if (!accountsProjectsLabeltriggerExists) {
-    const result = await db.raw({
+    const result = await db.unsafeExec({
       sql: `
       CREATE TRIGGER IF NOT EXISTS accounts_projects_label_trigger
         AFTER UPDATE OF projects_label_by ON accounts
@@ -47,7 +43,7 @@ export const generateAccountLabel = async (db) => {
     (column) => column.name === 'accounts_label_trigger',
   )
   if (!accountsLabelTriggerExists) {
-    const result = await db.raw({
+    const result = await db.unsafeExec({
       sql: `
       CREATE TRIGGER IF NOT EXISTS accounts_label_trigger
         AFTER UPDATE OF user_id, type ON accounts
@@ -55,7 +51,7 @@ export const generateAccountLabel = async (db) => {
         UPDATE accounts SET label = (SELECT email FROM users WHERE user_id = NEW.user_id) || ' (' || NEW.type || ')';
       END;`,
     })
-    const resultInsert = await db.raw({
+    const resultInsert = await db.unsafeExec({
       sql: `
       CREATE TRIGGER IF NOT EXISTS accounts_label_insert_trigger
         AFTER insert ON accounts
