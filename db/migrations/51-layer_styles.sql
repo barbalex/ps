52e838aa3b60a96efaf96f08d426860eda82833a
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-29 23:24:14 +0100

    add layer_styles

--- db/migrations/51-layer_styles.sql
+++ db/migrations/51-layer_styles.sql
@@ -26,7 +26,11 @@ DROP TABLE IF EXISTS layer_styles CASCADE;
 
 CREATE TABLE layer_styles(
   layer_style_id uuid PRIMARY KEY DEFAULT NULL, -- gen_random_uuid(),
-  vector_layer_id uuid UNIQUE DEFAULT NULL REFERENCES vector_layers(vector_layer_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  vector_layer_id uuid DEFAULT NULL REFERENCES vector_layers(vector_layer_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  place_id uuid DEFAULT NULL REFERENCES places(place_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  action_id uuid DEFAULT NULL REFERENCES actions(action_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  check_id uuid DEFAULT NULL REFERENCES checks(check_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  observation_id uuid DEFAULT NULL REFERENCES observations(observation_id) ON DELETE CASCADE ON UPDATE CASCADE, -- really?
   -- TODO: add ids for all tables with geometries
   --table_id uuid UNIQUE DEFAULT NULL REFERENCES tables(id) ON DELETE CASCADE ON UPDATE CASCADE,
   marker_type marker_type_enum DEFAULT NULL, -- 'circle',
@@ -34,7 +38,7 @@ CREATE TABLE layer_styles(
   marker_symbol text DEFAULT NULL,
   marker_size integer DEFAULT NULL, -- 16,
   marker_weight integer DEFAULT NULL,
-  stroke integer DEFAULT NULL, -- 1,
+  stroke boolean DEFAULT NULL, -- true,
   color text DEFAULT NULL, -- '#3388ff',
   weight integer DEFAULT NULL, -- 3,
   opacity_percent integer DEFAULT NULL, -- 100,
@@ -49,13 +53,23 @@ CREATE TABLE layer_styles(
   deleted boolean DEFAULT NULL -- false
 );
 
-CREATE INDEX ON layer_styles USING btree(table_id);
+CREATE INDEX ON layer_styles USING btree(place_id);
 
-CREATE INDEX ON layer_styles USING btree(deleted);
+CREATE INDEX ON layer_styles USING btree(action_id);
+
+CREATE INDEX ON layer_styles USING btree(check_id);
+
+CREATE INDEX ON layer_styles USING btree(observation_id);
 
 COMMENT ON TABLE layer_styles IS 'Goal: style table layers, project tile layers and project vector layers';
 
-COMMENT ON COLUMN layer_styles.table_id IS 'associated table';
+COMMENT ON COLUMN layer_styles.place_id IS 'associated place';
+
+COMMENT ON COLUMN layer_styles.action_id IS 'associated action';
+
+COMMENT ON COLUMN layer_styles.check_id IS 'associated check';
+
+COMMENT ON COLUMN layer_styles.observation_id IS 'associated observation';
 
 COMMENT ON COLUMN layer_styles.marker_symbol IS 'Name of the symbol used for the marker';
 
@@ -67,7 +81,7 @@ COMMENT ON COLUMN layer_styles.color IS 'Stroke color. https://leafletjs.com/ref
 
 COMMENT ON COLUMN layer_styles.weight IS 'Stroke width in pixels. https://leafletjs.com/reference.html#path-weight';
 
-COMMENT ON COLUMN layer_styles.opacity IS 'Stroke opacity. https://leafletjs.com/reference.html#path-opacity';
+COMMENT ON COLUMN layer_styles.opacity_percent IS 'Stroke opacity. https://leafletjs.com/reference.html#path-opacity';
 
 COMMENT ON COLUMN layer_styles.line_cap IS 'A string that defines shape to be used at the end of the stroke. https://leafletjs.com/reference.html#path-linecap. https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/stroke-linecap';
 
@@ -81,17 +95,11 @@ COMMENT ON COLUMN layer_styles.fill IS 'Whether to fill the path with color. Set
 
 COMMENT ON COLUMN layer_styles.fill_color IS 'Fill color. Defaults to the value of the color option. https://leafletjs.com/reference.html#path-fillcolor';
 
-COMMENT ON COLUMN layer_styles.fill_opacity IS 'Fill opacity. https://leafletjs.com/reference.html#path-fillopacity';
+COMMENT ON COLUMN layer_styles.fill_opacity_percent IS 'Fill opacity. https://leafletjs.com/reference.html#path-fillopacity';
 
 COMMENT ON COLUMN layer_styles.fill_rule IS 'A string that defines how the inside of a shape is determined. https://leafletjs.com/reference.html#path-fillrule. https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/fill-rule';
 
 COMMENT ON COLUMN layer_styles.deleted IS 'marks if the row is deleted';
-
-COMMENT ON COLUMN layer_styles.client_rev_at IS 'time of last edit on client';
-
-COMMENT ON COLUMN layer_styles.client_rev_by IS 'user editing last on client';
-
-COMMENT ON COLUMN layer_styles.server_rev_at IS 'time of last edit on server';
 
 ALTER TABLE layer_options ENABLE electric;
 
