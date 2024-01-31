2d5f7fbf972d50e848f65604a78ce7aec577de53
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-31 16:23:45 +0100

    modularize

---
+++ src/routes/checkTaxon/FormHeader.tsx
@@ -0,0 +1,86 @@
+import { useCallback, useMemo, memo } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+
+import { createCheckTaxon } from '../../modules/createRows'
+import { useElectric } from '../../ElectricProvider'
+import { FormHeader } from '../../components/FormHeader'
+
+export const FormHeaderComponent = memo(({ autoFocusRef }) => {
+  const {
+    project_id,
+    subproject_id,
+    place_id,
+    place_id2,
+    check_id,
+    check_taxon_id,
+  } = useParams()
+  const navigate = useNavigate()
+
+  const { db } = useElectric()
+
+  const baseUrl = useMemo(
+    () =>
+      `/projects/${project_id}/subprojects/${subproject_id}/places/${place_id}${
+        place_id2 ? `/places/${place_id2}` : ''
+      }/checks/${check_id}/taxa`,
+    [check_id, place_id, place_id2, project_id, subproject_id],
+  )
+
+  const addRow = useCallback(async () => {
+    const checkTaxon = createCheckTaxon()
+    await db.check_taxa.create({
+      data: {
+        ...checkTaxon,
+        check_id,
+      },
+    })
+    navigate(`${baseUrl}/${checkTaxon.check_taxon_id}`)
+    autoFocusRef.current?.focus()
+  }, [autoFocusRef, baseUrl, check_id, db.check_taxa, navigate])
+
+  const deleteRow = useCallback(async () => {
+    await db.check_taxa.delete({
+      where: {
+        check_taxon_id,
+      },
+    })
+    navigate(baseUrl)
+  }, [baseUrl, check_taxon_id, db.check_taxa, navigate])
+
+  const toNext = useCallback(async () => {
+    const checkTaxa = await db.check_taxa.findMany({
+      where: { deleted: false, check_id },
+      orderBy: { label: 'asc' },
+    })
+    const len = checkTaxa.length
+    const index = checkTaxa.findIndex(
+      (p) => p.check_taxon_id === check_taxon_id,
+    )
+    const next = checkTaxa[(index + 1) % len]
+    navigate(`${baseUrl}/${next.check_taxon_id}`)
+  }, [baseUrl, check_id, check_taxon_id, db.check_taxa, navigate])
+
+  const toPrevious = useCallback(async () => {
+    const checkTaxa = await db.check_taxa.findMany({
+      where: { deleted: false, check_id },
+      orderBy: { label: 'asc' },
+    })
+    const len = checkTaxa.length
+    const index = checkTaxa.findIndex(
+      (p) => p.check_taxon_id === check_taxon_id,
+    )
+    const previous = checkTaxa[(index + len - 1) % len]
+    navigate(`${baseUrl}/${previous.check_taxon_id}`)
+  }, [baseUrl, check_id, check_taxon_id, db.check_taxa, navigate])
+
+  return (
+    <FormHeader
+      title="Check taxon"
+      addRow={addRow}
+      deleteRow={deleteRow}
+      toNext={toNext}
+      toPrevious={toPrevious}
+      tableName="check taxon"
+    />
+  )
+})
--- src/routes/checkTaxon/index.tsx
+++ src/routes/checkTaxon/index.tsx
@@ -1,28 +1,19 @@
 import { useCallback, useMemo, useRef } from 'react'
 import { useLiveQuery } from 'electric-sql/react'
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 import { CheckTaxa as CheckTaxon } from '../../../generated/client'
-import { createCheckTaxon } from '../../modules/createRows'
 import { useElectric } from '../../ElectricProvider'
 import { TextField } from '../../components/shared/TextField'
 import { DropdownField } from '../../components/shared/DropdownField'
 import { TextFieldInactive } from '../../components/shared/TextFieldInactive'
 import { getValueFromChange } from '../../modules/getValueFromChange'
-import { FormHeader } from '../../components/FormHeader'
+import { FormHeaderComponent } from './FormHeader'
 
 import '../../form.css'
 
 export const Component = () => {
-  const {
-    project_id,
-    subproject_id,
-    place_id,
-    place_id2,
-    check_id,
-    check_taxon_id,
-  } = useParams()
-  const navigate = useNavigate()
+  const { check_taxon_id } = useParams()
 
   const autoFocusRef = useRef<HTMLInputElement>(null)
 
@@ -30,61 +21,6 @@ export const Component = () => {
   const { results } = useLiveQuery(
     db.check_taxa.liveUnique({ where: { check_taxon_id } }),
   )
-
-  const baseUrl = useMemo(
-    () =>
-      `/projects/${project_id}/subprojects/${subproject_id}/places/${place_id}${
-        place_id2 ? `/places/${place_id2}` : ''
-      }/checks/${check_id}/taxa`,
-    [check_id, place_id, place_id2, project_id, subproject_id],
-  )
-
-  const addRow = useCallback(async () => {
-    const checkTaxon = createCheckTaxon()
-    await db.check_taxa.create({
-      data: {
-        ...checkTaxon,
-        check_id,
-      },
-    })
-    navigate(`${baseUrl}/${checkTaxon.check_taxon_id}`)
-    autoFocusRef.current?.focus()
-  }, [baseUrl, check_id, db.check_taxa, navigate])
-
-  const deleteRow = useCallback(async () => {
-    await db.check_taxa.delete({
-      where: {
-        check_taxon_id,
-      },
-    })
-    navigate(baseUrl)
-  }, [baseUrl, check_taxon_id, db.check_taxa, navigate])
-
-  const toNext = useCallback(async () => {
-    const checkTaxa = await db.check_taxa.findMany({
-      where: { deleted: false, check_id },
-      orderBy: { label: 'asc' },
-    })
-    const len = checkTaxa.length
-    const index = checkTaxa.findIndex(
-      (p) => p.check_taxon_id === check_taxon_id,
-    )
-    const next = checkTaxa[(index + 1) % len]
-    navigate(`${baseUrl}/${next.check_taxon_id}`)
-  }, [baseUrl, check_id, check_taxon_id, db.check_taxa, navigate])
-
-  const toPrevious = useCallback(async () => {
-    const checkTaxa = await db.check_taxa.findMany({
-      where: { deleted: false, check_id },
-      orderBy: { label: 'asc' },
-    })
-    const len = checkTaxa.length
-    const index = checkTaxa.findIndex(
-      (p) => p.check_taxon_id === check_taxon_id,
-    )
-    const previous = checkTaxa[(index + len - 1) % len]
-    navigate(`${baseUrl}/${previous.check_taxon_id}`)
-  }, [baseUrl, check_id, check_taxon_id, db.check_taxa, navigate])
 
   const row: CheckTaxon = results
 
@@ -109,14 +45,7 @@ export const Component = () => {
 
   return (
     <div className="form-outer-container">
-      <FormHeader
-        title="Check taxon"
-        addRow={addRow}
-        deleteRow={deleteRow}
-        toNext={toNext}
-        toPrevious={toPrevious}
-        tableName="check taxon"
-      />
+      <FormHeaderComponent autoFocusRef={autoFocusRef} />
       <div className="form-container">
         <TextFieldInactive
           label="ID"
