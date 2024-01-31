f72c42f4aa2eec6c5f4acb0b77b10ec39308ad34
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-31 16:35:42 +0100

    modularize

---
+++ src/routes/goal/FormHeader.tsx
@@ -0,0 +1,64 @@
+import { useCallback, memo } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+
+import { createGoal } from '../../modules/createRows'
+import { useElectric } from '../../ElectricProvider'
+import { FormHeader } from '../../components/FormHeader'
+
+export const FormHeaderComponent = memo(({ autoFocusRef }) => {
+  const { project_id, subproject_id, goal_id } = useParams()
+  const navigate = useNavigate()
+
+  const { db } = useElectric()
+
+  const baseUrl = `/projects/${project_id}/subprojects/${subproject_id}/goals`
+
+  const addRow = useCallback(async () => {
+    const data = await createGoal({ db, project_id, subproject_id })
+    await db.goals.create({ data })
+    navigate(`${baseUrl}/${data.goal_id}`)
+    autoFocusRef.current?.focus()
+  }, [autoFocusRef, baseUrl, db, navigate, project_id, subproject_id])
+
+  const deleteRow = useCallback(async () => {
+    await db.goals.delete({
+      where: {
+        goal_id,
+      },
+    })
+    navigate(baseUrl)
+  }, [baseUrl, db.goals, goal_id, navigate])
+
+  const toNext = useCallback(async () => {
+    const goals = await db.goals.findMany({
+      where: { deleted: false, subproject_id },
+      orderBy: { label: 'asc' },
+    })
+    const len = goals.length
+    const index = goals.findIndex((p) => p.goal_id === goal_id)
+    const next = goals[(index + 1) % len]
+    navigate(`${baseUrl}/${next.goal_id}`)
+  }, [baseUrl, db.goals, goal_id, navigate, subproject_id])
+
+  const toPrevious = useCallback(async () => {
+    const goals = await db.goals.findMany({
+      where: { deleted: false, subproject_id },
+      orderBy: { label: 'asc' },
+    })
+    const len = goals.length
+    const index = goals.findIndex((p) => p.goal_id === goal_id)
+    const previous = goals[(index + len - 1) % len]
+    navigate(`${baseUrl}/${previous.goal_id}`)
+  }, [baseUrl, db.goals, goal_id, navigate, subproject_id])
+
+  return (
+    <FormHeader
+      title="Goal"
+      addRow={addRow}
+      deleteRow={deleteRow}
+      toNext={toNext}
+      toPrevious={toPrevious}
+      tableName="goal"
+    />
+  )
+})
---
+++ src/routes/goal/index.tsx
@@ -0,0 +1,73 @@
+import { useCallback, useRef } from 'react'
+import { useLiveQuery } from 'electric-sql/react'
+import { useParams } from 'react-router-dom'
+
+import { Goals as Goal } from '../../../generated/client'
+import { useElectric } from '../../ElectricProvider'
+import { TextField } from '../../components/shared/TextField'
+import { TextFieldInactive } from '../../components/shared/TextFieldInactive'
+import { Jsonb } from '../../components/shared/Jsonb'
+import { getValueFromChange } from '../../modules/getValueFromChange'
+import { FormHeaderComponent } from './FormHeader'
+
+import '../../form.css'
+
+export const Component = () => {
+  const { goal_id } = useParams()
+
+  const autoFocusRef = useRef<HTMLInputElement>(null)
+
+  const { db } = useElectric()
+  const { results } = useLiveQuery(db.goals.liveUnique({ where: { goal_id } }))
+
+  const row: Goal = results
+
+  const onChange = useCallback(
+    (e, data) => {
+      const { name, value } = getValueFromChange(e, data)
+      db.goals.update({
+        where: { goal_id },
+        data: { [name]: value },
+      })
+    },
+    [db.goals, goal_id],
+  )
+
+  if (!row) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div className="form-outer-container">
+      <FormHeaderComponent autoFocusRef={autoFocusRef} />
+      <div className="form-container">
+        <TextFieldInactive
+          label="ID"
+          name="goal_id"
+          value={row.goal_id ?? ''}
+        />
+        <TextField
+          label="Year"
+          name="year"
+          value={row.year ?? ''}
+          type="number"
+          onChange={onChange}
+        />
+        <TextField
+          label="Name"
+          name="name"
+          value={row.name ?? ''}
+          onChange={onChange}
+          autoFocus
+          ref={autoFocusRef}
+        />
+        <Jsonb
+          table="goals"
+          idField="goal_id"
+          id={row.goal_id}
+          data={row.data ?? {}}
+        />
+      </div>
+    </div>
+  )
+}
