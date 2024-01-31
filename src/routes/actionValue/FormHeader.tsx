b44e684859934c62fbc02124b4f07cd30d08439b
Alexander Gabriel <alex.barbalex@gmail.com>
2024-01-31 15:12:37 +0100

    modularize

---
+++ src/routes/actionValue/FormHeader.tsx
@@ -0,0 +1,88 @@
+import { useCallback, useMemo } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+
+import { createActionValue } from '../../modules/createRows'
+import { useElectric } from '../../ElectricProvider'
+import { FormHeader } from '../../components/FormHeader'
+
+import '../../form.css'
+
+export const FormHeaderComponent = ({ autoFocusRef }) => {
+  const {
+    project_id,
+    subproject_id,
+    place_id,
+    place_id2,
+    action_id,
+    action_value_id,
+  } = useParams()
+  const navigate = useNavigate()
+
+  const { db } = useElectric()
+
+  const baseUrl = useMemo(
+    () =>
+      `/projects/${project_id}/subprojects/${subproject_id}/places/${place_id}${
+        place_id2 ? `/places/${place_id2}` : ''
+      }/actions/${action_id}/values`,
+    [action_id, place_id, place_id2, project_id, subproject_id],
+  )
+
+  const addRow = useCallback(async () => {
+    const actionValue = createActionValue()
+    await db.action_values.create({
+      data: {
+        ...actionValue,
+        action_id,
+      },
+    })
+    navigate(`${baseUrl}/${actionValue.action_value_id}`)
+    autoFocusRef.current?.focus()
+  }, [action_id, autoFocusRef, baseUrl, db.action_values, navigate])
+
+  const deleteRow = useCallback(async () => {
+    await db.action_values.delete({
+      where: {
+        action_value_id,
+      },
+    })
+    navigate(baseUrl)
+  }, [action_value_id, baseUrl, db.action_values, navigate])
+
+  const toNext = useCallback(async () => {
+    const actionValues = await db.action_values.findMany({
+      where: { deleted: false, action_id },
+      orderBy: { label: 'asc' },
+    })
+    const len = actionValues.length
+    const index = actionValues.findIndex(
+      (p) => p.action_value_id === action_value_id,
+    )
+    const next = actionValues[(index + 1) % len]
+    navigate(`${baseUrl}/${next.action_value_id}`)
+  }, [action_id, action_value_id, baseUrl, db.action_values, navigate])
+
+  const toPrevious = useCallback(async () => {
+    const actionValues = await db.action_values.findMany({
+      where: { deleted: false, action_id },
+      orderBy: { label: 'asc' },
+    })
+    const len = actionValues.length
+    const index = actionValues.findIndex(
+      (p) => p.action_value_id === action_value_id,
+    )
+    const previous = actionValues[(index + len - 1) % len]
+    navigate(`${baseUrl}/${previous.action_value_id}`)
+  }, [action_id, action_value_id, baseUrl, db.action_values, navigate])
+
+  return (
+    <FormHeader
+      title="Action Value"
+      addRow={addRow}
+      deleteRow={deleteRow}
+      toNext={toNext}
+      toPrevious={toPrevious}
+      tableName="action value"
+    />
+  )
+}
--- src/routes/actionValue/index.tsx
+++ src/routes/actionValue/index.tsx
@@ -1,28 +1,19 @@
 import { useCallback, useMemo, useRef } from 'react'
 import { useLiveQuery } from 'electric-sql/react'
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 import { ActionValues as ActionValue } from '../../../generated/client'
-import { createActionValue } from '../../modules/createRows'
 import { useElectric } from '../../ElectricProvider'
 import { TextField } from '../../components/shared/TextField'
 import { TextFieldInactive } from '../../components/shared/TextFieldInactive'
 import { DropdownField } from '../../components/shared/DropdownField'
 import { getValueFromChange } from '../../modules/getValueFromChange'
-import { FormHeader } from '../../components/FormHeader'
+import { FormHeaderComponent } from './FormHeader'
 
 import '../../form.css'
 
 export const Component = () => {
-  const {
-    project_id,
-    subproject_id,
-    place_id,
-    place_id2,
-    action_id,
-    action_value_id,
-  } = useParams()
-  const navigate = useNavigate()
+  const { action_value_id } = useParams()
 
   const autoFocusRef = useRef<HTMLInputElement>(null)
 
@@ -30,61 +21,6 @@ export const Component = () => {
   const { results } = useLiveQuery(
     db.action_values.liveUnique({ where: { action_value_id } }),
   )
-
-  const baseUrl = useMemo(
-    () =>
-      `/projects/${project_id}/subprojects/${subproject_id}/places/${place_id}${
-        place_id2 ? `/places/${place_id2}` : ''
-      }/actions/${action_id}/values`,
-    [action_id, place_id, place_id2, project_id, subproject_id],
-  )
-
-  const addRow = useCallback(async () => {
-    const actionValue = createActionValue()
-    await db.action_values.create({
-      data: {
-        ...actionValue,
-        action_id,
-      },
-    })
-    navigate(`${baseUrl}/${actionValue.action_value_id}`)
-    autoFocusRef.current?.focus()
-  }, [action_id, baseUrl, db.action_values, navigate])
-
-  const deleteRow = useCallback(async () => {
-    await db.action_values.delete({
-      where: {
-        action_value_id,
-      },
-    })
-    navigate(baseUrl)
-  }, [action_value_id, baseUrl, db.action_values, navigate])
-
-  const toNext = useCallback(async () => {
-    const actionValues = await db.action_values.findMany({
-      where: { deleted: false, action_id },
-      orderBy: { label: 'asc' },
-    })
-    const len = actionValues.length
-    const index = actionValues.findIndex(
-      (p) => p.action_value_id === action_value_id,
-    )
-    const next = actionValues[(index + 1) % len]
-    navigate(`${baseUrl}/${next.action_value_id}`)
-  }, [action_id, action_value_id, baseUrl, db.action_values, navigate])
-
-  const toPrevious = useCallback(async () => {
-    const actionValues = await db.action_values.findMany({
-      where: { deleted: false, action_id },
-      orderBy: { label: 'asc' },
-    })
-    const len = actionValues.length
-    const index = actionValues.findIndex(
-      (p) => p.action_value_id === action_value_id,
-    )
-    const previous = actionValues[(index + len - 1) % len]
-    navigate(`${baseUrl}/${previous.action_value_id}`)
-  }, [action_id, action_value_id, baseUrl, db.action_values, navigate])
 
   const row: ActionValue = results
 
@@ -112,14 +48,7 @@ export const Component = () => {
 
   return (
     <div className="form-outer-container">
-      <FormHeader
-        title="Action Value"
-        addRow={addRow}
-        deleteRow={deleteRow}
-        toNext={toNext}
-        toPrevious={toPrevious}
-        tableName="action value"
-      />
+      <FormHeaderComponent autoFocusRef={autoFocusRef} />
       <div className="form-container">
         <TextFieldInactive
           label="ID"
