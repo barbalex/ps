9c289c8b0ae0bc8cc9cdf101292c204c3540a5a2
Alexander Gabriel <alex.barbalex@gmail.com>
2024-02-01 11:20:47 +0100

    modularize

---
+++ src/routes/tileLayer/Header.tsx
@@ -0,0 +1,62 @@
+import { useCallback, memo } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+
+import { createTileLayer } from '../../modules/createRows'
+import { useElectric } from '../../ElectricProvider'
+import { FormHeader } from '../../components/FormHeader'
+
+export const Header = memo(({ autoFocusRef }) => {
+  const { project_id, tile_layer_id } = useParams()
+  const navigate = useNavigate()
+
+  const { db } = useElectric()
+
+  const baseUrl = `/projects/${project_id}/tile-layers`
+
+  const addRow = useCallback(async () => {
+    const tileLayer = createTileLayer({ project_id })
+    await db.tile_layers.create({ data: tileLayer })
+    navigate(`${baseUrl}/${tileLayer.tile_layer_id}`)
+    autoFocusRef.current?.focus()
+  }, [autoFocusRef, baseUrl, db.tile_layers, navigate, project_id])
+
+  const deleteRow = useCallback(async () => {
+    await db.tile_layers.delete({
+      where: { tile_layer_id },
+    })
+    navigate(baseUrl)
+  }, [baseUrl, db.tile_layers, navigate, tile_layer_id])
+
+  const toNext = useCallback(async () => {
+    const tileLayers = await db.tile_layers.findMany({
+      where: { deleted: false, project_id },
+      orderBy: { label: 'asc' },
+    })
+    const len = tileLayers.length
+    const index = tileLayers.findIndex((p) => p.tile_layer_id === tile_layer_id)
+    const next = tileLayers[(index + 1) % len]
+    navigate(`${baseUrl}/${next.tile_layer_id}`)
+  }, [baseUrl, db.tile_layers, navigate, project_id, tile_layer_id])
+
+  const toPrevious = useCallback(async () => {
+    const tileLayers = await db.tile_layers.findMany({
+      where: { deleted: false, project_id },
+      orderBy: { label: 'asc' },
+    })
+    const len = tileLayers.length
+    const index = tileLayers.findIndex((p) => p.tile_layer_id === tile_layer_id)
+    const previous = tileLayers[(index + len - 1) % len]
+    navigate(`${baseUrl}/${previous.tile_layer_id}`)
+  }, [baseUrl, db.tile_layers, navigate, project_id, tile_layer_id])
+
+  return (
+    <FormHeader
+      title="Tile Layer"
+      addRow={addRow}
+      deleteRow={deleteRow}
+      toNext={toNext}
+      toPrevious={toPrevious}
+      tableName="tile layer"
+    />
+  )
+})
--- src/routes/tileLayer/index.tsx
+++ src/routes/tileLayer/index.tsx
@@ -1,9 +1,8 @@
 import { useCallback, useRef } from 'react'
 import { useLiveQuery } from 'electric-sql/react'
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 import { Tile_layers as TileLayer } from '../../../generated/client'
-import { createTileLayer } from '../../modules/createRows'
 import { useElectric } from '../../ElectricProvider'
 import { TextFieldInactive } from '../../components/shared/TextFieldInactive'
 import { TextField } from '../../components/shared/TextField'
@@ -13,14 +12,13 @@ import { RadioGroupField } from '../../components/shared/RadioGroupField'
 import { MultiSelectFromLayerOptions } from '../../components/shared/MultiSelectFromLayerOptions'
 import { DropdownFieldFromLayerOptions } from '../../components/shared/DropdownFieldFromLayerOptions'
 import { getValueFromChange } from '../../modules/getValueFromChange'
-import { FormHeader } from '../../components/FormHeader'
 import { BaseUrl } from './BaseUrl'
+import { Header } from './Header'
 
 import '../../form.css'
 
 export const Component = () => {
-  const { project_id, tile_layer_id } = useParams()
-  const navigate = useNavigate()
+  const { tile_layer_id } = useParams()
 
   const autoFocusRef = useRef<HTMLInputElement>(null)
 
@@ -28,44 +26,6 @@ export const Component = () => {
   const { results } = useLiveQuery(
     db.tile_layers.liveUnique({ where: { tile_layer_id } }),
   )
-
-  const baseUrl = `/projects/${project_id}/tile-layers`
-
-  const addRow = useCallback(async () => {
-    const tileLayer = createTileLayer({ project_id })
-    await db.tile_layers.create({ data: tileLayer })
-    navigate(`${baseUrl}/${tileLayer.tile_layer_id}`)
-    autoFocusRef.current?.focus()
-  }, [baseUrl, db.tile_layers, navigate, project_id])
-
-  const deleteRow = useCallback(async () => {
-    await db.tile_layers.delete({
-      where: { tile_layer_id },
-    })
-    navigate(baseUrl)
-  }, [baseUrl, db.tile_layers, navigate, tile_layer_id])
-
-  const toNext = useCallback(async () => {
-    const tileLayers = await db.tile_layers.findMany({
-      where: { deleted: false, project_id },
-      orderBy: { label: 'asc' },
-    })
-    const len = tileLayers.length
-    const index = tileLayers.findIndex((p) => p.tile_layer_id === tile_layer_id)
-    const next = tileLayers[(index + 1) % len]
-    navigate(`${baseUrl}/${next.tile_layer_id}`)
-  }, [baseUrl, db.tile_layers, navigate, project_id, tile_layer_id])
-
-  const toPrevious = useCallback(async () => {
-    const tileLayers = await db.tile_layers.findMany({
-      where: { deleted: false, project_id },
-      orderBy: { label: 'asc' },
-    })
-    const len = tileLayers.length
-    const index = tileLayers.findIndex((p) => p.tile_layer_id === tile_layer_id)
-    const previous = tileLayers[(index + len - 1) % len]
-    navigate(`${baseUrl}/${previous.tile_layer_id}`)
-  }, [baseUrl, db.tile_layers, navigate, project_id, tile_layer_id])
 
   const row: TileLayer = results
 
@@ -88,14 +48,7 @@ export const Component = () => {
 
   return (
     <div className="form-outer-container">
-      <FormHeader
-        title="Tile Layer"
-        addRow={addRow}
-        deleteRow={deleteRow}
-        toNext={toNext}
-        toPrevious={toPrevious}
-        tableName="tile layer"
-      />
+      <Header autoFocusRef={autoFocusRef} />
       <div className="form-container">
         <TextFieldInactive
           label="ID"
