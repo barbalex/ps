ef9b3b150a5eb2dfe0893995317227145cbd067b
Alexander Gabriel <alex.barbalex@gmail.com>
2024-02-01 11:17:45 +0100

    rename

--- src/routes/taxon/index.tsx
+++ src/routes/taxon/index.tsx
@@ -7,7 +7,7 @@ import { useElectric } from '../../ElectricProvider'
 import { TextField } from '../../components/shared/TextField'
 import { TextFieldInactive } from '../../components/shared/TextFieldInactive'
 import { getValueFromChange } from '../../modules/getValueFromChange'
-import { FormHeaderComponent } from './FormHeader'
+import { Header } from './Header'
 
 import '../../form.css'
 
@@ -38,7 +38,7 @@ export const Component = () => {
 
   return (
     <div className="form-outer-container">
-      <FormHeaderComponent autoFocusRef={autoFocusRef} />
+      <Header autoFocusRef={autoFocusRef} />
       <div className="form-container">
         <TextFieldInactive label="ID" name="taxon_id" value={row.taxon_id} />
         <TextField
