bc83a4344e7b05232c1202e5d5487e94522696d6
Alexander Gabriel <alex.barbalex@gmail.com>
2024-02-01 11:11:04 +0100

    rename

--- src/routes/observationSource/Header.tsx
+++ src/routes/observationSource/Header.tsx
@@ -5,7 +5,7 @@ import { createObservationSource } from '../../modules/createRows'
 import { useElectric } from '../../ElectricProvider'
 import { FormHeader } from '../../components/FormHeader'
 
-export const FormHeaderComponent = memo(({ autoFocusRef }) => {
+export const Header = memo(({ autoFocusRef }) => {
   const { project_id, observation_source_id } = useParams()
   const navigate = useNavigate()
 
