5a1bcb9e8c7e39d4402223f5075818a1a5c700bd
Alexander Gabriel <alex.barbalex@gmail.com>
2024-02-01 11:01:12 +0100

    rename

--- src/routes/actionValue/Header.tsx
+++ src/routes/actionValue/Header.tsx
@@ -5,7 +5,7 @@ import { createActionValue } from '../../modules/createRows'
 import { useElectric } from '../../ElectricProvider'
 import { FormHeader } from '../../components/FormHeader'
 
-export const FormHeaderComponent = memo(({ autoFocusRef }) => {
+export const Header = memo(({ autoFocusRef }) => {
   const {
     project_id,
     subproject_id,
