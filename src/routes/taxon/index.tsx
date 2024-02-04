0b13374757491f9f93180a223f13338f603acbe7
Alexander Gabriel <alex.barbalex@gmail.com>
2024-02-04 15:36:13 +0100

    type change events

--- src/routes/taxon/index.tsx
+++ src/routes/taxon/index.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useRef } from 'react'
 import { useLiveQuery } from 'electric-sql/react'
 import { useParams } from 'react-router-dom'
+import type { InputProps } from '@fluentui/react-components'
 
 import { Taxa as Taxon } from '../../../generated/client'
 import { useElectric } from '../../ElectricProvider'
@@ -21,7 +22,7 @@ export const Component = () => {
 
   const row: Taxon = results
 
-  const onChange = useCallback(
+  const onChange: InputProps['onChange'] = useCallback(
     (e, data) => {
       const { name, value } = getValueFromChange(e, data)
       db.taxa.update({
--- src/routes/tileLayer/index.tsx
+++ src/routes/tileLayer/index.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useRef } from 'react'
 import { useLiveQuery } from 'electric-sql/react'
 import { useParams } from 'react-router-dom'
+import type { InputProps } from '@fluentui/react-components'
 
 import { Tile_layers as TileLayer } from '../../../generated/client'
 import { useElectric } from '../../ElectricProvider'
@@ -28,7 +29,7 @@ export const Component = () => {
 
   const row: TileLayer = results
 
-  const onChange = useCallback(
+  const onChange: InputProps['onChange'] = useCallback(
     async (e, data) => {
       const { name, value } = getValueFromChange(e, data)
       return await db.tile_layers.update({
