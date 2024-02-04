3bd1b9321f01cd82f37cacaadd1783ef49d7b0a9
Alexander Gabriel <alex.barbalex@gmail.com>
2024-02-04 18:09:23 +0100

    style

--- src/components/shared/ColorPicker.tsx
+++ src/components/shared/ColorPicker.tsx
@@ -2,24 +2,17 @@ import { useState, useEffect, useCallback } from 'react'
 import { HexColorPicker } from 'react-colorful'
 import { Input, Field } from '@fluentui/react-components'
 
-import { Label } from './Label'
-
-const inputRowStyle = {
-  display: 'flex',
-}
-const inputLabelStyle = {
-  color: 'rgba(0, 0, 0, 0.8)',
-  fontSize: '0.8rem',
-  paddingRight: '8px',
-  alignSelf: 'center',
+const outerFieldStyle = {
+  paddingBottom: 19,
+  paddingTop: 19,
 }
 // need to vertically align, see: https://github.com/microsoft/fluentui/issues/30470
-const fieldStyle = { gridTemplateColumns: '80px 1fr', alignItems: 'center' }
+const horizontalFieldStyle = {
+  gridTemplateColumns: '80px 1fr',
+  alignItems: 'center',
+}
 const inputStyle = {
   width: 80,
-}
-const formControlStyle = {
-  paddingBottom: '19px',
 }
 
 interface Props {
@@ -59,23 +52,24 @@ export const ColorPicker = ({
 
   // weird placing without the div
   return (
-    <div>
-      <div style={formControlStyle} onBlur={onBlurControl}>
-        <Label label={label} />
-        <HexColorPicker color={val} onChange={setVal} />
-        <Field label="Hex-Wert" orientation="horizontal" style={fieldStyle}>
-          <Input
-            name={name}
-            value={val}
-            type="text"
-            onChange={(e) => setVal(e.target.value)}
-            onBlur={onBlurInput}
-            disabled={disabled}
-            style={inputStyle}
-            appearance="underline"
-          />
-        </Field>
-      </div>
-    </div>
+    <Field label={label} style={outerFieldStyle}>
+      <HexColorPicker color={val} onChange={setVal} />
+      <Field
+        label="Hex-Wert"
+        orientation="horizontal"
+        style={horizontalFieldStyle}
+      >
+        <Input
+          name={name}
+          value={val}
+          type="text"
+          onChange={(e) => setVal(e.target.value)}
+          onBlur={onBlurInput}
+          disabled={disabled}
+          style={inputStyle}
+          appearance="underline"
+        />
+      </Field>
+    </Field>
   )
 }
