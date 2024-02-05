ebbcbcf8ec376a05c81aecce3d12db03d23aee07
Alexander Gabriel <alex.barbalex@gmail.com>
2024-02-05 12:24:44 +0100

    name subprojects folder in tree

--- src/components/Tree/Subprojects.tsx
+++ src/components/Tree/Subprojects.tsx
@@ -20,11 +20,20 @@ export const SubprojectsNode = ({ project_id, level = 3 }) => {
   )
   const subprojects: Subproject[] = results ?? []
 
+  console.log('hello from SubprojectsNode, subprojects:', subprojects)
+
+  // get projects.subproject_name_plural to name the table
+  // can't include projects in subprojects query because there will be no result before subprojects are created
+  const { results: project } = useLiveQuery(
+    db.projects.liveUnique({ where: { project_id } }),
+  )
+  const namePlural = project?.subproject_name_plural ?? 'Subprojects'
+
   const subprojectsNode = useMemo(
     () => ({
-      label: `Subprojects (${subprojects.length})`,
+      label: `${namePlural} (${subprojects.length})`,
     }),
-    [subprojects.length],
+    [namePlural, subprojects.length],
   )
 
   const urlPath = location.pathname.split('/').filter((p) => p !== '')
@@ -38,6 +47,9 @@ export const SubprojectsNode = ({ project_id, level = 3 }) => {
     if (isOpen) return navigate(`/projects/${project_id}`)
     navigate(`/projects/${project_id}/subprojects`)
   }, [isOpen, navigate, project_id])
+
+  // prevent flash of different name that happens before namePlural is set
+  if (!namePlural) return null
 
   return (
     <>
