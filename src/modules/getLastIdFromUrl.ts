3257cf486df19b42e293dd1d643089ea1a9d2241
Alexander Gabriel <alex.barbalex@gmail.com>
2024-02-06 16:15:28 +0100

    comment

--- src/modules/getLastIdFromUrl.ts
+++ src/modules/getLastIdFromUrl.ts
@@ -4,6 +4,7 @@ export const getLastIdFromUrl = (url: string[]) => {
   if (!url) return undefined
   if (url.length === 0) return undefined
   const last = url.at(-1)
+  // TODO: always returns fals as isUuid does not work for v7
   if (isUuid.anyNonNil(last)) {
     return last
   }
