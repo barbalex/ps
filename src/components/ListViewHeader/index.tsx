7e1dffaf92b0cf8a66baa4e7ff4df9c6600ad7e6
Alexander Gabriel <alex.barbalex@gmail.com>
2024-02-06 23:41:55 +0100

    optimize

--- src/components/ListViewHeader/index.tsx
+++ src/components/ListViewHeader/index.tsx
@@ -1,11 +1,9 @@
 import { memo } from 'react'
 import { Menu } from './Menu'
 
-export const ListViewHeader = memo(({ title, addRow, tableName, menus }) => {
-  return (
-    <div className="list-view-header">
-      <h1>{title}</h1>
-      {!!addRow && <Menu addRow={addRow} tableName={tableName} menus={menus} />}
-    </div>
-  )
-})
+export const ListViewHeader = memo(({ title, addRow, tableName, menus }) => (
+  <div className="list-view-header">
+    <h1>{title}</h1>
+    {!!addRow && <Menu addRow={addRow} tableName={tableName} menus={menus} />}
+  </div>
+))
