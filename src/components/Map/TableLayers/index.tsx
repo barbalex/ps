6f98d2d1e792da92396eb13e0d346f2df0312b0d
Alexander Gabriel <alex.barbalex@gmail.com>
2024-02-09 22:54:52 +0100

    work on adding table layers

--- src/components/Map/TableLayers/index.tsx
+++ src/components/Map/TableLayers/index.tsx
@@ -5,12 +5,16 @@ import { Places1 } from './Places1'
 import { Places2 } from './Places2'
 import { Checks1 } from './Checks1'
 import { Checks2 } from './Checks2'
+import { Actions1 } from './Actions1'
+import { Actions2 } from './Actions2'
 
 const layerToComponent = {
   places1: Places1,
   places2: Places2,
   checks1: Checks1,
   checks2: Checks2,
+  actions1: Actions1,
+  actions2: Actions2,
 }
 
 export const TableLayers = () => {
@@ -22,13 +26,9 @@ export const TableLayers = () => {
     }),
   )
 
-  console.log(
-    'hello TableLayers, vectorLayerDisplayResults:',
-    vectorLayerDisplayResults,
-  )
-
   return vectorLayerDisplayResults.map((display) => {
     const Component = layerToComponent[display.data_table]
+
     return <Component key={display.vector_layer_display_id} display={display} />
   })
 }
