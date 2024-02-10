035bf6fc3a9d5cd1d461e48fbda9b2c1cb7826ba
Alexander Gabriel <alex.barbalex@gmail.com>
2024-02-10 19:06:04 +0100

    label better

--- src/components/SqlInitializer/labelGenerators/vectorLayerDisplays.ts
+++ src/components/SqlInitializer/labelGenerators/vectorLayerDisplays.ts
@@ -5,7 +5,7 @@ export const generateVectorLayerDisplayLabel = async (db) => {
   const hasLabel = columns.some((column) => column.name === 'label')
   if (!hasLabel) {
     await db.unsafeExec({
-      sql: `ALTER TABLE vector_layer_displays ADD COLUMN label text GENERATED ALWAYS AS (coalesce(concat(property_field, ': ', property_value), vector_layer_display_id))`,
+      sql: `ALTER TABLE vector_layer_displays ADD COLUMN label text GENERATED ALWAYS AS (iif(property_field is null, 'Single Display', concat(property_field, ': ', property_value)))`,
     })
     await db.unsafeExec({
       sql: 'CREATE INDEX IF NOT EXISTS vector_layer_displays_label_idx ON vector_layer_displays(label)',
