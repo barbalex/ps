9df146dcdb7bfa984db10672f7248f7935597a81
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2024-02-12 21:37:51 +0100

    clean

--- src/routes/vectorLayer/index.tsx
+++ src/routes/vectorLayer/index.tsx
@@ -43,7 +43,7 @@ export const Component = () => {
     return <div>Loading...</div>
   }
 
-  console.log('hello VectorLayerForm, row:', row)
+  // console.log('hello VectorLayerForm, row:', row)
 
   return (
     <div className="form-outer-container">
