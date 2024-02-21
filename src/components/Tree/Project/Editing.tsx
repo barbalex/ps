d28273f0592d5cc35909b5b026be3d5713231dff
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2024-02-21 23:34:35 +0100

    work on designing

--- src/components/Tree/Project/Editing.tsx
+++ src/components/Tree/Project/Editing.tsx
@@ -2,6 +2,7 @@ import { useCallback } from 'react'
 import { MdEdit, MdEditOff } from 'react-icons/md'
 import { useLiveQuery } from 'electric-sql/react'
 import { Button } from '@fluentui/react-components'
+import { useParams } from 'react-router-dom'
 
 import { useElectric } from '../../../ElectricProvider'
 import { user_id } from '../../SqlInitializer'
@@ -9,11 +10,16 @@ import { user_id } from '../../SqlInitializer'
 const buttonStyle = {
   borderRadius: 20,
   border: 'none',
-  backgroundColor: 'transparent',
   color: 'rgb(51, 51, 51) !important',
 }
 
+const svgStyle = {
+  fontSize: 'medium',
+}
+
 export const Editing = () => {
+  const { project_id } = useParams()
+
   const { db } = useElectric()!
   const { results: uiOption } = useLiveQuery(
     db.ui_options.liveUnique({ where: { user_id } }),
@@ -27,15 +33,32 @@ export const Editing = () => {
     })
   }, [db.ui_options, designing])
 
-  // if (!designing) return null
+  const { results: project } = useLiveQuery(
+    db.projects.liveUnique({ where: { project_id } }),
+  )
+  const { results: account } = useLiveQuery(
+    db.accounts.liveFirst({ where: { user_id } }),
+  )
+  const userIsOwner = project?.account_id === account?.account_id
+  const { results: projectUser } = useLiveQuery(
+    db.project_users.liveFirst({
+      where: { project_id, user_id, deleted: false },
+    }),
+  )
+  const userRole = projectUser?.role
+
+  const userMayDesign = userIsOwner || userRole === 'manager'
+
+  if (!userMayDesign) return null
 
   return (
     <Button
       size="small"
-      icon={designing ? <MdEdit /> : <MdEditOff />}
+      icon={
+        designing ? <MdEdit style={svgStyle} /> : <MdEditOff style={svgStyle} />
+      }
       onClick={onClick}
       style={buttonStyle}
-      className="tree-node"
       title={
         designing ? 'Designing this project. Click to stop' : 'Start designing'
       }
