e27a2a2719a1c28ec693793199413da8e9b0ed49
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2024-02-23 18:54:22 +0100

    modularize

--- src/routes/project/index.tsx
+++ src/routes/project/index.tsx
@@ -14,6 +14,8 @@ import { getValueFromChange } from '../../modules/getValueFromChange'
 import { LabelBy } from '../../components/shared/LabelBy'
 import { FieldList } from '../../components/shared/FieldList'
 import { Header } from './Header'
+import { SwitchField } from '../../components/shared/SwitchField'
+import { Design } from './Design'
 
 import '../../form.css'
 
@@ -66,131 +68,7 @@ export const Component = () => {
           data={row.data ?? {}}
         />
         <Divider />
-        <Label>Project configuration</Label>
-        <RadioGroupField
-          label="Type"
-          name="type"
-          list={['species', 'biotope']}
-          value={row.type ?? ''}
-          onChange={onChange}
-        />
-        <TextField
-          label="Name of subproject (singular)"
-          name="subproject_name_singular"
-          value={row.subproject_name_singular ?? ''}
-          onChange={onChange}
-        />
-        <TextField
-          label="Name of subproject (plural)"
-          name="subproject_name_plural"
-          value={row.subproject_name_plural ?? ''}
-          onChange={onChange}
-        />
-        <TextField
-          label="Order subproject by (field name)"
-          name="subproject_order_by"
-          value={row.subproject_order_by ?? ''}
-          onChange={onChange}
-        />
-        <LabelBy
-          label="Goal reports labelled by"
-          name="goal_reports_label_by"
-          table="goal_reports"
-          value={row.goal_reports_label_by ?? ''}
-          onChange={onChange}
-          extraFieldNames={['id']}
-        />
-        <LabelBy
-          label="Places labelled by"
-          name="places_label_by"
-          table="places"
-          value={row.places_label_by ?? ''}
-          onChange={onChange}
-          extraFieldNames={['id', 'level']}
-        />
-        <FieldList
-          label="Places ordered by"
-          name="places_order_by"
-          table="projects"
-          fieldsTable="places"
-          id={project_id}
-          valueArray={row.places_order_by ?? []}
-        />
-        <Divider />
-        <Label>{`Value(s) to use in reports when:`}</Label>
-        <RadioGroupField
-          label="...values exist on multiple place levels"
-          name="values_on_multiple_levels"
-          list={['first', 'second', 'all']}
-          value={row.values_on_multiple_levels ?? ''}
-          onChange={onChange}
-        />
-        <RadioGroupField
-          label="...multiple action values exist on the same place level"
-          name="multiple_action_values_on_same_level"
-          list={['first', 'last', 'all']}
-          value={row.multiple_action_values_on_same_level ?? ''}
-          onChange={onChange}
-        />
-        <RadioGroupField
-          label="...multiple check Values exist on the same place level"
-          name="multiple_check_values_on_same_level"
-          list={['first', 'last', 'all']}
-          value={row.multiple_check_values_on_same_level ?? ''}
-          onChange={onChange}
-        />
-        <Divider />
-        <div className="checkboxfield-list">
-          <Label>Enable uploading files to:</Label>
-          <CheckboxField
-            label="Projects"
-            name="files_active_projects"
-            value={row.files_active_projects ?? false}
-            onChange={onChange}
-          />
-          <CheckboxField
-            label="Project reports"
-            name="files_active_projects_reports"
-            value={row.files_active_projects_reports ?? false}
-            onChange={onChange}
-          />
-          <CheckboxField
-            label="Subprojects"
-            name="files_active_subprojects"
-            value={row.files_active_subprojects ?? false}
-            onChange={onChange}
-          />
-          <CheckboxField
-            label="Subproject reports"
-            name="files_active_subproject_reports"
-            value={row.files_active_subproject_reports ?? false}
-            onChange={onChange}
-          />
-          <CheckboxField
-            label="Places"
-            name="files_active_places"
-            value={row.files_active_places ?? false}
-            onChange={onChange}
-          />
-          <CheckboxField
-            label="Actions"
-            name="files_active_actions"
-            value={row.files_active_actions ?? false}
-            onChange={onChange}
-          />
-          <CheckboxField
-            label="Checks"
-            name="files_active_checks"
-            value={row.files_active_checks ?? false}
-            onChange={onChange}
-          />
-          <CheckboxField
-            label="Check reports"
-            name="files_active_check_reports"
-            value={row.files_active_check_reports ?? false}
-            onChange={onChange}
-          />
-        </div>
+        <Design row={row} />
       </div>
     </div>
   )
