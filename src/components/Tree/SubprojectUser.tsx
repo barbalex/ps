878bcce139470b3a8c2cb644a1821f335e6ef8c4
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2024-02-24 10:42:24 +0100

    memoize and optimize

--- src/components/Tree/SubprojectUser.tsx
+++ src/components/Tree/SubprojectUser.tsx
@@ -1,53 +1,49 @@
-import { useCallback } from 'react'
+import { useCallback, memo } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 
 import { Node } from './Node'
 import { SubprojectUsers as SubprojectUser } from '../../../generated/client'
 
-export const SubprojectUserNode = ({
-  project_id,
-  subproject_id,
-  subprojectUser,
-  level = 6,
-}: {
+type Props = {
   project_id: string
   subproject_id: string
   subprojectUser: SubprojectUser
-  level: number
-}) => {
-  const location = useLocation()
-  const navigate = useNavigate()
-
-  const urlPath = location.pathname.split('/').filter((p) => p !== '')
-  const isOpen =
-    urlPath[0] === 'projects' &&
-    urlPath[1] === project_id &&
-    urlPath[2] === 'subprojects' &&
-    urlPath[3] === subproject_id &&
-    urlPath[4] === 'users' &&
-    urlPath[5] === subprojectUser.subproject_user_id
-  const isActive = isOpen && urlPath.length === level
-
-  const onClickButton = useCallback(() => {
-    if (isOpen)
-      return navigate(
-        `/projects/${project_id}/subprojects/${subproject_id}/users`,
-      )
-    navigate(
-      `/projects/${project_id}/subprojects/${subproject_id}/users/${subprojectUser.subproject_user_id}`,
-    )
-  }, [isOpen, navigate, subprojectUser.subproject_user_id, project_id, subproject_id])
-
-  return (
-    <Node
-      node={subprojectUser}
-      level={level}
-      isOpen={isOpen}
-      isInActiveNodeArray={isOpen}
-      isActive={isActive}
-      childrenCount={10}
-      to={`/projects/${project_id}/subprojects/${subproject_id}/users/${subprojectUser.subproject_user_id}`}
-      onClickButton={onClickButton}
-    />
-  )
+  level?: number
 }
+
+export const SubprojectUserNode = memo(
+  ({ project_id, subproject_id, subprojectUser, level = 6 }: Props) => {
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    const urlPath = location.pathname.split('/').filter((p) => p !== '')
+    const isOpen =
+      urlPath[0] === 'projects' &&
+      urlPath[1] === project_id &&
+      urlPath[2] === 'subprojects' &&
+      urlPath[3] === subproject_id &&
+      urlPath[4] === 'users' &&
+      urlPath[5] === subprojectUser.subproject_user_id
+    const isActive = isOpen && urlPath.length === level
+
+    const baseUrl = `/projects/${project_id}/subprojects/${subproject_id}/users`
+
+    const onClickButton = useCallback(() => {
+      if (isOpen) return navigate(baseUrl)
+      navigate(`${baseUrl}/${subprojectUser.subproject_user_id}`)
+    }, [isOpen, navigate, baseUrl, subprojectUser.subproject_user_id])
+
+    return (
+      <Node
+        node={subprojectUser}
+        level={level}
+        isOpen={isOpen}
+        isInActiveNodeArray={isOpen}
+        isActive={isActive}
+        childrenCount={10}
+        to={`${baseUrl}/${subprojectUser.subproject_user_id}`}
+        onClickButton={onClickButton}
+      />
+    )
+  },
+)
--- src/components/Tree/Taxon.tsx
+++ src/components/Tree/Taxon.tsx
@@ -1,59 +1,50 @@
-import { useCallback } from 'react'
+import { useCallback, memo } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 
 import { Node } from './Node'
 import { Taxa as Taxon } from '../../../generated/client'
 
-export const TaxonNode = ({
-  project_id,
-  taxonomy_id,
-  taxon,
-  level = 6,
-}: {
+type Props = {
   project_id: string
+  taxonomy_id: string
   taxon: Taxon
-  level: number
-}) => {
-  const location = useLocation()
-  const navigate = useNavigate()
-
-  const urlPath = location.pathname.split('/').filter((p) => p !== '')
-  const isOpen =
-    urlPath[0] === 'projects' &&
-    urlPath[1] === project_id &&
-    urlPath[2] === 'taxonomies' &&
-    urlPath[3] === taxonomy_id &&
-    urlPath[4] === 'taxa' &&
-    urlPath[5] === taxon.taxon_id
-  const isActive = isOpen && urlPath.length === level
-
-  const onClickButton = useCallback(() => {
-    if (isOpen)
-      return navigate(
-        `/projects/${project_id}/taxonomies/${taxonomy_id}/taxa`,
-      )
-    navigate(
-      `/projects/${project_id}/taxonomies/${taxonomy_id}/taxa/${taxon.taxon_id}`,
-    )
-  }, [
-    isOpen,
-    navigate,
-    project_id,
-    taxonomy_id,
-    taxon.taxon_id,
-  ])
-
-  // TODO: childrenCount
-  return (
-    <Node
-      node={taxon}
-      level={level}
-      isOpen={isOpen}
-      isInActiveNodeArray={isOpen}
-      isActive={isActive}
-      childrenCount={0}
-      to={`/projects/${project_id}/taxonomies/${taxonomy_id}/taxa/${taxon.taxon_id}`}
-      onClickButton={onClickButton}
-    />
-  )
+  level?: number
 }
+
+export const TaxonNode = memo(
+  ({ project_id, taxonomy_id, taxon, level = 6 }: Props) => {
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    const urlPath = location.pathname.split('/').filter((p) => p !== '')
+    const isOpen =
+      urlPath[0] === 'projects' &&
+      urlPath[1] === project_id &&
+      urlPath[2] === 'taxonomies' &&
+      urlPath[3] === taxonomy_id &&
+      urlPath[4] === 'taxa' &&
+      urlPath[5] === taxon.taxon_id
+    const isActive = isOpen && urlPath.length === level
+
+    const baseUrl = `/projects/${project_id}/taxonomies/${taxonomy_id}/taxa`
+
+    const onClickButton = useCallback(() => {
+      if (isOpen) return navigate(baseUrl)
+      navigate(`${baseUrl}/${taxon.taxon_id}`)
+    }, [isOpen, navigate, baseUrl, taxon.taxon_id])
+
+    // TODO: childrenCount
+    return (
+      <Node
+        node={taxon}
+        level={level}
+        isOpen={isOpen}
+        isInActiveNodeArray={isOpen}
+        isActive={isActive}
+        childrenCount={0}
+        to={`${baseUrl}/${taxon.taxon_id}`}
+        onClickButton={onClickButton}
+      />
+    )
+  },
+)
