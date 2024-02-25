49c573d125ead167ac6475d4df203c9a8a60154f
Alexander Gabriel <alex.barbalex@gmail.com>
2024-02-25 13:52:41 +0100

    work on files

--- src/routes/files/Uploader.tsx
+++ src/routes/files/Uploader.tsx
@@ -1,7 +1,7 @@
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useLiveQuery } from 'electric-sql/react'
 import { useNavigate, useParams } from 'react-router-dom'
-// import { Button } from '@fluentui/react-components'
+import { Button } from '@fluentui/react-components'
 
 import '../../form.css'
 
@@ -17,10 +17,31 @@ export const Uploader = () => {
   const collectionState = uploaderCtx.getOutputCollectionState()
   console.log('Uploader, collectionState ', collectionState)
 
+  useEffect(() => {
+    uploaderCtx.addEventListener('change', (event: CustomEvent) => {
+      console.log('Uploader, change event', event)
+      // TODO:
+      // extract data from event
+      // e.detail.isFailed, e.detail.isSuccess, e.detail.isUploading, e.detail.status
+      // e.detail.allEntries[0]:
+      // - uuid
+      // - file.name
+      // - fileInfo.mimeType or fileInfo.mime.mime?
+    })
+    return () => {
+      ctx.removeEventListener('change', (event) => {})
+    }
+  }, [uploaderCtx])
+
+  const onClick = useCallback(() => {
+    uploaderCtx.initFlow()
+  }, [uploaderCtx])
+
   // TODO: get uploader css locally if it should be possible to upload files
   // offline to sqlite
   return (
     <>
+      <Button onClick={onClick}>Upload</Button>
       <lr-file-uploader-regular
         css-src="https://cdn.jsdelivr.net/npm/@uploadcare/blocks@0.32.4/web/lr-file-uploader-regular.min.css"
         ctx-name="uploadcare-uploader"
