4d155ac80530855ded978365ae1a4ba5a8056625
Alexander Gabriel <alex.barbalex@gmail.com>
2024-02-26 20:46:41 +0100

    log

--- src/routes/file/index.tsx
+++ src/routes/file/index.tsx
@@ -79,6 +79,8 @@ export const Component = () => {
     return <div>Loading...</div>
   }
 
+  console.log('hello file, row:', row)
+
   return (
     <div className="form-outer-container" ref={ref}>
       <Uploader />
