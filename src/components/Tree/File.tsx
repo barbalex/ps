0516b410984c96f6a1851d7baab186a7b4567612
Alexander Gabriel <alex.barbalex@gmail.com>
2024-02-26 20:43:35 +0100

    work on file preview

--- src/components/Tree/File.tsx
+++ src/components/Tree/File.tsx
@@ -14,6 +14,10 @@ type Props = {
 export const FileNode = ({
   project_id,
   subproject_id,
+  place_id,
+  place_id2,
+  check_id,
+  action_id,
   file,
   level = 2,
 }: Props) => {
@@ -22,23 +26,96 @@ export const FileNode = ({
   const navigate = useNavigate()
 
   const urlPath = location.pathname.split('/').filter((p) => p !== '')
-  const isOpen = subproject_id
-    ? urlPath[0] === 'projects' &&
-      urlPath[1] === project_id &&
-      urlPath[2] === 'subprojects' &&
-      urlPath[3] === subproject_id &&
-      urlPath[4] === 'files' &&
-      params.file_id === file.file_id
-    : project_id
-    ? urlPath[0] === 'projects' &&
-      urlPath[1] === project_id &&
-      urlPath[2] === 'files' &&
-      params.file_id === file.file_id
-    : urlPath[0] === 'files' && params.file_id === file.file_id
+  const isOpen =
+    place_id2 && action_id
+      ? urlPath[0] === 'projects' &&
+        urlPath[1] === project_id &&
+        urlPath[2] === 'subprojects' &&
+        urlPath[3] === subproject_id &&
+        urlPath[4] === 'places' &&
+        urlPath[5] === place_id &&
+        urlPath[6] === 'places' &&
+        urlPath[7] === place_id2 &&
+        urlPath[8] === 'actions' &&
+        urlPath[9] === action_id &&
+        urlPath[10] === 'files' &&
+        params.file_id === file.file_id
+      : place_id2 && check_id
+      ? urlPath[0] === 'projects' &&
+        urlPath[1] === project_id &&
+        urlPath[2] === 'subprojects' &&
+        urlPath[3] === subproject_id &&
+        urlPath[4] === 'places' &&
+        urlPath[5] === place_id &&
+        urlPath[6] === 'places' &&
+        urlPath[7] === place_id2 &&
+        urlPath[8] === 'checks' &&
+        urlPath[9] === check_id &&
+        urlPath[10] === 'files' &&
+        params.file_id === file.file_id
+      : !place_id2 && action_id
+      ? urlPath[0] === 'projects' &&
+        urlPath[1] === project_id &&
+        urlPath[2] === 'subprojects' &&
+        urlPath[3] === subproject_id &&
+        urlPath[4] === 'places' &&
+        urlPath[5] === place_id &&
+        urlPath[6] === 'actions' &&
+        urlPath[7] === action_id &&
+        urlPath[8] === 'files' &&
+        params.file_id === file.file_id
+      : !place_id2 && check_id
+      ? urlPath[0] === 'projects' &&
+        urlPath[1] === project_id &&
+        urlPath[2] === 'subprojects' &&
+        urlPath[3] === subproject_id &&
+        urlPath[4] === 'places' &&
+        urlPath[5] === place_id &&
+        urlPath[6] === 'checks' &&
+        urlPath[7] === check_id &&
+        urlPath[8] === 'files' &&
+        params.file_id === file.file_id
+      : place_id2
+      ? urlPath[0] === 'projects' &&
+        urlPath[1] === project_id &&
+        urlPath[2] === 'subprojects' &&
+        urlPath[3] === subproject_id &&
+        urlPath[4] === 'places' &&
+        urlPath[5] === place_id &&
+        urlPath[6] === 'places' &&
+        urlPath[7] === place_id2 &&
+        urlPath[8] === 'files' &&
+        params.file_id === file.file_id
+      : place_id
+      ? urlPath[0] === 'projects' &&
+        urlPath[1] === project_id &&
+        urlPath[2] === 'subprojects' &&
+        urlPath[3] === subproject_id &&
+        urlPath[4] === 'places' &&
+        urlPath[5] === place_id &&
+        urlPath[6] === 'files' &&
+        params.file_id === file.file_id
+      : subproject_id
+      ? urlPath[0] === 'projects' &&
+        urlPath[1] === project_id &&
+        urlPath[2] === 'subprojects' &&
+        urlPath[3] === subproject_id &&
+        urlPath[4] === 'files' &&
+        params.file_id === file.file_id
+      : project_id
+      ? urlPath[0] === 'projects' &&
+        urlPath[1] === project_id &&
+        urlPath[2] === 'files' &&
+        params.file_id === file.file_id
+      : urlPath[0] === 'files' && params.file_id === file.file_id
   const isActive = isOpen && urlPath.length === level
 
   const baseUrl = `${project_id ? `/projects/${project_id}` : ''}${
     subproject_id ? `/subprojects/${subproject_id}` : ''
+  }${place_id ? `/places/${place_id}` : ''}${
+    place_id2 ? `/places/${place_id2}` : ''
+  }${action_id ? `/actions/${action_id}` : ''}${
+    check_id ? `/checks/${check_id}` : ''
   }/files`
 
   const onClickButton = useCallback(() => {
