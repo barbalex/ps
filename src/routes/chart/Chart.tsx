3750128b033a52dc9eeac61d76383631a0e5bbcf
Alexander Gabriel <alex.barbalex@gmail.com>
2024-02-29 12:44:11 +0100

    work on chart

--- src/routes/chart/Chart.tsx
+++ src/routes/chart/Chart.tsx
@@ -1,3 +1,21 @@
-export const Chart = () => {
+import { memo } from 'react'
+import { useParams } from 'react-router-dom'
+import { useLiveQuery } from 'electric-sql/react'
+
+import { useElectric } from '../../ElectricProvider'
+
+export const Chart = memo(() => {
+  const { chart_id } = useParams()
+
+  const { db } = useElectric()!
+  const { results: row } = useLiveQuery(
+    db.charts.liveUnique({
+      where: { chart_id },
+      include: { chart_subjects: true },
+    }),
+  )
+
+  console.log('hello Chart', { row, chart_id })
+
   return <div>Chart</div>
-}
+})
