ec927cc0a16d9faa35267d5acbecaa56b92af4e3
Alexander Gabriel <alex.barbalex@gmail.com>
2024-03-02 12:30:35 +0100

    optimize

--- src/modules/getValueFromChange.ts
+++ src/modules/getValueFromChange.ts
@@ -1,35 +1,33 @@
 export const getValueFromChange = (e, data) => {
   const targetType = e.target.type
-  console.log('hello getValueFromChange 1', {
-    targetType,
-    e,
-    data,
-    valueAsNumber: e.target.valueAsNumber,
-    name: e.target.name,
-    dataValue: data.value,
-  })
+  // console.log('hello getValueFromChange 1', {
+  //   targetType,
+  //   e,
+  //   data,
+  //   valueAsNumber: e.target.valueAsNumber,
+  //   name: e.target.name,
+  //   dataValue: data.value,
+  // })
 
   const name = e.target.name
-  if (targetType === 'checkbox') {
-    return { value: data.checked, name }
+  switch (targetType) {
+    case 'checkbox':
+      return { value: data.checked, name }
+    case 'radio':
+      return { value: data.value, name }
+    case 'change':
+      return { value: data.value, name }
+    case 'number':
+      return {
+        value: isNaN(e.target.valueAsNumber) ? null : data.valueAsNumber,
+        name,
+      }
+    case 'range':
+      return {
+        value: isNaN(e.target.valueAsNumber) ? null : data.valueAsNumber,
+        name,
+      }
+    default:
+      return { value: e.target.value ?? null, name }
   }
-  if (targetType === 'radio') {
-    return { value: data.value, name }
-  }
-  if (targetType === 'change') {
-    return { value: data.value, name }
-  }
-  if (targetType === 'number') {
-    return {
-      value: isNaN(e.target.valueAsNumber) ? null : data.valueAsNumber,
-      name,
-    }
-  }
-  if (targetType === 'range') {
-    return {
-      value: isNaN(e.target.valueAsNumber) ? null : data.valueAsNumber,
-      name,
-    }
-  }
-  return { value: e.target.value ?? null, name }
 }
