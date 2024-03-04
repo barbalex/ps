e8c375c2793d38a5beff7783a4bc3d62f8e33806
Alexander Gabriel <alex.barbalex@gmail.com>
2024-03-04 16:54:24 +0100

    work on charts

--- src/routes/chart/Chart/buildData/index.ts
+++ src/routes/chart/Chart/buildData/index.ts
@@ -36,25 +36,7 @@ export const buildData = async ({
                     deleted: false,
                   },
                 })
-                const sinceYears = places.map((place) => place.since)
-                const thisYear = new Date().getFullYear()
-                const minYear = sinceYears.length
-                  ? Math.min(...sinceYears)
-                  : thisYear - 10
-                const yearRange = Array(thisYear - minYear + 1)
-                  .fill()
-                  .map((element, i) => minYear + i)
-                const data = {}
-                for (const year of yearRange) {
-                  const placesInYear = places.filter(
-                    (place) =>
-                      (place.since <= year || !place.since) &&
-                      (place.until >= year || !place.until),
-                  )
-                  data[year] = placesInYear.length
-                }
-                // console.log('hello dataFromChart, places level 1:', data)
-                dataPerSubject[subject.name] = data
+                processPlaces({ dataPerSubject, places, subject })
                 break
               }
               default:
