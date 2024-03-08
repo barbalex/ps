45662a09a840c4accf472c7d7c0b3267676de140
Alexander Gabriel <alex.barbalex@gmail.com>
2024-03-08 16:51:11 +0100

    refactor

--- db/migrations/01-tables.sql
+++ db/migrations/01-tables.sql
@@ -1656,6 +1656,270 @@ COMMENT ON COLUMN vector_layer_geoms.bbox_ne_lng IS 'bbox of the geometry. Set c
 
 COMMENT ON COLUMN vector_layer_geoms.bbox_ne_lat IS 'bbox of the geometry. Set client-side on every change of geometry. Used to filter geometries client-side for viewport';
 
+CREATE TYPE marker_type_enum AS enum(
+  'circle',
+  'marker'
+);
+
+CREATE TYPE line_cap_enum AS enum(
+  'butt',
+  'round',
+  'square'
+);
+
+CREATE TYPE vector_layer_table_enum AS enum(
+  'places1',
+  'places2',
+  'actions1',
+  'actions2',
+  'checks1',
+  'checks2',
+  'observations1',
+  'observations2'
+);
+
+-- CREATE TYPE line_join_enum AS enum(
+--   'arcs',
+--   'bevel',
+--   'miter',
+--   'miter-clip', // NOT supported by electric-sql
+--   'round'
+-- );
+CREATE TYPE fill_rule_enum AS enum(
+  'nonzero',
+  'evenodd'
+);
+
+DROP TABLE IF EXISTS vector_layer_displays CASCADE;
+
+-- manage all map related properties here? For imported/wfs and also own tables?
+CREATE TABLE vector_layer_displays(
+  vector_layer_display_id uuid PRIMARY KEY DEFAULT NULL,
+  account_id uuid DEFAULT NULL REFERENCES accounts(account_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  vector_layer_id uuid DEFAULT NULL REFERENCES vector_layers(vector_layer_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  display_property_value text DEFAULT NULL,
+  marker_type marker_type_enum DEFAULT NULL, -- 'circle',
+  circle_marker_radius integer DEFAULT NULL, -- 8,
+  marker_symbol text DEFAULT NULL,
+  marker_size integer DEFAULT NULL, -- 16,
+  stroke boolean DEFAULT NULL, -- true,
+  color text DEFAULT NULL, -- '#3388ff',
+  weight integer DEFAULT NULL, -- 3,
+  opacity_percent integer DEFAULT NULL, -- 100,
+  line_cap line_cap_enum DEFAULT NULL, -- 'round',
+  line_join text DEFAULT NULL, -- 'round',
+  dash_array text DEFAULT NULL,
+  dash_offset text DEFAULT NULL,
+  fill boolean DEFAULT NULL, -- true,
+  fill_color text DEFAULT NULL,
+  fill_opacity_percent integer DEFAULT NULL, -- 100,
+  fill_rule fill_rule_enum DEFAULT NULL, -- 'evenodd',
+  label_replace_by_generated_column text DEFAULT NULL,
+  deleted boolean DEFAULT NULL -- false
+);
+
+CREATE INDEX ON vector_layer_displays(account_id);
+
+CREATE INDEX ON vector_layer_displays USING btree(vector_layer_id);
+
+CREATE INDEX ON vector_layer_displays USING btree(display_property_value);
+
+COMMENT ON TABLE vector_layer_displays IS 'Goal: manage all map related properties of vector layers including places, actions, checks and observations';
+
+COMMENT ON COLUMN vector_layer_displays.display_property_value IS 'Enables styling per property value';
+
+COMMENT ON COLUMN vector_layer_displays.marker_symbol IS 'Name of the symbol used for the marker';
+
+COMMENT ON COLUMN vector_layer_displays.marker_size IS 'Size in pixels of the symbol used for the marker. Defaults to 16';
+
+COMMENT ON COLUMN vector_layer_displays.stroke IS 'Whether to draw stroke along the path. Set it to false to disable borders on polygons or circles. https://leafletjs.com/reference.html#path-stroke';
+
+COMMENT ON COLUMN vector_layer_displays.color IS 'Stroke color. https://leafletjs.com/reference.html#path-color';
+
+COMMENT ON COLUMN vector_layer_displays.weight IS 'Stroke width in pixels. https://leafletjs.com/reference.html#path-weight';
+
+COMMENT ON COLUMN vector_layer_displays.opacity_percent IS 'Stroke opacity. https://leafletjs.com/reference.html#path-opacity';
+
+COMMENT ON COLUMN vector_layer_displays.line_cap IS 'A string that defines shape to be used at the end of the stroke. https://leafletjs.com/reference.html#path-linecap. https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/stroke-linecap';
+
+COMMENT ON COLUMN vector_layer_displays.line_join IS 'A string that defines shape to be used at the corners of the stroke. https://leafletjs.com/reference.html#path-linejoin, https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/stroke-linejoin#usage_context';
+
+COMMENT ON COLUMN vector_layer_displays.dash_array IS 'A string that defines the stroke dash pattern. Doesn''t work on Canvas-powered layers in some old browsers. https://leafletjs.com/reference.html#path-dasharray. https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/stroke-dasharray';
+
+COMMENT ON COLUMN vector_layer_displays.dash_offset IS 'A string that defines the distance into the dash pattern to start the dash. Doesn''t work on Canvas-powered layers in some old browsers. https://leafletjs.com/reference.html#path-dashoffset. https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/stroke-dashoffset';
+
+COMMENT ON COLUMN vector_layer_displays.fill IS 'Whether to fill the path with color. Set it to false to disable filling on polygons or circles. https://leafletjs.com/reference.html#path-fill';
+
+COMMENT ON COLUMN vector_layer_displays.fill_color IS 'Fill color. Defaults to the value of the color option. https://leafletjs.com/reference.html#path-fillcolor';
+
+COMMENT ON COLUMN vector_layer_displays.fill_opacity_percent IS 'Fill opacity. https://leafletjs.com/reference.html#path-fillopacity';
+
+COMMENT ON COLUMN vector_layer_displays.fill_rule IS 'A string that defines how the inside of a shape is determined. https://leafletjs.com/reference.html#path-fillrule. https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/fill-rule';
+
+COMMENT ON COLUMN vector_layer_displays.deleted IS 'marks if the row is deleted';
+
+CREATE TYPE notification_intent_enum AS enum(
+  'success',
+  'error',
+  'warning',
+  'info'
+);
+
+DROP TABLE IF EXISTS notifications;
+
+CREATE TABLE notifications(
+  notification_id uuid PRIMARY KEY DEFAULT NULL,
+  user_id uuid REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE DEFAULT NULL,
+  title text DEFAULT NULL,
+  body text DEFAULT NULL,
+  intent notification_intent_enum DEFAULT NULL,
+  timeout integer DEFAULT NULL,
+  paused boolean DEFAULT NULL,
+  progress_percent integer DEFAULT NULL
+);
+
+COMMENT ON TABLE notifications IS 'Goal: Show notifications to the user. Notifications are either shown according to the timeout or, if paused, until they are dismissed i.e. paused = false.';
+
+COMMENT ON COLUMN notifications.timeout IS 'Timeout in milliseconds.';
+
+COMMENT ON COLUMN notifications.paused IS 'If true, the notification is not dismissed according to timeout. Instead, it is dismissed when pause is updated to false. A spinner is shown.';
+
+COMMENT ON COLUMN notifications.progress_percent IS 'Progress of a long running task. Only passed, if progress can be measured. A progress bar is shown.';
+
+CREATE TYPE chart_type AS enum(
+  'Pie',
+  'Radar',
+  'Area'
+);
+
+CREATE TABLE charts(
+  chart_id uuid PRIMARY KEY DEFAULT NULL, -- public.uuid_generate_v7(),
+  account_id uuid DEFAULT NULL REFERENCES accounts(account_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  project_id uuid DEFAULT NULL REFERENCES projects(project_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  subproject_id uuid DEFAULT NULL REFERENCES subprojects(subproject_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  place_id uuid DEFAULT NULL REFERENCES places(place_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  years_current boolean DEFAULT NULL, -- DATE_PART('year', now()::date),
+  years_previous boolean DEFAULT NULL, -- DATE_PART('year', now()::date) - 1,
+  years_specific integer DEFAULT NULL,
+  years_last_x integer DEFAULT NULL,
+  years_since integer DEFAULT NULL, -- DATE_PART('year', now()::date) - DATE_PART('year', start_date),
+  years_until integer DEFAULT NULL, -- DATE_PART('year', end_date) - DATE_PART('year', start_date),
+  chart_type chart_type DEFAULT NULL, -- 'SimpleLineChart'
+  title text DEFAULT NULL,
+  subjects_stacked boolean DEFAULT NULL, -- FALSE
+  subjects_single boolean DEFAULT NULL, -- FALSE
+  percent boolean DEFAULT NULL, -- FALSE
+  label_replace_by_generated_column text DEFAULT NULL, -- title
+  deleted boolean DEFAULT NULL -- false
+);
+
+CREATE INDEX ON charts USING btree(chart_id);
+
+CREATE INDEX ON charts USING btree(account_id);
+
+CREATE INDEX ON charts USING btree(project_id);
+
+CREATE INDEX ON charts USING btree(subproject_id);
+
+CREATE INDEX ON charts USING btree(place_id);
+
+-- CREATE INDEX ON charts((1))
+-- WHERE
+--   deleted;
+COMMENT ON TABLE charts IS 'Charts for projects, subprojects or places.';
+
+COMMENT ON COLUMN charts.account_id IS 'redundant account_id enhances data safety';
+
+COMMENT ON COLUMN charts.years_current IS 'If has value: the chart shows data of the current year';
+
+COMMENT ON COLUMN charts.years_previous IS 'If has value: the chart shows data of the previous year';
+
+COMMENT ON COLUMN charts.years_specific IS 'If has value: the chart shows data of the specific year';
+
+COMMENT ON COLUMN charts.years_last_x IS 'If has value: the chart shows data of the last {value} years';
+
+COMMENT ON COLUMN charts.years_since IS 'If has value: the chart shows data since the value specified. Can be the start date of the project, subproject or place';
+
+COMMENT ON COLUMN charts.years_until IS 'If has value: the chart shows data until the value specified. Can be the end date of the project, subproject or place';
+
+CREATE TYPE chart_subject_table AS enum(
+  'subprojects',
+  'places',
+  'checks',
+  'check_values',
+  'actions',
+  'action_values'
+);
+
+CREATE TYPE chart_subject_value_source AS enum(
+  'count_rows',
+  'count_rows_by_distinct_field_values',
+  'sum_values_of_field'
+);
+
+CREATE TYPE chart_subject_type AS enum(
+  'linear',
+  'monotone'
+);
+
+CREATE TABLE chart_subjects(
+  chart_subject_id uuid PRIMARY KEY DEFAULT NULL, -- public.uuid_generate_v7(),
+  account_id uuid DEFAULT NULL REFERENCES accounts(account_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  chart_id uuid DEFAULT NULL REFERENCES charts(chart_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  table_name chart_subject_table DEFAULT NULL, -- subprojects, places, checks, check_values, actions, action_values
+  table_level integer DEFAULT NULL, -- 1, 2 (not relevant for subprojects)
+  table_filter jsonb DEFAULT NULL, -- save a filter that is applied to the table
+  value_source chart_subject_value_source DEFAULT NULL, --how to source the value
+  value_field text DEFAULT NULL, -- field to be used for value_source
+  value_unit uuid DEFAULT NULL REFERENCES units(unit_id) ON DELETE CASCADE ON UPDATE CASCADE, -- needed for action_values, check_values
+  name text DEFAULT NULL,
+  label_replace_by_generated_column text DEFAULT NULL, -- table, value_source, ?value_field, ?unit
+  type chart_subject_type DEFAULT NULL, -- linear, monotone
+  stroke text DEFAULT NULL,
+  fill text DEFAULT NULL,
+  fill_graded boolean DEFAULT NULL, -- TRUE
+  connect_nulls boolean DEFAULT NULL, -- TRUE
+  sort integer DEFAULT NULL, -- 0
+  deleted boolean DEFAULT NULL -- FALSE
+);
+
+CREATE INDEX ON chart_subjects USING btree(chart_subject_id);
+
+CREATE INDEX ON chart_subjects USING btree(account_id);
+
+CREATE INDEX ON chart_subjects USING btree(chart_id);
+
+CREATE INDEX ON chart_subjects USING btree(table_name);
+
+CREATE INDEX ON chart_subjects USING btree(table_level);
+
+CREATE INDEX ON chart_subjects USING btree(value_field);
+
+CREATE INDEX ON chart_subjects USING btree(value_unit);
+
+-- CREATE INDEX ON chart_subjects((1))
+-- WHERE
+--   deleted;
+COMMENT ON TABLE chart_subjects IS 'Subjects for charts. Or: what is shown in the chart';
+
+COMMENT ON COLUMN chart_subjects.account_id IS 'redundant account_id enhances data safety';
+
+COMMENT ON COLUMN chart_subjects.table_name IS 'The table to be used as data source';
+
+COMMENT ON COLUMN chart_subjects.table_level IS 'The level of the table to be used as data source. 1 or 2 (not relevant for subprojects)';
+
+COMMENT ON COLUMN chart_subjects.table_filter IS 'A filter that is applied to the table';
+
+COMMENT ON COLUMN chart_subjects.value_source IS 'How to source the value';
+
+COMMENT ON COLUMN chart_subjects.value_field IS 'Field to be used for value_source';
+
+COMMENT ON COLUMN chart_subjects.value_unit IS 'Needed for action_values, check_values';
+
+COMMENT ON COLUMN chart_subjects.stroke IS 'Stroke color of the chart';
+
+COMMENT ON COLUMN chart_subjects.fill IS 'Fill color of the chart';
+
 -- enable electric
 ALTER TABLE users ENABLE electric;
 
@@ -1750,4 +2014,12 @@ ALTER TABLE vector_layers ENABLE electric;
 ALTER TABLE layer_options ENABLE electric;
 
 ALTER TABLE vector_layer_geoms ENABLE electric;
+
+ALTER TABLE vector_layer_displays ENABLE electric;
+
+ALTER TABLE notifications ENABLE electric;
+
+ALTER TABLE charts ENABLE electric;
+
+ALTER TABLE chart_subjects ENABLE ELECTRIC;
 
