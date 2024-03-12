8613f017967186c8d36552911fcd82d87cca633b
Alexander Gabriel <alex.barbalex@gmail.com>
2024-03-12 22:14:46 +0100

    optimize

--- src/components/SqlInitializer/labelGenerators/files.ts
+++ src/components/SqlInitializer/labelGenerators/files.ts
@@ -5,7 +5,9 @@ export const generateFileLabel = async (db) => {
   const hasLabel = columns.some((column) => column.name === 'label')
   if (!hasLabel) {
     await db.unsafeExec({
-      sql: 'ALTER TABLE files ADD COLUMN label text GENERATED ALWAYS AS (coalesce(name, file_id))',
+      sql: `
+        ALTER TABLE files ADD COLUMN label text GENERATED ALWAYS AS (coalesce(name, file_id));
+        ALTER TABLE files drop COLUMN label_replace_by_generated_column;`,
     })
     await db.unsafeExec({
       sql: 'CREATE INDEX IF NOT EXISTS files_label_idx ON files(label)',
--- src/components/SqlInitializer/labelGenerators/goals.ts
+++ src/components/SqlInitializer/labelGenerators/goals.ts
@@ -5,7 +5,9 @@ export const generateGoalLabel = async (db) => {
   const hasLabel = columns.some((column) => column.name === 'label')
   if (!hasLabel) {
     await db.unsafeExec({
-      sql: `ALTER TABLE goals ADD COLUMN label text GENERATED ALWAYS AS (iif(coalesce(year, name) is not null, year || ': ' || name, goal_id))`,
+      sql: `
+        ALTER TABLE goals ADD COLUMN label text GENERATED ALWAYS AS (iif(coalesce(year, name) is not null, year || ': ' || name, goal_id));
+        ALTER TABLE goals drop COLUMN label_replace_by_generated_column;`,
     })
     await db.unsafeExec({
       sql: 'CREATE INDEX IF NOT EXISTS goals_label_idx ON goals(label)',
