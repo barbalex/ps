fff73c564debd9b9b728c8f2b1c655c7cdbcffcd
Alexander Gabriel <alex.barbalex@gmail.com>
2024-03-12 22:20:19 +0100

    optimize

---
+++ src/components/SqlInitializer/labelGenerators/occurrenceImports.ts
@@ -0,0 +1,16 @@
+export const generateOccurrenceImportLabel = async (db) => {
+  const columns = await db.rawQuery({
+    sql: 'PRAGMA table_xinfo(occurrence_imports)',
+  })
+  const hasLabel = columns.some((column) => column.name === 'label')
+  if (!hasLabel) {
+    await db.unsafeExec({
+      sql: `
+        ALTER TABLE occurrence_imports ADD COLUMN label text GENERATED ALWAYS AS (occurrence_import_id);
+        ALTER TABLE occurrence_imports drop COLUMN label_replace_by_generated_column;`,
+    })
+    await db.unsafeExec({
+      sql: 'CREATE INDEX IF NOT EXISTS occurrence_imports_label_idx ON occurrence_imports(label)',
+    })
+  }
+}
--- src/components/SqlInitializer/labelGenerators/widgetTypes.ts
+++ src/components/SqlInitializer/labelGenerators/widgetTypes.ts
@@ -5,7 +5,9 @@ export const generateWidgetTypeLabel = async (db) => {
   const hasLabel = columns.some((column) => column.name === 'label')
   if (!hasLabel) {
     await db.unsafeExec({
-      sql: 'ALTER TABLE widget_types ADD COLUMN label text GENERATED ALWAYS AS (coalesce(name, widget_type_id))',
+      sql: `
+        ALTER TABLE widget_types ADD COLUMN label text GENERATED ALWAYS AS (coalesce(name, widget_type_id));
+        ALTER TABLE widget_types drop COLUMN label_replace_by_generated_column;`,
     })
     await db.unsafeExec({
       sql: 'CREATE INDEX IF NOT EXISTS widget_types_label_idx ON widget_types(label)',
