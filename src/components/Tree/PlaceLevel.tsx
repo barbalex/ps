f9826018ce906b4e6d999b434fb2424572f40ea7
Alexander Gabriel <alex.barbalex@gmail.com>
2024-03-18 23:42:58 +0100

    preserve search params on navigation

--- src/components/Tree/PlaceLevel.tsx
+++ src/components/Tree/PlaceLevel.tsx
@@ -1,5 +1,5 @@
 import { useCallback, memo } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom'
 
 import { Node } from './Node'
 import { PlaceLevels as PlaceLevel } from '../../../generated/client'
@@ -14,6 +14,7 @@ export const PlaceLevelNode = memo(
   ({ project_id, placeLevel, level = 4 }: Props) => {
     const location = useLocation()
     const navigate = useNavigate()
+    const [searchParams] = useSearchParams()
 
     const urlPath = location.pathname.split('/').filter((p) => p !== '')
     const isOpen =
@@ -26,9 +27,14 @@ export const PlaceLevelNode = memo(
     const baseUrl = `/projects/${project_id}/place-levels`
 
     const onClickButton = useCallback(() => {
-      if (isOpen) return navigate(baseUrl)
-      navigate(`${baseUrl}/${placeLevel.place_level_id}`)
-    }, [isOpen, navigate, baseUrl, placeLevel.place_level_id])
+      if (isOpen) {
+        return navigate({ pathname: baseUrl, search: searchParams.toString() })
+      }
+      navigate({
+        pathname: `${baseUrl}/${placeLevel.place_level_id}`,
+        search: searchParams.toString(),
+      })
+    }, [isOpen, navigate, baseUrl, placeLevel.place_level_id, searchParams])
 
     return (
       <Node
