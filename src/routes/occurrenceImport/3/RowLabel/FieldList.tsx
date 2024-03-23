b77e2aa46c80a7695e14f22022d64c4c0f4232cf
Alexander Gabriel <alex.barbalex@gmail.com>
2024-03-23 14:47:23 +0100

    work on occurrence import label

--- src/routes/occurrenceImport/3/RowLabel/FieldList.tsx
+++ src/routes/occurrenceImport/3/RowLabel/FieldList.tsx
@@ -1,5 +1,4 @@
 import { Draggable, Droppable } from 'react-beautiful-dnd'
-import styled from '@emotion/styled'
 import { BsArrowsMove } from 'react-icons/bs'
 
 const containerStyle = {
@@ -20,40 +19,38 @@ const titleStyle = {
 const fieldListStyle = {
   padding: 4,
 }
-const FieldContainer = styled.div`
-  position: relative;
-  padding: 4px 7px;
-  border: 1px solid lightgrey;
-  margin-bottom: 8px;
-  border-radius: 4px;
-  font-size: x-small;
-  line-height: 16.6px;
-  user-select: none;
-  background-color: ${(props) =>
-    props.isDragging ? 'rgb(74, 20, 140)' : 'white'};
-  transition: background-color 0.2s ease;
-  color: ${(props) => (props.isDragging ? 'white' : 'black')};
-  transition: color 0.2s ease;
-`
-const FieldHandle = styled(BsArrowsMove)`
-  color: #989898;
-  position: absolute;
-  top: -6.8px;
-  left: -2px;
-  transform: rotate(90deg);
-  height: 1.2em;
-  width: 1.2em;
-`
-const DividerContainer = styled(FieldContainer)`
-  margin-top: 4px;
-  position: relative;
-`
-const Explainer = styled.p`
-  font-size: x-small;
-  margin: 0;
-  padding: 0 4px;
-  color: grey;
-`
+const fieldContainerStyle = {
+  position: 'relative',
+  padding: '4px 7px',
+  border: '1px solid lightgrey',
+  marginBottom: 8,
+  borderRadius: 4,
+  fontSize: 'x-small',
+  lineHeight: '16.6px',
+  userSelect: 'none',
+  transition: 'background-color 0.2s ease',
+  transition: 'color 0.2s ease',
+}
+const dividerContainerStyle = {
+  ...fieldContainerStyle,
+  marginTop: 4,
+  position: 'relative',
+}
+const fieldHandleStyle = {
+  color: '#989898',
+  position: 'absolute',
+  top: -6.8,
+  left: -2,
+  transform: 'rotate(90deg)',
+  height: '1.2em',
+  width: '1.2em',
+}
+const explainerStyle = {
+  fontSize: 'x-small',
+  margin: 0,
+  padding: '0 4px',
+  color: 'grey',
+}
 
 interface Props {
   fields: string[]
@@ -76,23 +73,29 @@ export const FieldList = ({ fields }: Props) => (
               index={index}
             >
               {(provided, snapshot) => (
-                <FieldContainer
+                <div
                   {...provided.draggableProps}
                   {...provided.dragHandleProps}
                   ref={provided.innerRef}
-                  isDragging={snapshot.isDragging}
+                  style={{
+                    ...fieldContainerStyle,
+                    backgroundColor: snapshot.isDragging
+                      ? 'rgb(74, 20, 140)'
+                      : 'white',
+                    color: snapshot.isDragging ? 'white' : 'black',
+                  }}
                 >
                   {field}
-                  <FieldHandle />
-                </FieldContainer>
+                  <BsArrowsMove style={fieldHandleStyle} />
+                </div>
               )}
             </Draggable>
           ))}
           {(fields ?? []).length === 0 && (
-            <Explainer>
+            <p style={explainerStyle}>
               Sie müssen Felder erzeugen, um die Datensatz-Beschriftung zu
               bestimmen.
-            </Explainer>
+            </p>
           )}
           <h5 style={titleStyle}>Zeichen</h5>
           <Draggable
@@ -101,14 +104,21 @@ export const FieldList = ({ fields }: Props) => (
             index={fields.length}
           >
             {(provided) => (
-              <DividerContainer
+              <div
                 {...provided.draggableProps}
                 {...provided.dragHandleProps}
                 ref={provided.innerRef}
+                style={{
+                  ...dividerContainerStyle,
+                  backgroundColor: snapshot.isDragging
+                    ? 'rgb(74, 20, 140)'
+                    : 'white',
+                  color: snapshot.isDragging ? 'white' : 'black',
+                }}
               >
                 Any text
-                <FieldHandle />
-              </DividerContainer>
+                <BsArrowsMove style={fieldHandleStyle} />
+              </div>
             )}
           </Draggable>
           {provided.placeholder}
