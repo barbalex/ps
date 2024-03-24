3c0d2913eefced99f0145df7440f7a948c411a05
Alexander Gabriel <alex.barbalex@gmail.com>
2024-03-24 14:58:32 +0100

    work on occurrence import geometry

--- src/routes/occurrenceImport/2/Set.tsx
+++ src/routes/occurrenceImport/2/Set.tsx
@@ -1,16 +1,31 @@
 import { memo, useCallback, useState } from 'react'
-import { Button } from '@fluentui/react-components'
+import { Button, Spinner } from '@fluentui/react-components'
 import { useLiveQuery } from 'electric-sql/react'
+import { MdDone } from 'react-icons/md'
 
 import { useElectric } from '../../../ElectricProvider'
 import { setGeometries } from './setGeometries'
 
+const spinnerStyle = {
+  paddingRight: 8,
+}
 const notificationStyle = {
   color: 'red',
+}
+const allSetStyle = {
+  color: 'rgba(38, 82, 37, 0.9)',
+  fontWeight: 'bold',
+}
+const doneIconStyle = {
+  paddingRight: 8,
+  fontSize: '1.4em',
+  fontWeight: 'bold',
+  verticalAlign: 'text-bottom',
 }
 
 export const Set = memo(({ occurrenceImport }) => {
   const [notification, setNotification] = useState()
+  const [settingGeometries, setSettingGeometries] = useState(false)
   const { db } = useElectric()!
   const { results: occurrences } = useLiveQuery(
     db.occurrences.liveMany({
@@ -25,6 +40,7 @@ export const Set = memo(({ occurrenceImport }) => {
   const toSetCount = occurrencesWithoutGeometry?.length ?? 0
 
   const onClick = useCallback(async () => {
+    setSettingGeometries(true)
     setGeometries({ occurrenceImport, db, setNotification })
   }, [db, occurrenceImport])
 
@@ -32,14 +48,22 @@ export const Set = memo(({ occurrenceImport }) => {
 
   if (toSetCount === 0) {
     return (
-      <div>{`All ${new Intl.NumberFormat().format(
-        occurrences.length,
-      )} occurrences's geometries are set`}</div>
+      <div style={allSetStyle}>
+        <MdDone style={doneIconStyle} />
+        {`All ${new Intl.NumberFormat().format(
+          occurrences.length,
+        )} occurrences's geometries are set`}
+      </div>
     )
   }
 
   return (
-    <Button onClick={onClick}>
+    <Button
+      onClick={onClick}
+      icon={
+        settingGeometries ? <Spinner size="small" style={spinnerStyle} /> : null
+      }
+    >
       <>
         <div>{`Set coordinates of ${toSetCount} occurrences`}</div>
         {notification && <div style={notificationStyle}>{notification}</div>}
