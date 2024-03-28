9ffc7d161d69f88568f05f018f7bad6f9e0e7ff7
Alexander Gabriel <alex.barbalex@gmail.com>
2024-03-28 15:39:00 +0100

    optimize

--- src/routes/occurrenceImport/3/LabelCreator/FieldList.tsx
+++ src/routes/occurrenceImport/3/LabelCreator/FieldList.tsx
@@ -64,82 +64,79 @@ interface Props {
 
 // only show fields not yet added to label
 // TODO: build labelElements from fields
-export const FieldList = memo(({ fieldLabels }: Props) => {
-  return (
-    <div style={containerStyle}>
-      <Droppable droppableId="fieldList">
-        {(provided) => (
-          <>
-            <div
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-              style={fieldListStyle}
-            >
-              <h5 style={titleStyle}>
-                Fields{' '}
-                <span style={titleSpanStyle}>({fieldLabels.length})</span>
-              </h5>
-              <div style={fieldsListStyle}>
-                {(fieldLabels ?? []).map((fieldLabel, index) => (
-                  <Draggable
-                    key={fieldLabel.id}
-                    draggableId={fieldLabel.id}
-                    index={index}
-                  >
-                    {(provided, snapshot) => (
-                      <div
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        ref={provided.innerRef}
-                        style={{
-                          ...fieldContainerStyle,
-                          backgroundColor: snapshot.isDragging
-                            ? 'rgba(38, 82, 37, 0.9)'
-                            : 'rgba(103, 216, 101, 0.07)',
-                          color: snapshot.isDragging ? 'white' : 'black',
-                          // BEWARE: without this, onDragEnd doesn't fire and the dragged item does not move
-                          ...provided.draggableProps.style,
-                        }}
-                      >
-                        {fieldLabel.value}
-                        <BsArrowsMove style={fieldHandleStyle} />
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-              </div>
-              <h5 style={titleStyle}>Separating text/characters</h5>
-              {/* TODO: this draggable needs a uuidv7 draggableId that changes after every addition of a separator */}
-              <Draggable
-                key="separator"
-                draggableId="separator"
-                index={fieldLabels.length}
-              >
-                {(provided, snapshot) => (
-                  <div
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    ref={provided.innerRef}
-                    style={{
-                      ...dividerContainerStyle,
-                      backgroundColor: snapshot.isDragging
-                        ? 'rgba(38, 82, 37, 0.9)'
-                        : 'white',
-                      color: snapshot.isDragging ? 'white' : 'black',
-                      // BEWARE: without this, onDragEnd doesn't fire and the dragged item does not move
-                      ...provided.draggableProps.style,
-                    }}
-                  >
-                    Any text
-                    <BsArrowsMove style={fieldHandleStyle} />
-                  </div>
-                )}
-              </Draggable>
+export const FieldList = memo(({ fieldLabels }: Props) => (
+  <div style={containerStyle}>
+    <Droppable droppableId="fieldList">
+      {(provided) => (
+        <>
+          <div
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            style={fieldListStyle}
+          >
+            <h5 style={titleStyle}>
+              Fields <span style={titleSpanStyle}>({fieldLabels.length})</span>
+            </h5>
+            <div style={fieldsListStyle}>
+              {(fieldLabels ?? []).map((fieldLabel, index) => (
+                <Draggable
+                  key={fieldLabel.id}
+                  draggableId={fieldLabel.id}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <div
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                      style={{
+                        ...fieldContainerStyle,
+                        backgroundColor: snapshot.isDragging
+                          ? 'rgba(38, 82, 37, 0.9)'
+                          : 'rgba(103, 216, 101, 0.07)',
+                        color: snapshot.isDragging ? 'white' : 'black',
+                        // BEWARE: without this, onDragEnd doesn't fire and the dragged item does not move
+                        ...provided.draggableProps.style,
+                      }}
+                    >
+                      {fieldLabel.value}
+                      <BsArrowsMove style={fieldHandleStyle} />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
             </div>
-            {provided.placeholder}
-          </>
-        )}
-      </Droppable>
-    </div>
-  )
-})
+            <h5 style={titleStyle}>Separating text/characters</h5>
+            {/* TODO: this draggable needs a uuidv7 draggableId that changes after every addition of a separator */}
+            <Draggable
+              key="separator"
+              draggableId="separator"
+              index={fieldLabels.length}
+            >
+              {(provided, snapshot) => (
+                <div
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  ref={provided.innerRef}
+                  style={{
+                    ...dividerContainerStyle,
+                    backgroundColor: snapshot.isDragging
+                      ? 'rgba(38, 82, 37, 0.9)'
+                      : 'white',
+                    color: snapshot.isDragging ? 'white' : 'black',
+                    // BEWARE: without this, onDragEnd doesn't fire and the dragged item does not move
+                    ...provided.draggableProps.style,
+                  }}
+                >
+                  Any text
+                  <BsArrowsMove style={fieldHandleStyle} />
+                </div>
+              )}
+            </Draggable>
+          </div>
+          {provided.placeholder}
+        </>
+      )}
+    </Droppable>
+  </div>
+))
