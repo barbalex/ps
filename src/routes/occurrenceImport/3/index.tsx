05a75a0a22817a42d47221f0128cb983fa1a514d
Alexander Gabriel <alex.barbalex@gmail.com>
2024-03-28 22:27:19 +0100

    optimize

--- src/routes/occurrenceImport/3/index.tsx
+++ src/routes/occurrenceImport/3/index.tsx
@@ -2,15 +2,12 @@ import { memo } from 'react'
 import { LabelCreator } from './LabelCreator'
 
 export const Three = memo(
-  ({ occurrenceImport, occurrenceFields, onChange }) => {
-    console.log('Three, occurrenceImport', occurrenceImport)
-    return (
-      <LabelCreator
-        label={occurrenceImport?.label_creation ?? []}
-        fields={occurrenceFields}
-        name="label_creation"
-        onChange={onChange}
-      />
-    )
-  },
+  ({ occurrenceImport, occurrenceFields, onChange }) => (
+    <LabelCreator
+      label={occurrenceImport?.label_creation ?? []}
+      fields={occurrenceFields}
+      name="label_creation"
+      onChange={onChange}
+    />
+  ),
 )
