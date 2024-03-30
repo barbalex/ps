4142e269c8d0e00dc6d6a260cadf8278681c66d8
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2024-03-30 18:50:01 +0100

    build loading indicator

--- src/components/shared/Loading.tsx
+++ src/components/shared/Loading.tsx
@@ -1,3 +1,15 @@
-export const Loading = ({ message }) => {
-  return <div>Loading...</div>
+import { Spinner } from '@fluentui/react-components'
+
+const containerStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%',
+  width: '100%',
 }
+
+export const Loading = ({ label }) => (
+  <div style={containerStyle}>
+    <Spinner labelPosition="below" label={label} />
+  </div>
+)
