19368c564725d28ef14fa85a67690c0d54a02f21
Alexander Gabriel <alex.barbalex@gmail.com>
2024-04-02 10:59:49 +0200

    work on labels

---
+++ src/components/SqlInitializer/labelGenerators/occurrences.sql
@@ -0,0 +1,25 @@
+UPDATE
+  occurrences
+SET
+  label = iif(oi.label_creation IS NOT NULL,
+    -- TODO: loop all labelElements using json_each()
+    -- https://www.sqlite.org/json1.html#jeach, TODO: see: https://stackoverflow.com/a/55937176/712005
+    -- then iif type is separator, add value, else add value of json_extract(data, '$.value'):
+    concat('TODO:', 'loop all labelElements and replace with values from occurrences'), NEW.occurrence_id)
+FROM (
+  SELECT
+    label_creation
+  FROM
+    occurrence_imports
+  WHERE
+    occurrence_import_id = NEW.occurrence_import_id) AS oi,
+(
+    SELECT
+      json_each(occurrence_imports.label_creation)
+    FROM
+      occurrence_imports
+    WHERE
+      occurrence_import_id = NEW.occurrence_import_id) AS labelElements
+WHERE
+  occurrences.occurrence_id = NEW.occurrence_id;
+
