b340bc95b29d408f49568cbbc83b80708a893315
Alexander Gabriel <alex.barbalex@gmail.com>
2024-04-02 15:16:32 +0200

    work on labels

--- src/components/SqlInitializer/labelGenerators/occurrences.sql
+++ src/components/SqlInitializer/labelGenerators/occurrences.sql
@@ -26,12 +26,30 @@ WHERE
 -- 1. extract type and value from label elements
 SELECT
   occurrence_imports.occurrence_import_id,
-  json_extract(labelElements.value, '$.type') as type,
-  json_extract(labelElements.value, '$.value') as value
+  json_extract(labelElements.value, '$.type') AS type,
+  json_extract(labelElements.value, '$.value') AS value
 FROM
   occurrence_imports
   JOIN json_each(occurrence_imports.label_creation) AS labelElements
 WHERE
   occurrence_imports.occurrence_import_id = 1;
 
+-- 2. get occurrence with label elements
+SELECT
+  occurrences.occurrence_id,
+  json_extract(labelElements.value, '$.type') AS type,
+  json_extract(labelElements.value, '$.value') AS value
+FROM
+  occurrences
+  INNER JOIN occurrence_imports ON occurrences.occurrence_import_id = occurrence_imports.occurrence_import_id
+  JOIN json_each(occurrence_imports.label_creation) AS labelElements
+WHERE
+  occurrences.occurrence_id = 1;
 
+-- 3. group by occurrence_id and get all label elements
+-- https://www.sqlite.org/json1.html#jgrouparray
+SELECT
+  occurrences.occurrence_id,
+  json_group_array(json_extract(labelElements.value, '$.type')) AS types,
+  json_group_array(json_extract(labelElements.value, '$.value')) AS
+VALUES
