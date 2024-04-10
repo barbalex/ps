ff05ad29d81d6526612206c2614fefa510c083d0
Alexander Gabriel <alex.barbalex@gmail.com>
2024-04-10 11:58:20 +0200

    optimize

---
+++ src/components/SessionStorageSetter.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react'
+import { useCorbadoSession } from '@corbado/react'
+
+import { subKey } from '../auth'
+
+export const SessionStorageSetter = () => {
+  const { shortSession } = useCorbadoSession()
+
+  useEffect(() => {
+    if (!shortSession) return
+
+    console.log('hello SessionStorageSetter, new shortSession:', shortSession)
+    window.sessionStorage.setItem(subKey, shortSession)
+  }, [shortSession])
+
+  return null
+}
