af761dd32135147a9dc5694da096698b3c5def26
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2024-04-11 10:24:19 +0200

    clean

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -7,7 +7,7 @@ import { Auth } from './Auth'
 // https://www.robinwieruch.de/react-router-authentication/
 export const ProtectedRoute = memo(({ children }) => {
   const { loading, isAuthenticated } = useCorbado()
-  console.log('hello ProtectedRoute', { loading, isAuthenticated })
+  // console.log('hello ProtectedRoute', { loading, isAuthenticated })
 
   if (loading) return <Loading label="Authenticating" />
 
