e99205e0d9bfd01899f6d0feebf795f9dba539d8
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2024-04-11 10:15:32 +0200

    comment

--- src/components/LayoutProtected/index.tsx
+++ src/components/LayoutProtected/index.tsx
@@ -7,6 +7,7 @@ import { Notifications } from '../Notifications'
 import { ProtectedRoute } from '../ProtectedRoute'
 import { Header } from './Header'
 
+// memoizing this component creates error
 export const Layout = () => {
   // onlyForm is a query parameter that allows the user to view a form without the rest of the app
   // used for popups inside the map
