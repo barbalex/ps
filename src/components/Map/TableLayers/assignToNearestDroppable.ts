0fa7222c492c5f1ba168385732c508a04bd4d834
Alexander Gabriel <alex.barbalex@gmail.com>
2024-04-21 12:47:16 +0200

    work on assigning occurrences

--- src/components/Map/TableLayers/assignToNearestDroppable.ts
+++ src/components/Map/TableLayers/assignToNearestDroppable.ts
@@ -1,6 +1,10 @@
+import nearestPoint from '@turf/nearest-point'
+import { featureCollection, point } from '@turf/helpers'
+
 import {
   Electric,
   Vector_layers as VectorLayer,
+  Places as Place,
 } from '../../../generated/client'
 
 interface Props {
@@ -10,10 +14,22 @@ interface Props {
   latLng: [number, number]
 }
 
-export const assignToNearestDroppable = ({ db, authUser, layer, latLng }) => {
+export const assignToNearestDroppable = ({
+  db,
+  authUser,
+  layer,
+  latLng,
+}: Props) => {
   // 1. get droppable layer
   const appState = db.app_states.liveFirst({
     where: { user_email: authUser?.email },
   })
   const droppableLayer = appState?.droppable_layer
+  // 2. get all features from droppable layer
+  const places: Place[] = db.places.findMany({
+    where: {
+      parent_id: droppableLayer === 'places1' ? null : { not: null },
+      geometry: { not: null },
+    },
+  })
 }
