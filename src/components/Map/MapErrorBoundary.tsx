7a39ec4a6f4c794f75917b90ecf2f534e97f5e0a
Alexander Gabriel <alex.barbalex@gmail.com>
2024-04-23 10:24:32 +0200

    optimize creating notifications

--- src/components/Map/MapErrorBoundary.tsx
+++ src/components/Map/MapErrorBoundary.tsx
@@ -1,23 +1,21 @@
 import { PropsWithChildren } from 'react'
 import { ErrorBoundary as ErrorBoundaryComponent } from 'react-error-boundary'
-import { uuidv7 } from '@kripod/uuidv7'
 
 import { TileLayer } from '../../dexieClient'
 import { useElectric } from '../../ElectricProvider'
+import { createNotification } from '../../modules/createRows'
 
 const onReload = () => {
   window.location.reload(true)
 }
 
 const ErrorFallback = ({ error, db, layer }) => {
-  db.notifications.create({
-    data: {
-      title: `Fehler in Vektor-Layer '${layer.label}'`,
-      body: error.message,
-      intent: 'error', // 'success' | 'error' | 'warning' | 'info'
-      notification_id: uuidv7(),
-    },
+  const data = createNotification({
+    title: `Fehler in Vektor-Layer '${layer.label}'`,
+    body: error.message,
+    intent: 'error',
   })
+  db.notifications.create({ data })
 
   return null
 }
