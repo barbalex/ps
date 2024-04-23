c446ff3a5c50aff9b91828797d95a74f5396ee2a
Alexander Gabriel <alex.barbalex@gmail.com>
2024-04-23 10:23:15 +0200

    optimize creating notifications

--- src/routes/vectorLayer/Url.tsx
+++ src/routes/vectorLayer/Url.tsx
@@ -1,10 +1,10 @@
 import { useCallback, memo } from 'react'
-import { uuidv7 } from '@kripod/uuidv7'
 import { createWorkerFactory, useWorker } from '@shopify/react-web-worker'
 
 import { Vector_layers as VectorLayer } from '../../../generated/client'
 import { useElectric } from '../../ElectricProvider'
 import { TextField } from '../../components/shared/TextField'
+import { createNotification } from '../../modules/createRows'
 
 const createWorker = createWorkerFactory(() => import('./getCapabilitiesData'))
 
@@ -17,15 +17,12 @@ export const Url = memo(
       if (!row?.wfs_url) return
       // TODO: compare with old value and only update if changed
       // show loading indicator
-      const notification_id = uuidv7()
-      await db.notifications.create({
-        data: {
-          notification_id,
-          title: `Loading capabilities data for ${row.wfs_url}`,
-          intent: 'info',
-          paused: true,
-        },
+      const data = createNotification({
+        title: `Loading capabilities data for ${row.wfs_url}`,
+        intent: 'info',
+        paused: true,
       })
+      await db.notifications.create({ data })
       try {
         // await getCapabilitiesData({ row, db })
         await worker.getCapabilitiesData({ row, db })
@@ -37,7 +34,7 @@ export const Url = memo(
         // TODO: surface error to user
       }
       await db.notifications.update({
-        where: { notification_id },
+        where: { notification_id: data.notification_id },
         data: { paused: false, timeout: 500 },
       })
     }, [db, row, worker])
