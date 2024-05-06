41f53f42f89db80598b38a8f01d8b5a942efb92f
Alexander Gabriel <alex.barbalex@gmail.com>
2024-05-06 17:40:40 +0200

    lint

--- src/components/Tree/Accounts.tsx
+++ src/components/Tree/Accounts.tsx
@@ -3,8 +3,8 @@ import { useLiveQuery } from 'electric-sql/react'
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom'
 
 import { useElectric } from '../../ElectricProvider.tsx'
-import { Node } from './Node'
-import { AccountNode } from './Account'
+import { Node } from './Node.tsx'
+import { AccountNode } from './Account.tsx'
 
 export const AccountsNode = memo(() => {
   const location = useLocation()
