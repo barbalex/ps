9ad1f73f29a042895d1891a9652e0c449668880f
Alexander Gabriel <alex.barbalex@gmail.com>
2024-05-22 15:22:50 +0200

    query and pass open nodes

--- src/components/Tree/index.tsx
+++ src/components/Tree/index.tsx
@@ -1,5 +1,8 @@
 import { memo } from 'react'
+import { useLiveQuery } from 'electric-sql/react'
+import { useCorbado } from '@corbado/react'
 
+import { useElectric } from '../../ElectricProvider.tsx'
 import { ProjectsNode } from './Projects.tsx'
 import { UsersNode } from './Users.tsx'
 import { AccountsNode } from './Accounts.tsx'
@@ -18,19 +21,31 @@ const containerStyle = {
   contain: 'paint layout style',
 }
 
-export const Tree = memo(({ designing }) => (
-  <div style={containerStyle}>
-    <ProjectsNode />
-    <UsersNode />
-    <AccountsNode />
-    {designing && (
-      <>
-        <FieldTypesNode />
-        <WidgetTypesNode />
-        <WidgetsForFieldsNode />
-        <FieldsNode />
-      </>
-    )}
-    <MessagesNode />
-  </div>
-))
+// all nodes need app_states.tree_open_nodes
+// all nodes will rerender when app_states.tree_open_nodes changes
+// so query it here once and pass it down
+export const Tree = memo(({ designing }) => {
+  const { user: authUser } = useCorbado()
+  const { db } = useElectric()!
+  const { results: appState } = useLiveQuery(
+    db.app_states.liveFirst({ where: { user_email: authUser?.email } }),
+  )
+  const openNodes = appState?.tree_open_nodes || []
+
+  return (
+    <div style={containerStyle}>
+      <ProjectsNode openNodes={openNodes} />
+      <UsersNode openNodes={openNodes} />
+      <AccountsNode openNodes={openNodes} />
+      {designing && (
+        <>
+          <FieldTypesNode openNodes={openNodes} />
+          <WidgetTypesNode openNodes={openNodes} />
+          <WidgetsForFieldsNode openNodes={openNodes} />
+          <FieldsNode openNodes={openNodes} />
+        </>
+      )}
+      <MessagesNode openNodes={openNodes} />
+    </div>
+  )
+})
