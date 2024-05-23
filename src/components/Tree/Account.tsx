ff509e42d9eae82d909801810694df8a71fff169
Alexander Gabriel <alex.barbalex@gmail.com>
2024-05-23 13:16:22 +0200

    remove child nodes on close

--- src/components/Tree/Account.tsx
+++ src/components/Tree/Account.tsx
@@ -5,6 +5,8 @@ import {
   useNavigate,
   useSearchParams,
 } from 'react-router-dom'
+import { useLiveQuery } from 'electric-sql/react'
+import { useCorbado } from '@corbado/react'
 
 import { Node } from './Node.tsx'
 import { Accounts as Account } from '../../../generated/client/index.ts'
@@ -19,6 +21,12 @@ export const AccountNode = memo(({ account, level = 2 }: Props) => {
   const location = useLocation()
   const navigate = useNavigate()
   const [searchParams] = useSearchParams()
+  const { user: authUser } = useCorbado()
+
+  const { db } = useElectric()!
+  const { results: appState } = useLiveQuery(
+    db.app_states.liveFirst({ where: { user_email: authUser?.email } }),
+  )
 
   const urlPath = location.pathname.split('/').filter((p) => p !== '')
   const isOpen =
@@ -26,16 +34,29 @@ export const AccountNode = memo(({ account, level = 2 }: Props) => {
   const isActive = isOpen && urlPath.length === 2
 
   const onClickButton = useCallback(() => {
-    if (isOpen)
+    if (isOpen) {
+      removeChildNodes({
+        node: ['accounts', account.account_id],
+        db,
+        appStateId: appState?.app_state_id,
+      })
       return navigate({
         pathname: '/accounts',
         search: searchParams.toString(),
       })
+    }
     navigate({
       pathname: `/accounts/${account.account_id}`,
       search: searchParams.toString(),
     })
-  }, [isOpen, navigate, account.account_id, searchParams])
+  }, [
+    isOpen,
+    navigate,
+    account.account_id,
+    searchParams,
+    db,
+    appState?.app_state_id,
+  ])
 
   return (
     <Node
