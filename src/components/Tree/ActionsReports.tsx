41b6f0ab5cdab34cfd22d6211a851ea3f67f2d46
Alexander Gabriel <alex.barbalex@gmail.com>
2024-05-23 14:57:24 +0200

    remove child nodes on close

--- src/components/Tree/ActionsReports.tsx
+++ src/components/Tree/ActionsReports.tsx
@@ -69,19 +69,44 @@ export const ActionReportsNode = memo(
         urlPath[8] === 'reports'
     const isActive = isOpen && urlPath.length === level
 
-    const baseUrl = `/projects/${project_id}/subprojects/${subproject_id}/places/${
-      place_id ?? place.place_id
-    }${place_id ? `/places/${place.place_id}` : ''}/actions/${action_id}`
+    const baseArray = useMemo(
+      () => [
+        'projects',
+        project_id,
+        'subprojects',
+        subproject_id,
+        'places',
+        place_id ?? place.place_id,
+        ...(place_id ? ['places', place.place_id] : []),
+        'actions',
+        action_id,
+      ],
+      [action_id, place.place_id, place_id, project_id, subproject_id],
+    )
+    const baseUrl = baseArray.join('/')
 
     const onClickButton = useCallback(() => {
       if (isOpen) {
+        removeChildNodes({
+          node: [...baseArray, 'reports'],
+          db,
+          appStateId: appState?.app_state_id,
+        })
         return navigate({ pathname: baseUrl, search: searchParams.toString() })
       }
       navigate({
         pathname: `${baseUrl}/reports`,
         search: searchParams.toString(),
       })
-    }, [baseUrl, isOpen, navigate, searchParams])
+    }, [
+      appState?.app_state_id,
+      baseArray,
+      baseUrl,
+      db,
+      isOpen,
+      navigate,
+      searchParams,
+    ])
 
     return (
       <>
