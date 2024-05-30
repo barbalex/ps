0257a766b028d708d7db49e54cce29f182798ecb
Alexander Gabriel <alex.barbalex@gmail.com>
2024-05-30 11:22:40 +0200

    tree: enable multiple branches

--- src/components/Tree/Places.tsx
+++ src/components/Tree/Places.tsx
@@ -2,11 +2,13 @@ import { useCallback, useMemo, memo } from 'react'
 import { useLiveQuery } from 'electric-sql/react'
 import { useCorbado } from '@corbado/react'
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom'
+import isEqual from 'lodash/isEqual'
 
 import { useElectric } from '../../ElectricProvider.tsx'
 import { Node } from './Node.tsx'
 import { PlaceNode } from './Place/index.tsx'
 import { removeChildNodes } from '../../modules/tree/removeChildNodes.ts'
+import { addOpenNodes } from '../../modules/tree/addOpenNodes.ts'
 
 interface Props {
   project_id: string
@@ -29,8 +31,13 @@ export const PlacesNode = memo(
         orderBy: { label: 'asc' },
       }),
     )
+
     const { results: appState } = useLiveQuery(
       db.app_states.liveFirst({ where: { user_email: authUser?.email } }),
+    )
+    const openNodes = useMemo(
+      () => appState?.tree_open_nodes ?? [],
+      [appState?.tree_open_nodes],
     )
 
     const { results: placeLevels } = useLiveQuery(
@@ -50,6 +57,7 @@ export const PlacesNode = memo(
       [placeNamePlural, places.length],
     )
 
+    const urlPath = location.pathname.split('/').filter((p) => p !== '')
     const parentArray = useMemo(
       () => [
         'data',
@@ -61,16 +69,14 @@ export const PlacesNode = memo(
       ],
       [place_id, project_id, subproject_id],
     )
-    const parentUrl = parentArray.join('/')
+    const parentUrl = `/${parentArray.join('/')}`
     const ownArray = useMemo(() => [...parentArray, 'places'], [parentArray])
-    const ownUrl = ownArray.join('/')
+    const ownUrl = `/${ownArray.join('/')}`
 
-    const urlPath = location.pathname.split('/').filter((p) => p !== '')
-
-    // isOpen if urlPath includes the parentArray
-    const isOpen = parentArray.every((part, i) => urlPath[i] === part)
-
-    const isActive = isOpen && urlPath.length === level + 1
+    // needs to work not only works for urlPath, for all opened paths!
+    const isOpen = openNodes.some((array) => isEqual(array, ownArray))
+    const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+    const isActive = isEqual(urlPath, ownArray)
 
     const onClickButton = useCallback(() => {
       if (isOpen) {
@@ -79,24 +85,32 @@ export const PlacesNode = memo(
           db,
           appStateId: appState?.app_state_id,
         })
-        return navigate({
-          pathname: parentUrl,
-          search: searchParams.toString(),
-        })
+        // only navigate if urlPath includes ownArray
+        if (isInActiveNodeArray && ownArray.length < urlPath.length) {
+          navigate({
+            pathname: parentUrl,
+            search: searchParams.toString(),
+          })
+        }
+        return
       }
-      navigate({
-        pathname: ownUrl,
-        search: searchParams.toString(),
+      // add to openNodes without navigating
+      addOpenNodes({
+        nodes: [ownArray],
+        db,
+        appStateId: appState?.app_state_id,
       })
     }, [
       isOpen,
-      navigate,
-      ownUrl,
-      searchParams,
-      parentArray,
+      ownArray,
       db,
       appState?.app_state_id,
+      parentArray,
+      isInActiveNodeArray,
+      urlPath.length,
+      navigate,
       parentUrl,
+      searchParams,
     ])
 
     return (
@@ -105,7 +119,7 @@ export const PlacesNode = memo(
           node={placesNode}
           level={level}
           isOpen={isOpen}
-          isInActiveNodeArray={isOpen}
+          isInActiveNodeArray={isInActiveNodeArray}
           isActive={isActive}
           childrenCount={places.length}
           to={ownUrl}
