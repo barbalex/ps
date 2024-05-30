6f0edb36fac7ec41b9e5d80b0003cc882630e020
Alexander Gabriel <alex.barbalex@gmail.com>
2024-05-30 11:49:37 +0200

    tree: enable multiple branches

--- src/components/Tree/ActionsReportValues.tsx
+++ src/components/Tree/ActionsReportValues.tsx
@@ -58,31 +58,7 @@ export const ActionReportValuesNode = memo(
     )
 
     const urlPath = location.pathname.split('/').filter((p) => p !== '')
-    const isOpenBase =
-      urlPath[1] === 'projects' &&
-      urlPath[2] === project_id &&
-      urlPath[3] === 'subprojects' &&
-      urlPath[4] === subproject_id &&
-      urlPath[5] === 'places' &&
-      urlPath[6] === (place_id ?? place.place_id)
-    const isOpen = place_id
-      ? isOpenBase &&
-        urlPath[7] === 'places' &&
-        urlPath[8] === place.place_id &&
-        urlPath[9] === 'actions' &&
-        urlPath[10] === action_id &&
-        urlPath[11] === 'reports' &&
-        urlPath[12] === action_report_id &&
-        urlPath[13] === 'values'
-      : isOpenBase &&
-        urlPath[7] === 'actions' &&
-        urlPath[8] === action_id &&
-        urlPath[9] === 'reports' &&
-        urlPath[10] === action_report_id &&
-        urlPath[11] === 'values'
-    const isActive = isOpen && urlPath.length === level + 1
-
-    const baseArray = useMemo(
+    const parentArray = useMemo(
       () => [
         'data',
         'projects',
@@ -106,28 +82,47 @@ export const ActionReportValuesNode = memo(
         subproject_id,
       ],
     )
-    const baseUrl = baseArray.join('/')
+    const parentUrl = `/${parentArray.join('/')}`
+    const ownArray = useMemo(() => [...parentArray, 'values'], [parentArray])
+    const ownUrl = `/${ownArray.join('/')}`
+
+    // needs to work not only works for urlPath, for all opened paths!
+    const isOpen = openNodes.some((array) => isEqual(array, ownArray))
+    const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+    const isActive = isEqual(urlPath, ownArray)
 
     const onClickButton = useCallback(() => {
       if (isOpen) {
         removeChildNodes({
-          node: [...baseArray],
+          node: parentArray,
           db,
           appStateId: appState?.app_state_id,
         })
-        return navigate({ pathname: baseUrl, search: searchParams.toString() })
+        // only navigate if urlPath includes ownArray
+        if (isInActiveNodeArray && ownArray.length <= urlPath.length) {
+          navigate({
+            pathname: parentUrl,
+            search: searchParams.toString(),
+          })
+        }
+        return
       }
-      navigate({
-        pathname: `${baseUrl}/values`,
-        search: searchParams.toString(),
+      // add to openNodes without navigating
+      addOpenNodes({
+        nodes: [ownArray],
+        db,
+        appStateId: appState?.app_state_id,
       })
     }, [
-      appState?.app_state_id,
-      baseArray,
-      baseUrl,
-      db,
       isOpen,
+      ownArray,
+      db,
+      appState?.app_state_id,
+      parentArray,
+      isInActiveNodeArray,
+      urlPath.length,
       navigate,
+      parentUrl,
       searchParams,
     ])
 
@@ -137,10 +132,10 @@ export const ActionReportValuesNode = memo(
           node={actionReportValuesNode}
           level={level}
           isOpen={isOpen}
-          isInActiveNodeArray={isOpen}
+          isInActiveNodeArray={isInActiveNodeArray}
           isActive={isActive}
           childrenCount={actionReportValues.length}
-          to={`${baseUrl}/values`}
+          to={ownUrl}
           onClickButton={onClickButton}
         />
         {isOpen &&
