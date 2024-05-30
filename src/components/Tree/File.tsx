0f7e342e98397c88080e9246d0f4b4e0ace013e2
Alexander Gabriel <alex.barbalex@gmail.com>
2024-05-30 20:54:42 +0200

    tree: enable multiple branches

--- src/components/Tree/File.tsx
+++ src/components/Tree/File.tsx
@@ -1,18 +1,9 @@
-import { useCallback, useMemo, memo } from 'react'
-import {
-  useLocation,
-  useParams,
-  useNavigate,
-  useSearchParams,
-} from 'react-router-dom'
-import { useLiveQuery } from 'electric-sql/react'
-import { useCorbado } from '@corbado/react'
+import { useMemo, memo } from 'react'
+import { useLocation } from 'react-router-dom'
 import isEqual from 'lodash/isEqual'
 
 import { Node } from './Node.tsx'
 import { Files as File } from '../../../generated/client/index.ts'
-import { removeChildNodes } from '../../modules/tree/removeChildNodes.ts'
-import { useElectric } from '../../ElectricProvider.tsx'
 
 interface Props {
   project_id?: string
@@ -32,104 +23,12 @@ export const FileNode = memo(
     file,
     level = 2,
   }: Props) => {
-    const params = useParams()
     const location = useLocation()
-    const navigate = useNavigate()
-    const [searchParams] = useSearchParams()
-    const { user: authUser } = useCorbado()
-
-    const { db } = useElectric()!
-    const { results: appState } = useLiveQuery(
-      db.app_states.liveFirst({ where: { user_email: authUser?.email } }),
-    )
 
     const isPreview = location.pathname.endsWith('/preview')
     const urlPath = location.pathname.split('/').filter((p) => p !== '')
-    const isOpen =
-      place_id2 && action_id
-        ? urlPath[1] === 'projects' &&
-          urlPath[2] === project_id &&
-          urlPath[3] === 'subprojects' &&
-          urlPath[4] === subproject_id &&
-          urlPath[5] === 'places' &&
-          urlPath[6] === place_id &&
-          urlPath[7] === 'places' &&
-          urlPath[8] === place_id2 &&
-          urlPath[9] === 'actions' &&
-          urlPath[10] === action_id &&
-          urlPath[11] === 'files' &&
-          params.file_id === file.file_id
-        : place_id2 && check_id
-        ? urlPath[1] === 'projects' &&
-          urlPath[2] === project_id &&
-          urlPath[3] === 'subprojects' &&
-          urlPath[4] === subproject_id &&
-          urlPath[5] === 'places' &&
-          urlPath[6] === place_id &&
-          urlPath[7] === 'places' &&
-          urlPath[8] === place_id2 &&
-          urlPath[9] === 'checks' &&
-          urlPath[10] === check_id &&
-          urlPath[11] === 'files' &&
-          params.file_id === file.file_id
-        : !place_id2 && action_id
-        ? urlPath[1] === 'projects' &&
-          urlPath[2] === project_id &&
-          urlPath[3] === 'subprojects' &&
-          urlPath[4] === subproject_id &&
-          urlPath[5] === 'places' &&
-          urlPath[6] === place_id &&
-          urlPath[7] === 'actions' &&
-          urlPath[8] === action_id &&
-          urlPath[9] === 'files' &&
-          params.file_id === file.file_id
-        : !place_id2 && check_id
-        ? urlPath[1] === 'projects' &&
-          urlPath[2] === project_id &&
-          urlPath[3] === 'subprojects' &&
-          urlPath[4] === subproject_id &&
-          urlPath[5] === 'places' &&
-          urlPath[6] === place_id &&
-          urlPath[7] === 'checks' &&
-          urlPath[8] === check_id &&
-          urlPath[9] === 'files' &&
-          params.file_id === file.file_id
-        : place_id2
-        ? urlPath[1] === 'projects' &&
-          urlPath[2] === project_id &&
-          urlPath[3] === 'subprojects' &&
-          urlPath[4] === subproject_id &&
-          urlPath[5] === 'places' &&
-          urlPath[6] === place_id &&
-          urlPath[7] === 'places' &&
-          urlPath[8] === place_id2 &&
-          urlPath[9] === 'files' &&
-          params.file_id === file.file_id
-        : place_id
-        ? urlPath[1] === 'projects' &&
-          urlPath[2] === project_id &&
-          urlPath[3] === 'subprojects' &&
-          urlPath[4] === subproject_id &&
-          urlPath[5] === 'places' &&
-          urlPath[6] === place_id &&
-          urlPath[7] === 'files' &&
-          params.file_id === file.file_id
-        : subproject_id
-        ? urlPath[1] === 'projects' &&
-          urlPath[2] === project_id &&
-          urlPath[3] === 'subprojects' &&
-          urlPath[4] === subproject_id &&
-          urlPath[5] === 'files' &&
-          params.file_id === file.file_id
-        : project_id
-        ? urlPath[1] === 'projects' &&
-          urlPath[2] === project_id &&
-          urlPath[3] === 'files' &&
-          params.file_id === file.file_id
-        : urlPath[1] === 'files' && params.file_id === file.file_id
-    const isActive = isOpen && urlPath.length === level + 1
 
-    const baseArray = useMemo(
+    const ownArray = useMemo(
       () => [
         'data',
         ...(project_id ? ['projects', project_id] : []),
@@ -139,46 +38,34 @@ export const FileNode = memo(
         ...(action_id ? ['actions', action_id] : []),
         ...(check_id ? ['checks', check_id] : []),
         'files',
+        file.file_id,
+        ...(isPreview ? ['preview'] : []),
       ],
-      [action_id, check_id, place_id, place_id2, project_id, subproject_id],
+      [
+        action_id,
+        check_id,
+        file.file_id,
+        isPreview,
+        place_id,
+        place_id2,
+        project_id,
+        subproject_id,
+      ],
     )
-    const baseUrl = baseArray.join('/')
+    const ownUrl = `/${ownArray.join('/')}`
 
-    const onClickButton = useCallback(() => {
-      if (isOpen) {
-        removeChildNodes({
-          node: [...baseArray, file.file_id],
-          db,
-          appStateId: appState?.app_state_id,
-        })
-        return navigate({ pathname: baseUrl, search: searchParams.toString() })
-      }
-      navigate({
-        pathname: `${baseUrl}/${file.file_id}`,
-        search: searchParams.toString(),
-      })
-    }, [
-      isOpen,
-      navigate,
-      baseUrl,
-      file.file_id,
-      searchParams,
-      baseArray,
-      db,
-      appState?.app_state_id,
-    ])
+    const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+    const isActive = isEqual(urlPath, ownArray)
 
     return (
       <Node
         node={file}
         id={file.file_id}
         level={level}
-        isOpen={isOpen}
-        isInActiveNodeArray={isOpen}
+        isInActiveNodeArray={isInActiveNodeArray}
         isActive={isActive}
         childrenCount={0}
-        to={`${baseUrl}/${file.file_id}${isPreview ? '/preview' : ''}`}
-        onClickButton={onClickButton}
+        to={ownUrl}
       />
     )
   },
