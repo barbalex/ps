ef3b762bcac0f2268bfd65e8af78bf783a4014c8
Alexander Gabriel <alex.barbalex@gmail.com>
2024-05-31 13:17:27 +0200

    tree: enable multiple branches

--- src/components/Tree/PlaceUsers.tsx
+++ src/components/Tree/PlaceUsers.tsx
@@ -48,21 +48,6 @@ export const PlaceUsersNode = memo(
     )
 
     const urlPath = location.pathname.split('/').filter((p) => p !== '')
-    const isOpenBase =
-      urlPath[1] === 'projects' &&
-      urlPath[2] === project_id &&
-      urlPath[3] === 'subprojects' &&
-      urlPath[4] === subproject_id &&
-      urlPath[5] === 'places' &&
-      urlPath[6] === (place_id ?? place.place_id)
-    const isOpen = place_id
-      ? isOpenBase &&
-        urlPath[7] === 'places' &&
-        urlPath[8] === place.place_id &&
-        urlPath[9] === 'users'
-      : isOpenBase && urlPath[7] === 'users'
-    const isActive = isOpen && urlPath.length === level + 1
-
     const parentArray = useMemo(
       () => [
         'data',
@@ -76,29 +61,48 @@ export const PlaceUsersNode = memo(
       ],
       [project_id, subproject_id, place_id, place.place_id],
     )
-    const baseUrl = parentArray.join('/')
+    const parentUrl = `/${parentArray.join('/')}`
+    const ownArray = useMemo(() => [...parentArray, 'users'], [parentArray])
+    const ownUrl = `/${ownArray.join('/')}`
+
+    // needs to work not only works for urlPath, for all opened paths!
+    const isOpen = openNodes.some((array) => isEqual(array, ownArray))
+    const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+    const isActive = isEqual(urlPath, ownArray)
 
     const onClickButton = useCallback(() => {
       if (isOpen) {
         removeChildNodes({
-          node: [...parentArray, 'users'],
+          node: parentArray,
           db,
           appStateId: appState?.app_state_id,
         })
-        return navigate({ pathname: baseUrl, search: searchParams.toString() })
+        // only navigate if urlPath includes ownArray
+        if (isInActiveNodeArray && ownArray.length <= urlPath.length) {
+          navigate({
+            pathname: parentUrl,
+            search: searchParams.toString(),
+          })
+        }
+        return
       }
-      navigate({
-        pathname: `${baseUrl}/users`,
-        search: searchParams.toString(),
+      // add to openNodes without navigating
+      addOpenNodes({
+        nodes: [ownArray],
+        db,
+        appStateId: appState?.app_state_id,
       })
     }, [
       appState?.app_state_id,
-      parentArray,
-      baseUrl,
       db,
+      isInActiveNodeArray,
       isOpen,
       navigate,
+      ownArray,
+      parentArray,
+      parentUrl,
       searchParams,
+      urlPath.length,
     ])
 
     return (
@@ -107,10 +111,10 @@ export const PlaceUsersNode = memo(
           node={placeUsersNode}
           level={level}
           isOpen={isOpen}
-          isInActiveNodeArray={isOpen}
+          isInActiveNodeArray={isInActiveNodeArray}
           isActive={isActive}
           childrenCount={placeUsers.length}
-          to={`${baseUrl}/users`}
+          to={ownUrl}
           onClickButton={onClickButton}
         />
         {isOpen &&
