c46c194c796bc88e964f57318ea6e1cd61382158
Alexander Gabriel <alex.barbalex@gmail.com>
2024-05-31 14:25:28 +0200

    tree: enable multiple branches

--- src/components/Tree/Subprojects.tsx
+++ src/components/Tree/Subprojects.tsx
@@ -50,39 +50,52 @@ export const SubprojectsNode = memo(({ project_id, level = 3 }: Props) => {
   )
 
   const urlPath = location.pathname.split('/').filter((p) => p !== '')
-  const isOpen =
-    urlPath[1] === 'projects' &&
-    urlPath[2] === project_id &&
-    urlPath[3] === 'subprojects'
-  const isActive = isOpen && urlPath.length === level + 1
-
   const parentArray = useMemo(
     () => ['data', 'projects', project_id],
     [project_id],
   )
-  const baseUrl = parentArray.join('/')
+  const parentUrl = `/${parentArray.join('/')}`
+  const ownArray = useMemo(() => [...parentArray, 'subprojects'], [parentArray])
+  const ownUrl = `/${ownArray.join('/')}`
+
+  // needs to work not only works for urlPath, for all opened paths!
+  const isOpen = openNodes.some((array) => isEqual(array, ownArray))
+  const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+  const isActive = isEqual(urlPath, ownArray)
 
   const onClickButton = useCallback(() => {
     if (isOpen) {
       removeChildNodes({
-        node: [...parentArray, 'subprojects'],
+        node: parentArray,
         db,
         appStateId: appState?.app_state_id,
       })
-      return navigate({ pathname: baseUrl, search: searchParams.toString() })
+      // only navigate if urlPath includes ownArray
+      if (isInActiveNodeArray && ownArray.length <= urlPath.length) {
+        navigate({
+          pathname: parentUrl,
+          search: searchParams.toString(),
+        })
+      }
+      return
     }
-    navigate({
-      pathname: `${baseUrl}/subprojects`,
-      search: searchParams.toString(),
+    // add to openNodes without navigating
+    addOpenNodes({
+      nodes: [ownArray],
+      db,
+      appStateId: appState?.app_state_id,
     })
   }, [
     appState?.app_state_id,
-    parentArray,
-    baseUrl,
     db,
+    isInActiveNodeArray,
     isOpen,
     navigate,
+    ownArray,
+    parentArray,
+    parentUrl,
     searchParams,
+    urlPath.length,
   ])
 
   // prevent flash of different name that happens before namePlural is set
@@ -94,10 +107,10 @@ export const SubprojectsNode = memo(({ project_id, level = 3 }: Props) => {
         node={subprojectsNode}
         level={level}
         isOpen={isOpen}
-        isInActiveNodeArray={isOpen}
+        isInActiveNodeArray={isInActiveNodeArray}
         isActive={isActive}
         childrenCount={subprojects.length}
-        to={`${baseUrl}/subprojects`}
+        to={ownUrl}
         onClickButton={onClickButton}
       />
       {isOpen &&
