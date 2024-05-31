da58aa780586898623295d950611a2b163fe7bb9
Alexander Gabriel <alex.barbalex@gmail.com>
2024-05-31 14:23:13 +0200

    tree: enable multiple branches

--- src/components/Tree/SubprojectReports.tsx
+++ src/components/Tree/SubprojectReports.tsx
@@ -45,41 +45,52 @@ export const SubprojectReportsNode = memo(
     )
 
     const urlPath = location.pathname.split('/').filter((p) => p !== '')
-    const isOpen =
-      urlPath[1] === 'projects' &&
-      urlPath[2] === project_id &&
-      urlPath[3] === 'subprojects' &&
-      urlPath[4] === subproject_id &&
-      urlPath[5] === 'reports'
-    const isActive = isOpen && urlPath.length === level + 1
-
     const parentArray = useMemo(
       () => ['data', 'projects', project_id, 'subprojects', subproject_id],
       [project_id, subproject_id],
     )
-    const baseUrl = parentArray.join('/')
+    const parentUrl = `/${parentArray.join('/')}`
+    const ownArray = useMemo(() => [...parentArray, 'reports'], [parentArray])
+    const ownUrl = `/${ownArray.join('/')}`
+
+    // needs to work not only works for urlPath, for all opened paths!
+    const isOpen = openNodes.some((array) => isEqual(array, ownArray))
+    const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+    const isActive = isEqual(urlPath, ownArray)
 
     const onClickButton = useCallback(() => {
       if (isOpen) {
         removeChildNodes({
-          node: [...parentArray, 'reports'],
+          node: parentArray,
           db,
           appStateId: appState?.app_state_id,
         })
-        return navigate({ pathname: baseUrl, search: searchParams.toString() })
+        // only navigate if urlPath includes ownArray
+        if (isInActiveNodeArray && ownArray.length <= urlPath.length) {
+          navigate({
+            pathname: parentUrl,
+            search: searchParams.toString(),
+          })
+        }
+        return
       }
-      navigate({
-        pathname: `${baseUrl}/reports`,
-        search: searchParams.toString(),
+      // add to openNodes without navigating
+      addOpenNodes({
+        nodes: [ownArray],
+        db,
+        appStateId: appState?.app_state_id,
       })
     }, [
       appState?.app_state_id,
-      parentArray,
-      baseUrl,
       db,
+      isInActiveNodeArray,
       isOpen,
       navigate,
+      ownArray,
+      parentArray,
+      parentUrl,
       searchParams,
+      urlPath.length,
     ])
 
     return (
@@ -88,10 +99,10 @@ export const SubprojectReportsNode = memo(
           node={subprojectReportsNode}
           level={level}
           isOpen={isOpen}
-          isInActiveNodeArray={isOpen}
+          isInActiveNodeArray={isInActiveNodeArray}
           isActive={isActive}
           childrenCount={subprojectReports.length}
-          to={`${baseUrl}/reports`}
+          to={ownUrl}
           onClickButton={onClickButton}
         />
         {isOpen &&
