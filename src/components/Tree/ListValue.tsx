c2315c182d74721eab2000cea8b10c510aeb8b22
Alexander Gabriel <alex.barbalex@gmail.com>
2024-05-31 09:21:46 +0200

    tree: enable multiple branches

--- src/components/Tree/ListValue.tsx
+++ src/components/Tree/ListValue.tsx
@@ -1,13 +1,9 @@
-import { useCallback, memo, useMemo } from 'react'
-import { useLocation, useNavigate, useSearchParams } from 'react-router-dom'
-import { useLiveQuery } from 'electric-sql/react'
-import { useCorbado } from '@corbado/react'
+import { memo, useMemo } from 'react'
+import { useLocation } from 'react-router-dom'
 import isEqual from 'lodash/isEqual'
 
 import { Node } from './Node.tsx'
 import { ListValues as ListValue } from '../../../generated/client/index.ts'
-import { removeChildNodes } from '../../modules/tree/removeChildNodes.ts'
-import { useElectric } from '../../ElectricProvider.tsx'
 
 interface Props {
   project_id: string
@@ -19,66 +15,34 @@ interface Props {
 export const ListValueNode = memo(
   ({ project_id, list_id, listValue, level = 6 }: Props) => {
     const location = useLocation()
-    const navigate = useNavigate()
-    const [searchParams] = useSearchParams()
-    const { user: authUser } = useCorbado()
-
-    const { db } = useElectric()!
-    const { results: appState } = useLiveQuery(
-      db.app_states.liveFirst({ where: { user_email: authUser?.email } }),
-    )
 
     const urlPath = location.pathname.split('/').filter((p) => p !== '')
-    const isOpen =
-      urlPath[1] === 'projects' &&
-      urlPath[2] === project_id &&
-      urlPath[3] === 'lists' &&
-      urlPath[4] === list_id &&
-      urlPath[5] === 'values' &&
-      urlPath[6] === listValue.list_value_id
-    const isActive = isOpen && urlPath.length === level + 1
-
-    const baseArray = useMemo(
-      () => ['data', 'projects', project_id, 'lists', list_id, 'values'],
-      [project_id, list_id],
+    const ownArray = useMemo(
+      () => [
+        'data',
+        'projects',
+        project_id,
+        'lists',
+        list_id,
+        'values',
+        listValue.list_value_id,
+      ],
+      [project_id, list_id, listValue.list_value_id],
     )
-    const baseUrl = baseArray.join('/')
+    const ownUrl = `/${ownArray.join('/')}`
 
-    const onClickButton = useCallback(() => {
-      if (isOpen) {
-        removeChildNodes({
-          node: [...baseArray, listValue.list_value_id],
-          db,
-          appStateId: appState?.app_state_id,
-        })
-        return navigate({ pathname: baseUrl, search: searchParams.toString() })
-      }
-      navigate({
-        pathname: `${baseUrl}/${listValue.list_value_id}`,
-        search: searchParams.toString(),
-      })
-    }, [
-      isOpen,
-      navigate,
-      baseUrl,
-      listValue.list_value_id,
-      searchParams,
-      baseArray,
-      db,
-      appState?.app_state_id,
-    ])
+    const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+    const isActive = isEqual(urlPath, ownArray)
 
     return (
       <Node
         node={listValue}
         id={listValue.list_value_id}
         level={level}
-        isOpen={isOpen}
-        isInActiveNodeArray={isOpen}
+        isInActiveNodeArray={isInActiveNodeArray}
         isActive={isActive}
         childrenCount={0}
-        to={`${baseUrl}/${listValue.list_value_id}`}
-        onClickButton={onClickButton}
+        to={ownUrl}
       />
     )
   },
