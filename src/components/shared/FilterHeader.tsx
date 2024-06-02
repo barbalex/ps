e47e25d6e05b4d83a6dd8b9e4948a97da9180180
Alexander Gabriel <alex.barbalex@gmail.com>
2024-06-02 19:29:13 +0200

    work on filtering

--- src/components/shared/FilterHeader.tsx
+++ src/components/shared/FilterHeader.tsx
@@ -3,22 +3,29 @@ import { Button } from '@fluentui/react-components'
 import { FaChevronLeft } from 'react-icons/fa'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 
-export const FilterHeader = memo(({ title }) => {
-  const navigate = useNavigate()
-  const [searchParams] = useSearchParams()
-  const onClickBack = useCallback(() => {
-    navigate({ pathname: '..', search: searchParams.toString() })
-  }, [navigate, searchParams])
+type Props = {
+  title: string
+  backTo: 'list' | 'form'
+}
 
-  return (
-    <div className="filter-header">
-      <h1>{title}</h1>
-      <Button
-        size="medium"
-        icon={<FaChevronLeft />}
-        onClick={onClickBack}
-        title={`Back`}
-      />
-    </div>
-  )
-})
+export const FilterHeader = memo(
+  ({ title = 'Filter', backTo = 'form' }: Props) => {
+    const navigate = useNavigate()
+    const [searchParams] = useSearchParams()
+    const onClickBack = useCallback(() => {
+      navigate({ pathname: '..', search: searchParams.toString() })
+    }, [navigate, searchParams])
+
+    return (
+      <div className="filter-header">
+        <h1>{title}</h1>
+        <Button
+          size="medium"
+          icon={<FaChevronLeft />}
+          onClick={onClickBack}
+          title={`Back`}
+        />
+      </div>
+    )
+  },
+)
