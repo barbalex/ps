b42add1107376d6858448d0ebff72dfe0ac09cb3
Alexander Gabriel <alex.barbalex@gmail.com>
2024-06-03 13:38:42 +0200

    work on filtering

--- src/components/shared/Filter.tsx
+++ src/components/shared/Filter.tsx
@@ -1,6 +1,14 @@
 import { QueryBuilder } from 'react-querybuilder'
+import { useLiveQuery } from 'electric-sql/react'
+import { useCorbado } from '@corbado/react'
+
 import 'react-querybuilder/dist/query-builder.css'
 
+import { useElectric } from '../../ElectricProvider.tsx'
+
 export const Filter = () => {
+  const { user: authUser } = useCorbado()
+  const { db } = useElectric()!
+
   return 'here comes react query builder'
 }
