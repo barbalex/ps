8a3534da269f9945d49c5bd93fccbf6f78cbe565
Alexander Gabriel <alex.barbalex@gmail.com>
2024-06-03 14:34:39 +0200

    work on filtering

--- src/components/shared/Filter.tsx
+++ src/components/shared/Filter.tsx
@@ -6,9 +6,13 @@ import 'react-querybuilder/dist/query-builder.css'
 
 import { useElectric } from '../../ElectricProvider.tsx'
 
-export const Filter = () => {
+// Use params to get parentTable and parentTableId?
+export const Filter = ({ table, parentTable, parentTableId }) => {
   const { user: authUser } = useCorbado()
   const { db } = useElectric()!
+
+  // const fields = await db.rawQuery({ sql: `PRAGMA table_info(${tableName})` })
+  // console.log('hello Filter, fields:', fields)
 
   return 'here comes react query builder'
 }
