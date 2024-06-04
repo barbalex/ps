d95bbea0135528f8fc66ff676419fa40b324e6b7
Alexander Gabriel <alex.barbalex@gmail.com>
2024-06-04 15:20:46 +0200

    work on or filtering

--- src/routes/widgetForField/Filter.tsx
+++
@@ -1,9 +0,0 @@
-import { Filter } from '../../components/shared/Filter/index.tsx'
-
-import { WidgetForFieldForm } from './Form.tsx'
-
-export const Component = () => (
-  <Filter>
-    <WidgetForFieldForm />
-  </Filter>
-)
