ee27b72def982ae67c6cd1c462eeb7fbd9eaf663
Alexander Gabriel <alex.barbalex@gmail.com>
2024-06-05 18:51:54 +0200

    work on filters

---
+++ src/routes/action/Form.tsx
@@ -0,0 +1,45 @@
+import { memo } from 'react'
+import { useOutletContext } from 'react-router-dom'
+
+import { DateField } from '../../components/shared/DateField.tsx'
+import { SwitchField } from '../../components/shared/SwitchField.tsx'
+import { Jsonb } from '../../components/shared/Jsonb/index.tsx'
+import { EditingGeometry } from '../../components/shared/EditingGeometry.tsx'
+
+import '../../form.css'
+
+// this form is rendered from a parent or outlet
+export const Component = memo(
+  ({ onChange: onChangeFromProps, row: rowFromProps, autoFocusRef }) => {
+    // beware: contextFromOutlet is undefined if not inside an outlet
+    const outletContext = useOutletContext()
+    const onChange = onChangeFromProps ?? outletContext?.onChange
+    const row = rowFromProps ?? outletContext?.row ?? {}
+
+    return (
+      <>
+        <DateField
+          label="Date"
+          name="date"
+          value={row.date}
+          onChange={onChange}
+        />
+        <SwitchField
+          label="relevant for reports"
+          name="relevant_for_reports"
+          value={row.relevant_for_reports}
+          onChange={onChange}
+        />
+        <Jsonb
+          table="actions"
+          idField="action_id"
+          id={row.action_id}
+          data={row.data ?? {}}
+          autoFocus
+          ref={autoFocusRef}
+        />
+        <EditingGeometry row={row} table="actions" />
+      </>
+    )
+  },
+)
--- src/routes/action/index.tsx
+++ src/routes/action/index.tsx
@@ -5,13 +5,10 @@ import type { InputProps } from '@fluentui/react-components'
 
 import { useElectric } from '../../ElectricProvider.tsx'
 import { TextFieldInactive } from '../../components/shared/TextFieldInactive.tsx'
-import { DateField } from '../../components/shared/DateField.tsx'
-import { SwitchField } from '../../components/shared/SwitchField.tsx'
-import { Jsonb } from '../../components/shared/Jsonb/index.tsx'
 import { getValueFromChange } from '../../modules/getValueFromChange.ts'
-import { EditingGeometry } from '../../components/shared/EditingGeometry.tsx'
 import { Header } from './Header.tsx'
 import { Loading } from '../../components/shared/Loading.tsx'
+import { Component as Form } from './Form.tsx'
 
 import '../../form.css'
 
@@ -43,27 +40,7 @@ export const Component = () => {
       <Header autoFocusRef={autoFocusRef} />
       <div className="form-container">
         <TextFieldInactive label="ID" name="action_id" value={row.action_id} />
-        <DateField
-          label="Date"
-          name="date"
-          value={row.date}
-          onChange={onChange}
-        />
-        <SwitchField
-          label="relevant for reports"
-          name="relevant_for_reports"
-          value={row.relevant_for_reports}
-          onChange={onChange}
-        />
-        <Jsonb
-          table="actions"
-          idField="action_id"
-          id={row.action_id}
-          data={row.data ?? {}}
-          autoFocus
-          ref={autoFocusRef}
-        />
-        <EditingGeometry row={row} table="actions" />
+        <Form onChange={onChange} row={row} autoFocusRef={autoFocusRef} />
       </div>
     </div>
   )
