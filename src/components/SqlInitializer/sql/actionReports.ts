54cd1b1aa810f5598d1b653da3f43508a1ff3707
Alexander Gabriel <alex.barbalex@gmail.com>
2024-06-05 13:29:02 +0200

    migrate sql

--- src/components/SqlInitializer/sql/actionReports.ts
+++ src/components/SqlInitializer/sql/actionReports.ts
@@ -1,16 +1,36 @@
 export const generateActionReportLabel = async (db) => {
+  // const columns = await db.rawQuery({
+  //   sql: 'PRAGMA table_xinfo(action_reports)',
+  // })
+  // for PostgreSQL:
   const columns = await db.rawQuery({
-    sql: 'PRAGMA table_xinfo(action_reports)',
+    sql: `SELECT column_name AS name FROM information_schema.columns WHERE table_name = 'action_reports';`,
   })
   const hasLabel = columns.some((column) => column.name === 'label')
   if (!hasLabel) {
-    await db.unsafeExec({
-      sql: `
-        ALTER TABLE action_reports ADD COLUMN label text GENERATED ALWAYS AS (coalesce(year, action_report_id));`,
-    })
-    await db.unsafeExec({
-      sql: 'CREATE INDEX IF NOT EXISTS action_reports_label_idx ON action_reports(label)',
-    })
+    try {
+      await db.unsafeExec({
+        sql: `ALTER TABLE action_reports ADD COLUMN label text GENERATED ALWAYS AS (coalesce(year::text, action_report_id::text)) stored;`,
+      })
+    } catch (e) {
+      return console.error(
+        'SqlInitializer: generateActionReportLabel error when generating label:',
+        e,
+      )
+    }
+    // error: expected one dependency record for TOAST table, found 4
+    console.log('SqlInitializer: generateActionReportLabel 2')
+    try {
+      await db.unsafeExec({
+        sql: 'CREATE INDEX IF NOT EXISTS action_reports_label_idx ON action_reports(label)',
+      })
+    } catch (e) {
+      console.error(
+        'SqlInitializer: generateActionReportLabel error when indexing label:',
+        e,
+      )
+    }
+    console.log('SqlInitializer: generateActionReportLabel 3')
     console.log('generated action report labels')
   }
 }
