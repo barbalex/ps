47fb08bcd473de49af395f6df6c1cc1217287c8c
Alexander Gabriel <alex.barbalex@gmail.com>
2024-06-12 18:30:37 +0200

    work on map ui

--- src/components/Map/BottomRightControl/UiButton.tsx
+++ src/components/Map/BottomRightControl/UiButton.tsx
@@ -1,4 +1,9 @@
-import { useRef, useEffect, memo } from 'react'
+import { useRef, useEffect, useCallback, memo } from 'react'
+import { Switch } from '@fluentui/react-components'
+import { useLiveQuery } from 'electric-sql/react'
+import { useCorbado } from '@corbado/react'
+
+import { useElectric } from '../../../ElectricProvider.tsx'
 
 const uibuttonStyle = {
   gridArea: 'uibutton',
@@ -12,9 +17,18 @@ const uibuttonStyle = {
   flexWrap: 'nowrap',
   flexDirection: 'row',
   alignItems: 'center',
+  justifyContent: 'center',
 }
 
 export const UiButton = memo(() => {
+  const { user: authUser } = useCorbado()
+
+  const { db } = useElectric()!
+  const { results: appState } = useLiveQuery(
+    db.app_states.liveFirst({ where: { user_email: authUser?.email } }),
+  )
+  const hideMapUi = appState?.map_hide_ui ?? false
+
   // prevent click propagation on to map
   // https://stackoverflow.com/a/57013052/712005
   const ref = useRef()
@@ -23,9 +37,20 @@ export const UiButton = memo(() => {
     L.DomEvent.disableScrollPropagation(ref.current)
   }, [])
 
+  const onChange = useCallback(() => {
+    db.app_states.update({
+      where: { app_state_id: appState?.app_state_id },
+      data: { map_hide_ui: !hideMapUi },
+    })
+  }, [db.app_states, appState?.app_state_id, hideMapUi])
+
   return (
     <div style={uibuttonStyle} ref={ref}>
-      uibotton
+      <Switch
+        title={hideMapUi ? 'Show UI' : 'Hide UI'}
+        checked={hideMapUi}
+        onChange={onChange}
+      />
     </div>
   )
 })
