636c5e7b931227e9b3c056d0488cf7fb8177fbbb
Alexander Gabriel <alex.barbalex@gmail.com>
2024-06-13 11:05:43 +0200

    work on map ui

--- src/components/Map/BottomRightControl/index.tsx
+++ src/components/Map/BottomRightControl/index.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect, memo } from 'react'
+import { memo } from 'react'
 
 import { UiButton } from './UiButton.tsx'
 import { VerticalButtons } from './VerticalButtons.tsx'
@@ -22,21 +22,10 @@ const containerStyle = {
   alignItems: 'stretch',
 }
 
-// TODO: only prevent click propagation in active grid areas
-export const BottomRightControl = memo(() => {
-  // prevent click propagation on to map
-  // https://stackoverflow.com/a/57013052/712005
-  const ref = useRef()
-  useEffect(() => {
-    L.DomEvent.disableClickPropagation(ref.current)
-    L.DomEvent.disableScrollPropagation(ref.current)
-  }, [])
-
-  return (
-    <div style={containerStyle} ref={ref}>
-      <VerticalButtons />
-      <HorizontalButtons />
-      <UiButton />
-    </div>
-  )
-})
+export const BottomRightControl = memo(() => (
+  <div style={containerStyle}>
+    <VerticalButtons />
+    <HorizontalButtons />
+    <UiButton />
+  </div>
+))
