dddaa0b812ecddef0468d39de9fa8283d5160438
Alexander Gabriel <alex.barbalex@gmail.com>
2024-06-17 14:55:31 +0200

    work on it

--- src/components/Map/ClickListener/index.tsx
+++ src/components/Map/ClickListener/index.tsx
@@ -1,4 +1,4 @@
-import { memo, useMemo } from 'react'
+import { memo, useMemo, useCallback } from 'react'
 import { useLiveQuery } from 'electric-sql/react'
 import { useCorbado } from '@corbado/react'
 import { useParams } from 'react-router-dom'
@@ -24,26 +24,37 @@ export const ClickListener = memo(() => {
   )
   const where = filter.length > 1 ? { OR: filter } : filter[0]
 
-  // Three types of querying:
-  // 1. Vector Layers from own tables (type !== 'wfs')
-  // 2. Vector Layers from WFS with pre-downloaded data
-  // 3. Vector Layers from WFS with no pre-downloaded data
-  // TODO: filter own layers and layers with pre-downloaded data
-  // by querying db.vector_layer_geoms using ST_CONTAINS once postgis arrives in pglite
-
-  const { results: vectorLayers = [] } = useLiveQuery(
-    db.vector_layers.liveMany({
-      where: { project_id, active: true, ...where },
-      orderBy: [{ sort: 'asc' }, { label: 'asc' }],
-    }),
-  )
-
   console.log('Map ClickListener', {
     project_id,
     where,
     vectorLayers,
     vectorLayerTypes: vectorLayers.map((vl) => vl.type),
   })
+
+  const onClick = useCallback(
+    async (event) => {
+      const { lat, lng } = event.latlng
+      const zoom = map.getZoom()
+      console.log('Map ClickListener, onClick', { lat, lng, zoom })
+
+      const layersData = []
+
+      // Three types of querying:
+      // 1. Vector Layers from own tables (type !== 'wfs')
+      // 2. Vector Layers from WFS with pre-downloaded data
+      // 3. Vector Layers from WFS with no pre-downloaded data
+      // TODO: filter own layers and layers with pre-downloaded data
+      // by querying db.vector_layer_geoms using ST_CONTAINS once postgis arrives in pglite
+      const vectorLayers = await db.vector_layers.findMany({
+        where: { project_id, active: true, type: 'wfs', ...where },
+        orderBy: [{ sort: 'asc' }, { label: 'asc' }],
+      })
+      console.log('Map ClickListener, onClick', { vectorLayers })
+    },
+    [db.vector_layers, map, project_id, where],
+  )
+
+  const map = useMapEvent('click', onClick)
 
   return null
 })
