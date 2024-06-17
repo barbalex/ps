16eda8862a9ecb4dc9814338e616d56143a4d62a
Alexander Gabriel <alex.barbalex@gmail.com>
2024-06-17 14:58:45 +0200

    work on it

--- src/components/Map/ClickListener/index.tsx
+++ src/components/Map/ClickListener/index.tsx
@@ -2,34 +2,19 @@ import { memo, useMemo, useCallback } from 'react'
 import { useLiveQuery } from 'electric-sql/react'
 import { useCorbado } from '@corbado/react'
 import { useParams } from 'react-router-dom'
-import { useMapEvent } from 'react-leaflet/hooks'
+import { useMapEvent, useMap } from 'react-leaflet/hooks'
 
 import { useElectric } from '../../../ElectricProvider.tsx'
 
 export const ClickListener = memo(() => {
   const { project_id } = useParams()
   const { user: authUser } = useCorbado()
+  const map = useMap()
 
   const { db } = useElectric()!
   const { results: appState } = useLiveQuery(
     db.app_states.liveFirst({ where: { user_email: authUser?.email } }),
   )
-
-  const filter = useMemo(
-    () =>
-      appState?.filter_vector_layers?.filter(
-        (f) => Object.keys(f).length > 0,
-      ) ?? [],
-    [appState?.filter_vector_layers],
-  )
-  const where = filter.length > 1 ? { OR: filter } : filter[0]
-
-  console.log('Map ClickListener', {
-    project_id,
-    where,
-    vectorLayers,
-    vectorLayerTypes: vectorLayers.map((vl) => vl.type),
-  })
 
   const onClick = useCallback(
     async (event) => {
@@ -38,6 +23,12 @@ export const ClickListener = memo(() => {
       console.log('Map ClickListener, onClick', { lat, lng, zoom })
 
       const layersData = []
+
+      const filter =
+        appState?.filter_vector_layers?.filter(
+          (f) => Object.keys(f).length > 0,
+        ) ?? []
+      const where = filter.length > 1 ? { OR: filter } : filter[0]
 
       // Three types of querying:
       // 1. Vector Layers from own tables (type !== 'wfs')
@@ -49,12 +40,12 @@ export const ClickListener = memo(() => {
         where: { project_id, active: true, type: 'wfs', ...where },
         orderBy: [{ sort: 'asc' }, { label: 'asc' }],
       })
-      console.log('Map ClickListener, onClick', { vectorLayers })
+      console.log('Map ClickListener, onClick', { vectorLayers, where })
     },
-    [db.vector_layers, map, project_id, where],
+    [appState?.filter_vector_layers, db.vector_layers, map, project_id],
   )
 
-  const map = useMapEvent('click', onClick)
+  useMapEvent('click', onClick)
 
   return null
 })
