30b9b3198e14e00d9d3e1e14bf76722b776e9ed0
Alexander Gabriel <alex.barbalex@gmail.com>
2024-06-19 13:48:05 +0200

    work on it

--- src/components/Map/Info/index.tsx
+++ src/components/Map/Info/index.tsx
@@ -1,14 +1,5 @@
 import { useCallback, useRef, useState, memo } from 'react'
-import { useLiveQuery } from 'electric-sql/react'
-import { useCorbado } from '@corbado/react'
-import {
-  DrawerBody,
-  DrawerHeader,
-  DrawerHeaderTitle,
-  InlineDrawer,
-} from '@fluentui/react-components'
 
-import { useElectric } from '../../../ElectricProvider.tsx'
 import { ErrorBoundary } from '../../shared/ErrorBoundary.tsx'
 import { Resize } from './Resize.tsx'
 import { Drawer } from './Drawer.tsx'
@@ -17,21 +8,8 @@ const drawerContainerStyle = {
   position: 'relative',
   display: 'flex',
 }
-const drawerStyle = {
-  willChange: 'width',
-  transitionProperty: 'width',
-  transitionDuration: 100,
-}
 
 export const Info = memo(({ redrawMap }) => {
-  const { user: authUser } = useCorbado()
-
-  const { db } = useElectric()!
-  const { results: appState } = useLiveQuery(
-    db.app_states.liveFirst({ where: { user_email: authUser?.email } }),
-  )
-  const mapInfo = appState?.map_info
-
   const animationFrame = useRef<number>(0)
   const sidebarRef = useRef<HTMLDivElement>(null)
   const [sidebarWidth, setSidebarWidth] = useState(320)
@@ -40,9 +18,10 @@ export const Info = memo(({ redrawMap }) => {
       const clientX = props?.location?.current?.input?.clientX
       animationFrame.current = requestAnimationFrame(() => {
         if (sidebarRef.current) {
-          setSidebarWidth(
-            sidebarRef.current.getBoundingClientRect().right - clientX,
-          )
+          const newWidth =
+            sidebarRef.current.getBoundingClientRect().right - clientX
+          // TODO: if newWidth is less than 50, close the sidebar
+          setSidebarWidth(newWidth)
           setTimeout(redrawMap, 200)
         }
       })
