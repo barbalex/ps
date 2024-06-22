9822ab703de8efb42a8cfc6fb3f38de0a8ab3aa9
Alexander Gabriel <alex.barbalex@gmail.com>
2024-06-22 15:08:45 +0200

    work on map ui crs

--- src/routes/crs/Choose.tsx
+++ src/routes/crs/Choose.tsx
@@ -1,6 +1,5 @@
 import { memo, useCallback, useState } from 'react'
 import { Button } from '@fluentui/react-components'
-import axios from 'redaxios'
 
 const containerStyle = {
   borderTop: '1px solid rgb(55, 118, 28)',
@@ -12,26 +11,12 @@ export const Choose = memo(() => {
   const [data, setData] = useState([])
 
   const onClickLoad = useCallback(async () => {
-    let res
-    try {
-      res = await axios({
-        method: 'get',
-        url: 'https://spatialreference.org/crslist.json',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-    } catch (error) {
-      return await db.notifications.create({
-        data: createNotification({
-          title: `Error loading CRS`,
-          body: error.message,
-          intent: 'error',
-        }),
-      })
-    }
-    console.log('Choose, res:', res)
+    // dynamically import big json file crs.json
+    const { default: data } = await import('./crs.json')
+    setData(data)
   }, [])
+
+  console.log('Choose, data:', data)
 
   return (
     <div style={containerStyle}>
