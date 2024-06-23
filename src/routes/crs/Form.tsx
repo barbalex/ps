36c0e62321cf0417712dcf593b7f73492124fb46
Alexander Gabriel <alex.barbalex@gmail.com>
2024-06-23 12:07:17 +0200

    refactor

--- src/routes/crs/Form.tsx
+++ src/routes/crs/Form.tsx
@@ -1,49 +1,62 @@
-import { memo } from 'react'
-import { useOutletContext } from 'react-router-dom'
+import { memo, useCallback } from 'react'
+import { useLiveQuery } from 'electric-sql/react'
+import { useParams } from 'react-router-dom'
+import type { InputProps } from '@fluentui/react-components'
 
+import { useElectric } from '../../ElectricProvider.tsx'
 import { TextField } from '../../components/shared/TextField.tsx'
 import { TextArea } from '../../components/shared/TextArea.tsx'
 import { ComboboxFilteringOptions } from './Combobox/index.tsx'
+import { Loading } from '../../components/shared/Loading.tsx'
 
 import '../../form.css'
 
 // this form is rendered from a parent or outlet
-export const Component = memo(
-  ({
-    onChange: onChangeFromProps,
-    row: rowFromProps,
-    autoFocusRef,
-  }) => {
-    // beware: contextFromOutlet is undefined if not inside an outlet
-    const outletContext = useOutletContext()
-    const onChange = onChangeFromProps ?? outletContext?.onChange
-    const row = rowFromProps ?? outletContext?.row ?? {}
+export const Component = memo(({ autoFocusRef }) => {
+  const { crs_id } = useParams()
 
-    return (
-      <>
-        <ComboboxFilteringOptions autoFocus={!row.code} ref={autoFocusRef} />
-        <TextField
-          label="Code"
-          name="code"
-          type="code"
-          value={row.code ?? ''}
-          onChange={onChange}
-        />
-        <TextField
-          label="Name"
-          name="name"
-          type="name"
-          value={row.name ?? ''}
-          onChange={onChange}
-        />
-        <TextArea
-          label="Proj4 Value"
-          name="proj4"
-          type="proj4"
-          value={row.proj4 ?? ''}
-          onChange={onChange}
-        />
-      </>
-    )
-  },
-)
+  const { db } = useElectric()!
+  const { results: row } = useLiveQuery(
+    db.crs.liveUnique({ where: { crs_id } }),
+  )
+
+  const onChange = useCallback<InputProps['onChange']>(
+    (e, data) => {
+      const { name, value } = getValueFromChange(e, data)
+      db.crs.update({
+        where: { crs_id },
+        data: { [name]: value },
+      })
+    },
+    [db.crs, crs_id],
+  )
+
+  if (!row) return <Loading />
+
+  return (
+    <>
+      <ComboboxFilteringOptions autoFocus={!row.code} ref={autoFocusRef} />
+      <TextField
+        label="Code"
+        name="code"
+        type="code"
+        value={row.code ?? ''}
+        onChange={onChange}
+      />
+      <TextField
+        label="Name"
+        name="name"
+        type="name"
+        value={row.name ?? ''}
+        onChange={onChange}
+      />
+      <TextArea
+        label="Proj4 Value"
+        name="proj4"
+        type="proj4"
+        value={row.proj4 ?? ''}
+        onChange={onChange}
+      />
+    </>
+  )
+})
--- src/routes/crs/index.tsx
+++ src/routes/crs/index.tsx
@@ -1,13 +1,8 @@
-import { useCallback, useRef, memo } from 'react'
-import { useLiveQuery } from 'electric-sql/react'
+import { useRef, memo } from 'react'
 import { useParams } from 'react-router-dom'
-import type { InputProps } from '@fluentui/react-components'
 
-import { useElectric } from '../../ElectricProvider.tsx'
 import { TextFieldInactive } from '../../components/shared/TextFieldInactive.tsx'
-import { getValueFromChange } from '../../modules/getValueFromChange.ts'
 import { Header } from './Header.tsx'
-import { Loading } from '../../components/shared/Loading.tsx'
 import { Component as Form } from './Form.tsx'
 
 import '../../form.css'
@@ -17,30 +12,12 @@ export const Component = memo(() => {
 
   const autoFocusRef = useRef<HTMLInputElement>(null)
 
-  const { db } = useElectric()!
-  const { results: row } = useLiveQuery(
-    db.crs.liveUnique({ where: { crs_id } }),
-  )
-
-  const onChange = useCallback<InputProps['onChange']>(
-    (e, data) => {
-      const { name, value } = getValueFromChange(e, data)
-      db.crs.update({
-        where: { crs_id },
-        data: { [name]: value },
-      })
-    },
-    [db.crs, crs_id],
-  )
-
-  if (!row) return <Loading />
-
   return (
     <div className="form-outer-container">
       <Header autoFocusRef={autoFocusRef} />
       <div className="form-container">
-        <TextFieldInactive label="ID" name="crs_id" value={row.crs_id} />
-        <Form onChange={onChange} row={row} autoFocusRef={autoFocusRef} />
+        <TextFieldInactive label="ID" name="crs_id" value={crs_id} />
+        <Form autoFocusRef={autoFocusRef} />
       </div>
     </div>
   )
