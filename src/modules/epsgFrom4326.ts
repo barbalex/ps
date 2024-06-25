c01c56ab636dd3b52cee2f3ebefefb1486c2af5e
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2024-06-25 07:07:57 +0200

    work on map presentation crs

---
+++ src/modules/epsgFrom4326.ts
@@ -0,0 +1,28 @@
+import proj4 from 'proj4'
+
+// proj4.defs(
+//   'EPSG:2056',
+//   '+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs',
+// )
+proj4.defs('EPSG:4326', '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+
+const epsg4326to2056 = ({ x: xPassed, y: yPassed, project_id, db }) => {
+  // 1. get project.map_presentation_crs
+  const mapPresentationCrs = db.projects.findUnique({
+    where: { id: project_id },
+    select: { map_presentation_crs: true },
+  })
+  const crsCode = mapPresentationCrs.map_presentation_crs
+  // 2. get crs.proj4
+  const crs = db.crs.findFirst({
+    where: { code: crsCode },
+    select: { proj4: true },
+  })
+  proj4.defs(crs.code, crs.proj4)
+
+  const [x, y] = proj4('EPSG:4326', 'EPSG:2056', [+xPassed, +yPassed])
+  // round to the integer
+  return [parseInt(x, 10), parseInt(y, 10)]
+}
+
+export default epsg4326to2056
