5519b00853f95cabda1830e0c73dcf79f7b82112
Alexander Gabriel <alex.barbalex@gmail.com>
2024-06-28 11:10:11 +0200

    work on project crs

--- db/migrations/01-tables.sql
+++ db/migrations/01-tables.sql
@@ -1825,8 +1825,6 @@ CREATE TABLE crs(
 -- CREATE INDEX ON crs USING btree(crs_id);
 CREATE INDEX ON crs USING btree(account_id);
 
-CREATE INDEX ON crs USING btree(project_id);
-
 COMMENT ON TABLE crs IS 'List of crs. From: https://spatialreference.org/crslist.json. Can be inserted when configuring a project. We need the entire list because wfs/wms have a default crs that needs to be used for bbox calls. TODO: decide when to download the list.';
 
 COMMENT ON COLUMN crs.proj4 IS 'proj4 string for the crs. From (example): https://epsg.io/4326.proj4';
@@ -1835,19 +1833,20 @@ COMMENT ON COLUMN crs.proj4 IS 'proj4 string for the crs. From (example): https:
 -- same as crs - data will be copied from crs to project_crs
 CREATE TABLE project_crs(
   project_crs_id uuid PRIMARY KEY DEFAULT NULL,
-  crs_id uuid REFERENCES crs(crs_id) ON DELETE
-    DO NOTHING ON UPDATE CASCADE,
-    project_id uuid DEFAULT NULL REFERENCES projects(project_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    account_id uuid DEFAULT NULL REFERENCES accounts(account_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    code text DEFAULT NULL,
-    name text DEFAULT NULL,
-    proj4 text DEFAULT NULL,
-    label_replace_by_generated_column text DEFAULT NULL
+  crs_id uuid REFERENCES crs(crs_id) ON DELETE NO action ON UPDATE CASCADE,
+  project_id uuid DEFAULT NULL REFERENCES projects(project_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  account_id uuid DEFAULT NULL REFERENCES accounts(account_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  code text DEFAULT NULL,
+  name text DEFAULT NULL,
+  proj4 text DEFAULT NULL,
+  label_replace_by_generated_column text DEFAULT NULL
 );
 
 CREATE INDEX ON project_crs USING btree(account_id);
 
 CREATE INDEX ON project_crs USING btree(project_id);
+
+CREATE INDEX ON project_crs USING btree(code);
 
 COMMENT ON TABLE project_crs IS 'List of crs used in a project. Can be set when configuring a project. Values copied from crs table.';
 
@@ -1949,4 +1948,6 @@ ALTER TABLE charts ENABLE electric;
 ALTER TABLE chart_subjects ENABLE ELECTRIC;
 
 ALTER TABLE crs ENABLE electric;
+
+ALTER TABLE project_crs ENABLE electric;
 
