a46f4e79103813f956a1680bbe14fcebe1d80856
Alexander Gabriel <alex.barbalex@gmail.com>
2024-06-28 11:00:11 +0200

    work on project crs

---
+++ src/routes/projectCrss/Info.tsx
@@ -0,0 +1,17 @@
+import { memo } from 'react'
+
+import './info.css'
+
+const containerStyle = {
+  padding: '5px 10px',
+  backgroundColor: 'rgba(103, 216, 101, 0.07)',
+}
+
+export const Info = memo(() => (
+  <div style={containerStyle} tabIndex={-1}>
+    <p>You can add multiple CRS.</p>
+    <p>One of them can be set as the map presentation crs.</p>
+    <p>It will be used to show coordinates in the map.</p>
+    <p>Users can choose in the map one of the CRS'es you add here.</p>
+  </div>
+))
