ed9977e71388966459434454311fdf1c6ff18aac
Alexander Gabriel <alex.barbalex@gmail.com>
2024-06-28 21:23:16 +0200

    optimize

--- src/components/Map/Info/Drawer/Layer.tsx
+++ src/components/Map/Info/Drawer/Layer.tsx
@@ -12,7 +12,6 @@ const titleStyle = {
 const propertyListStyle = {
   display: 'grid',
   gridTemplateColumns: 'minmax(100px, 30%) 1fr',
-  // gap: 5,
 }
 const textStyle = {
   padding: 5,
@@ -29,7 +28,7 @@ const labelStyle = {
 }
 
 export const Layer = memo(({ layerData }) => {
-  const { label, properties = [], html, json, text } = layerData
+  const { label, properties, html, json, text } = layerData
   console.log('Info Drawer Layer, layerData:', layerData)
 
   if (text) {
@@ -63,7 +62,7 @@ export const Layer = memo(({ layerData }) => {
     <div style={containerStyle}>
       <div style={titleStyle}>{label}</div>
       <div style={propertyListStyle}>
-        {properties.map((p, i) => {
+        {(properties ?? []).map((p, i) => {
           const key = p[0]
           const value = p[1]
           const backgroundColor = i % 2 === 0 ? 'rgba(0, 0, 0, 0.05)' : 'unset'
