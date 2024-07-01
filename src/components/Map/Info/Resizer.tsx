555de9607a87d9aa1653bc3ae9a83b53fed94a9a
Alexander Gabriel <alex.barbalex@gmail.com>
2024-07-01 14:56:25 +0200

    work on map info

--- src/components/Map/Info/Resizer.tsx
+++ src/components/Map/Info/Resizer.tsx
@@ -6,6 +6,7 @@ export const Resizer = memo(({ startResizing, isResizing }) => (
   <div
     className="map-info-resizer"
     onMouseDown={startResizing}
+    // need to set this because the element looses the hover on drag
     style={{
       ...(isResizing ? { backgroundColor: 'rgba(38, 82, 37, 0.9)' } : {}),
     }}
