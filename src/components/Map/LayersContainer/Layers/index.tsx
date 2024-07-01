a472bdd05ca479cc22e585fd5221c7ed2187f5cf
Alexander Gabriel <alex.barbalex@gmail.com>
2024-07-01 18:49:30 +0200

    work on layers presentation

--- src/components/Map/LayersContainer/Layers/index.tsx
+++ src/components/Map/LayersContainer/Layers/index.tsx
@@ -1,19 +1,31 @@
 import { memo } from 'react'
 import { useLiveQuery } from 'electric-sql/react'
-import { useCorbado } from '@corbado/react'
+// import { useCorbado } from '@corbado/react'
+import { useParams } from 'react-router-dom'
 
 import { useElectric } from '../../../../ElectricProvider.tsx'
 import { ErrorBoundary } from '../../../shared/ErrorBoundary.tsx'
 import { FormHeader } from '../../../FormHeader/index.tsx'
 
 export const Layers = memo(({ isNarrow }) => {
-  const { user: authUser } = useCorbado()
+  const { project_id } = useParams()
+  // const { user: authUser } = useCorbado()
 
   const { db } = useElectric()!
-  // query all active layers - tile or vector
-  const { results: appState } = useLiveQuery(
-    db.app_states.liveFirst({ where: { user_email: authUser?.email } }),
+  // 1. list all layers (own, tile, vector)
+  const { results: tileLayers = [] } = useLiveQuery(
+    db.tile_layers.liveMany({
+      where: { ...(project_id ? { project_id } : {}) },
+    }),
   )
+  const { results: vectorLayers = [] } = useLiveQuery(
+    db.vector_layers.liveMany({
+      where: { ...(project_id ? { project_id } : {}) },
+    }),
+  )
+  // 2. when one is set active, add layer_presentations for it
+
+  console.log('Map Layers:', { tileLayers, vectorLayers })
 
   return (
     <ErrorBoundary>
@@ -27,7 +39,14 @@ export const Layers = memo(({ isNarrow }) => {
           title="Layers"
           titleMarginLeft={isNarrow ? 34 : undefined}
         />
-        layers
+        <h2>Tile Layers</h2>
+        {tileLayers?.map((l) => (
+          <div key={l.tile_layer_id}>{l.label}</div>
+        ))}
+        <h2>Vector Layers</h2>
+        {vectorLayers?.map((l) => (
+          <div key={l.vector_layer_id}>{l.label}</div>
+        ))}
       </div>
     </ErrorBoundary>
   )
