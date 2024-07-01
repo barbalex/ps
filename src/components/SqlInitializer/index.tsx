a6dfeac6626b2cea9b2962d711ba79669a141255
Alexander Gabriel <alex.barbalex@gmail.com>
2024-07-01 19:15:05 +0200

    work on layers presentation

--- src/components/SqlInitializer/index.tsx
+++ src/components/SqlInitializer/index.tsx
@@ -46,6 +46,7 @@ import { generateChartSubjectLabel } from './sql/chartSubjects.ts'
 import { generateOccurrenceImportLabel } from './sql/occurrenceImports.ts'
 // ISSUE: how to create v7 uuids? https://github.com/rhashimoto/wa-sqlite/discussions/169, https://github.com/craigpastro/sqlite-uuidv7/issues/3
 // import { generateVectorLayerTriggers } from './labelGenerators/vectorLayers'
+// import { generateTileLayerTriggers } from './labelGenerators/tileLayers'
 import { seedTestData } from './seedTestData.ts'
 
 // how to get work:
@@ -104,6 +105,7 @@ export const SqlInitializer = () => {
       await generateChartSubjectLabel(db)
       await generateOccurrenceImportLabel(db)
       // await generateVectorLayerTriggers(db)
+      // await generateTileLayerTriggers(db)
       // console.log('generated vector layer triggers')
       await seedTestData(db)
     }
