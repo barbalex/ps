fab3bf49422b62249fb342b37e39f601b43dc7ff
Alexander Gabriel <alex.barbalex@gmail.com>
2024-07-02 13:45:30 +0200

    work on layers presentation

--- src/components/Map/LayersContainer/Layers/index.tsx
+++ src/components/Map/LayersContainer/Layers/index.tsx
@@ -2,10 +2,25 @@ import { memo } from 'react'
 import { useLiveQuery } from 'electric-sql/react'
 // import { useCorbado } from '@corbado/react'
 import { useParams } from 'react-router-dom'
+import { Checkbox } from '@fluentui/react-components'
 
 import { useElectric } from '../../../../ElectricProvider.tsx'
 import { ErrorBoundary } from '../../../shared/ErrorBoundary.tsx'
 import { FormHeader } from '../../../FormHeader/index.tsx'
+
+const formStyle = {
+  paddingLeft: 10,
+  paddingRight: 10,
+  paddingBottom: 10,
+  // enable scrolling
+  overflowY: 'auto',
+  height: '100%',
+}
+const layerListStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+}
 
 export const Layers = memo(({ isNarrow }) => {
   const { project_id } = useParams()
@@ -25,19 +40,41 @@ export const Layers = memo(({ isNarrow }) => {
   const { results: vectorLayers = [] } = useLiveQuery(
     db.vector_layers.liveMany({
       where,
+      // TODO: this only returns vector layers that have a presentation
+      // https://github.com/electric-sql/electric/issues/1417
       // include: { layer_presentations: true },
-      // include: { vector_layer_displays: true },
+    }),
+  )
+  // fetch all layer_presentations for the vector layers
+  const { results: layerPresentations = [] } = useLiveQuery(
+    db.layer_presentations.liveMany({
+      where: {
+        OR: [
+          {
+            vector_layer_id: { in: vectorLayers.map((l) => l.vector_layer_id) },
+          },
+          { tile_layer_id: { in: tileLayers.map((l) => l.tile_layer_id) } },
+        ],
+      },
     }),
   )
   // 2. when one is set active, add layer_presentations for it
 
-  console.log('Map Layers:', { tileLayers, vectorLayers, where })
+  console.log('Map Layers:', {
+    tileLayers,
+    vectorLayers,
+    where,
+    layerPresentations,
+  })
 
   return (
     <ErrorBoundary>
       <div
         style={{
           width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
           ...(isNarrow ? { marginTop: 5 } : { marginRight: 5 }),
         }}
       >
@@ -45,15 +82,21 @@ export const Layers = memo(({ isNarrow }) => {
           title="Layers"
           titleMarginLeft={isNarrow ? 34 : undefined}
         />
-        <h2>Active Layers</h2>
-        <h2>Tile Layers</h2>
-        {tileLayers?.map((l) => (
-          <div key={l.tile_layer_id}>{l.label}</div>
-        ))}
-        <h2>Vector Layers</h2>
-        {vectorLayers?.map((l) => (
-          <div key={l.vector_layer_id}>{l.label}</div>
-        ))}
+        <div style={formStyle}>
+          <h2>Active Layers</h2>
+          <h2>Tile Layers</h2>
+          <div style={layerListStyle}>
+            {tileLayers?.map((l) => (
+              <Checkbox key={l.tile_layer_id} size="large" label={l.label} />
+            ))}
+          </div>
+          <h2>Vector Layers</h2>
+          <div style={layerListStyle}>
+            {vectorLayers?.map((l) => (
+              <Checkbox key={l.vector_layer_id} size="large" label={l.label} />
+            ))}
+          </div>
+        </div>
       </div>
     </ErrorBoundary>
   )
