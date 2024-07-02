e564d8c7d12e60356d074d4e91a10f36bd0bd782
Alexander Gabriel <alex.barbalex@gmail.com>
2024-07-02 17:12:30 +0200

    work on layers presentation

--- src/modules/sqlFromFilter.ts
+++ src/modules/sqlFromFilter.ts
@@ -1,9 +1,7 @@
 const sqlFromFilterObject = ({ filter, columnPrefix }) => {
-  console.log('sqlFromFilterObject, filter:', filter)
   let sql = ``
   for (const key in filter) {
     const value = filter[key]
-    console.log('sqlFromFilterObject 2', { key, value })
     // value can be a basic value or an object using contains
     if (value?.contains) {
       sql += `${columnPrefix ?? ''}${key} LIKE '%${value.contains}%'`
@@ -16,8 +14,9 @@ const sqlFromFilterObject = ({ filter, columnPrefix }) => {
 
 // receives a filter, for instance: appState?.filter_tile_layers
 // returns a sql string for a where clause
+// if this were a server, we would need to separate args and pass both the sql and the args back
+// but: the worst that can happen is that a malicious user hacks their own data...
 export const sqlFromFilter = ({ filter, columnPrefix }) => {
-  console.log('sqlFromFilter, filter:', filter)
   if (!filter) return undefined
 
   if (filter?.label) {
