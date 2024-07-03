73a5e969177b8fb879ebcd538b24fe80175f3ea9
Alexander Gabriel <alex.barbalex@gmail.com>
2024-07-03 19:59:12 +0200

    work on sections

--- src/components/Map/LayersContainer/Layers/index.tsx
+++ src/components/Map/LayersContainer/Layers/index.tsx
@@ -10,13 +10,23 @@ import { FormHeader } from '../../../FormHeader/index.tsx'
 import { createLayerPresentation } from '../../../../modules/createRows.ts'
 import { ActiveLayers } from './Actives/index.tsx'
 
+const containerStyle = {
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-start',
+}
+
 const formStyle = {
-  paddingLeft: 10,
-  paddingRight: 10,
-  paddingBottom: 10,
   // enable scrolling
   overflowY: 'auto',
   height: '100%',
+}
+const sectionStyle = {
+  paddingLeft: 10,
+  paddingRight: 10,
+  paddingBottom: 10,
 }
 const layerListStyle = {
   display: 'flex',
@@ -124,86 +134,87 @@ export const Layers = memo(({ isNarrow }) => {
 
   return (
     <ErrorBoundary>
-      <div
-        style={{
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          ...(isNarrow ? { marginTop: 5 } : { marginRight: 5 }),
-        }}
-      >
+      <div style={containerStyle}>
         <FormHeader
           title="Layers"
           titleMarginLeft={isNarrow ? 34 : undefined}
         />
         <div style={formStyle}>
-          <ActiveLayers />
-          <h2>Tiled</h2>
-          <div style={layerListStyle}>
-            {tiles.length ? (
-              tiles?.map((l) => (
-                <Checkbox
-                  key={l.tile_layer_id}
-                  size="large"
-                  label={l.label}
-                  // checked if layer has an active presentation
-                  checked={
-                    !!layerPresentations.find(
-                      (lp) => lp.tile_layer_id === l.tile_layer_id && lp.active,
-                    )
-                  }
-                  onChange={() => onChangeNonActive(l)}
-                />
-              ))
-            ) : (
-              <p>No tile layers</p>
-            )}
-          </div>
-          <h2>Vectors</h2>
-          <div style={layerListStyle}>
-            {vectors.length ? (
-              vectors.map((l) => (
-                <Checkbox
-                  key={l.vector_layer_id}
-                  size="large"
-                  label={l.label}
-                  // checked if layer has an active presentation
-                  checked={
-                    !!layerPresentations.find(
-                      (lp) =>
-                        lp.vector_layer_id === l.vector_layer_id && lp.active,
-                    )
-                  }
-                  onChange={() => onChangeNonActive(l)}
-                />
-              ))
-            ) : (
-              <p>No vector layers</p>
-            )}
-          </div>
-          <h2>Own</h2>
-          <div style={layerListStyle}>
-            {own.length ? (
-              own.map((l) => (
-                <Checkbox
-                  key={l.vector_layer_id}
-                  size="large"
-                  label={l.label}
-                  // checked if layer has an active presentation
-                  checked={
-                    !!layerPresentations.find(
-                      (lp) =>
-                        lp.vector_layer_id === l.vector_layer_id && lp.active,
-                    )
-                  }
-                  onChange={() => onChangeNonActive(l)}
-                />
-              ))
-            ) : (
-              <p>No own layers</p>
-            )}
-          </div>
+          <section style={sectionStyle}>
+            <ActiveLayers />
+          </section>
+          <section style={sectionStyle}>
+            <h2>Tiled</h2>
+            <div style={layerListStyle}>
+              {tiles.length ? (
+                tiles?.map((l) => (
+                  <Checkbox
+                    key={l.tile_layer_id}
+                    size="large"
+                    label={l.label}
+                    // checked if layer has an active presentation
+                    checked={
+                      !!layerPresentations.find(
+                        (lp) =>
+                          lp.tile_layer_id === l.tile_layer_id && lp.active,
+                      )
+                    }
+                    onChange={() => onChangeNonActive(l)}
+                  />
+                ))
+              ) : (
+                <p>No tile layers</p>
+              )}
+            </div>
+          </section>
+          <section style={sectionStyle}>
+            <h2>Vectors</h2>
+            <div style={layerListStyle}>
+              {vectors.length ? (
+                vectors.map((l) => (
+                  <Checkbox
+                    key={l.vector_layer_id}
+                    size="large"
+                    label={l.label}
+                    // checked if layer has an active presentation
+                    checked={
+                      !!layerPresentations.find(
+                        (lp) =>
+                          lp.vector_layer_id === l.vector_layer_id && lp.active,
+                      )
+                    }
+                    onChange={() => onChangeNonActive(l)}
+                  />
+                ))
+              ) : (
+                <p>No vector layers</p>
+              )}
+            </div>
+          </section>
+          <section style={sectionStyle}>
+            <h2>Own</h2>
+            <div style={layerListStyle}>
+              {own.length ? (
+                own.map((l) => (
+                  <Checkbox
+                    key={l.vector_layer_id}
+                    size="large"
+                    label={l.label}
+                    // checked if layer has an active presentation
+                    checked={
+                      !!layerPresentations.find(
+                        (lp) =>
+                          lp.vector_layer_id === l.vector_layer_id && lp.active,
+                      )
+                    }
+                    onChange={() => onChangeNonActive(l)}
+                  />
+                ))
+              ) : (
+                <p>No own layers</p>
+              )}
+            </div>
+          </section>
         </div>
       </div>
     </ErrorBoundary>
