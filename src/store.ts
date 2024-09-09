e421c08bf22e6c648cfb5f74aa82061b8c1e4566
Alexander Gabriel <alex.barbalex@gmail.com>
2024-09-09 18:45:37 +0200

    comment

--- src/store.ts
+++ src/store.ts
@@ -1,19 +1,20 @@
 // import { atom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
 
-export const userIdAtom = atomWithStorage('userIdAtom', null)
+export const userIdAtom = atomWithStorage('userIdAtom', null) // TODO: update usage
 
-export const userEmailAtom = atomWithStorage('userEmailAtom', null)
+export const userEmailAtom = atomWithStorage('userEmailAtom', null) // TODO: update usage
 
 export const mapMaximizedAtom = atomWithStorage('mapMaximizedAtom', false)
 
 export const designingAtom = atomWithStorage('designingAtom', false)
 
-export const navsOverflowingAtom = atomWithStorage('navsOverflowingAtom', false)
+export const navsOverflowingAtom = atomWithStorage('navsOverflowingAtom', false) // TODO: update usage
 
 export const breadcrumbsOverflowingAtom = atomWithStorage(
+  // TODO: update usage
   'breadcrumbsOverflowingAtom',
   false,
 )
 
-export const tabsAtom = atomWithStorage('tabsAtom', ['tree', 'data'])
+export const tabsAtom = atomWithStorage('tabsAtom', ['tree', 'data']) // TODO: update usage
