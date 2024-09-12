0182fe8e472b41c9939d8753388969d5b7c3d8ba
Alexander Gabriel <alex.barbalex@gmail.com>
2024-09-12 13:57:05 +0200

    clean

--- src/modules/tree/addOpenNodes.ts
+++ src/modules/tree/addOpenNodes.ts
@@ -7,17 +7,11 @@ interface Props {
 }
 
 export const addOpenNodes = ({ nodes = [] }: Props): void => {
-  console.log('addOpenNodes 1, nodes passed in:', nodes)
   if (!nodes.length) return
 
   const openNodes = store.get(treeOpenNodesAtom)
-  console.log('addOpenNodes 2, openNodes:', openNodes)
   const nodesToAdd = nodes.filter((node) => !isNodeOpen({ node, openNodes }))
-  console.log('addOpenNodes 3, nodesToAdd:', nodesToAdd)
   if (!nodesToAdd.length) return
-
-  const newNodes = [...openNodes, ...nodesToAdd]
-  console.log('addOpenNodes 4, newNodes:', newNodes)
 
   store.set(treeOpenNodesAtom, (openNodes) => [...openNodes, ...nodesToAdd])
 }
