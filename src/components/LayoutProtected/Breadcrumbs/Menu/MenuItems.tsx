4cefb8de05fe7bb5ad7ac6a91d64e1dfbae665bf
Alexander Gabriel <alex.barbalex@gmail.com>
2024-09-12 14:36:44 +0200

    catch

--- src/components/LayoutProtected/Breadcrumbs/Menu/MenuItems.tsx
+++ src/components/LayoutProtected/Breadcrumbs/Menu/MenuItems.tsx
@@ -1,7 +1,12 @@
 import { MenuItem } from './MenuItem.tsx'
 
-export const MenuItems = ({ navs }) => {
-  return navs.map(({ path, text }) => (
-    <MenuItem key={path} path={path} text={text} />
+// adding the index to the key as somehow the id in the path
+// would sometimes be returned as undefined
+export const MenuItems = ({ navs }) =>
+  navs.map(({ path, text }, i) => (
+    <MenuItem
+      key={`${path}/${i}`}
+      path={path}
+      text={text}
+    />
   ))
-}
