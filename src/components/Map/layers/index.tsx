1123a82b9a46a5ac1fc41aaa87175ca863cc0ca3
Alexander Gabriel <alex.barbalex@gmail.com>
2024-09-27 17:37:52 +0200

    modularize

--- src/components/Map/layers/index.tsx
+++ src/components/Map/layers/index.tsx
@@ -11,30 +11,8 @@ import {
 import { OsmColor } from './OsmColor.tsx'
 import { WmsLayerComponent } from './WmsLayer/index.tsx'
 import { VectorLayerChooser } from './VectorLayer/index.tsx'
-import { Places1 } from './TableLayers/Places1.tsx'
-import { Places2 } from './TableLayers/Places2.tsx'
-import { Checks1 } from './TableLayers/Checks1.tsx'
-import { Checks2 } from './TableLayers/Checks2.tsx'
-import { Actions1 } from './TableLayers/Actions1.tsx'
-import { Actions2 } from './TableLayers/Actions2.tsx'
-import { OccurrencesAssigned1 } from './TableLayers/OccurrencesAssigned1.tsx'
-import { OccurrencesAssigned2 } from './TableLayers/OccurrencesAssigned2.tsx'
-import { OccurrencesToAssess } from './TableLayers/OccurrencesToAssess.tsx'
-import { OccurrencesNotToAssign } from './TableLayers/OccurrencesNotToAssign.tsx'
+import { tableLayerToComponent } from './tableLayerToComponent.ts'
 import { mapLayerSortingAtom } from '../../../store.ts'
-
-const tableLayerToComponent = {
-  places1: Places1,
-  places2: Places2,
-  checks1: Checks1,
-  checks2: Checks2,
-  actions1: Actions1,
-  actions2: Actions2,
-  occurrences_assigned1: OccurrencesAssigned1,
-  occurrences_assigned2: OccurrencesAssigned2,
-  occurrences_to_assess: OccurrencesToAssess,
-  occurrences_not_to_assign: OccurrencesNotToAssign,
-}
 
 const paneBaseIndex = 400 // was: 200. then wfs layers covered lower ones
 
