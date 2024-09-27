5b235ef02024ac5d841948bc9d4bbf2b8e857218
Alexander Gabriel <alex.barbalex@gmail.com>
2024-09-27 17:03:29 +0200

    work on vector legends

--- src/components/Map/LeftMenuDrawer/Legends/VectorLegend.tsx
+++ src/components/Map/LeftMenuDrawer/Legends/VectorLegend.tsx
@@ -1,4 +1,8 @@
 import { memo } from 'react'
+// idea:
+// fetch all vector_layer_displays for this layer
+// use a leaflet map to display for every vector_layer_display:
+// a rectangle, line, point
 
 export const VectorLegend = memo(({ layer }) => {
   return <>{`TODO: Vector layer '${layer.label}'`}</>
