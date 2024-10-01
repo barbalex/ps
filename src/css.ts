bbefecffd6e2360680a6303e172f07292684952d
Alexander Gabriel <alex.barbalex@gmail.com>
2024-10-01 11:13:31 +0200

    prepare

--- src/css.ts
+++ src/css.ts
@@ -1,23 +1,8 @@
 import { createHooks } from '@css-hooks/react'
-import { recommended } from '@css-hooks/recommended'
 
-export const { styleSheet, css } = createHooks({
-  hooks: {
-    // implement breakpoints and color schemes later
-    ...recommended({
-      // breakpoints: ["500px", "1000px"],
-      // colorSchemes: ["dark", "light"],
-      pseudoClasses: [':hover', ':focus', ':active', ':disabled'],
-    }),
-    '&:not(:first-of-type)': '&:not(:first-of-type)',
-    '&:nth-child(odd)': '&:nth-child(odd)',
-    span: 'span',
-    'path:nth-of-type(2)': 'path:nth-of-type(2)',
-    '&:before': '&:before',
-    '&:first-of-type': '&:first-of-type',
-    '&:focus-visible': '&:focus-visible',
-  },
-})
+export const {styleSheet,  on, and, or, not} = createHooks('&:hover', '&:focus', '&:not(:first-of-type)', '&:nth-child(odd)', '&:focus-visible')
+
+
 
 /* Hooks created:
  - @media (width < 500px)
