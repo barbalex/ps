6251258430dfeade74c689bf412daafa066a7056
Alexander Gabriel <alex.barbalex@gmail.com>
2024-10-03 12:28:27 +0200

    work on vl properties

--- src/components/Map/VectorLayersPropertiesProvider.tsx
+++ src/components/Map/VectorLayersPropertiesProvider.tsx
@@ -1,47 +1,74 @@
-import { memo, useEffect, useState, useMemo } from 'react'
+import { memo, useEffect, useMemo } from 'react'
 import { useLiveQuery } from 'electric-sql/react'
-import { usePrevious } from '@uidotdev/usehooks'
+import { useParams } from 'react-router-dom'
+import isEqual from 'lodash/isEqual'
 
 import { useElectric } from '../../ElectricProvider.tsx'
 
 export const VectorLayersPropertiesProvider = memo(() => {
+  const { project_id = '99999999-9999-9999-9999-999999999999' } = useParams()
   const { db } = useElectric()!
-  const [keys, setKeys] = useState([])
-  const previousKeys = usePrevious(keys)
 
-  const {
-    results: placesData = [],
-    error,
-    updatedAt,
-  } = useLiveQuery(
+  // get vector_layers
+  const { results: vectorLayers = [] } = useLiveQuery(
+    db.vector_layers.liveMany({
+      where: { project_id },
+      select: { vector_layer_id: true, properties: true, type: true },
+    }),
+  )
+  const { results: subprojects = [] } = useLiveQuery(
+    db.subprojects.liveMany({
+      where: { project_id },
+    }),
+  )
+  const { results: placesData = [] } = useLiveQuery(
     db.places.liveMany({
+      where: {
+        subproject_id: {
+          in: subprojects.map((subproject) => subproject.subproject_id),
+        },
+      },
       select: { data: true },
     }),
   )
-  const currentKeys = useMemo(() => [], [])
-  for (const place of placesData) {
-    currentKeys.push(...Object.keys(place.data))
-  }
-  const currentUniqueKeys = useMemo(
-    () => [...new Set(currentKeys)],
-    [currentKeys],
-  )
-  // console.log('VectorLayersPropertiesProvider', {
-  //   placesData,
-  //   error,
-  //   updatedAt,
-  //   keys,
-  //   uniqueKeys,
-  // })
-  console.log('VectorLayersPropertiesProvider, keys:', currentUniqueKeys)
+  const placesProperties = useMemo(() => {
+    const keys = []
+    for (const place of placesData) {
+      const myKeys = Object.keys(place.data)
+      for (const key of myKeys) {
+        if (!keys.includes(key)) keys.push(key)
+      }
+    }
+    return keys
+  }, [placesData])
+
+  // get vector_layers.properties
+
+  console.log('VectorLayersPropertiesProvider', {
+    vectorLayers,
+    subprojects,
+    placesData,
+    placesProperties,
+  })
+
   // when a key inside places.data changes, update vector_layers.properties:
   // set it as an array of places.data's keys
   useEffect(() => {
-    if (!currentUniqueKeys.length) return
-    if (currentUniqueKeys === previousKeys) return
-    setKeys(currentUniqueKeys)
-    console.log('VectorLayersPropertiesProvider.useEffect, got data')
-  }, [currentUniqueKeys, currentUniqueKeys.length, previousKeys])
+    if (!project_id) return
+    // TODO: loop all vector_layers
+    // if vector_layer.type includes 'places' and vector_layer.properties is not equal to placesProperties
+    // update vector_layer.properties to placesProperties
+    for (const vectorLayer of vectorLayers) {
+      if (vectorLayer.type.includes('places')) {
+        if (!isEqual(vectorLayer.properties, placesProperties)) {
+          db.vector_layers.update({
+            where: { vector_layer_id: vectorLayer.vector_layer_id },
+            data: { properties: placesProperties },
+          })
+        }
+      }
+    }
+  }, [db.vector_layers, placesProperties, project_id, vectorLayers])
 
   return null
 })
