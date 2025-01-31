47bdf824da8e6503a22e884d6d1c05f0bce5dd71
Alexander Gabriel <alex.barbalex@gmail.com>
2025-01-31 18:43:11 +0100

    migrate

--- src/routes/occurrenceImports.tsx
+++ src/routes/occurrenceImports.tsx
@@ -14,28 +14,34 @@ export const Component = memo(() => {
   const { subproject_id } = useParams()
 
   const db = usePGlite()
-  const { results: occurrenceImports = [] } = useLiveQuery(
-    db.occurrence_imports.liveMany({
-      where: { subproject_id },
-      orderBy: { label: 'asc' },
-    }),
+
+  const result = useLiveQuery(
+    `SELECT * FROM occurrence_imports WHERE subproject_id = $1 order by label asc`,
+    [subproject_id],
   )
+  const occurrenceImports = result?.rows ?? []
 
   const add = useCallback(async () => {
     const data = createOccurrenceImport({ subproject_id })
-    await db.occurrence_imports.create({ data })
+    const columns = Object.keys(data).join(',')
+    const values = Object.values(data)
+    const sql = `insert into occurrence_imports (${columns}) values ($1)`
+    await db.query(sql, values)
     navigate({
       pathname: data.occurrence_import_id,
       search: searchParams.toString(),
     })
-  }, [db.occurrence_imports, navigate, searchParams, subproject_id])
+  }, [db, navigate, searchParams, subproject_id])
 
   return (
     <div className="list-view">
       <ListViewHeader
-        title="Occurrence imports"
+        namePlural="Occurrence imports"
+        nameSingular="occurrence import"
+        tableName="occurrence_imports"
+        isFiltered={false}
+        countFiltered={occurrenceImports.length}
         addRow={add}
-        tableName="occurrence import"
       />
       <div className="list-container">
         {occurrenceImports.map(({ occurrence_import_id, label }) => (
