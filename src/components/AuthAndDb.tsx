705490b89f54a9446409fc206ea444405e4c8449
Alexander Gabriel <alex.barbalex@gmail.com>
2025-02-01 22:45:06 +0100

    migrate

--- src/components/AuthAndDb.tsx
+++ src/components/AuthAndDb.tsx
@@ -1,6 +1,5 @@
 import { memo, Suspense } from 'react'
 import { CorbadoProvider } from '@corbado/react'
-import { usePGlite } from '@electric-sql/pglite-react'
 
 import { SqlInitializer } from './SqlInitializer/index.tsx'
 // import { Syncer } from './Syncer.tsx'
@@ -11,8 +10,6 @@ import { Layout } from './LayoutProtected/index.tsx'
 const CORBADO_PROJECT_ID = import.meta.env.ELECTRIC_CORBADO_PROJECT_ID
 
 export const AuthAndDb = memo(() => {
-  const db = usePGlite()
-
   return (
     <CorbadoProvider
       projectId={CORBADO_PROJECT_ID}
