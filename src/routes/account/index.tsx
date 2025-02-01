35f642270e787683abd5297cec47b2cf12a1be31
Alexander Gabriel <alex.barbalex@gmail.com>
2025-02-01 12:38:44 +0100

    migrate

--- src/routes/account/index.tsx
+++ src/routes/account/index.tsx
@@ -19,6 +19,7 @@ export const Component = memo(() => {
   const autoFocusRef = useRef<HTMLInputElement>(null)
 
   const db = usePGlite()
+  // TODO: Uncaught (in promise) error: could not determine data type of parameter $2
   const result = useLiveQuery(`SELECT * FROM accounts WHERE account_id = $1`, [
     account_id,
   ])
