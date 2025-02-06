bc418e5fe50f41f6a9c06dc7a0b81c5250e6fd1b
Alexander Gabriel <alex.barbalex@gmail.com>
2025-02-06 16:22:03 +0100

    migrate

--- src/routes/person/index.tsx
+++ src/routes/person/index.tsx
@@ -1,8 +1,8 @@
 import { useCallback, useRef, memo } from 'react'
-import { useLiveQuery } from '@electric-sql/pglite-react'
 import { useParams } from 'react-router-dom'
 import type { InputProps } from '@fluentui/react-components'
 import { usePGlite } from '@electric-sql/pglite-react'
+import { useLiveQuery } from '@electric-sql/pglite-react'
 
 import { TextFieldInactive } from '../../components/shared/TextFieldInactive.tsx'
 import { getValueFromChange } from '../../modules/getValueFromChange.ts'
@@ -18,19 +18,21 @@ export const Component = memo(() => {
   const autoFocusRef = useRef<HTMLInputElement>(null)
 
   const db = usePGlite()
-  const { results: row } = useLiveQuery(
-    db.persons.liveUnique({ where: { person_id } }),
-  )
+
+  const result = useLiveQuery(`SELECT * FROM persons WHERE person_id = $1`, [
+    person_id,
+  ])
+  const row = result?.rows?.[0]
 
   const onChange = useCallback<InputProps['onChange']>(
     (e, data) => {
       const { name, value } = getValueFromChange(e, data)
-      db.persons.update({
-        where: { person_id },
-        data: { [name]: value },
-      })
+      db.query(`UPDATE persons SET ${name} = $1 WHERE person_id = $2`, [
+        value,
+        person_id,
+      ])
     },
-    [db.persons, person_id],
+    [db, person_id],
   )
 
   if (!row) return <Loading />
@@ -39,8 +41,16 @@ export const Component = memo(() => {
     <div className="form-outer-container">
       <Header autoFocusRef={autoFocusRef} />
       <div className="form-container">
-        <TextFieldInactive label="ID" name="person_id" value={row.person_id} />
-        <Form onChange={onChange} row={row} autoFocusRef={autoFocusRef} />
+        <TextFieldInactive
+          label="ID"
+          name="person_id"
+          value={row.person_id}
+        />
+        <Form
+          onChange={onChange}
+          row={row}
+          autoFocusRef={autoFocusRef}
+        />
       </div>
     </div>
   )
