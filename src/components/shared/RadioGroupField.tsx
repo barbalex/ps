8f6133c96baffd667a290778cf6c5efac946578e
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-02-22 23:35:36 +0100

    optimize

--- src/components/shared/RadioGroupField.tsx
+++ src/components/shared/RadioGroupField.tsx
@@ -1,69 +1,68 @@
-import { memo, forwardRef, useCallback } from 'react'
+import { memo, useCallback } from 'react'
 import { Field, RadioGroup, Radio } from '@fluentui/react-components'
 import { useResizeDetector } from 'react-resize-detector'
 
-export const RadioGroupField = memo(
-  forwardRef((props, ref) => {
-    const {
-      name,
-      label,
-      list = [],
-      value,
-      onChange: onChangePassed,
-      validationMessage,
-      validationState = 'none',
-      autoFocus,
-      disabled = false,
-      replaceUnderscoreInLabel = false,
-    } = props
+export const RadioGroupField = memo((props) => {
+  const {
+    name,
+    label,
+    list = [],
+    value,
+    onChange: onChangePassed,
+    validationMessage,
+    validationState = 'none',
+    autoFocus,
+    disabled = false,
+    replaceUnderscoreInLabel = false,
+    ref,
+  } = props
 
-    const { width, ref: widthRef } = useResizeDetector({
-      handleHeight: false,
-      refreshMode: 'debounce',
-      refreshRate: 100,
-      refreshOptions: { leading: false, trailing: true },
-    })
+  const { width, ref: widthRef } = useResizeDetector({
+    handleHeight: false,
+    refreshMode: 'debounce',
+    refreshRate: 100,
+    refreshOptions: { leading: false, trailing: true },
+  })
 
-    const verticalLayout = !!width && width < 500
+  const verticalLayout = !!width && width < 500
 
-    const onClick = useCallback(
-      (e) => {
-        const valueChoosen = e.target.value
-        // if valueChoosen equals value, set value to null
-        // else set value to valueChoosen
-        onChangePassed(e, {
-          value: valueChoosen === value ? null : valueChoosen,
-        })
-      },
-      [onChangePassed, value],
-    )
+  const onClick = useCallback(
+    (e) => {
+      const valueChoosen = e.target.value
+      // if valueChoosen equals value, set value to null
+      // else set value to valueChoosen
+      onChangePassed(e, {
+        value: valueChoosen === value ? null : valueChoosen,
+      })
+    },
+    [onChangePassed, value],
+  )
 
-    return (
-      <Field
-        label={label ?? '(no label provided)'}
-        validationMessage={validationMessage}
-        validationState={validationState}
-        ref={widthRef}
+  return (
+    <Field
+      label={label ?? '(no label provided)'}
+      validationMessage={validationMessage}
+      validationState={validationState}
+      ref={widthRef}
+    >
+      <RadioGroup
+        layout={verticalLayout ? 'vertical' : 'horizontal'}
+        name={name}
+        value={value}
+        appearance="underline"
+        disabled={disabled}
       >
-        <RadioGroup
-          layout={verticalLayout ? 'vertical' : 'horizontal'}
-          name={name}
-          value={value}
-          appearance="underline"
-          disabled={disabled}
-        >
-          {list.map((val, index) => (
-            <Radio
-              key={val}
-              label={replaceUnderscoreInLabel ? val.replaceAll('_', ' ') : val}
-              value={val}
-              onClick={onClick}
-              autoFocus={index === 0 && autoFocus}
-              ref={index === 0 ? ref : undefined}
-            />
-          ))}
-        </RadioGroup>
-      </Field>
-    )
-  }),
-)
+        {list.map((val, index) => (
+          <Radio
+            key={val}
+            label={replaceUnderscoreInLabel ? val.replaceAll('_', ' ') : val}
+            value={val}
+            onClick={onClick}
+            autoFocus={index === 0 && autoFocus}
+            ref={index === 0 ? ref : undefined}
+          />
+        ))}
+      </RadioGroup>
+    </Field>
+  )
+})
--- src/components/shared/RadioGroupFromOptions.tsx
+++ src/components/shared/RadioGroupFromOptions.tsx
@@ -1,70 +1,69 @@
-import { memo, forwardRef, useCallback } from 'react'
+import { memo, useCallback } from 'react'
 import { Field, RadioGroup, Radio } from '@fluentui/react-components'
 import { useResizeDetector } from 'react-resize-detector'
 
-export const RadioGroupFromOptions = memo(
-  forwardRef((props, ref) => {
-    const {
-      name,
-      label,
-      options = [],
-      value,
-      onChange: onChangePassed,
-      validationMessage,
-      validationState = 'none',
-      autoFocus,
-      disabled = false,
-      // replaceUnderscoreInLabel = false,
-    } = props
+export const RadioGroupFromOptions = memo((props) => {
+  const {
+    name,
+    label,
+    options = [],
+    value,
+    onChange: onChangePassed,
+    validationMessage,
+    validationState = 'none',
+    autoFocus,
+    disabled = false,
+    // replaceUnderscoreInLabel = false,
+    ref,
+  } = props
 
-    const { width, ref: widthRef } = useResizeDetector({
-      handleHeight: false,
-      refreshMode: 'debounce',
-      refreshRate: 100,
-      refreshOptions: { leading: false, trailing: true },
-    })
+  const { width, ref: widthRef } = useResizeDetector({
+    handleHeight: false,
+    refreshMode: 'debounce',
+    refreshRate: 100,
+    refreshOptions: { leading: false, trailing: true },
+  })
 
-    const verticalLayout = !!width && width < 500
+  const verticalLayout = !!width && width < 500
 
-    // TODO: enable nulling when clicking on the selected radio
-    // as in other RedioGroup components
-    // do this when this component is actually used (not used now)
-    const onChange = useCallback(
-      (e, data) => {
-        const fakeEvent = {
-          target: { name, value: data.value },
-        }
-        onChangePassed(fakeEvent)
-      },
-      [name, onChangePassed],
-    )
+  // TODO: enable nulling when clicking on the selected radio
+  // as in other RedioGroup components
+  // do this when this component is actually used (not used now)
+  const onChange = useCallback(
+    (e, data) => {
+      const fakeEvent = {
+        target: { name, value: data.value },
+      }
+      onChangePassed(fakeEvent)
+    },
+    [name, onChangePassed],
+  )
 
-    return (
-      <Field
-        label={label ?? '(no label provided)'}
-        validationMessage={validationMessage}
-        validationState={validationState}
-        ref={widthRef}
+  return (
+    <Field
+      label={label ?? '(no label provided)'}
+      validationMessage={validationMessage}
+      validationState={validationState}
+      ref={widthRef}
+    >
+      <RadioGroup
+        layout={verticalLayout ? 'vertical' : 'horizontal'}
+        name={name}
+        value={value}
+        onChange={onChange}
+        appearance="underline"
+        disabled={disabled}
       >
-        <RadioGroup
-          layout={verticalLayout ? 'vertical' : 'horizontal'}
-          name={name}
-          value={value}
-          onChange={onChange}
-          appearance="underline"
-          disabled={disabled}
-        >
-          {options.map((val, index) => (
-            <Radio
-              key={val.value}
-              label={val.label}
-              value={val.value}
-              autoFocus={index === 0 && autoFocus}
-              ref={index === 0 ? ref : undefined}
-            />
-          ))}
-        </RadioGroup>
-      </Field>
-    )
-  }),
-)
+        {options.map((val, index) => (
+          <Radio
+            key={val.value}
+            label={val.label}
+            value={val.value}
+            autoFocus={index === 0 && autoFocus}
+            ref={index === 0 ? ref : undefined}
+          />
+        ))}
+      </RadioGroup>
+    </Field>
+  )
+})
