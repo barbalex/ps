30bc98c9e6c5675f71731a2774ab7544c0110359
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-02-22 18:31:00 +0100

    make input fast

--- src/components/shared/TextField.tsx
+++ src/components/shared/TextField.tsx
@@ -1,4 +1,4 @@
-import { memo, forwardRef } from 'react'
+import { memo, useState, useCallback, useEffect } from 'react'
 import { Input, Field } from '@fluentui/react-components'
 import type { InputProps } from '@fluentui/react-components'
 
@@ -10,43 +10,65 @@ const rowStyle = {
   userSelect: 'none',
 }
 
-export const TextField = memo(
-  forwardRef((props: InputProps, ref) => {
-    const {
-      label,
-      hint,
-      validationMessage,
-      validationState = 'none',
-      placeholder,
-      autoFocus,
-      value,
-      disabled = false,
-      button,
-    } = props
+export const TextField = memo((props: InputProps) => {
+  const {
+    label,
+    name,
+    onChange: onChangeIn,
+    hint,
+    validationMessage,
+    validationState = 'none',
+    placeholder,
+    autoFocus,
+    value,
+    disabled = false,
+    button,
+    ref,
+  } = props
 
-    // console.log('hello TextField, props:', props)
+  // console.log('hello TextField, props:', props)
 
-    return (
-      <Field
-        label={label ?? '(no label provided)'}
-        validationMessage={validationMessage}
-        validationState={validationState}
-        hint={hint}
-      >
-        <div style={rowStyle}>
-          <Input
-            {...props}
-            value={value ?? ''}
-            placeholder={placeholder}
-            appearance="underline"
-            autoFocus={autoFocus}
-            ref={ref}
-            disabled={disabled}
-            style={{ flexGrow: 1 }}
-          />
-          {!!button && button}
-        </div>
-      </Field>
-    )
-  }),
-)
+  const [stateValue, setStateValue] = useState(
+    value || value === 0 ? value : '',
+  )
+  const onChange = useCallback((event) => setStateValue(event.target.value), [])
+  useEffect(() => {
+    setStateValue(value || value === 0 ? value : '')
+  }, [value])
+
+  const onKeyPress = useCallback(
+    (event) => {
+      if (event.key === 'Enter') {
+        onChangeIn(event)
+      }
+    },
+    [onChangeIn],
+  )
+
+  return (
+    <Field
+      label={label ?? '(no label provided)'}
+      validationMessage={validationMessage}
+      validationState={validationState}
+      hint={hint}
+    >
+      <div style={rowStyle}>
+        <Input
+          // {...props}
+          name={name}
+          value={stateValue}
+          placeholder={placeholder}
+          appearance="underline"
+          autoFocus={autoFocus}
+          ref={ref}
+          onChange={onChange}
+          onKeyPress={onKeyPress}
+          onBlur={onChangeIn}
+          disabled={disabled}
+          style={{ flexGrow: 1 }}
+        />
+        {!!button && button}
+      </div>
+    </Field>
+  )
+})
