2d51be45422b649aa4683537f558e612318e44e4
Alexander Gabriel <alex.barbalex@gmail.com>
2025-02-24 22:13:05 +0100

    migrate

--- src/routes/action/index.tsx
+++ src/routes/action/index.tsx
@@ -18,31 +18,38 @@ export const Component = memo(() => {
   const autoFocusRef = useRef<HTMLInputElement>(null)
 
   const db = usePGlite()
-  const { results: row } = useLiveQuery(
-    db.actions.liveUnique({ where: { action_id } }),
-  )
+  const res = useLiveQuery(`SELECT * FROM actions WHERE action_id = $1`, [
+    action_id,
+  ])
+  const row = res?.rows?.[0]
 
   const onChange = useCallback<InputProps['onChange']>(
     (e, data) => {
       const { name, value } = getValueFromChange(e, data)
-      db.actions.update({
-        where: { action_id },
-        data: { [name]: value },
-      })
+      db.query(`UPDATE actions SET ${name} = $1 WHERE action_id = $2`, [
+        value,
+        action_id,
+      ])
     },
-    [db.actions, action_id],
+    [db, action_id],
   )
 
   if (!row) return <Loading />
-
-  console.log('hello Action, row:', row)
 
   return (
     <div className="form-outer-container">
       <Header autoFocusRef={autoFocusRef} />
       <div className="form-container">
-        <TextFieldInactive label="ID" name="action_id" value={row.action_id} />
-        <Form onChange={onChange} row={row} autoFocusRef={autoFocusRef} />
+        <TextFieldInactive
+          label="ID"
+          name="action_id"
+          value={row.action_id}
+        />
+        <Form
+          onChange={onChange}
+          row={row}
+          autoFocusRef={autoFocusRef}
+        />
       </div>
     </div>
   )
