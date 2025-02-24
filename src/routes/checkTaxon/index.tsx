d6e644c8886dba2bad3afcfd90901bcd83b36687
Alexander Gabriel <alex.barbalex@gmail.com>
2025-02-24 22:49:21 +0100

    migrate

--- src/routes/checkTaxon/index.tsx
+++ src/routes/checkTaxon/index.tsx
@@ -1,8 +1,7 @@
 import { useCallback, useRef, memo } from 'react'
-import { useLiveQuery } from '@electric-sql/pglite-react'
 import { useParams } from 'react-router-dom'
 import type { InputProps } from '@fluentui/react-components'
-import { usePGlite } from '@electric-sql/pglite-react'
+import { usePGlite, useLiveQuery } from '@electric-sql/pglite-react'
 
 import { TextField } from '../../components/shared/TextField.tsx'
 import { DropdownField } from '../../components/shared/DropdownField.tsx'
@@ -19,21 +18,23 @@ export const Component = memo(() => {
   const autoFocusRef = useRef<HTMLInputElement>(null)
 
   const db = usePGlite()
-  const { results: row } = useLiveQuery(
-    db.check_taxa.liveUnique({ where: { check_taxon_id } }),
+  const res = useLiveQuery(
+    `SELECT * FROM check_taxa WHERE check_taxon_id = $1`,
+    [check_taxon_id],
   )
+  const row = res?.rows?.[0]
 
   // console.log('CheckTaxon', { row, results })
 
   const onChange = useCallback<InputProps['onChange']>(
     (e, data) => {
       const { name, value } = getValueFromChange(e, data)
-      db.check_taxa.update({
-        where: { check_taxon_id },
-        data: { [name]: value },
-      })
+      db.query(`UPDATE check_taxa SET ${name} = $1 WHERE check_taxon_id = $2`, [
+        value,
+        check_taxon_id,
+      ])
     },
-    [db.check_taxa, check_taxon_id],
+    [db, check_taxon_id],
   )
 
   if (!row) return <Loading />
