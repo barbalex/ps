3db9b06d30f25a34c57c517ee36f69113e47327c
Alexander Gabriel <alex.barbalex@gmail.com>
2025-02-24 22:39:00 +0100

    migrate

--- src/routes/actionReportValue/index.tsx
+++ src/routes/actionReportValue/index.tsx
@@ -1,8 +1,7 @@
 import { useCallback, useRef, memo } from 'react'
-import { useLiveQuery } from '@electric-sql/pglite-react'
 import { useParams } from 'react-router-dom'
 import type { InputProps } from '@fluentui/react-components'
-import { usePGlite } from '@electric-sql/pglite-react'
+import { usePGlite, useLiveQuery } from '@electric-sql/pglite-react'
 
 import { TextField } from '../../components/shared/TextField.tsx'
 import { TextFieldInactive } from '../../components/shared/TextFieldInactive.tsx'
@@ -19,21 +18,21 @@ export const Component = memo(() => {
   const autoFocusRef = useRef<HTMLInputElement>(null)
 
   const db = usePGlite()
-  const { results: row } = useLiveQuery(
-    db.action_report_values.liveUnique({ where: { action_report_value_id } }),
+  const res = useLiveQuery(
+    `SELECT * FROM action_report_values WHERE action_report_value_id = $1`,
+    [action_report_value_id],
   )
+  const row = res.rows?.[0]
 
   const onChange = useCallback<InputProps['onChange']>(
     (e, data) => {
       const { name, value } = getValueFromChange(e, data)
-      db.action_report_values.update({
-        where: { action_report_value_id },
-        data: {
-          [name]: value,
-        },
-      })
+      db.query(
+        `UPDATE action_report_values SET ${name} = $1 WHERE action_report_value_id = $2`,
+        [value, action_report_value_id],
+      )
     },
-    [db.action_report_values, action_report_value_id],
+    [db, action_report_value_id],
   )
 
   if (!row) return <Loading />
