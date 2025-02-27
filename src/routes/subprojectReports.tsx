3a0b15c34fe06af3d9d7ac8378df74d61cb6c8d7
Alexander Gabriel <alex.barbalex@gmail.com>
2025-02-27 12:35:32 +0100

    useLiveIncrementalQuery

--- src/routes/subprojectReports.tsx
+++ src/routes/subprojectReports.tsx
@@ -1,7 +1,7 @@
 import { useCallback, memo } from 'react'
 import { useParams, useNavigate, useSearchParams } from 'react-router-dom'
 import { useAtom } from 'jotai'
-import { usePGlite, useLiveQuery } from '@electric-sql/pglite-react'
+import { usePGlite, useLiveIncrementalQuery } from '@electric-sql/pglite-react'
 
 import { createSubprojectReport } from '../modules/createRows.ts'
 import { ListViewHeader } from '../components/ListViewHeader/index.tsx'
@@ -19,11 +19,12 @@ export const Component = memo(() => {
   const [searchParams] = useSearchParams()
   const db = usePGlite()
 
-  const result = useLiveQuery(
-    `SELECT * FROM subproject_reports WHERE subproject_id = $1${
+  const result = useLiveIncrementalQuery(
+    `SELECT subproject_report_id, label FROM subproject_reports WHERE subproject_id = $1${
       isFiltered ? ` AND(${filter})` : ''
     } order by label asc`,
     [subproject_id],
+    'subproject_report_id',
   )
   const subprojectReports = result?.rows ?? []
 
--- src/routes/subprojects.tsx
+++ src/routes/subprojects.tsx
@@ -1,7 +1,7 @@
 import { useCallback, memo } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { useAtom } from 'jotai'
-import { usePGlite, useLiveQuery } from '@electric-sql/pglite-react'
+import { usePGlite, useLiveIncrementalQuery } from '@electric-sql/pglite-react'
 
 import { createSubproject } from '../modules/createRows.ts'
 import { ListViewHeader } from '../components/ListViewHeader/index.tsx'
@@ -19,30 +19,20 @@ export const Component = memo(() => {
   const db = usePGlite()
 
   // TODO: ensure passing in filter works as expected (beware of joined table...)
-  const result = useLiveQuery(
+  const result = useLiveIncrementalQuery(
     `SELECT sp.subproject_id, sp.label, p.subproject_name_plural, p.subproject_name_singular 
       FROM subprojects sp inner join projects p on p.project_id = sp.project_id 
       WHERE sp.project_id = $1${
         isFiltered ? ` AND(${filter})` : ''
       } order by sp.label asc`,
     [project_id],
+    'subproject_id',
   )
   const subprojects = result?.rows ?? []
   const namePlural = result?.rows?.[0]?.subproject_name_plural ?? 'Subprojects'
   const nameSingular =
     result?.rows?.[0]?.subproject_name_singular ?? 'Subproject'
   const nameSingularLower = nameSingular.toLowerCase()
-
-  // get projects.subproject_name_plural to name the table
-  // const resultProject = useLiveQuery(
-  //   `SELECT * FROM projects WHERE project_id = $1`,
-  //   [project_id],
-  // )
-  // const namePlural =
-  //   resultProject?.rows?.[0]?.subproject_name_plural ?? 'Subprojects'
-  // const nameSingular =
-  //   resultProject?.rows?.[0]?.subproject_name_singular ?? 'Subproject'
-  // const nameSingularLower = nameSingular.toLowerCase()
 
   const add = useCallback(async () => {
     const res = await createSubproject({ db, project_id })
