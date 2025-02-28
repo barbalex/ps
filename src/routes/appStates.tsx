8032442cabd70cbe66ff71a02593848e6636c655
Alexander Gabriel <alex.barbalex@gmail.com>
2025-02-28 20:33:23 +0100

    work on dump download

--- src/routes/appStates.tsx
+++ src/routes/appStates.tsx
@@ -1,5 +1,8 @@
-import { memo } from 'react'
+import { memo, useCallback } from 'react'
 import { useAtom } from 'jotai'
+import { Button } from '@fluentui/react-components'
+import { pgDump } from '@electric-sql/pglite-tools/pg_dump'
+import { usePGlite } from '@electric-sql/pglite-react'
 
 import { SwitchField } from '../components/shared/SwitchField.tsx'
 import { FormHeader } from '../components/FormHeader/index.tsx'
@@ -12,6 +15,30 @@ export const Component = memo(() => {
     breadcrumbsOverflowingAtom,
   )
   const [navsOverflowing, setNavsOverflowing] = useAtom(navsOverflowingAtom)
+  const db = usePGlite()
+
+  const downloadDump = useCallback(async () => {
+    // TODO
+    const dump = await pgDump({ pg: db })
+    console.log('dump:', dump)
+    // Create blob link to download
+    const url = URL.createObjectURL(dump)
+
+    const link = document.createElement('a')
+    link.href = url
+    link.download = dump.name
+    // link.setAttribute('download', `data.dump`)
+
+    // Append to html link element page
+    document.body.appendChild(link)
+
+    // Start download
+    link.click()
+
+    // Clean up and remove the link
+    // link.parentNode.removeChild(link)
+    // url.revokeObjectUrl()
+  }, [db])
 
   return (
     <div className="form-outer-container">
@@ -30,6 +57,14 @@ export const Component = memo(() => {
           onChange={() => setNavsOverflowing(!navsOverflowing)}
           validationMessage="If true, navs will only use a single line. When they overflow, the overflowing navs will be collected in a menu on the left"
         />
+        <Button
+          size="medium"
+          // icon={<FaChevronLeft />}
+          onClick={downloadDump}
+          title="Download dump"
+        >
+          Download dump
+        </Button>
       </div>
     </div>
   )
