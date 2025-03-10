8f44faa242a3a2a158e0793c7e576c767acb6007
Alexander Gabriel <alex.barbalex@gmail.com>
2025-03-10 14:11:01 +0100

    ignore non changes

--- src/routes/actionReport/index.tsx
+++ src/routes/actionReport/index.tsx
@@ -28,12 +28,15 @@ export const Component = memo(() => {
   const onChange = useCallback<InputProps['onChange']>(
     (e, data) => {
       const { name, value } = getValueFromChange(e, data)
+      // only change if value has changed: maybe only focus entered and left
+      if (row[name] === value) return
+
       db.query(
         `UPDATE action_reports SET ${name} = $1 WHERE action_report_id = $2`,
         [value, action_report_id],
       )
     },
-    [db, action_report_id],
+    [row, db, action_report_id],
   )
 
   if (!row) return <Loading />
