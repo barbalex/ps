924049896b72fec8c489db8b8ea29e1c34e5b37d
Alexander Gabriel <alex.barbalex@gmail.com>
2025-03-13 13:22:50 +0100

    configure vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+  ],
   envPrefix: 'ELECTRIC_',
   optimizeDeps: {
     exclude: ['@electric-sql/pglite', '@electric-sql/pglite-tools'],
