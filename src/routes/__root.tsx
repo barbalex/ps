92f5369c6bc70668164b963194d97ba3449cba77
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-03-16 00:17:52 +0100

    add error pages

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,10 +3,13 @@ import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 import { NotFound } from '../components/NotFound.tsx'
+import { ErrorPage } from '../formsAndLists/error.tsx'
 
 export const Route = createRootRoute({
   component: RootComponent,
   defaultNotFoundComponent: NotFound,
+  errorComponent: ErrorPage,
+  defaultErrorComponent: ErrorPage,
 })
 
 function RootComponent() {
