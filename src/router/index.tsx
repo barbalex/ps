224c4b5b6f2e6e9a4f85b2a5ec220449d2e05b37
Alexander Gabriel <alex.barbalex@gmail.com>
2025-03-20 16:54:57 +0100

    implementing tanstack router project files

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -514,42 +514,5 @@ export const router = () => {
         },
       ],
     },
-    {
-      path: 'files',
-      element: null,
-      handle: {
-        crumb: {
-          text: 'Files',
-          table: 'files',
-          folder: true,
-        },
-      },
-      children: [
-        {
-          index: true,
-          lazy: () => import('../routes/files.tsx'),
-        },
-        {
-          path: ':file_id',
-          element: null,
-          handle: {
-            crumb: {
-              table: 'files',
-              folder: false,
-            },
-          },
-          children: [
-            {
-              index: true,
-              lazy: () => import('../routes/file/index.tsx'),
-            },
-            {
-              path: 'preview',
-              lazy: () => import('../routes/filePreview/index.tsx'),
-            },
-          ],
-        },
-      ],
-    },
   ])
 }
