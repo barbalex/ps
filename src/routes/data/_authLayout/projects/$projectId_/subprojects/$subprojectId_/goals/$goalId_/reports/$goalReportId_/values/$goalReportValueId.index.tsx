70766be79653d6b5c7b76e9c9ecbb426871f5b90
Alexander Gabriel <alex.barbalex@gmail.com>
2025-03-20 22:23:33 +0100

    implementing tanstack router sp goal reports

--- src/routes/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/goals/$goalId_/reports/$goalReportId_/values/$goalReportValueId.index.tsx
+++ src/routes/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/goals/$goalId_/reports/$goalReportId_/values/$goalReportValueId.index.tsx
@@ -1,16 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { GoalReportValue } from '../../../../../../../../../../../../formsAndLists/goalReportValue/index.tsx'
+
 export const Route = createFileRoute(
   '/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/goals/$goalId_/reports/$goalReportId_/values/$goalReportValueId/',
 )({
-  component: RouteComponent,
+  component: GoalReportValue,
 })
-
-function RouteComponent() {
-  return (
-    <div>
-      Hello
-      "/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/goals/$goalId_/reports/$goalReportId_/values/$goalReportValueId/"!
-    </div>
-  )
-}
--- src/routes/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/goals/$goalId_/reports/$goalReportId_/values/index.tsx
+++ src/routes/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/goals/$goalId_/reports/$goalReportId_/values/index.tsx
@@ -6,15 +6,6 @@ import { NotFound } from '../../../../../../../../../../../../components/NotFoun
 export const Route = createFileRoute(
   '/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/goals/$goalId_/reports/$goalReportId_/values/',
 )({
-  component: RouteComponent,
+  component: GoalReportValues,
   notFoundComponent: NotFound,
 })
-
-function RouteComponent() {
-  return (
-    <div>
-      Hello
-      "/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/goals/$goalId_/reports/$goalReportId_/values/"!
-    </div>
-  )
-}
