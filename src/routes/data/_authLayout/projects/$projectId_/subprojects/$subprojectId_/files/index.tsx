8d3a6adefc48e279d1ecfd8e39dfc5e1eb15c039
Alexander Gabriel <alex.barbalex@gmail.com>
2025-03-21 11:56:24 +0100

    implementing tanstack router sp files

--- src/routes/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/files/index.tsx
+++ src/routes/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/files/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useParams } from '@tanstack/react-router'
 
 import { Files } from '../../../../../../../../formsAndLists/files.tsx'
 import { NotFound } from '../../../../../../../../components/NotFound.tsx'
