ef1dbfa7a9a87988a76c47ecfc2216452c2ba427
Alexander Gabriel <alex.barbalex@gmail.com>
2025-03-23 23:50:05 +0100

    work on place2 routing

--- src/routes/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/places/$placeId2_/checks/$checkId_/files/$fileId_/preview.tsx
+++ src/routes/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/places/$placeId2_/checks/$checkId_/files/$fileId_/preview.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-import { FilePreview } from '../../../../../../../../../../../../../../../../formsAndLists/filePreview/index.tsx'
+import { FilePreview } from '../../../../../../../../../../../../../../../formsAndLists/filePreview/index.tsx'
 
 export const Route = createFileRoute(
   '/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/places/$placeId2_/checks/$checkId_/files/$fileId_/preview',
@@ -10,6 +10,6 @@ export const Route = createFileRoute(
 
 function RouteComponent() {
   return (
-    <FilePreview from="/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/checks/$checkId_/files/$fileId_/preview" />
+    <FilePreview from="/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/places/$placeId2_/checks/$checkId_/files/$fileId_/preview" />
   )
 }
--- src/routes/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/places/$placeId2_/checks/$checkId_/files/index.tsx
+++ src/routes/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/places/$placeId2_/checks/$checkId_/files/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute, useParams } from '@tanstack/react-router'
 
-import { Files } from '../../../../../../../../../../../../../../../formsAndLists/files.tsx'
-import { NotFound } from '../../../../../../../../../../../../../../../components/NotFound.tsx'
+import { Files } from '../../../../../../../../../../../../../../formsAndLists/files.tsx'
+import { NotFound } from '../../../../../../../../../../../../../../components/NotFound.tsx'
 
 export const Route = createFileRoute(
   '/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/places/$placeId2_/checks/$checkId_/files/',
@@ -12,7 +12,7 @@ export const Route = createFileRoute(
 
 function RouteComponent() {
   const { projectId, subprojectId, placeId, checkId } = useParams({
-    from: '/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/checks/$checkId_/files/',
+    from: '/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/places/$placeId2_/checks/$checkId_/files/',
   })
 
   return (
