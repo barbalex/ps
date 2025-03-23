5123e066e10fc3613ec138628b4f2eab796a3b54
Alexander Gabriel <alex.barbalex@gmail.com>
2025-03-24 00:00:19 +0100

    work on place2 routing

--- src/routes/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/places/$placeId2_/occurrences-assigned/$occurrenceId.index.tsx
+++ src/routes/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/places/$placeId2_/occurrences-assigned/$occurrenceId.index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-import { Occurrence } from '../../../../../../../../../../../formsAndLists/occurrence/index.tsx'
+import { Occurrence } from '../../../../../../../../../../../../formsAndLists/occurrence/index.tsx'
 
 export const Route = createFileRoute(
   '/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/places/$placeId2_/occurrences-assigned/$occurrenceId/',
@@ -10,6 +10,6 @@ export const Route = createFileRoute(
 
 function RouteComponent() {
   return (
-    <Occurrence from="/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/occurrences-assigned/$occurrenceId/" />
+    <Occurrence from="/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/places/$placeId2_/occurrences-assigned/$occurrenceId/" />
   )
 }
--- src/routes/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/places/$placeId2_/occurrences-assigned/index.tsx
+++ src/routes/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/places/$placeId2_/occurrences-assigned/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-import { Occurrences } from '../../../../../../../../../../../formsAndLists/occurrences.tsx'
-import { NotFound } from '../../../../../../../../../../../components/NotFound.tsx'
+import { Occurrences } from '../../../../../../../../../../../../formsAndLists/occurrences.tsx'
+import { NotFound } from '../../../../../../../../../../../../components/NotFound.tsx'
 
 export const Route = createFileRoute(
   '/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/places/$placeId2_/occurrences-assigned/',
@@ -14,7 +14,7 @@ function RouteComponent() {
   return (
     <Occurrences
       isAssigned={true}
-      from="/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/occurrences-assigned/"
+      from="/data/_authLayout/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/places/$placeId2_/occurrences-assigned/"
     />
   )
 }
