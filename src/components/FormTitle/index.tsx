d78554af5d97b99d73356cda8ae2fd9d57d949dd
Alexander Gabriel <alex.barbalex@gmail.com>
2025-03-26 22:34:12 +0100

    work on form title

---
+++ src/components/FormTitle/index.tsx
@@ -0,0 +1,50 @@
+import {
+  memo,
+  useCallback,
+  useContext,
+  useState,
+  useRef,
+  useEffect,
+} from 'react'
+import { useAtom } from 'jotai'
+
+import './style.css'
+
+const containerStyle = {
+  backgroundColor: 'rgb(225, 247, 224)',
+  flexShrink: 0,
+  display: 'flex',
+  flexDirection: 'column',
+  overflow: 'hidden',
+}
+const titleRowStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  minHeight: 42,
+  flexWrap: 'nowrap',
+  overflow: 'hidden',
+}
+const titleStyle = {
+  display: 'block',
+  flewGrow: 0,
+  flexShrink: 1,
+  marginTop: 'auto',
+  marginBottom: 'auto',
+  color: 'white',
+  fontWeight: 'bold',
+  overflow: 'hidden',
+  whiteSpace: 'nowrap',
+  textOverflow: 'ellipsis',
+}
+
+export const FormTitle = memo(({ title }) => {
+  return (
+    <div
+      style={containerStyle}
+      className="form-title-container"
+    >
+      <h1>{title}</h1>
+    </div>
+  )
+})
