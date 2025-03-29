a7e257ca603df97bfe237d9144b3244a57e8adf6
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-03-29 09:11:57 +0100

    use nav data

--- src/formsAndLists/files.tsx
+++ src/formsAndLists/files.tsx
@@ -11,57 +11,67 @@ import { UploaderContext } from '../UploaderContext.ts'
 
 import '../form.css'
 
-export const Files = memo(() => {
-  const { loading, navData } = useFilesNavData({})
-  const { navs, label, nameSingular } = navData
+export const Files = memo(
+  ({ projectId, subprojectId, placeId, placeId2, actionId, checkId }) => {
+    const { loading, navData } = useFilesNavData({
+      projectId,
+      subprojectId,
+      placeId,
+      placeId2,
+      actionId,
+      checkId,
+    })
+    const { navs, label, nameSingular } = navData
 
-  const uploaderCtx = useContext(UploaderContext)
-  const api = uploaderCtx?.current?.getAPI?.()
-  const onClickAdd = useCallback(() => api?.initFlow?.(), [api])
+    const uploaderCtx = useContext(UploaderContext)
+    const api = uploaderCtx?.current?.getAPI?.()
+    const onClickAdd = useCallback(() => api?.initFlow?.(), [api])
 
-  // TODO: get uploader css locally if it should be possible to upload files
-  // offline to SQLite
-  return (
-    <div className="list-view">
-      <ListHeader
-        label={label}
-        nameSingular={nameSingular}
-        menus={
-          <Button
-            size="medium"
-            title="add File"
-            icon={<FaPlus />}
-            onClick={onClickAdd}
-          />
-        }
-      />
-      <div className="list-container">
-        <Uploader />
-        {loading ?
-          <Loading />
-        : <>
-            {navs.map(({ file_id, label, url, mimetype }) => {
-              let imgSrc = undefined
-              if (
-                (mimetype.includes('image') || mimetype.includes('pdf')) &&
-                url
-              ) {
-                imgSrc = `${url}-/resize/x50/-/format/auto/-/quality/smart/`
-              }
+    // TODO: get uploader css locally if it should be possible to upload files
+    // offline to SQLite
+    return (
+      <div className="list-view">
+        <ListHeader
+          label={label}
+          nameSingular={nameSingular}
+          menus={
+            <Button
+              size="medium"
+              title="add File"
+              icon={<FaPlus />}
+              onClick={onClickAdd}
+            />
+          }
+        />
+        <div className="list-container">
+          <Uploader />
+          {loading ? (
+            <Loading />
+          ) : (
+            <>
+              {navs.map(({ file_id, label, url, mimetype }) => {
+                let imgSrc = undefined
+                if (
+                  (mimetype.includes('image') || mimetype.includes('pdf')) &&
+                  url
+                ) {
+                  imgSrc = `${url}-/resize/x50/-/format/auto/-/quality/smart/`
+                }
 
-              return (
-                <Row
-                  key={file_id}
-                  label={label ?? file_id}
-                  to={file_id}
-                  imgSrc={imgSrc}
-                  lastHasImages={true}
-                />
-              )
-            })}
-          </>
-        }
+                return (
+                  <Row
+                    key={file_id}
+                    label={label ?? file_id}
+                    to={file_id}
+                    imgSrc={imgSrc}
+                    lastHasImages={true}
+                  />
+                )
+              })}
+            </>
+          )}
+        </div>
       </div>
-    </div>
-  )
-})
+    )
+  },
+)
