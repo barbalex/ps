a139b3b6bc5d44fe1c902a7d4ace5907bd3a986d
Alexander Gabriel <alex.barbalex@gmail.com>
2025-04-01 11:40:40 +0200

    use nav data

--- src/modules/useCrssNavData.ts
+++ src/modules/useCrssNavData.ts
@@ -7,31 +7,43 @@ import isEqual from 'lodash/isEqual'
 import { formatNumber } from './formatNumber.ts'
 import { treeOpenNodesAtom } from '../store.ts'
 
+const parentArray = ['data']
+const limit = 100
+
 export const useCrssNavData = () => {
   const [openNodes] = useAtom(treeOpenNodesAtom)
   const location = useLocation()
 
+  // TODO:
+  // only query all labels if isOpen
+  // if is not open, query only the count
+  const urlPath = location.pathname.split('/').filter((p) => p !== '')
+  const parentUrl = `/${parentArray.join('/')}`
+  const ownArray = useMemo(() => [...parentArray, 'crs'], [])
+  const isOpen = openNodes.some((array) => isEqual(array, ownArray))
+
+  const resCount = useLiveQuery(`SELECT COUNT(crs_id) as count FROM crs`)
+  const count = resCount?.rows?.[0]?.count ?? 0
+
   const res = useLiveQuery(`
-    SELECT 
-      crs_id as id, 
-      label 
-    FROM crs 
-    ORDER BY label`)
+      SELECT
+        crs_id as id,
+        label
+      FROM crs
+      ORDER BY label
+      limit 500`)
 
   const loading = res === undefined
 
   const navData = useMemo(() => {
     const navs = res?.rows ?? []
-    const urlPath = location.pathname.split('/').filter((p) => p !== '')
-    const parentArray = ['data']
-    const parentUrl = `/${parentArray.join('/')}`
-    const ownArray = [...parentArray, 'crs']
     const ownUrl = `/${ownArray.join('/')}`
 
     // needs to work not only works for urlPath, for all opened paths!
-    const isOpen = openNodes.some((array) => isEqual(array, ownArray))
     const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
     const isActive = isEqual(urlPath, ownArray)
+    const isLimited = count > limit && isOpen
+    const label = `CRS (${resCount === undefined ? '...' : formatNumber(count)}${isLimited ? `, first ${limit}` : ''})`
 
     return {
       isInActiveNodeArray,
@@ -43,11 +55,11 @@ export const useCrssNavData = () => {
       ownUrl,
       urlPath,
       toParams: {},
-      label: `CRS (${loading ? '...' : formatNumber(navs.length)})`,
+      label,
       nameSingular: 'CRS',
       navs,
     }
-  }, [loading, location.pathname, openNodes, res?.rows])
+  }, [count, isOpen, ownArray, parentUrl, res?.rows, resCount, urlPath])
 
   return { loading, navData }
 }
