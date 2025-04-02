0f859f192840f98b2ab4b71a5f0670e85cea24cc
Alexander Gabriel <alex.barbalex@gmail.com>
2025-04-02 18:00:03 +0200

    use nav data

--- src/modules/useSubprojectReportsNavData.ts
+++ src/modules/useSubprojectReportsNavData.ts
@@ -12,46 +12,58 @@ export const useSubprojectReportsNavData = ({ projectId, subprojectId }) => {
   const [openNodes] = useAtom(treeOpenNodesAtom)
   const location = useLocation()
 
+  const parentArray = useMemo(
+    () => ['data', 'projects', projectId, 'subprojects', subprojectId],
+    [projectId, subprojectId],
+  )
+  const ownArray = useMemo(() => [...parentArray, 'reports'], [parentArray])
+  // needs to work not only works for urlPath, for all opened paths!
+  const isOpen = useMemo(
+    () => openNodes.some((array) => isEqual(array, ownArray)),
+    [openNodes, ownArray],
+  )
+
   const [filter] = useAtom(subprojectReportsFilterAtom)
   const filterString = filterStringFromFilter(filter)
   const isFiltered = !!filterString
 
-  const res = useLiveQuery(
-    `
-      SELECT 
+  const sql =
+    isOpen ?
+      `
+      WITH
+        count_unfiltered AS (SELECT count(*) FROM subproject_reports WHERE subproject_id = '${subprojectId}'),
+        count_filtered AS (SELECT count(*) FROM subproject_reports WHERE subproject_id = '${subprojectId}' ${isFiltered ? ` AND ${filterString}` : ''})
+      SELECT
         subproject_report_id AS id,
-        label 
-      FROM subproject_reports 
-      WHERE 
-        subproject_id = $1
-        ${filterString ? ` AND (${filterString})` : ''} 
-      ORDER BY label`,
-    [subprojectId],
-  )
+        label,
+        count_unfiltered.count AS count_unfiltered,
+        count_filtered.count AS count_filtered
+      FROM subproject_reports, count_unfiltered, count_filtered
+      WHERE
+        subproject_id = '${subprojectId}'
+        ${isFiltered ? ` AND ${filterString}` : ''}
+      ORDER BY label
+    `
+    : `
+      WITH
+        count_unfiltered AS (SELECT count(*) FROM subproject_reports WHERE subproject_id = '${subprojectId}'),
+        count_filtered AS (SELECT count(*) FROM subproject_reports WHERE subproject_id = '${subprojectId}' ${isFiltered ? ` AND ${filterString}` : ''})
+      SELECT
+        count_unfiltered.count AS count_unfiltered,
+        count_filtered.count AS count_filtered
+      FROM count_unfiltered, count_filtered
+    `
+  const res = useLiveQuery(sql)
 
   const loading = res === undefined
 
-  const resultCountUnfiltered = useLiveQuery(
-    `SELECT count(*) FROM subproject_reports WHERE subproject_id = $1`,
-    [subprojectId],
-  )
-  const countUnfiltered = resultCountUnfiltered?.rows?.[0]?.count ?? 0
-  const countLoading = resultCountUnfiltered === undefined
-
   const navData = useMemo(() => {
     const navs = res?.rows ?? []
-    const parentArray = [
-      'data',
-      'projects',
-      projectId,
-      'subprojects',
-      subprojectId,
-    ]
+    const countUnfiltered = navs[0]?.count_unfiltered ?? 0
+    const countFiltered = navs[0]?.count_filtered ?? 0
+
     const parentUrl = `/${parentArray.join('/')}`
-    const ownArray = [...parentArray, 'reports']
     const ownUrl = `/${ownArray.join('/')}`
-    // needs to work not only works for urlPath, for all opened paths!
-    const isOpen = openNodes.some((array) => isEqual(array, ownArray))
     const urlPath = location.pathname.split('/').filter((p) => p !== '')
     const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
     const isActive = isEqual(urlPath, ownArray)
@@ -67,7 +79,7 @@ export const useSubprojectReportsNavData = ({ projectId, subprojectId }) => {
       label: buildNavLabel({
         loading,
         isFiltered,
-        countFiltered: navs.length,
+        countFiltered,
         countUnfiltered,
         namePlural: 'Reports',
       }),
@@ -75,14 +87,14 @@ export const useSubprojectReportsNavData = ({ projectId, subprojectId }) => {
       navs,
     }
   }, [
-    countUnfiltered,
+    isFiltered,
+    isOpen,
     loading,
     location.pathname,
-    openNodes,
-    projectId,
+    ownArray,
+    parentArray,
     res?.rows,
-    subprojectId,
   ])
 
-  return { loading, navData, isFiltered: !!filterString }
+  return { loading, navData, isFiltered }
 }
