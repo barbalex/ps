8d5ba6db59129dcb2d4877a21a4c98aa46812f11
Alexander Gabriel <alex.barbalex@gmail.com>
2025-04-02 12:21:18 +0200

    use nav data

--- src/modules/useCheckValuesNavData.ts
+++ src/modules/useCheckValuesNavData.ts
@@ -4,7 +4,6 @@ import { useAtom } from 'jotai'
 import { useLocation } from '@tanstack/react-router'
 import isEqual from 'lodash/isEqual'
 
-import { formatNumber } from './formatNumber.ts'
 import { buildNavLabel } from './buildNavLabel.ts'
 import { treeOpenNodesAtom } from '../store.ts'
 
@@ -62,7 +61,11 @@ export const useCheckValuesNavData = ({
       ownArray,
       urlPath,
       ownUrl,
-      label: `Values (${loading ? '...' : formatNumber(navs.length)})`,
+      label: buildNavLabel({
+        countFiltered: navs.length,
+        namePlural: 'Values',
+        loading,
+      }),
       nameSingular: 'Check Value',
       navs,
     }
