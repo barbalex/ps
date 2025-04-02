a8bfa53c1cd5d7c2dea667f6c3c79405df6f9e97
Alexander Gabriel <alex.barbalex@gmail.com>
2025-04-02 21:18:51 +0200

    use nav data

--- src/modules/useProjectNavData.ts
+++ src/modules/useProjectNavData.ts
@@ -3,17 +3,27 @@ import { useLiveQuery } from '@electric-sql/pglite-react'
 import { useParams } from '@tanstack/react-router'
 import { useAtom } from 'jotai'
 
-import { treeOpenNodesAtom } from '../store.ts'
+import {
+  subprojectsFilterAtom,
+  projectReportsFilterAtom,
+  treeOpenNodesAtom,
+} from '../store.ts'
+import { buildNavLabel } from './buildNavLabel.ts'
+import { filterStringFromFilter } from './filterStringFromFilter.ts'
 
 export const useProjectNavData = () => {
   const [openNodes] = useAtom(treeOpenNodesAtom)
   const { projectId } = useParams({ strict: false })
 
+  const [subprojectsFilter] = useAtom(subprojectsFilterAtom)
+  const subprojectsFilterString = filterStringFromFilter(subprojectsFilter)
+  const subprojectIsFiltered = !!subprojectsFilterString
+
   const res = useLiveQuery(
     `
       WITH 
         subprojects_count_unfiltered AS (SELECT count(*) FROM subprojects WHERE project_id = '${projectId}'),
-        subprojects_count_filtered AS (SELECT count(*) FROM subprojects WHERE project_id = '${projectId}' ${isFiltered ? ` AND ${filterString}` : ''} ),
+        subprojects_count_filtered AS (SELECT count(*) FROM subprojects WHERE project_id = '${projectId}' ${subprojectIsFiltered ? ` AND ${subprojectsFilterString}` : ''} ),
         subprojects_names AS (SELECT subproject_name_singular, subproject_name_plural FROM projects WHERE project_id = '${projectId}')
       SELECT
         project_id AS id,
@@ -28,11 +38,6 @@ export const useProjectNavData = () => {
   )
   const loading = res === undefined
   const row = res?.rows?.[0]
-
-  const subprojectsCountUnfiltered = row?.subprojects_count_unfiltered ?? 0
-  const subprojectsCountFiltered = row?.subprojects_count_filtered ?? 0
-  const subprojectsNameSingular = row?.subprojects_name_singular ?? 'Subproject'
-  const subprojectsNamePlural = row?.subprojects_name_plural ?? 'Subprojects'
 
   const navData = useMemo(() => {
     const parentArray = ['data', 'projects']
@@ -56,13 +61,27 @@ export const useProjectNavData = () => {
       label: row.label,
       // nameSingular: 'Project',
       navs: [
-        { label: 'Project', isSelf: true },
+        { label: 'Project', array: [...ownArray, 'project'] },
         {
-          label: `Subprojects`,
-          fetcherName: `useSubprojectsNavData`,
-          params: { projectId },
+          label: buildNavLabel({
+            loading,
+            isFiltered: subprojectIsFiltered,
+            countFiltered: row?.subprojects_count_filtered ?? 0,
+            countUnfiltered: row?.subprojects_count_unfiltered ?? 0,
+            namePlural: row?.subprojects_name_plural ?? 'Subprojects',
+          }),
+          array: [...ownArray, 'subprojects'],
         },
       ],
     }
-  }, [])
+  }, [
+    loading,
+    openNodes,
+    row.id,
+    row.label,
+    row?.subprojects_count_filtered,
+    row?.subprojects_count_unfiltered,
+    row?.subprojects_name_plural,
+    subprojectIsFiltered,
+  ])
 }
