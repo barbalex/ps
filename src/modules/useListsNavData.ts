f8430e333b7abedb5800cc23f839304cafe570c8
Alexander Gabriel <alex.barbalex@gmail.com>
2025-04-02 14:35:17 +0200

    use nav data

--- src/modules/useListsNavData.ts
+++ src/modules/useListsNavData.ts
@@ -12,40 +12,57 @@ export const useListsNavData = ({ projectId }) => {
   const [openNodes] = useAtom(treeOpenNodesAtom)
   const location = useLocation()
 
+  const parentArray = useMemo(
+    () => ['data', 'projects', projectId],
+    [projectId],
+  )
+  const ownArray = useMemo(() => [...parentArray, 'lists'], [parentArray])
+  // needs to work not only works for urlPath, for all opened paths!
+  const isOpen = useMemo(
+    () => openNodes.some((array) => isEqual(array, ownArray)),
+    [openNodes, ownArray],
+  )
+
   const [filter] = useAtom(listsFilterAtom)
   const filterString = filterStringFromFilter(filter)
   const isFiltered = !!filterString
 
-  const res = useLiveQuery(
+  const sql =
+    isOpen ?
+      `
+      WITH
+        count_unfiltered AS (SELECT count(*) FROM lists WHERE project_id = '${projectId}'),
+        count_filtered AS (SELECT count(*) FROM lists WHERE project_id = '${projectId}' ${isFiltered ? ` AND ${filterString}` : ''})
+      SELECT
+        list_id AS id,
+        label,
+        count_unfiltered.count AS count_unfiltered,
+        count_filtered.count AS count_filtered
+      FROM lists, count_unfiltered, count_filtered
+      WHERE project_id = '${projectId}'
+        ${isFiltered ? ` AND ${filterString}` : ''}
+      ORDER BY label
     `
-    SELECT 
-      list_id AS id,
-      label
-    FROM lists 
-    WHERE 
-      project_id = $1
-      ${isFiltered ? ` AND ${filterString} ` : ''} 
-    ORDER BY label`,
-    [projectId],
-  )
+    : `
+      WITH
+        count_unfiltered AS (SELECT count(*) FROM lists WHERE project_id = '${projectId}'),
+        count_filtered AS (SELECT count(*) FROM lists WHERE project_id = '${projectId}' ${isFiltered ? ` AND ${filterString}` : ''})
+      SELECT
+        count_unfiltered.count AS count_unfiltered,
+        count_filtered.count AS count_filtered
+      FROM count_unfiltered, count_filtered
+    `
+  const res = useLiveQuery(sql)
 
   const loading = res === undefined
 
-  const resultCountUnfiltered = useLiveQuery(
-    `SELECT COUNT(*) FROM lists WHERE project_id = $1`,
-    [projectId],
-  )
-  const countUnfiltered = resultCountUnfiltered?.rows?.[0]?.count ?? 0
-  const countLoading = resultCountUnfiltered === undefined
-
   const navData = useMemo(() => {
     const navs = res?.rows ?? []
-    const parentArray = ['data', 'projects', projectId]
+    const countUnfiltered = navs[0]?.count_unfiltered ?? 0
+    const countFiltered = navs[0]?.count_filtered ?? 0
+
     const parentUrl = `/${parentArray.join('/')}`
-    const ownArray = [...parentArray, 'lists']
     const ownUrl = `/${ownArray.join('/')}`
-    // needs to work not only works for urlPath, for all opened paths!
-    const isOpen = openNodes.some((array) => isEqual(array, ownArray))
     const urlPath = location.pathname.split('/').filter((p) => p !== '')
     const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
     const isActive = isEqual(urlPath, ownArray)
@@ -61,7 +78,7 @@ export const useListsNavData = ({ projectId }) => {
       label: buildNavLabel({
         loading,
         isFiltered,
-        countFiltered: navs.length,
+        countFiltered,
         countUnfiltered,
         namePlural: 'Lists',
       }),
@@ -69,12 +86,12 @@ export const useListsNavData = ({ projectId }) => {
       navs,
     }
   }, [
-    countUnfiltered,
     isFiltered,
+    isOpen,
     loading,
     location.pathname,
-    openNodes,
-    projectId,
+    ownArray,
+    parentArray,
     res?.rows,
   ])
 
