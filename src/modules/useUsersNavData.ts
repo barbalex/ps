1fe0a8e72c7ea6a1fd5b64af624d8a983b0f577e
Alexander Gabriel <alex.barbalex@gmail.com>
2025-04-02 12:16:38 +0200

    use nav data

--- src/modules/useUsersNavData.ts
+++ src/modules/useUsersNavData.ts
@@ -4,7 +4,6 @@ import { useLiveQuery } from '@electric-sql/pglite-react'
 import { useLocation } from '@tanstack/react-router'
 import isEqual from 'lodash/isEqual'
 
-import { formatNumber } from './formatNumber.ts'
 import { buildNavLabel } from './buildNavLabel.ts'
 import { treeOpenNodesAtom } from '../store.ts'
 
@@ -43,7 +42,11 @@ export const useUsersNavData = () => {
       ownArray,
       ownUrl,
       urlPath,
-      label: `Users (${loading ? '...' : formatNumber(navs.length)})`,
+      label: buildNavLabel({
+        countFiltered: navs.length,
+        namePlural: 'Users',
+        loading,
+      }),
       nameSingular: 'User',
       navs,
     }
--- src/modules/useVectorLayerDisplaysNavData.ts
+++ src/modules/useVectorLayerDisplaysNavData.ts
@@ -4,7 +4,6 @@ import { useAtom } from 'jotai'
 import { useLocation } from '@tanstack/react-router'
 import isEqual from 'lodash/isEqual'
 
-import { formatNumber } from './formatNumber.ts'
 import { buildNavLabel } from './buildNavLabel.ts'
 import { treeOpenNodesAtom } from '../store.ts'
 
@@ -51,7 +50,11 @@ export const useVectorLayerDisplaysNavData = ({ projectId, vectorLayerId }) => {
       ownArray,
       urlPath,
       ownUrl,
-      label: `Displays (${loading ? '...' : formatNumber(navs.length)})`,
+      label: buildNavLabel({
+        countFiltered: navs.length,
+        namePlural: 'Displays',
+        loading,
+      }),
       nameSingular: 'Vector Layer Display',
       navs,
     }
