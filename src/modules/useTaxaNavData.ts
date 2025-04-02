1efc50dc1d95a8dbb5186273c8d53dd277f90ae7
Alexander Gabriel <alex.barbalex@gmail.com>
2025-04-02 12:06:54 +0200

    use nav data

--- src/modules/useTaxaNavData.ts
+++ src/modules/useTaxaNavData.ts
@@ -4,7 +4,7 @@ import { useAtom } from 'jotai'
 import { useLocation } from '@tanstack/react-router'
 import isEqual from 'lodash/isEqual'
 
-import { formatNumber } from './formatNumber.ts'
+import { buildNavLabel } from './buildNavLabel.ts'
 import { treeOpenNodesAtom } from '../store.ts'
 
 export const useTaxaNavData = ({ projectId, taxonomyId }) => {
@@ -50,7 +50,11 @@ export const useTaxaNavData = ({ projectId, taxonomyId }) => {
       ownArray,
       urlPath,
       ownUrl,
-      label: `Taxa (${loading ? '...' : formatNumber(navs.length)})`,
+      label: buildNavLabel({
+        loading,
+        countFiltered: navs.length,
+        namePlural: 'Taxa',
+      }),
       nameSingular: 'Taxon',
       navs,
     }
