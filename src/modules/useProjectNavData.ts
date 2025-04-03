5c83adfce89630962f24a73fab3bdf526d4cb15e
Alexander Gabriel <alex.barbalex@gmail.com>
2025-04-03 15:16:31 +0200

    use nav data

--- src/modules/useProjectNavData.ts
+++ src/modules/useProjectNavData.ts
@@ -6,6 +6,7 @@ import isEqual from 'lodash/isEqual'
 import {
   subprojectsFilterAtom,
   projectReportsFilterAtom,
+  personsFilterAtom,
   treeOpenNodesAtom,
 } from '../store.ts'
 import { buildNavLabel } from './buildNavLabel.ts'
@@ -23,6 +24,10 @@ export const useProjectNavData = ({ projectId }) => {
     filterStringFromFilter(projectReportsFilter)
   const projectReportsIsFiltered = !!projectReportsFilterString
 
+  const [personsFilter] = useAtom(personsFilterAtom)
+  const personsFilterString = filterStringFromFilter(personsFilter)
+  const personsIsFiltered = !!personsFilterString
+
   const res = useLiveQuery(
     `
       WITH 
@@ -30,7 +35,9 @@ export const useProjectNavData = ({ projectId }) => {
         subprojects_count_filtered AS (SELECT count(*) FROM subprojects WHERE project_id = '${projectId}' ${subprojectIsFiltered ? ` AND ${subprojectsFilterString}` : ''} ),
         subprojects_names AS (SELECT subproject_name_singular, subproject_name_plural FROM projects WHERE project_id = '${projectId}'),
         project_reports_count_unfiltered AS (SELECT count(*) FROM project_reports WHERE project_id = '${projectId}'),
-        project_reports_count_filtered AS (SELECT count(*) FROM project_reports WHERE project_id = '${projectId}' ${projectReportsIsFiltered ? ` AND ${projectReportsFilterString}` : ''})
+        project_reports_count_filtered AS (SELECT count(*) FROM project_reports WHERE project_id = '${projectId}' ${projectReportsIsFiltered ? ` AND ${projectReportsFilterString}` : ''}),
+        persons_count_unfiltered AS (SELECT count(*) FROM persons WHERE project_id = '${projectId}'),
+        persons_count_filtered AS (SELECT count(*) FROM persons WHERE project_id = '${projectId}' ${personsIsFiltered ? ` AND ${personsFilterString}` : ''})
       SELECT
         project_id AS id,
         label,
@@ -39,15 +46,19 @@ export const useProjectNavData = ({ projectId }) => {
         subprojects_names.subproject_name_singular AS subprojects_name_singular,
         subprojects_names.subproject_name_plural AS subprojects_name_plural,
         project_reports_count_unfiltered.count AS project_reports_count_unfiltered,
-        project_reports_count_filtered.count AS project_reports_count_filtered
+        project_reports_count_filtered.count AS project_reports_count_filtered,
+        persons_count_unfiltered.count AS persons_count_unfiltered,
+        persons_count_filtered.count AS persons_count_filtered
       FROM 
         projects, 
         subprojects_count_unfiltered, 
         subprojects_count_filtered, 
         subprojects_names, 
         project_reports_count_unfiltered, 
-        project_reports_count_filtered
-      WHERE project_id = '${projectId}'`,
+        project_reports_count_filtered,
+        persons_count_unfiltered,
+        persons_count_filtered
+      WHERE projects.project_id = '${projectId}'`,
   )
   const loading = res === undefined
   const row = res?.rows?.[0]
@@ -95,14 +106,27 @@ export const useProjectNavData = ({ projectId }) => {
             namePlural: 'Reports',
           }),
         },
+        {
+          id: 'persons',
+          label: buildNavLabel({
+            loading,
+            isFiltered: personsIsFiltered,
+            countFiltered: row?.persons_count_filtered ?? 0,
+            countUnfiltered: row?.persons_count_unfiltered ?? 0,
+            namePlural: 'Persons',
+          }),
+        },
       ],
     }
   }, [
     loading,
     openNodes,
+    personsIsFiltered,
     projectReportsIsFiltered,
     row?.id,
     row?.label,
+    row?.persons_count_filtered,
+    row?.persons_count_unfiltered,
     row?.project_reports_count_filtered,
     row?.project_reports_count_unfiltered,
     row?.subprojects_count_filtered,
