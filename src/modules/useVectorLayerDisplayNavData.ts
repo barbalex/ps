5f6d516deaa091def988be0438834a801bb5187f
Alexander Gabriel <alex.barbalex@gmail.com>
2025-04-09 09:53:16 +0200

    work on breadcrumbs

---
+++ src/modules/useVectorLayerDisplayNavData.ts
@@ -0,0 +1,69 @@
+import { useMemo } from 'react'
+import { useLiveQuery } from '@electric-sql/pglite-react'
+import { useAtom } from 'jotai'
+import { useLocation } from '@tanstack/react-router'
+import isEqual from 'lodash/isEqual'
+
+import { treeOpenNodesAtom } from '../store.ts'
+
+export const useVectorLayerDisplayNavData = ({
+  projectId,
+  vectorLayerId,
+  vectorLayerDisplayId,
+}) => {
+  const [openNodes] = useAtom(treeOpenNodesAtom)
+  const location = useLocation()
+
+  const res = useLiveQuery(
+    `
+      SELECT
+        vector_layer_display_id AS id,
+        label
+      FROM vector_layer_displays 
+      WHERE vector_layer_display_id = $1`,
+    [vectorLayerDisplayId],
+  )
+
+  const loading = res === undefined
+
+  const navData = useMemo(() => {
+    const nav = res?.rows?.[0]
+    const parentArray = [
+      'data',
+      'projects',
+      projectId,
+      'vector-layers',
+      vectorLayerId,
+      'displays',
+    ]
+    const parentUrl = `/${parentArray.join('/')}`
+    const ownArray = [...parentArray, vectorLayerDisplayId]
+    const ownUrl = `/${ownArray.join('/')}`
+    // needs to work not only works for urlPath, for all opened paths!
+    const isOpen = openNodes.some((array) => isEqual(array, ownArray))
+    const urlPath = location.pathname.split('/').filter((p) => p !== '')
+    const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+    const isActive = isEqual(urlPath, ownArray)
+
+    return {
+      isInActiveNodeArray,
+      isActive,
+      isOpen,
+      parentUrl,
+      ownArray,
+      urlPath,
+      ownUrl,
+      label: nav?.label ?? nav?.id,
+      nameSingular: 'Vector Layer Display',
+    }
+  }, [
+    location.pathname,
+    openNodes,
+    projectId,
+    res?.rows,
+    vectorLayerDisplayId,
+    vectorLayerId,
+  ])
+
+  return { loading, navData }
+}
