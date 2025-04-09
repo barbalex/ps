cbf097b58ed2002e70c1995e36648f3036e6749f
Alexander Gabriel <alex.barbalex@gmail.com>
2025-04-09 09:58:21 +0200

    work on breadcrumbs

---
+++ src/modules/useWmsLayerNavData.ts
@@ -0,0 +1,54 @@
+import { useMemo } from 'react'
+import { useLiveQuery } from '@electric-sql/pglite-react'
+import { useAtom } from 'jotai'
+import { useLocation } from '@tanstack/react-router'
+import isEqual from 'lodash/isEqual'
+
+import { treeOpenNodesAtom } from '../store.ts'
+
+export const useWmsLayerNavData = ({ projectId, wmsLayerId }) => {
+  const [openNodes] = useAtom(treeOpenNodesAtom)
+  const location = useLocation()
+
+  const res = useLiveQuery(
+    `
+      SELECT
+        wms_layer_id AS id,
+        label
+      FROM wms_layers
+      WHERE
+        wms_layer_id = $1
+    `,
+    [wmsLayerId],
+  )
+
+  const loading = res === undefined
+
+  const navData = useMemo(() => {
+    const nav = res?.rows?.[0]
+
+    const parentArray = ['data', 'projects', projectId, 'wms-layers']
+    const ownArray = [...parentArray, wmsLayerId]
+    // needs to work not only works for urlPath, for all opened paths!
+    const isOpen = openNodes.some((array) => isEqual(array, ownArray))
+    const parentUrl = `/${parentArray.join('/')}`
+    const ownUrl = `/${ownArray.join('/')}`
+    const urlPath = location.pathname.split('/').filter((p) => p !== '')
+    const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+    const isActive = isEqual(urlPath, ownArray)
+
+    return {
+      isInActiveNodeArray,
+      isActive,
+      isOpen,
+      parentUrl,
+      ownArray,
+      urlPath,
+      ownUrl,
+      label: nav?.label ?? nav?.id,
+      nameSingular: 'WMS Layer',
+    }
+  }, [location.pathname, openNodes, projectId, res?.rows, wmsLayerId])
+
+  return { loading, navData }
+}
