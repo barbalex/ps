95b4b172da49f36375d5b4a2134f0fecdc81d031
Alexander Gabriel <alex.barbalex@gmail.com>
2025-04-09 09:30:24 +0200

    work on breadcrumbs

---
+++ src/modules/usePlaceLevelNavData.ts
@@ -0,0 +1,52 @@
+import { useMemo } from 'react'
+import { useLiveQuery } from '@electric-sql/pglite-react'
+import { useAtom } from 'jotai'
+import { useLocation } from '@tanstack/react-router'
+import isEqual from 'lodash/isEqual'
+
+import { buildNavLabel } from './buildNavLabel.ts'
+import { treeOpenNodesAtom } from '../store.ts'
+
+export const usePlaceLevelNavData = ({ projectId, placeLevelId }) => {
+  const [openNodes] = useAtom(treeOpenNodesAtom)
+  const location = useLocation()
+
+  const res = useLiveQuery(
+    `
+    SELECT
+      place_level_id AS id,
+      label 
+    FROM place_levels 
+    WHERE place_level_id = $1`,
+    [placeLevelId],
+  )
+
+  const loading = res === undefined
+
+  const navData = useMemo(() => {
+    const nav = res?.rows?.[0]
+    const parentArray = ['data', 'projects', projectId, 'place-levels']
+    const parentUrl = `/${parentArray.join('/')}`
+    const ownArray = [...parentArray, placeLevelId]
+    const ownUrl = `/${ownArray.join('/')}`
+    // needs to work not only works for urlPath, for all opened paths!
+    const isOpen = openNodes.some((array) => isEqual(array, ownArray))
+    const urlPath = location.pathname.split('/').filter((p) => p !== '')
+    const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+    const isActive = isEqual(urlPath, ownArray)
+
+    return {
+      isInActiveNodeArray,
+      isActive,
+      isOpen,
+      parentUrl,
+      ownArray,
+      urlPath,
+      ownUrl,
+      label: nav?.label ?? nav?.id,
+      nameSingular: 'Place Level',
+    }
+  }, [location.pathname, openNodes, placeLevelId, projectId, res?.rows])
+
+  return { loading, navData }
+}
