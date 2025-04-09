97cb241efe4e64004de737dbdf7198786c74ffd5
Alexander Gabriel <alex.barbalex@gmail.com>
2025-04-09 14:57:37 +0200

    work on breadcrumbs

---
+++ src/modules/useActionValueNavData.ts
@@ -0,0 +1,80 @@
+import { useMemo } from 'react'
+import { useLiveQuery } from '@electric-sql/pglite-react'
+import { useAtom } from 'jotai'
+import { useLocation } from '@tanstack/react-router'
+import isEqual from 'lodash/isEqual'
+
+import { treeOpenNodesAtom } from '../store.ts'
+
+export const useActionValueNavData = ({
+  projectId,
+  subprojectId,
+  placeId,
+  placeId2,
+  actionId,
+  actionValueId,
+}) => {
+  const [openNodes] = useAtom(treeOpenNodesAtom)
+  const location = useLocation()
+
+  const res = useLiveQuery(
+    `
+      SELECT
+        action_value_id as id,
+        label
+      FROM action_values 
+      WHERE action_value_id = $1`,
+    [actionValueId],
+  )
+
+  const loading = res === undefined
+
+  const navData = useMemo(() => {
+    const nav = res?.rows?.[0]
+    const parentArray = [
+      'data',
+      'projects',
+      projectId,
+      'subprojects',
+      subprojectId,
+      'places',
+      placeId,
+      ...(placeId2 ? ['places', placeId2] : []),
+      'actions',
+      actionId,
+      'values',
+    ]
+    const parentUrl = `/${parentArray.join('/')}`
+    const ownArray = [...parentArray, actionValueId]
+    const ownUrl = `/${ownArray.join('/')}`
+    // needs to work not only works for urlPath, for all opened paths!
+    const isOpen = openNodes.some((array) => isEqual(array, ownArray))
+    const urlPath = location.pathname.split('/').filter((p) => p !== '')
+    const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+    const isActive = isEqual(urlPath, ownArray)
+
+    return {
+      isInActiveNodeArray,
+      isActive,
+      isOpen,
+      parentUrl,
+      ownArray,
+      urlPath,
+      ownUrl,
+      label: nav?.label ?? nav?.id,
+      nameSingular: 'Action Value',
+    }
+  }, [
+    actionId,
+    actionValueId,
+    location.pathname,
+    openNodes,
+    placeId,
+    placeId2,
+    projectId,
+    res?.rows,
+    subprojectId,
+  ])
+
+  return { loading, navData }
+}
