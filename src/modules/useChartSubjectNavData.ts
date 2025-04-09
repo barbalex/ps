fa35a73abf85f87ebe5c8fb70d32134d40cd4745
Alexander Gabriel <alex.barbalex@gmail.com>
2025-04-09 10:09:15 +0200

    work on breadcrumbs

---
+++ src/modules/useChartSubjectNavData.ts
@@ -0,0 +1,77 @@
+import { useMemo } from 'react'
+import { useLiveQuery } from '@electric-sql/pglite-react'
+import { useAtom } from 'jotai'
+import { useLocation } from '@tanstack/react-router'
+import isEqual from 'lodash/isEqual'
+
+import { treeOpenNodesAtom } from '../store.ts'
+
+export const useChartSubjectNavData = ({
+  projectId,
+  subprojectId,
+  placeId,
+  placeId2,
+  chartId,
+  chartSubjectId,
+}) => {
+  const [openNodes] = useAtom(treeOpenNodesAtom)
+  const location = useLocation()
+
+  const res = useLiveQuery(
+    `
+      SELECT
+        chart_subject_id as id,
+        label 
+      FROM chart_subjects 
+      WHERE chart_subject_id = $1`,
+    [chartSubjectId],
+  )
+
+  const loading = res === undefined
+
+  const navData = useMemo(() => {
+    const nav = res?.rows?.[0]
+    const parentArray = [
+      'data',
+      ...(projectId ? ['projects', projectId] : []),
+      ...(subprojectId ? ['subprojects', subprojectId] : []),
+      ...(placeId ? ['places', placeId] : []),
+      ...(placeId2 ? ['places', placeId2] : []),
+      'charts',
+      chartId,
+      'subjects',
+    ]
+    const parentUrl = `/${parentArray.join('/')}`
+    const ownArray = [...parentArray, chartSubjectId]
+    const ownUrl = `/${ownArray.join('/')}`
+    // needs to work not only works for urlPath, for all opened paths!
+    const isOpen = openNodes.some((array) => isEqual(array, ownArray))
+    const urlPath = location.pathname.split('/').filter((p) => p !== '')
+    const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+    const isActive = isEqual(urlPath, ownArray)
+
+    return {
+      isInActiveNodeArray,
+      isActive,
+      isOpen,
+      parentUrl,
+      ownArray,
+      urlPath,
+      ownUrl,
+      label: nav?.label ?? nav?.id,
+      nameSingular: 'Chart Subject',
+    }
+  }, [
+    chartId,
+    chartSubjectId,
+    location.pathname,
+    openNodes,
+    placeId,
+    placeId2,
+    projectId,
+    res?.rows,
+    subprojectId,
+  ])
+
+  return { loading, navData }
+}
