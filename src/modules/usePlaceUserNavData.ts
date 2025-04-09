c77ff4076edda2291ae84422c13a5bd9edbf9243
Alexander Gabriel <alex.barbalex@gmail.com>
2025-04-09 14:53:09 +0200

    work on breadcrumbs

---
+++ src/modules/usePlaceUserNavData.ts
@@ -0,0 +1,76 @@
+import { useMemo } from 'react'
+import { useLiveQuery } from '@electric-sql/pglite-react'
+import { useAtom } from 'jotai'
+import { useLocation } from '@tanstack/react-router'
+import isEqual from 'lodash/isEqual'
+
+import { treeOpenNodesAtom } from '../store.ts'
+
+export const usePlaceUserNavData = ({
+  projectId,
+  subprojectId,
+  placeId,
+  placeId2,
+  placeUserId,
+}) => {
+  const [openNodes] = useAtom(treeOpenNodesAtom)
+  const location = useLocation()
+
+  const res = useLiveQuery(
+    `
+      SELECT
+        place_user_id AS id,
+        label
+      FROM place_users 
+      WHERE place_user_id = $1`,
+    [placeUserId],
+  )
+
+  const loading = res === undefined
+
+  const navData = useMemo(() => {
+    const nav = res?.rows?.[0]
+    const parentArray = [
+      'data',
+      'projects',
+      projectId,
+      'subprojects',
+      subprojectId,
+      'places',
+      placeId,
+      ...(placeId2 ? ['places', placeId2] : []),
+      'users',
+    ]
+    const parentUrl = `/${parentArray.join('/')}`
+    const ownArray = [...parentArray, placeUserId]
+    const ownUrl = `/${ownArray.join('/')}`
+    // needs to work not only works for urlPath, for all opened paths!
+    const isOpen = openNodes.some((array) => isEqual(array, ownArray))
+    const urlPath = location.pathname.split('/').filter((p) => p !== '')
+    const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+    const isActive = isEqual(urlPath, ownArray)
+
+    return {
+      isInActiveNodeArray,
+      isActive,
+      isOpen,
+      parentUrl,
+      ownArray,
+      urlPath,
+      ownUrl,
+      label: nav?.label ?? nav?.id,
+      nameSingular: 'User',
+    }
+  }, [
+    location.pathname,
+    openNodes,
+    placeId,
+    placeId2,
+    placeUserId,
+    projectId,
+    res?.rows,
+    subprojectId,
+  ])
+
+  return { loading, navData }
+}
