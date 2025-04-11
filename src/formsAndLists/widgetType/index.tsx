ff521744b25485daec06e1199d38e9b6c09dd524
Alexander Gabriel <alex.barbalex@gmail.com>
2025-04-11 11:30:28 +0200

    work on not found

--- src/formsAndLists/widgetType/index.tsx
+++ src/formsAndLists/widgetType/index.tsx
@@ -7,6 +7,7 @@ import { getValueFromChange } from '../../modules/getValueFromChange.ts'
 import { Header } from './Header.tsx'
 import { Loading } from '../../components/shared/Loading.tsx'
 import { WidgetTypeForm as Form } from './Form.tsx'
+import { NotFound } from '../../components/NotFound.tsx'
 
 import '../../form.css'
 
@@ -37,13 +38,26 @@ export const WidgetType = memo(() => {
     [db, row, widgetTypeId],
   )
 
-  if (!row) return <Loading />
+  if (!res) return <Loading />
+
+  if (!row) {
+    return (
+      <NotFound
+        table="Widget Type"
+        id={widgetTypeId}
+      />
+    )
+  }
 
   return (
     <div className="form-outer-container">
       <Header autoFocusRef={autoFocusRef} />
       <div className="form-container">
-        <Form onChange={onChange} row={row} autoFocusRef={autoFocusRef} />
+        <Form
+          onChange={onChange}
+          row={row}
+          autoFocusRef={autoFocusRef}
+        />
       </div>
     </div>
   )
