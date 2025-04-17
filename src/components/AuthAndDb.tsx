a6f3b294541389461bff812ed1d6106200ada4a3
Alexander Gabriel <alex.barbalex@gmail.com>
2025-04-17 11:02:08 +0200

    deactivate syncer

--- src/components/AuthAndDb.tsx
+++ src/components/AuthAndDb.tsx
@@ -20,7 +20,7 @@ export const AuthAndDb = memo(() => {
       <Suspense fallback="loading...">
         <SqlInitializer />
       </Suspense>
-      <Syncer />
+      {/* <Syncer /> */}
       <TreeOpenNodesSetter />
       <Layout />
     </CorbadoProvider>
