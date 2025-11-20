4cb1fb5a09ae37f2b9baf76488f0871a169e0658
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-20 16:42:52 +0100

    de-memoize

--- src/components/shared/FieldList/index.tsx
+++ src/components/shared/FieldList/index.tsx
@@ -1,6 +1,4 @@
-import { memo, useMemo, useCallback } from 'react'
 import { Field, TagGroup, Tag } from '@fluentui/react-components'
-import type { InputProps } from '@fluentui/react-components'
 import { useParams } from '@tanstack/react-router'
 import { usePGlite, useLiveQuery } from '@electric-sql/pglite-react'
 
@@ -17,97 +15,94 @@ interface Props {
   from: string
 }
 
-export const FieldList = memo(
-  ({ name, label, table, fieldsTable, id, valueArray = [], from }: Props) => {
-    const { projectId } = useParams({ from })
+export const FieldList = ({
+  name,
+  label,
+  table,
+  fieldsTable,
+  id,
+  valueArray = [],
+  from,
+}: Props) => {
+  const { projectId } = useParams({ from })
 
-    const db = usePGlite()
-    const res = useLiveQuery(
-      `
+  const db = usePGlite()
+  const res = useLiveQuery(
+    `
       SELECT name 
       FROM fields 
       WHERE project_id = $1 AND table_name = $2 
       ORDER BY table_name, label`,
-      [projectId, fieldsTable],
-    )
-    const fields = useMemo(() => res?.rows ?? [], [res])
-    const options = useMemo(() => fields.map(({ name }) => name), [fields])
-    const unusedOptions = useMemo(
-      () => options.filter((o) => !valueArray.includes(o)),
-      [options, valueArray],
-    )
+    [projectId, fieldsTable],
+  )
+  const fields = res?.rows ?? []
+  const options = fields.map(({ name }) => name)
+  const unusedOptions = options.filter((o) => !valueArray.includes(o))
 
-    const removeItem = useCallback(
-      (e, { value }) => {
-        const idField = idFieldFromTable(table)
-        // TODO: test
-        db.query(
-          `UPDATE ${table} SET data = jsonb_set(data, '{${name}}', $1) WHERE ${idField} = $2`,
-          [valueArray.filter((v) => v !== value), id],
-        )
-      },
-      [db, id, name, table, valueArray],
+  const removeItem = (e, { value }) => {
+    const idField = idFieldFromTable(table)
+    // TODO: test
+    db.query(
+      `UPDATE ${table} SET data = jsonb_set(data, '{${name}}', $1) WHERE ${idField} = $2`,
+      [valueArray.filter((v) => v !== value), id],
     )
+  }
 
-    const onChange = useCallback<InputProps['onChange']>(
-      ({ value, previousValue }) => {
-        let val = [...valueArray]
-        if (!value) {
-          // need to remove the key from the json object
-          val = val.filter((v) => v !== previousValue)
-        } else {
-          // replace the previous value with the new value
-          const index = val.findIndex((v) => v === previousValue)
-          if (index !== -1) {
-            val[index] = value
-          } else {
-            val.push(value)
-          }
-        }
-        const idField = idFieldFromTable(table)
-        db.query(
-          `UPDATE ${table} SET data = jsonb_set(data, '{${name}}', $1) WHERE ${idField} = $2`,
-          [val, id],
-        )
-      },
-      [db, id, name, table, valueArray],
+  const onChange = ({ value, previousValue }) => {
+    let val = [...valueArray]
+    if (!value) {
+      // need to remove the key from the json object
+      val = val.filter((v) => v !== previousValue)
+    } else {
+      // replace the previous value with the new value
+      const index = val.findIndex((v) => v === previousValue)
+      if (index !== -1) {
+        val[index] = value
+      } else {
+        val.push(value)
+      }
+    }
+    const idField = idFieldFromTable(table)
+    db.query(
+      `UPDATE ${table} SET data = jsonb_set(data, '{${name}}', $1) WHERE ${idField} = $2`,
+      [val, id],
     )
+  }
 
-    return (
-      <Field
-        label={label ?? '(no label provided)'}
-        validationState="none"
-        validationMessage={
-          <>
-            <div>{`Add multiple items in the order you want ${fieldsTable} to be ordered.`}</div>
-            <div>{`If no value is set, ${fieldsTable} are ordered by label.`}</div>
-          </>
-        }
-      >
-        <TagGroup onDismiss={removeItem}>
-          {valueArray.map((value) => (
-            <Tag
-              key={value}
-              dismissible
-              dismissIcon={{ 'aria-label': 'remove' }}
-              value={value}
-              secondaryText={
-                !options.includes(value) ? 'not defined in fields' : undefined
-              }
-            >
-              {value}
-            </Tag>
-          ))}
-          {unusedOptions.length > 0 && (
-            <DropdownField
-              placeholder="Select an option"
-              value={''}
-              onChange={onChange}
-              options={unusedOptions}
-            />
-          )}
-        </TagGroup>
-      </Field>
-    )
-  },
-)
+  return (
+    <Field
+      label={label ?? '(no label provided)'}
+      validationState="none"
+      validationMessage={
+        <>
+          <div>{`Add multiple items in the order you want ${fieldsTable} to be ordered.`}</div>
+          <div>{`If no value is set, ${fieldsTable} are ordered by label.`}</div>
+        </>
+      }
+    >
+      <TagGroup onDismiss={removeItem}>
+        {valueArray.map((value) => (
+          <Tag
+            key={value}
+            dismissible
+            dismissIcon={{ 'aria-label': 'remove' }}
+            value={value}
+            secondaryText={
+              !options.includes(value) ? 'not defined in fields' : undefined
+            }
+          >
+            {value}
+          </Tag>
+        ))}
+        {unusedOptions.length > 0 && (
+          <DropdownField
+            placeholder="Select an option"
+            value={''}
+            onChange={onChange}
+            options={unusedOptions}
+          />
+        )}
+      </TagGroup>
+    </Field>
+  )
+}
