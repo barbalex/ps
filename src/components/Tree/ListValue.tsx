e30bba62ddbc1c142b6320321d688eb2b8f026c3
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-20 18:25:40 +0100

    de-memoize

--- src/components/Tree/ListValue.tsx
+++ src/components/Tree/ListValue.tsx
@@ -1,41 +1,35 @@
-import { memo, useMemo } from 'react'
 import { useLocation } from '@tanstack/react-router'
 import { isEqual } from 'es-toolkit'
 
 import { Node } from './Node.tsx'
 
-export const ListValueNode = memo(
-  ({ projectId, listId, listValue, level = 6 }) => {
-    const location = useLocation()
+export const ListValueNode = ({ projectId, listId, listValue, level = 6 }) => {
+  const location = useLocation()
 
-    const urlPath = location.pathname.split('/').filter((p) => p !== '')
-    const ownArray = useMemo(
-      () => [
-        'data',
-        'projects',
-        projectId,
-        'lists',
-        listId,
-        'values',
-        listValue.list_value_id,
-      ],
-      [projectId, listId, listValue.list_value_id],
-    )
-    const ownUrl = `/${ownArray.join('/')}`
+  const urlPath = location.pathname.split('/').filter((p) => p !== '')
+  const ownArray = [
+    'data',
+    'projects',
+    projectId,
+    'lists',
+    listId,
+    'values',
+    listValue.list_value_id,
+  ]
+  const ownUrl = `/${ownArray.join('/')}`
 
-    const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
-    const isActive = isEqual(urlPath, ownArray)
+  const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+  const isActive = isEqual(urlPath, ownArray)
 
-    return (
-      <Node
-        label={listValue.label}
-        id={listValue.list_value_id}
-        level={level}
-        isInActiveNodeArray={isInActiveNodeArray}
-        isActive={isActive}
-        childrenCount={0}
-        to={ownUrl}
-      />
-    )
-  },
-)
+  return (
+    <Node
+      label={listValue.label}
+      id={listValue.list_value_id}
+      level={level}
+      isInActiveNodeArray={isInActiveNodeArray}
+      isActive={isActive}
+      childrenCount={0}
+      to={ownUrl}
+    />
+  )
+}
