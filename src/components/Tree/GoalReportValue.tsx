c24dcedd370a084e95918c52956e338930d1818d
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-20 18:22:57 +0100

    de-memoize

--- src/components/Tree/GoalReportValue.tsx
+++ src/components/Tree/GoalReportValue.tsx
@@ -1,45 +1,46 @@
-import { memo, useMemo } from 'react'
 import { useLocation } from '@tanstack/react-router'
 import { isEqual } from 'es-toolkit'
 
 import { Node } from './Node.tsx'
 
-export const GoalReportValueNode = memo(
-  ({ projectId, subprojectId, goalId, goalReportId, nav, level = 10 }) => {
-    const location = useLocation()
+export const GoalReportValueNode = ({
+  projectId,
+  subprojectId,
+  goalId,
+  goalReportId,
+  nav,
+  level = 10,
+}) => {
+  const location = useLocation()
 
-    const urlPath = location.pathname.split('/').filter((p) => p !== '')
-    const ownArray = useMemo(
-      () => [
-        'data',
-        'projects',
-        projectId,
-        'subprojects',
-        subprojectId,
-        'goals',
-        goalId,
-        'reports',
-        goalReportId,
-        'values',
-        nav.id,
-      ],
-      [projectId, subprojectId, goalId, goalReportId, nav.id],
-    )
-    const ownUrl = `/${ownArray.join('/')}`
+  const urlPath = location.pathname.split('/').filter((p) => p !== '')
+  const ownArray = [
+    'data',
+    'projects',
+    projectId,
+    'subprojects',
+    subprojectId,
+    'goals',
+    goalId,
+    'reports',
+    goalReportId,
+    'values',
+    nav.id,
+  ]
+  const ownUrl = `/${ownArray.join('/')}`
 
-    const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
-    const isActive = isEqual(urlPath, ownArray)
+  const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+  const isActive = isEqual(urlPath, ownArray)
 
-    return (
-      <Node
-        label={nav.label}
-        id={nav.id}
-        level={level}
-        isInActiveNodeArray={isInActiveNodeArray}
-        isActive={isActive}
-        childrenCount={0}
-        to={ownUrl}
-      />
-    )
-  },
-)
+  return (
+    <Node
+      label={nav.label}
+      id={nav.id}
+      level={level}
+      isInActiveNodeArray={isInActiveNodeArray}
+      isActive={isActive}
+      childrenCount={0}
+      to={ownUrl}
+    />
+  )
+}
--- src/components/Tree/GoalReports.tsx
+++ src/components/Tree/GoalReports.tsx
@@ -1,4 +1,3 @@
-import { useCallback, memo } from 'react'
 import { useNavigate } from '@tanstack/react-router'
 
 import { Node } from './Node.tsx'
@@ -14,73 +13,69 @@ interface Props {
   level?: number
 }
 
-export const GoalReportsNode = memo(
-  ({ projectId, subprojectId, goalId, level = 7 }: Props) => {
-    const navigate = useNavigate()
+export const GoalReportsNode = ({
+  projectId,
+  subprojectId,
+  goalId,
+  level = 7,
+}: Props) => {
+  const navigate = useNavigate()
 
-    const { navData } = useGoalReportsNavData({
-      projectId,
-      subprojectId,
-      goalId,
-    })
-    const {
-      label,
-      parentUrl,
-      ownArray,
-      ownUrl,
-      urlPath,
-      isOpen,
-      isInActiveNodeArray,
-      isActive,
-      navs,
-    } = navData
+  const { navData } = useGoalReportsNavData({
+    projectId,
+    subprojectId,
+    goalId,
+  })
+  const {
+    label,
+    parentUrl,
+    ownArray,
+    ownUrl,
+    urlPath,
+    isOpen,
+    isInActiveNodeArray,
+    isActive,
+    navs,
+  } = navData
 
-    const onClickButton = useCallback(() => {
-      if (isOpen) {
-        removeChildNodes({ node: ownArray })
-        // only navigate if urlPath includes ownArray
-        if (isInActiveNodeArray && ownArray.length <= urlPath.length) {
-          navigate({ to: parentUrl })
-        }
-        return
+  const onClickButton = () => {
+    if (isOpen) {
+      removeChildNodes({ node: ownArray })
+      // only navigate if urlPath includes ownArray
+      if (isInActiveNodeArray && ownArray.length <= urlPath.length) {
+        navigate({ to: parentUrl })
       }
-      // add to openNodes without navigating
-      addOpenNodes({ nodes: [ownArray] })
-    }, [
-      isInActiveNodeArray,
-      isOpen,
-      navigate,
-      ownArray,
-      parentUrl,
-      urlPath.length,
-    ])
+      return
+    }
+    // add to openNodes without navigating
+    addOpenNodes({ nodes: [ownArray] })
+  }
 
-    // only list navs if isOpen AND the first nav has an id
-    const showNavs = isOpen && navs.length > 0 && navs[0].id
+  // only list navs if isOpen AND the first nav has an id
+  const showNavs = isOpen && navs.length > 0 && navs[0].id
 
-    return (
-      <>
-        <Node
-          label={label}
-          level={level}
-          isOpen={isOpen}
-          isInActiveNodeArray={isInActiveNodeArray}
-          isActive={isActive}
-          childrenCount={navs.length}
-          to={ownUrl}
-          onClickButton={onClickButton}
-        />
-        {showNavs &&
-          navs.map((nav, i) => (
-            <GoalReportNode
-              key={`${nav.id}-${i}`}
-              projectId={projectId}
-              subprojectId={subprojectId}
-              goalId={goalId}
-              nav={nav}
-            />
-          ))}
-      </>
-    )
-  },
-)
+  return (
+    <>
+      <Node
+        label={label}
+        level={level}
+        isOpen={isOpen}
+        isInActiveNodeArray={isInActiveNodeArray}
+        isActive={isActive}
+        childrenCount={navs.length}
+        to={ownUrl}
+        onClickButton={onClickButton}
+      />
+      {showNavs &&
+        navs.map((nav, i) => (
+          <GoalReportNode
+            key={`${nav.id}-${i}`}
+            projectId={projectId}
+            subprojectId={subprojectId}
+            goalId={goalId}
+            nav={nav}
+          />
+        ))}
+    </>
+  )
+}
