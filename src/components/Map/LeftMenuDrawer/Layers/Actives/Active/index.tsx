c100b85c44ec7f000cbfeeb96a413974c4c100eb
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-20 14:00:31 +0100

    de-memoize

--- src/components/Map/LeftMenuDrawer/Layers/Actives/Active/index.tsx
+++ src/components/Map/LeftMenuDrawer/Layers/Actives/Active/index.tsx
@@ -1,4 +1,4 @@
-import { memo, useRef, useEffect, useState, useContext, useMemo } from 'react'
+import { useRef, useEffect, useState, useContext } from 'react'
 import { createPortal } from 'react-dom'
 import { AccordionItem } from '@fluentui/react-components'
 import { DropIndicator } from '@atlaskit/pragmatic-drag-and-drop-react-drop-indicator/box'
@@ -51,148 +51,143 @@ const previewStyle = {
   borderRadius: '0.25rem',
 }
 
-export const ActiveLayer = memo(
-  ({ layer, index, isLast, isOpen, layerCount }) => {
-    const { registerItem, instanceId } = useDragAndDropContext()
-    const [closestEdge, setClosestEdge] = useState<Edge | null>(null)
-    const elementRef = useRef<HTMLDivElement>(null)
-    const dragHandleRef = useRef<HTMLDivElement>(null)
-    const [draggableState, setDraggableState] =
-      useState<DraggableState>(idleState)
+export const ActiveLayer = ({ layer, index, isLast, isOpen, layerCount }) => {
+  const { registerItem, instanceId } = useDragAndDropContext()
+  const [closestEdge, setClosestEdge] = useState<Edge | null>(null)
+  const elementRef = useRef<HTMLDivElement>(null)
+  const dragHandleRef = useRef<HTMLDivElement>(null)
+  const [draggableState, setDraggableState] =
+    useState<DraggableState>(idleState)
 
-    useEffect(() => {
-      const element = elementRef.current
-      const dragHandle = dragHandleRef.current
-      if (!element || !dragHandle) return
+  useEffect(() => {
+    const element = elementRef.current
+    const dragHandle = dragHandleRef.current
+    if (!element || !dragHandle) return
 
-      invariant(element)
-      invariant(dragHandle)
+    invariant(element)
+    invariant(dragHandle)
 
-      const data = getItemData({ layer, index, instanceId })
+    const data = getItemData({ layer, index, instanceId })
 
-      // draggable returns its cleanup function
-      return combine(
-        registerItem({
-          itemId: layer.layer_presentation_id,
-          element,
-        }),
-        draggable({
-          element: dragHandle,
-          canDrag: () => layerCount > 1,
-          getInitialData: () => data,
-          onGenerateDragPreview({ nativeSetDragImage }) {
-            setCustomNativeDragPreview({
-              nativeSetDragImage,
-              getOffset: pointerOutsideOfPreview({ x: '16px', y: '8px' }),
-              render({ container }) {
-                setDraggableState({ type: 'preview', container })
-
-                return () => setDraggableState(draggingState)
-              },
-            })
-          },
-          onDragStart() {
-            setDraggableState(draggingState)
-          },
-          onDrop() {
-            setDraggableState(idleState)
-          },
-        }),
-        dropTargetForElements({
-          element,
-          canDrop({ source }) {
-            return (
-              isItemData(source.data) && source.data.instanceId === instanceId
-            )
-          },
-          getData({ input }) {
-            return attachClosestEdge(data, {
-              element,
-              input,
-              allowedEdges: ['top', 'bottom'],
-            })
-          },
-          onDrag({ self, source }) {
-            const isSource =
-              source.data.layer.layer_presentation_id ===
-              element.dataset.presentationId
-            if (isSource) {
-              setClosestEdge(null)
-              return
-            }
-
-            const closestEdge = extractClosestEdge(self.data)
-
-            const sourceIndex = source.data.index
-            invariant(typeof sourceIndex === 'number')
-
-            const isItemBeforeSource = index === sourceIndex - 1
-            const isItemAfterSource = index === sourceIndex + 1
-
-            const isDropIndicatorHidden =
-              (isItemBeforeSource && closestEdge === 'bottom') ||
-              (isItemAfterSource && closestEdge === 'top')
-
-            if (isDropIndicatorHidden) {
-              setClosestEdge(null)
-              return
-            }
-
-            setClosestEdge(closestEdge)
-          },
-          onDragLeave() {
-            setClosestEdge(null)
-          },
-          onDrop() {
-            setClosestEdge(null)
-          },
-        }),
-      )
-    }, [index, instanceId, layer, layerCount, registerItem])
-
-    const accordionStyle = useMemo(
-      () => ({
-        // needed for the drop indicator to appear
-        position: 'relative',
-        borderTop: `${isOpen ? 3 : 1}px solid rgba(55, 118, 28, 0.5)`,
-        ...(isLast ?
-          {
-            borderBottom: `1px solid rgba(55, 118, 28, 0.5)`,
-          }
-        : {}),
-        ...(isOpen ? { borderBottom: `3px solid rgba(55, 118, 28, 0.5)` } : {}),
+    // draggable returns its cleanup function
+    return combine(
+      registerItem({
+        itemId: layer.layer_presentation_id,
+        element,
       }),
-      [isLast, isOpen],
-    )
+      draggable({
+        element: dragHandle,
+        canDrag: () => layerCount > 1,
+        getInitialData: () => data,
+        onGenerateDragPreview({ nativeSetDragImage }) {
+          setCustomNativeDragPreview({
+            nativeSetDragImage,
+            getOffset: pointerOutsideOfPreview({ x: '16px', y: '8px' }),
+            render({ container }) {
+              setDraggableState({ type: 'preview', container })
 
-    // drag and drop items by dragging the drag icon
-    // https://atlassian.design/components/pragmatic-drag-and-drop/core-package
-    return (
-      <ErrorBoundary>
-        <AccordionItem
-          value={layer.layer_presentation_id}
-          ref={elementRef}
-          style={accordionStyle}
-        >
-          <Content
-            layer={layer}
-            isOpen={isOpen}
-            layerCount={layerCount}
-            dragHandleRef={dragHandleRef}
-          />
-          {closestEdge && (
-            <DropIndicator
-              edge={closestEdge}
-              gap="1px"
-            />
-          )}
-        </AccordionItem>
-        {draggableState.type === 'preview' &&
-          createPortal(
-            <div style={previewStyle}>{layer.label}</div>,
-            draggableState.container,
-          )}
-      </ErrorBoundary>
+              return () => setDraggableState(draggingState)
+            },
+          })
+        },
+        onDragStart() {
+          setDraggableState(draggingState)
+        },
+        onDrop() {
+          setDraggableState(idleState)
+        },
+      }),
+      dropTargetForElements({
+        element,
+        canDrop({ source }) {
+          return (
+            isItemData(source.data) && source.data.instanceId === instanceId
+          )
+        },
+        getData({ input }) {
+          return attachClosestEdge(data, {
+            element,
+            input,
+            allowedEdges: ['top', 'bottom'],
+          })
+        },
+        onDrag({ self, source }) {
+          const isSource =
+            source.data.layer.layer_presentation_id ===
+            element.dataset.presentationId
+          if (isSource) {
+            setClosestEdge(null)
+            return
+          }
+
+          const closestEdge = extractClosestEdge(self.data)
+
+          const sourceIndex = source.data.index
+          invariant(typeof sourceIndex === 'number')
+
+          const isItemBeforeSource = index === sourceIndex - 1
+          const isItemAfterSource = index === sourceIndex + 1
+
+          const isDropIndicatorHidden =
+            (isItemBeforeSource && closestEdge === 'bottom') ||
+            (isItemAfterSource && closestEdge === 'top')
+
+          if (isDropIndicatorHidden) {
+            setClosestEdge(null)
+            return
+          }
+
+          setClosestEdge(closestEdge)
+        },
+        onDragLeave() {
+          setClosestEdge(null)
+        },
+        onDrop() {
+          setClosestEdge(null)
+        },
+      }),
     )
-  },
-)
+  }, [index, instanceId, layer, layerCount, registerItem])
+
+  const accordionStyle = {
+    // needed for the drop indicator to appear
+    position: 'relative',
+    borderTop: `${isOpen ? 3 : 1}px solid rgba(55, 118, 28, 0.5)`,
+    ...(isLast ?
+      {
+        borderBottom: `1px solid rgba(55, 118, 28, 0.5)`,
+      }
+    : {}),
+    ...(isOpen ? { borderBottom: `3px solid rgba(55, 118, 28, 0.5)` } : {}),
+  }
+
+  // drag and drop items by dragging the drag icon
+  // https://atlassian.design/components/pragmatic-drag-and-drop/core-package
+  return (
+    <ErrorBoundary>
+      <AccordionItem
+        value={layer.layer_presentation_id}
+        ref={elementRef}
+        style={accordionStyle}
+      >
+        <Content
+          layer={layer}
+          isOpen={isOpen}
+          layerCount={layerCount}
+          dragHandleRef={dragHandleRef}
+        />
+        {closestEdge && (
+          <DropIndicator
+            edge={closestEdge}
+            gap="1px"
+          />
+        )}
+      </AccordionItem>
+      {draggableState.type === 'preview' &&
+        createPortal(
+          <div style={previewStyle}>{layer.label}</div>,
+          draggableState.container,
+        )}
+    </ErrorBoundary>
+  )
+}
