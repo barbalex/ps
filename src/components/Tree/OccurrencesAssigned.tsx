69f3ca92b1654b9a294c58a72aa8264b548406bd
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-20 18:31:22 +0100

    de-memoize

--- src/components/Tree/OccurrencesAssigned.tsx
+++ src/components/Tree/OccurrencesAssigned.tsx
@@ -1,4 +1,3 @@
-import { useCallback, memo } from 'react'
 import { useNavigate } from '@tanstack/react-router'
 
 import { Node } from './Node.tsx'
@@ -7,75 +6,72 @@ import { removeChildNodes } from '../../modules/tree/removeChildNodes.ts'
 import { addOpenNodes } from '../../modules/tree/addOpenNodes.ts'
 import { useOccurrencesNavData } from '../../modules/useOccurrencesNavData.ts'
 
-export const OccurrencesAssignedNode = memo(
-  ({ projectId, subprojectId, placeId, placeId2, level = 7 }) => {
-    const navigate = useNavigate()
+export const OccurrencesAssignedNode = ({
+  projectId,
+  subprojectId,
+  placeId,
+  placeId2,
+  level = 7,
+}) => {
+  const navigate = useNavigate()
 
-    const { navData } = useOccurrencesNavData({
-      projectId,
-      subprojectId,
-      placeId,
-      placeId2,
-      isToAssess: false,
-      isNotToAssign: false,
-    })
-    const {
-      label,
-      parentUrl,
-      ownArray,
-      ownUrl,
-      urlPath,
-      isOpen,
-      isInActiveNodeArray,
-      isActive,
-      navs,
-    } = navData
+  const { navData } = useOccurrencesNavData({
+    projectId,
+    subprojectId,
+    placeId,
+    placeId2,
+    isToAssess: false,
+    isNotToAssign: false,
+  })
+  const {
+    label,
+    parentUrl,
+    ownArray,
+    ownUrl,
+    urlPath,
+    isOpen,
+    isInActiveNodeArray,
+    isActive,
+    navs,
+  } = navData
 
-    const onClickButton = useCallback(() => {
-      if (isOpen) {
-        removeChildNodes({ node: ownArray })
-        // only navigate if urlPath includes ownArray
-        if (isInActiveNodeArray && ownArray.length <= urlPath.length) {
-          navigate({ to: parentUrl })
-        }
-        return
+  const onClickButton = () => {
+    if (isOpen) {
+      removeChildNodes({ node: ownArray })
+      // only navigate if urlPath includes ownArray
+      if (isInActiveNodeArray && ownArray.length <= urlPath.length) {
+        navigate({ to: parentUrl })
       }
-      // add to openNodes without navigating
-      addOpenNodes({ nodes: [ownArray] })
-    }, [
-      isInActiveNodeArray,
-      isOpen,
-      navigate,
-      ownArray,
-      parentUrl,
-      urlPath.length,
-    ])
+      return
+    }
+    // add to openNodes without navigating
+    addOpenNodes({ nodes: [ownArray] })
+  }
 
-    return (
-      <>
-        <Node
-          label={label}
-          level={level}
-          isOpen={isOpen}
-          isInActiveNodeArray={isInActiveNodeArray}
-          isActive={isActive}
-          childrenCount={navs.length}
-          to={ownUrl}
-          onClickButton={onClickButton}
-        />
-        {isOpen &&
-          navs.map((nav) => (
-            <OccurrenceAssignedNode
-              key={nav.id}
-              projectId={projectId}
-              subprojectId={subprojectId}
-              placeId={placeId}
-              placeId2={placeId2}
-              nav={nav}
-              level={level + 1}
-            />
-          ))}
-      </>
-    )
-  },
-)
+  return (
+    <>
+      <Node
+        label={label}
+        level={level}
+        isOpen={isOpen}
+        isInActiveNodeArray={isInActiveNodeArray}
+        isActive={isActive}
+        childrenCount={navs.length}
+        to={ownUrl}
+        onClickButton={onClickButton}
+      />
+      {isOpen &&
+        navs.map((nav) => (
+          <OccurrenceAssignedNode
+            key={nav.id}
+            projectId={projectId}
+            subprojectId={subprojectId}
+            placeId={placeId}
+            placeId2={placeId2}
+            nav={nav}
+            level={level + 1}
+          />
+        ))}
+    </>
+  )
+}
