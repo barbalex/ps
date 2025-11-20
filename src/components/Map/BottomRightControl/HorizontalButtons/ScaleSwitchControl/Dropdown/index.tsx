c8ec22c314c8e923ea50d18069e354f4e2aafe45
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-20 11:12:36 +0100

    de-memoize

--- src/components/Map/BottomRightControl/HorizontalButtons/ScaleSwitchControl/Dropdown/index.tsx
+++ src/components/Map/BottomRightControl/HorizontalButtons/ScaleSwitchControl/Dropdown/index.tsx
@@ -1,4 +1,3 @@
-import { memo } from 'react'
 import { createPortal } from 'react-dom'
 
 import { Item } from './Item.tsx'
@@ -14,46 +13,44 @@ const dropdownStyle = {
   zIndex: 1000,
 }
 
-export const Dropdown = memo(
-  ({
-    scales = [
-      2000, 5000, 10000, 25000, 50000, 100000, 200000, 500000, 1000000, 2500000,
-      5000000, 10000000,
-    ],
-    open,
-    boundingRect,
-    width,
-    close,
-    pixelsInMeterWidth,
-  }) => {
-    if (!open) return null
-    if (!boundingRect) return null
+export const Dropdown = ({
+  scales = [
+    2000, 5000, 10000, 25000, 50000, 100000, 200000, 500000, 1000000, 2500000,
+    5000000, 10000000,
+  ],
+  open,
+  boundingRect,
+  width,
+  close,
+  pixelsInMeterWidth,
+}) => {
+  if (!open) return null
+  if (!boundingRect) return null
 
-    const rootHeight = document.getElementById('root')?.clientHeight
+  const rootHeight = document.getElementById('root')?.clientHeight
 
-    return createPortal(
-      <div
-        style={{
-          ...dropdownStyle,
-          maxHeight: open ? `${scales.length * 2}em` : 0,
-          border: open ? '1px solid black' : 'none',
-          borderBottom: 'none',
-          bottom: rootHeight - boundingRect.y - 1,
-          left: boundingRect.x,
-          width,
-        }}
-      >
-        {scales.map((scale) => (
-          <Item
-            key={scale}
-            scale={scale}
-            close={close}
-            pixelsInMeterWidth={pixelsInMeterWidth}
-          />
-        ))}
-      </div>,
-      // insert below root to get all the style configs (font-family etc.)
-      document.getElementById('router-container'),
-    )
-  },
-)
+  return createPortal(
+    <div
+      style={{
+        ...dropdownStyle,
+        maxHeight: open ? `${scales.length * 2}em` : 0,
+        border: open ? '1px solid black' : 'none',
+        borderBottom: 'none',
+        bottom: rootHeight - boundingRect.y - 1,
+        left: boundingRect.x,
+        width,
+      }}
+    >
+      {scales.map((scale) => (
+        <Item
+          key={scale}
+          scale={scale}
+          close={close}
+          pixelsInMeterWidth={pixelsInMeterWidth}
+        />
+      ))}
+    </div>,
+    // insert below root to get all the style configs (font-family etc.)
+    document.getElementById('router-container'),
+  )
+}
