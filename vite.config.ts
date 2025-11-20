81f0b9561bddbda1d8217f2a142ab47f00ec0606
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-20 10:46:41 +0100

    try

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,10 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
-    react({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      },
-    }),
+    // TODO: the compiler provokes: "Error: Rendered fewer hooks than expected"
+    // WHEN breadcrumbs are rendered
+    react({ babel: { plugins: ['babel-plugin-react-compiler'] } }),
+    // react(),
     svgr(),
   ],
   envPrefix: 'ELECTRIC_',
