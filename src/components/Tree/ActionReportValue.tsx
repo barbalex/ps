8c2e7cde107ec67ed972bd4529aecebedeea865e
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-20 17:13:10 +0100

    de-memoize

--- src/components/Tree/ActionReportValue.tsx
+++ src/components/Tree/ActionReportValue.tsx
@@ -1,61 +1,52 @@
-import { memo, useMemo } from 'react'
 import { useLocation } from '@tanstack/react-router'
 import { isEqual } from 'es-toolkit'
 
 import { Node } from './Node.tsx'
 
-export const ActionReportValueNode = memo(
-  ({
+export const ActionReportValueNode = ({
+  projectId,
+  subprojectId,
+  placeId,
+  placeId2,
+  actionId,
+  actionReportId,
+  nav,
+  level = 12,
+}) => {
+  const location = useLocation()
+
+  const urlPath = location.pathname.split('/').filter((p) => p !== '')
+  const parentArray = [
+    'data',
+    'projects',
     projectId,
+    'subprojects',
     subprojectId,
+    'places',
     placeId,
-    placeId2,
+    ...(placeId2 ? ['places', placeId2] : []),
+    'actions',
     actionId,
+    'reports',
     actionReportId,
-    nav,
-    level = 12,
-  }) => {
-    const location = useLocation()
+    'values',
+  ]
+  const ownArray = [...parentArray, nav.id]
+  const ownUrl = `/${ownArray.join('/')}`
 
-    const urlPath = location.pathname.split('/').filter((p) => p !== '')
-    const parentArray = useMemo(
-      () => [
-        'data',
-        'projects',
-        projectId,
-        'subprojects',
-        subprojectId,
-        'places',
-        placeId,
-        ...(placeId2 ? ['places', placeId2] : []),
-        'actions',
-        actionId,
-        'reports',
-        actionReportId,
-        'values',
-      ],
-      [projectId, subprojectId, placeId, placeId2, actionId, actionReportId],
-    )
-    const ownArray = useMemo(
-      () => [...parentArray, nav.id],
-      [nav.id, parentArray],
-    )
-    const ownUrl = `/${ownArray.join('/')}`
+  // needs to work not only works for urlPath, for all opened paths!
+  const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+  const isActive = isEqual(urlPath, ownArray)
 
-    // needs to work not only works for urlPath, for all opened paths!
-    const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
-    const isActive = isEqual(urlPath, ownArray)
-
-    return (
-      <Node
-        label={nav.label}
-        id={nav.id}
-        level={level}
-        isInActiveNodeArray={isInActiveNodeArray}
-        isActive={isActive}
-        childrenCount={0}
-        to={ownUrl}
-      />
-    )
-  },
-)
+  return (
+    <Node
+      label={nav.label}
+      id={nav.id}
+      level={level}
+      isInActiveNodeArray={isInActiveNodeArray}
+      isActive={isActive}
+      childrenCount={0}
+      to={ownUrl}
+    />
+  )
+}
--- src/components/Tree/ActionsReports.tsx
+++ src/components/Tree/ActionsReports.tsx
@@ -1,4 +1,3 @@
-import { useCallback, memo } from 'react'
 import { useNavigate } from '@tanstack/react-router'
 
 import { Node } from './Node.tsx'
@@ -7,78 +6,76 @@ import { removeChildNodes } from '../../modules/tree/removeChildNodes.ts'
 import { addOpenNodes } from '../../modules/tree/addOpenNodes.ts'
 import { useActionReportsNavData } from '../../modules/useActionReportsNavData.ts'
 
-export const ActionReportsNode = memo(
-  ({ projectId, subprojectId, placeId, placeId2, actionId, level = 9 }) => {
-    const navigate = useNavigate()
+export const ActionReportsNode = ({
+  projectId,
+  subprojectId,
+  placeId,
+  placeId2,
+  actionId,
+  level = 9,
+}) => {
+  const navigate = useNavigate()
 
-    const { navData } = useActionReportsNavData({
-      projectId,
-      subprojectId,
-      placeId,
-      placeId2,
-      actionId,
-    })
-    const {
-      label,
-      parentUrl,
-      ownArray,
-      ownUrl,
-      urlPath,
-      isOpen,
-      isInActiveNodeArray,
-      isActive,
-      navs,
-    } = navData
+  const { navData } = useActionReportsNavData({
+    projectId,
+    subprojectId,
+    placeId,
+    placeId2,
+    actionId,
+  })
+  const {
+    label,
+    parentUrl,
+    ownArray,
+    ownUrl,
+    urlPath,
+    isOpen,
+    isInActiveNodeArray,
+    isActive,
+    navs,
+  } = navData
 
-    const onClickButton = useCallback(() => {
-      if (isOpen) {
-        removeChildNodes({ node: ownArray })
-        // only navigate if urlPath includes ownArray
-        if (isInActiveNodeArray && ownArray.length <= urlPath.length) {
-          navigate({ to: parentUrl })
-        }
-        return
+  const onClickButton = () => {
+    if (isOpen) {
+      removeChildNodes({ node: ownArray })
+      // only navigate if urlPath includes ownArray
+      if (isInActiveNodeArray && ownArray.length <= urlPath.length) {
+        navigate({ to: parentUrl })
       }
-      // add to openNodes without navigating
-      addOpenNodes({ nodes: [ownArray] })
-    }, [
-      isOpen,
-      ownArray,
-      isInActiveNodeArray,
-      urlPath.length,
-      navigate,
-      parentUrl,
-    ])
+      return
+    }
+    // add to openNodes without navigating
+    addOpenNodes({ nodes: [ownArray] })
+  }
 
-    // only list navs if isOpen AND the first nav has an id
-    const showNavs = isOpen && navs.length > 0 && navs[0].id
+  // only list navs if isOpen AND the first nav has an id
+  const showNavs = isOpen && navs.length > 0 && navs[0].id
 
-    return (
-      <>
-        <Node
-          label={label}
-          level={level}
-          isOpen={isOpen}
-          isInActiveNodeArray={isInActiveNodeArray}
-          isActive={isActive}
-          childrenCount={navs.length}
-          to={ownUrl}
-          onClickButton={onClickButton}
-        />
-        {showNavs &&
-          navs.map((nav, i) => (
-            <ActionReportNode
-              key={`${nav.id}-${i}`}
-              projectId={projectId}
-              subprojectId={subprojectId}
-              placeId={placeId}
-              placeId2={placeId2}
-              actionId={actionId}
-              nav={nav}
-              level={level + 1}
-            />
-          ))}
-      </>
-    )
-  },
-)
+  return (
+    <>
+      <Node
+        label={label}
+        level={level}
+        isOpen={isOpen}
+        isInActiveNodeArray={isInActiveNodeArray}
+        isActive={isActive}
+        childrenCount={navs.length}
+        to={ownUrl}
+        onClickButton={onClickButton}
+      />
+      {showNavs &&
+        navs.map((nav, i) => (
+          <ActionReportNode
+            key={`${nav.id}-${i}`}
+            projectId={projectId}
+            subprojectId={subprojectId}
+            placeId={placeId}
+            placeId2={placeId2}
+            actionId={actionId}
+            nav={nav}
+            level={level + 1}
+          />
+        ))}
+    </>
+  )
+}
