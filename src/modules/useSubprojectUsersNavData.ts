774d8bbfd317f996d3f6e149c37a10e93969ebe2
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-21 14:42:32 +0100

    de-memoize

--- src/modules/useSubprojectUsersNavData.ts
+++ src/modules/useSubprojectUsersNavData.ts
@@ -1,4 +1,3 @@
-import { useMemo } from 'react'
 import { useLiveQuery } from '@electric-sql/pglite-react'
 import { useAtom } from 'jotai'
 import { useLocation } from '@tanstack/react-router'
@@ -24,48 +23,39 @@ export const useSubprojectUsersNavData = ({ projectId, subprojectId }) => {
 
   const loading = res === undefined
 
-  const navData = useMemo(() => {
-    const navs = res?.rows ?? []
-    const parentArray = [
-      'data',
-      'projects',
-      projectId,
-      'subprojects',
-      subprojectId,
-    ]
-    const parentUrl = `/${parentArray.join('/')}`
-    const ownArray = [...parentArray, 'users']
-    const ownUrl = `/${ownArray.join('/')}`
-    // needs to work not only works for urlPath, for all opened paths!
-    const isOpen = openNodes.some((array) => isEqual(array, ownArray))
-    const urlPath = location.pathname.split('/').filter((p) => p !== '')
-    const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
-    const isActive = isEqual(urlPath, ownArray)
-
-    return {
-      isInActiveNodeArray,
-      isActive,
-      isOpen,
-      parentUrl,
-      ownArray,
-      urlPath,
-      ownUrl,
-      label: buildNavLabel({
-        loading,
-        countFiltered: navs.length,
-        namePlural: 'Users',
-      }),
-      nameSingular: 'Subproject User',
-      navs,
-    }
-  }, [
-    loading,
-    location.pathname,
-    openNodes,
+  const navs = res?.rows ?? []
+  const parentArray = [
+    'data',
+    'projects',
     projectId,
-    res?.rows,
+    'subprojects',
     subprojectId,
-  ])
+  ]
+  const parentUrl = `/${parentArray.join('/')}`
+  const ownArray = [...parentArray, 'users']
+  const ownUrl = `/${ownArray.join('/')}`
+  // needs to work not only works for urlPath, for all opened paths!
+  const isOpen = openNodes.some((array) => isEqual(array, ownArray))
+  const urlPath = location.pathname.split('/').filter((p) => p !== '')
+  const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+  const isActive = isEqual(urlPath, ownArray)
+
+  const navData = {
+    isInActiveNodeArray,
+    isActive,
+    isOpen,
+    parentUrl,
+    ownArray,
+    urlPath,
+    ownUrl,
+    label: buildNavLabel({
+      loading,
+      countFiltered: navs.length,
+      namePlural: 'Users',
+    }),
+    nameSingular: 'Subproject User',
+    navs,
+  }
 
   return { loading, navData }
 }
