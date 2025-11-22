4dad8fc483db3ad81c210f733a3b7b17bda69301
Alexander Gabriel <alex.barbalex@gmail.com>
2025-11-22 11:54:25 +0100

    clean

--- src/components/LayoutProtected/Breadcrumbs/FetcherRouter.tsx
+++ src/components/LayoutProtected/Breadcrumbs/FetcherRouter.tsx
@@ -96,8 +96,6 @@ import { MessagesFetcher } from './MessagesFetcher.tsx'
 import { MessageFetcher } from './MessageFetcher.tsx'
 
 export const FetcherRouter = ({ fetcherName, ...other }) => {
-  console.log('FetcherRouter, name:', fetcherName)
-
   // need to get params here and pass as props otherwise
   // causes the compiler to: "Error: Rendered fewer hooks than expected"
   const params = useParams({ strict: false })
