710774c2e37c987af7c1aa1aa55324c271282689
Alexander Gabriel <alex.barbalex@gmail.com>
2025-12-09 11:13:06 +0100

    set postgrest uri to constants

--- src/components/PostgrestClientInitator.tsx
+++ src/components/PostgrestClientInitator.tsx
@@ -2,9 +2,11 @@ import { useEffect } from 'react'
 import { PostgrestClient } from '@supabase/postgrest-js'
 import { useAtom } from 'jotai'
 
+import { constants } from '../modules/constants.ts'
+
 import { postgrestClientAtom } from '../store.ts'
 
-const postgrestClientUrl = 'http://localhost:3001'
+const postgrestClientUrl = constants.getPostgrestUri()
 
 export const PostgrestClientInitator = () => {
   const [, setPostgrestClient] = useAtom(postgrestClientAtom)
--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -1,19 +1,9 @@
 // TODO: most of these constants are not used yet
 export const constants = {
-  // appBarHeight: 64,
   titleRowHeight: 52,
   mobileViewMaxWidth: 999,
-  // singleRowHeight: 48,
-  // getAppUri: () =>
-  //   window?.location?.hostname === 'localhost'
-  //     ? `http://${window.location.hostname}:5172`
-  //     : 'https://erfassen.app',
-  // tree: {
-  //   minimalWidth: 331,
-  //   minimalWindowWidth: 1000,
-  // },
-  // sidebar: {
-  //   width: 420,
-  //   minimalWindowWidth: 1000,
-  // },
+  getPostgrestUri: () =>
+    window?.location?.hostname === 'localhost' ?
+      `http://${window.location.hostname}:3001`
+    : 'https://api.promote-species.app',
 }
