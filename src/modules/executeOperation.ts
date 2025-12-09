ddb87a290d3f6e96b1aeae6d87e8027c133bd40d
Alexander Gabriel <alex.barbalex@gmail.com>
2025-12-09 18:19:21 +0100

    work on operation queue

--- src/modules/executeOperation.ts
+++ src/modules/executeOperation.ts
@@ -1,4 +1,39 @@
 import { store, postgrestClientAtom } from '../store.ts'
 const postgrestClient = store.get(postgrestClientAtom)
 
-export const executeOperation = (o) => {}
+export const executeOperation = async (o) => {
+  if (!o) return
+
+  const {
+    id,
+    time,
+    // name,
+    table,
+    rowIdName,
+    rowId,
+    operation,
+    // variables,
+    column,
+    newValue,
+  } = o
+
+  if (operation === 'update') {
+    const { error } = await postgrestClient
+      .from(table)
+      .update({ [column]: newValue })
+      .eq(rowIdName, rowId)
+
+    if (error) throw error
+  }
+  if (operation === 'insert') {
+    const { error } = await postgrestClient
+      .from(table)
+      .insert({ [rowIdName]: rowId })
+  }
+  if (operation === 'delete') {
+    const { error } = await postgrestClient
+      .from(table)
+      .delete()
+      .eq(rowIdName, rowId)
+  }
+}
