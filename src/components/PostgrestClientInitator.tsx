18d86f0848e812ee8785f09fc32d040e5f5ce86f
Alexander Gabriel <alex.barbalex@gmail.com>
2025-12-10 21:39:29 +0100

    optimize

--- src/components/PostgrestClientInitator.tsx
+++ src/components/PostgrestClientInitator.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { PostgrestClient } from '@supabase/postgrest-js'
-import { useAtom } from 'jotai'
+import { useSetAtom } from 'jotai'
 
 import { constants } from '../modules/constants.ts'
 
@@ -9,7 +9,7 @@ import { postgrestClientAtom } from '../store.ts'
 const postgrestClientUrl = constants.getPostgrestUri()
 
 export const PostgrestClientInitator = () => {
-  const [, setPostgrestClient] = useAtom(postgrestClientAtom)
+  const setPostgrestClient = useSetAtom(postgrestClientAtom)
 
   useEffect(() => {
     const postgrestClient = new PostgrestClient(postgrestClientUrl)
