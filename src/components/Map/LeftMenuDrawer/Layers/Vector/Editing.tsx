971a3387b682eb7261e54eafedffbcb783c3f438
Alexander Gabriel <alex.barbalex@gmail.com>
2025-12-13 15:07:29 +0100

    work on adding operations

--- src/components/Map/LeftMenuDrawer/Layers/Vector/Editing.tsx
+++ src/components/Map/LeftMenuDrawer/Layers/Vector/Editing.tsx
@@ -1,8 +1,10 @@
 import { usePGlite } from '@electric-sql/pglite-react'
+import { useSetAtom } from 'jotai'
 
 import { ErrorBoundary } from '../../../../shared/ErrorBoundary.tsx'
 import { getValueFromChange } from '../../../../../modules/getValueFromChange.ts'
 import { VectorLayerForm } from '../../../../../formsAndLists/vectorLayer/Form/index.tsx'
+import { addOperationAtom } from '../../../../../store.ts'
 
 // inset form and give it green shadow
 const formContainerStyle = {
@@ -11,6 +13,7 @@ const formContainerStyle = {
 
 export const VectorLayerEditing = ({ layer: row }) => {
   const db = usePGlite()
+  const addOperation = useSetAtom(addOperationAtom)
 
   const onChange = async (e, data) => {
     const { name, value } = getValueFromChange(e, data)
@@ -25,6 +28,16 @@ export const VectorLayerEditing = ({ layer: row }) => {
     } catch (error) {
       console.log('hello VectorLayer, onChange, error:', error)
     }
+    // add task to update server and rollback PGlite in case of error
+    addOperation({
+      table: 'vector_layers',
+      rowIdName: 'vector_layer_id',
+      rowId: row.vector_layer_id,
+      operation: 'update',
+      draft: { [name]: value },
+      prev: { ...row },
+    })
+
     return
   }
 
