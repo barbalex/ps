823dcc64a45213f98e346c433cd9320cf2daa657
Alexander Gabriel <alex.barbalex@gmail.com>
2025-12-20 00:31:08 +0100

    optimize

--- src/components/FormHeader/index.tsx
+++ src/components/FormHeader/index.tsx
@@ -14,29 +14,27 @@ export const FormHeader = ({
   siblings,
   // needed to leave space for the map layer drawer's close button
   titleMarginLeft,
-}) => {
-  return (
-    <>
-      <div className="form-header">
-        <h1
-          style={{
-            ...titleStyle,
-            ...(titleMarginLeft ? { marginLeft: titleMarginLeft } : {}),
-          }}
-        >
-          {title}
-        </h1>
-        {(addRow || deleteRow || toNext || toPrevious || siblings) && (
-          <FormMenu
-            addRow={addRow}
-            deleteRow={deleteRow}
-            toNext={toNext}
-            toPrevious={toPrevious}
-            nameSingular={tableName}
-            siblings={siblings}
-          />
-        )}
-      </div>
-    </>
-  )
-}
+}) => (
+  <>
+    <div className="form-header">
+      <h1
+        style={{
+          ...titleStyle,
+          ...(titleMarginLeft ? { marginLeft: titleMarginLeft } : {}),
+        }}
+      >
+        {title}
+      </h1>
+      {(addRow || deleteRow || toNext || toPrevious || siblings) && (
+        <FormMenu
+          addRow={addRow}
+          deleteRow={deleteRow}
+          toNext={toNext}
+          toPrevious={toPrevious}
+          nameSingular={tableName}
+          siblings={siblings}
+        />
+      )}
+    </div>
+  </>
+)
