88cb463c3d37ac69cb492b5928b36a7158a94062
Alexander Gabriel <alex.barbalex@gmail.com>
2025-12-21 14:53:18 +0100

    use css modules

--- src/components/FormTitle/index.tsx
+++ src/components/FormTitle/index.tsx
@@ -1,49 +1,15 @@
-import './style.css'
+import styles from './index.module.css'
 
-const containerStyle = {
-  backgroundColor: 'rgb(225, 247, 224)',
-  flexShrink: 0,
-  display: 'flex',
-  flexDirection: 'column',
-  overflow: 'hidden',
-}
-const titleRowStyle = {
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  minHeight: 42,
-  flexWrap: 'nowrap',
-  overflow: 'hidden',
-}
-// TODO: make this a h2 or h3?
-const titleStyle = {
-  display: 'block',
-  flewGrow: 0,
-  flexShrink: 1,
-  marginTop: 'auto',
-  marginBottom: 'auto',
-  color: 'white',
-  fontWeight: 'bold',
-  overflow: 'hidden',
-  whiteSpace: 'nowrap',
-  textOverflow: 'ellipsis',
-}
-
-export const FormTitle = ({ title, MenuComponent = null, menuProps = {} }) => {
-  return (
-    <div
-      style={containerStyle}
-      className="form-title-container"
-    >
-      <div style={titleRowStyle}>
-        <div style={titleStyle}>{title}</div>
-        {!!MenuComponent && (
-          <MenuComponent
-            toggleFilterInput={toggleFilterInput}
-            {...menuProps}
-          />
-        )}
-      </div>
+export const FormTitle = ({ title, MenuComponent = null, menuProps = {} }) => (
+  <div className={styles.container}>
+    <div className={styles.titleRow}>
+      <div className={styles.title}>{title}</div>
+      {!!MenuComponent && (
+        <MenuComponent
+          toggleFilterInput={toggleFilterInput}
+          {...menuProps}
+        />
+      )}
     </div>
-  )
-}
+  </div>
+)
