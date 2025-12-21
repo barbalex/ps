d8aed7745a7247009ede2d29f64d0682b0c96c6f
Alexander Gabriel <alex.barbalex@gmail.com>
2025-12-21 15:17:29 +0100

    use css modules

--- src/components/LayoutProtected/Header/index.tsx
+++ src/components/LayoutProtected/Header/index.tsx
@@ -1,50 +1,18 @@
 import { Link } from '@tanstack/react-router'
-import { pipe } from 'remeda'
 
-import { on } from '../../../css.ts'
 import { Menu } from './Menu.tsx'
+import styles from './index.module.css'
 
-import './header.css'
-
-const containerStyle = {
-  display: 'flex',
-  flexWrap: 'nowrap',
-  justifyContent: 'space-between',
-  backgroundColor: 'rgba(38, 82, 37, 0.9)',
-  color: 'white',
-}
-const titleStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  fontSize: 'large',
-  userSelect: 'none',
-  minHeight: 'var(--lineHeightBase300)',
-}
-const linkStyle = { color: 'white', textDecoration: 'none' }
-
-export const Header = () => {
-  return (
-    <div
-      className="header"
-      style={containerStyle}
-    >
-      <h1 style={titleStyle}>
-        <Link
-          to="/"
-          className="header__title__link"
-          style={pipe(
-            linkStyle,
-            on('&:hover', {
-              filter: 'brightness(85%)',
-              textDecoration: 'underline',
-            }),
-          )}
-        >
-          Promoting Species
-        </Link>
-      </h1>
-      <Menu />
-    </div>
-  )
-}
+export const Header = () => (
+  <div className={styles.container}>
+    <h1 className={styles.title}>
+      <Link
+        to="/"
+        className={styles.link}
+      >
+        Promoting Species
+      </Link>
+    </h1>
+    <Menu />
+  </div>
+)
