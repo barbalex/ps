2547d5284c29295ca793e5e5b28e13e361c41896
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-12-24 17:13:59 +0100

    use css modules

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -4,22 +4,13 @@ import { Map } from './Map.tsx'
 import { RightMenuDrawer } from './RightMenuDrawer/index.tsx'
 import { LeftMenuDrawer } from './LeftMenuDrawer/index.tsx'
 
-const containerStyle = {
-  width: '100%',
-  height: '100%',
-  position: 'relative',
-  overflow: 'hidden',
-  containerType: 'inline-size',
-}
+import styles from './index.module.css'
 
 export const MapContainer = () => {
   const containerRef = useRef<HTMLDivElement>(null)
 
   return (
-    <div
-      style={containerStyle}
-      ref={containerRef}
-    >
+    <div className={styles.container} ref={containerRef}>
       <LeftMenuDrawer containerRef={containerRef} />
       <Map />
       <RightMenuDrawer containerRef={containerRef} />
