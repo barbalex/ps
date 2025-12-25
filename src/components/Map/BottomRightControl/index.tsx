8c4cade85652aafb4b23815fd455eb5d9ef5f7f2
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-12-25 10:55:52 +0100

    use css modules

--- src/components/Map/BottomRightControl/index.tsx
+++ src/components/Map/BottomRightControl/index.tsx
@@ -4,28 +4,13 @@ import { UiButton } from './UiButton.tsx'
 import { VerticalButtons } from './VerticalButtons/index.tsx'
 import { HorizontalButtons } from './HorizontalButtons/index.tsx'
 import { mapHideUiAtom } from '../../../store.ts'
-
-const containerStyle = {
-  position: 'absolute',
-  right: 5,
-  bottom: 20,
-  display: 'grid',
-  gridTemplateColumns: '1fr 40px',
-  gridTemplateRows: '1fr 40px',
-  gridTemplateAreas: `
-    '. verticalbuttons'
-    'horizontalbuttons uibutton'
-  `,
-  gap: 5,
-  justifyItems: 'stretch',
-  alignItems: 'stretch',
-}
+import styles from './index.module.css'
 
 export const BottomRightControl = () => {
   const hideMapUi = useAtomValue(mapHideUiAtom)
 
   return (
-    <div style={containerStyle}>
+    <div className={styles.container}>
       {!hideMapUi && (
         <>
           <VerticalButtons />
