af79c3214f2f515b4af1f77e4098c1afd9c8e885
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-12-26 22:37:58 +0100

    use css modules

--- src/components/shared/Label.tsx
+++ src/components/shared/Label.tsx
@@ -1,14 +1,6 @@
-const labelStyle = {
-  marginTop: '10px',
-  cursor: 'text',
-  fontSize: '0.8rem',
-  color: 'rgba(0, 0, 0, 0.8)',
-  pointerEvents: 'none',
-  userSelect: 'none',
-  paddingBottom: '8px',
-}
+import styles from './Label.module.css'
 
 // TODO: use fluent ui label
 export const Label = ({ label }: { label: string }) => (
-  <div style={labelStyle}>{label}</div>
+  <div className={styles.label}>{label}</div>
 )
