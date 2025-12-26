0c3789cb0c7ca789dd7b08c2649214caa30c4cd8
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-12-26 18:40:32 +0100

    use css modules

--- src/components/Map/RightMenuDrawer/Marker.tsx
+++ src/components/Map/RightMenuDrawer/Marker.tsx
@@ -2,6 +2,8 @@ import * as ReactDOMServer from 'react-dom/server'
 import { Marker } from 'react-leaflet'
 import { MdNotListedLocation } from 'react-icons/md'
 
+import styles from './Marker.module.css'
+
 export const InfoMarker = ({ mapInfo }) => {
   if (!mapInfo?.lat) return null
 
@@ -10,14 +12,7 @@ export const InfoMarker = ({ mapInfo }) => {
       position={{ lat: mapInfo.lat, lng: mapInfo.lng }}
       icon={L.divIcon({
         html: ReactDOMServer.renderToString(
-          <MdNotListedLocation
-            style={{
-              color: 'yellow',
-              fontSize: '3.5em',
-              transform: 'translate(-0.38em, -0.75em)',
-              filter: 'drop-shadow(0 0 2px rgb(0 0 0 / 1))',
-            }}
-          />,
+          <MdNotListedLocation className={styles.icon} />,
         ),
       })}
     />
