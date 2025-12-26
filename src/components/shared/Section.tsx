7a95a71ec24a1d28afa3eb10c8362ea7cfa3e9a9
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-12-26 22:46:47 +0100

    use css modules

--- src/components/shared/Section.tsx
+++ src/components/shared/Section.tsx
@@ -1,21 +1,10 @@
-const titleStyle = {
-  margin: '5px 0',
-  padding: '5px 10px',
-  backgroundColor: 'rgb(225, 247, 224)',
-  fontSize: '1em',
-  marginLeft: -10,
-  marginRight: -10,
-  position: 'sticky',
-  top: -12,
-  zIndex: 1,
-  userSelect: 'none',
-}
+import styles from './Section.module.css'
 
 // need to place children under their own parent
 // because some have position: relative which makes them overlay the section title
 export const Section = ({ title, children }) => (
   <section>
-    <h2 style={titleStyle}>{title}</h2>
+    <h2 className={styles.title}>{title}</h2>
     {children}
   </section>
 )
