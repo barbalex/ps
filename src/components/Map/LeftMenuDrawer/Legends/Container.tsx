24fdf32352225413611b8ee291db513904b1c422
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-12-26 16:32:04 +0100

    use css modules

--- src/components/Map/LeftMenuDrawer/Legends/Container.tsx
+++ src/components/Map/LeftMenuDrawer/Legends/Container.tsx
@@ -1,20 +1,11 @@
-const containerStyle = {
-  padding: 10,
-}
-const titleStyle = {
-  fontWeight: 'bold',
-  paddingBottom: 5,
-}
+import styles from './Container.module.css'
 
 export const Container = ({ children, layer, isLast }) => (
   <section
-    style={{
-      ...containerStyle,
-      borderTop: '1px solid rgba(55, 118, 28, 0.5)',
-      ...(isLast ? { borderBottom: '1px solid rgba(55, 118, 28, 0.5)' } : {}),
-    }}
+    style={isLast ? { borderBottom: '1px solid rgba(55, 118, 28, 0.5)' } : {}}
+    className={styles.section}
   >
-    <div style={titleStyle}>{layer.label}</div>
+    <div className={styles.title}>{layer.label}</div>
     {children}
   </section>
 )
