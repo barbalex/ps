50215d87007b3fdb87b5735263e970a28f8744c3
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-12-28 17:45:11 +0100

    use css modules

--- src/components/shared/LabelCreator/Target/index.tsx
+++ src/components/shared/LabelCreator/Target/index.tsx
@@ -4,49 +4,7 @@ import { Button, Spinner } from '@fluentui/react-components'
 
 import { TargetElements } from './TargetElements.tsx'
 import { LabelElement } from '../index.tsx'
-
-const containerStyle = {
-  margin: 0,
-  marginRight: 8,
-  outline: '1px dotted lightgrey',
-  borderRadius: 4,
-  borderTopRightRadius: 0,
-  borderBottomRightRadius: 0,
-  borderCollapse: 'collapse',
-  boxSizing: 'border-box',
-  flexGrow: 1,
-}
-const droppableStyle = {
-  flexGrow: 1,
-  height: '100%',
-  width: '100%',
-  display: 'flex',
-  flexDirection: 'column',
-}
-const titleContainerStyle = {
-  padding: 8,
-  display: 'flex',
-  flexDirection: 'column',
-  flexGrow: 0,
-  userSelect: 'none',
-}
-const titleStyle = {
-  margin: 0,
-}
-const explainerStyle = {
-  fontSize: 'x-small',
-  margin: 0,
-  lineHeight: '1.3em',
-}
-const innerContainerStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  flexGrow: 1,
-  height: '100%',
-}
-const buttonStyle = {
-  margin: 8,
-}
+import styles from './index.module.css'
 
 /**
  * Have two versions:
@@ -64,14 +22,14 @@ const buttonStyle = {
  */
 
 interface Props {
-  name: string
+  // name: string
   label: LabelElement[]
   labelChanged: boolean
   onChange: () => void
   saveToDb: () => void
 }
 
-export const Target = ({ label, labelChanged, onChange, saveToDb }) => {
+export const Target = ({ label, labelChanged, onChange, saveToDb }: Props) => {
   // TODO: on apply changes: set loading until labelChanged is false
   const [changing, setChanging] = useState(false)
   const onClick = () => {
@@ -84,22 +42,22 @@ export const Target = ({ label, labelChanged, onChange, saveToDb }) => {
   }, [changing, labelChanged])
 
   return (
-    <div style={containerStyle}>
+    <div className={styles.container}>
       <Droppable
         droppableId="target"
         direction="horizontal"
-        style={droppableStyle}
+        className={styles.droppable}
       >
         {(provided, snapshot) => (
-          <div style={innerContainerStyle}>
-            <div style={titleContainerStyle}>
-              <h4 style={titleStyle}>Label Creator</h4>
-              <p style={explainerStyle}>Build your own label.</p>
-              <p style={explainerStyle}>
+          <div className={styles.innerContainer}>
+            <div className={styles.titleContainer}>
+              <h4 className={styles.title}>Label Creator</h4>
+              <p className={styles.explainer}>Build your own label.</p>
+              <p className={styles.explainer}>
                 Pull fields here. A field's value will be used in the label.
               </p>
-              <p style={explainerStyle}>Combine multiple fields.</p>
-              <p style={explainerStyle}>
+              <p className={styles.explainer}>Combine multiple fields.</p>
+              <p className={styles.explainer}>
                 Place separating text using the separator tool.
               </p>
             </div>
@@ -111,7 +69,7 @@ export const Target = ({ label, labelChanged, onChange, saveToDb }) => {
             />
             <Button
               onClick={onClick}
-              style={buttonStyle}
+              className={styles.button}
               disabled={!labelChanged}
               icon={changing ? <Spinner size="tiny" /> : undefined}
             >
