249d2b9a816f4333c36fbeeac1acfeb0a008c631
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-12-28 18:34:48 +0100

    use css modules

--- src/formsAndLists/crss/Info.tsx
+++ src/formsAndLists/crss/Info.tsx
@@ -1,15 +1,7 @@
-import './info.css'
-
-const containerStyle = {
-  padding: '5px 10px',
-  backgroundColor: 'rgba(103, 216, 101, 0.07)',
-}
+import styles from './Info.module.css'
 
 export const Info = () => (
-  <div
-    style={containerStyle}
-    tabIndex={-1}
-  >
+  <div className={styles.container} tabIndex={-1}>
     <p>This is a (hopefully) complete list of Coordinate Reference Systems.</p>
     <p>They can be added to projects.</p>
   </div>
