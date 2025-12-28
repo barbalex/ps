da279ff0dd672ebdf3f08190998201f21c95c546
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-12-28 16:34:59 +0100

    use css modules

--- src/components/shared/DragAndDrop/DragHandle.tsx
+++ src/components/shared/DragAndDrop/DragHandle.tsx
@@ -1,24 +1,15 @@
 import { MdDragIndicator } from 'react-icons/md'
 
-const dragHandleStyle = {
-  display: 'flex',
-  alignItems: 'center',
-}
-const dragIndicatorStyle = {
-  fontSize: 'x-large',
-  color: 'rgba(55, 118, 28, 0.6)',
-  paddingRight: 5,
-  cursor: 'grab',
-}
+import styles from './DragAndDrop.module.css'
 
 // receives drag handle ref
 export const DragHandle = ({ ref }) => (
   <div
     ref={ref}
-    style={dragHandleStyle}
+    className={styles.dragHandle}
     onClick={(e) => e.preventDefault()}
     title="drag to reorder"
   >
-    <MdDragIndicator style={dragIndicatorStyle} />
+    <MdDragIndicator className={styles.dragIndicator} />
   </div>
 )
