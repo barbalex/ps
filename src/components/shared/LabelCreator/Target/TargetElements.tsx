269f26e99f08972cd878aa934ebdf7ca7c40dc3d
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-12-28 17:50:11 +0100

    use css modules

--- src/components/shared/LabelCreator/Target/TargetElements.tsx
+++ src/components/shared/LabelCreator/Target/TargetElements.tsx
@@ -2,41 +2,7 @@ import { Draggable } from 'react-beautiful-dnd'
 import { BsArrowsMove } from 'react-icons/bs'
 
 import { BetweenCharacters } from './BetweenCharacters.tsx'
-
-const targetContainerStyle = {
-  display: 'flex',
-  flexWrap: 'wrap',
-  alignItems: 'center',
-  alignContent: 'flex-start',
-  flexGrow: 1,
-  minHeight: '42px',
-  padding: '8px',
-  transition: 'background-color 0.2s ease',
-  userSelect: 'none',
-}
-const fieldElementStyle = {
-  padding: '8.5px 14px',
-  backgroundColor: 'rgba(103, 216, 101, 0.07)',
-  border: '1px solid lightgrey',
-  marginRight: 6,
-  marginTop: 8,
-  borderRadius: 4,
-  fontSize: 'small',
-  lineHeight: '16.6px',
-  userSelect: 'none',
-  position: 'relative',
-}
-const fieldHandleStyle = {
-  color: '#989898',
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  height: '0.95em',
-  width: '0.95em',
-}
-const elementContainerStyle = {
-  display: 'flex',
-}
+import styles from './TargetElements.module.css'
 
 /**
  * Have two versions:
@@ -61,10 +27,10 @@ export const TargetElements = ({
 }) => (
   <div
     style={{
-      ...targetContainerStyle,
       backgroundColor: isDraggingOver ? 'rgba(74,20,140,0.1)' : 'white',
       transition: 'background-color 0.2s ease',
     }}
+    className={styles.targetContainer}
     ref={provided.innerRef}
     {...provided.droppableProps}
   >
@@ -79,24 +45,24 @@ export const TargetElements = ({
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
-            style={elementContainerStyle}
+            className={styles.elementContainer}
           >
-            {labelElement.type === 'field' ?
+            {labelElement.type === 'field' ? (
               <div
                 style={{
-                  ...fieldElementStyle,
-                  backgroundColor:
-                    snapshot.isDragging ?
-                      'rgba(38, 82, 37, 0.9)'
+                  backgroundColor: snapshot.isDragging
+                    ? 'rgba(38, 82, 37, 0.9)'
                     : 'rgba(103, 216, 101, 0.07)',
                   color: snapshot.isDragging ? 'white' : 'black',
                   ...provided.draggableProps.style,
                 }}
+                className={styles.fieldElement}
               >
                 {labelElement.value}
-                <BsArrowsMove style={fieldHandleStyle} />
+                <BsArrowsMove className={styles.fieldHandle} />
               </div>
-            : <BetweenCharacters
+            ) : (
+              <BetweenCharacters
                 el={labelElement}
                 label={label}
                 onChange={onChange}
@@ -104,9 +70,9 @@ export const TargetElements = ({
                 snapshot={snapshot}
                 provided={provided}
               >
-                <BsArrowsMove style={fieldHandleStyle} />
+                <BsArrowsMove className={styles.fieldHandle} />
               </BetweenCharacters>
-            }
+            )}
           </div>
         )}
       </Draggable>
