710434556848fe55a41e83aec8202b198f7b887e
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-12-29 14:06:43 +0100

    use css modules

--- src/components/Map/LeftMenuDrawer/Resizer.tsx
+++ src/components/Map/LeftMenuDrawer/Resizer.tsx
@@ -1,13 +1,19 @@
-import './resizer.css'
+import styles from './Resizer.module.css'
 
-export const Resizer = ({ startResizing, isResizing }) => (
+interface ResizerProps {
+  startResizing: () => void
+  isResizing: boolean
+}
+
+export const Resizer = ({ startResizing, isResizing }: ResizerProps) => (
   <div
-    className="map-layers-resizer"
+    className={styles.resizer}
     onMouseDown={startResizing}
     // need to set background-color when resizing because the element looses the hover on drag
     style={{
-      backgroundColor:
-        isResizing ? 'rgba(38, 82, 37, 0.9)' : 'rgba(240, 255, 240, 1)',
+      backgroundColor: isResizing
+        ? 'rgba(38, 82, 37, 0.9)'
+        : 'rgba(240, 255, 240, 1)',
     }}
   />
 )
