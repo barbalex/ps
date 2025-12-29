612dcc4c0c36e374d2a0babc808f036b5125e59a
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-12-29 10:09:13 +0100

    use css modules

--- src/routes/_layout.index.tsx
+++ src/routes/_layout.index.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-import './index.css'
 import styles from './_layout.index.module.css'
 
 export const Route = createFileRoute('/_layout/')({
@@ -23,84 +22,84 @@ const Index = () => (
       />
     </picture>
     <div className={styles.scrollContainer}>
-      <h6 className="page-title">Bedrohte Arten und Biotope fördern</h6>
+      <h6 className={styles.pageTitle}>Bedrohte Arten und Biotope fördern</h6>
       <div className={styles.cardContainer}>
-        <div className="card">
-          <h3 className="card-title">Arten und Biotope</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Arten und Biotope</h3>
           Seltene und bedrohte Arten und Biotope überwachen und fördern.
         </div>
-        <div className="card">
-          <h3 className="card-title">Ziele</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Ziele</h3>
           ...beschreiben die gewünschte Entwicklung.
         </div>
-        <div className="card">
-          <h3 className="card-title">Räume</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Räume</h3>
           Arten in Populationen gliedern, Biotope in Flächen.
         </div>
-        <div className="card">
-          <h3 className="card-title">Zweistufige Räume</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Zweistufige Räume</h3>
           Populationen und Biotopflächen weiter unterteilen.
         </div>
-        <div className="card">
-          <h3 className="card-title">Verantwortliche</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Verantwortliche</h3>
           ...beschreiben Räume, organisieren Kontrollen und Massnahmen,
           analysieren und berichten.
         </div>
-        <div className="card">
-          <h3 className="card-title">Massnahmen</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Massnahmen</h3>
           ...verbessern den Zustand einer Art oder eines Biotops.
         </div>
-        <div className="card">
-          <h3 className="card-title">Kontrollen</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Kontrollen</h3>
           ...erfassen den Zustand von Räumen und die Wirkung von Massnahmen.
         </div>
-        <div className="card">
-          <h3 className="card-title">Berichte</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Berichte</h3>
           ...beschreiben die Entwick&shy;lung von Populationen, beurteilen den
           Erfolg der Massnahmen und die Errei&shy;chung der Ziele.
         </div>
-        <div className="card">
-          <h3 className="card-title">Beobachtungen Dritter</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Beobachtungen Dritter</h3>
           ...prüfen und den Popula&shy;tionen und Biotopen zuordnen.
         </div>
-        <div className="card">
-          <h3 className="card-title">Mobilfähig</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Mobilfähig</h3>
           Promoting Species passt sich jeder Bildschirmgrösse an. Arbeiten Sie
           effizient auf Handy, Tablet oder Computer.
         </div>
-        <div className="card">
-          <h3 className="card-title">Bring your own device</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Bring your own device</h3>
           Promoting Species funktioniert auf jedem Betriebssystem mit modernem
           Browser: Windows, MacOS, Linux, Android, iOS...
         </div>
-        <div className="card">
-          <h3 className="card-title">Kein Internet? Egal!</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Kein Internet? Egal!</h3>
           Sie sind offline? Einfach weiter arbeiten. Sobald Sie wieder online
           sind, werden Ihre Daten synchronisiert.
         </div>
-        <div className="card">
-          <h3 className="card-title">Flexible Konfiguration</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Flexible Konfiguration</h3>
           Sie können Ihr Projekt Ihren eigenen Bedürfnissen anpassen. Vermissen
           Sie eine Funktion? Ich bin interessiert!
         </div>
-        <div className="card">
-          <h3 className="card-title">Professionelle Konfiguration</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Professionelle Konfiguration</h3>
           Ich helfe Ihnen gerne bei der Erst-Konfiguration, damit Ihr Team
           möglichst effizient arbeitet.
         </div>
-        <div className="card">
-          <h3 className="card-title">Einfaches Onboarding</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Einfaches Onboarding</h3>
           Erfassen Sie neue Mitarbeitende mit ihrer Email. Ergänzen Sie diese
           Person bei allen Arten und Biotopen, in denen sie mitarbeiten soll.
           Fertig!
         </div>
-        <div className="card">
-          <h3 className="card-title">Teamwork</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Teamwork</h3>
           Da die Daten laufend synchronisiert werden, können mehrere
           Mitarbeitende effizient gleichzeitig arbeiten.
         </div>
-        <div className="card">
-          <h3 className="card-title">Zeitmaschine</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Zeitmaschine</h3>
           Mehrere Mitarbeitende haben aus versehen offline die gleichen Daten
           geändert?
           <br />
@@ -108,13 +107,13 @@ const Index = () => (
           Das lässt sich einfach korrigieren. Sie sehen, wer was wann geändert
           hat.
         </div>
-        <div className="card">
-          <h3 className="card-title">Ihre Daten gehören Ihnen</h3>
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>Ihre Daten gehören Ihnen</h3>
           100% Ihrer Daten werden auf Ihr Gerät synchronisiert. Sie können
           jederzeit exportiert werden.
         </div>
-        <div className="card">
-          <h3 className="card-title">
+        <div className={styles.card}>
+          <h3 className={styles.cardTitle}>
             Wer zahlt, befiehlt. Wer befiehlt, zahlt...
           </h3>
           Es zahlt, wer Projekte konfiguriert.
