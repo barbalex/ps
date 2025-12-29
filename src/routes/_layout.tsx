92c0666bc2e10025f89ff4f7b93591aff8b2beee
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-12-29 10:11:38 +0100

    use css modules

--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -1,23 +1,16 @@
 import { Outlet, createFileRoute } from '@tanstack/react-router'
 import { Header } from '../components/Layout/Header/index.tsx'
+import styles from './_layout.module.css'
 
 export const Route = createFileRoute('/_layout')({
   component: Component,
 })
 
-const homeOutletStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  flexGrow: 1,
-  overflow: 'hidden',
-  position: 'relative',
-}
-
 function Component() {
   return (
     <>
       <Header />
-      <div style={homeOutletStyle}>
+      <div className={styles.outlet}>
         <Outlet />
       </div>
     </>
