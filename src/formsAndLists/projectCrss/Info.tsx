0b16ba40b747a114c2941bc29370acd29825837e
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-12-29 09:38:39 +0100

    use css modules

--- src/formsAndLists/projectCrss/Info.tsx
+++ src/formsAndLists/projectCrss/Info.tsx
@@ -1,15 +1,7 @@
-import './info.css'
-
-const containerStyle = {
-  padding: '5px 10px',
-  backgroundColor: 'rgba(103, 216, 101, 0.07)',
-}
+import styles from './Info.module.css'
 
 export const Info = () => (
-  <div
-    style={containerStyle}
-    tabIndex={-1}
-  >
+  <div className={styles.container} tabIndex={-1}>
     <p>You can add multiple CRS.</p>
     <p>One of them can be set as the map presentation crs.</p>
     <p>It will be used to show coordinates in the map.</p>
