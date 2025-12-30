84eb3d76de4ae54734f6e253053f761785d92e86
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2025-12-30 22:12:11 +0100

    type query results

--- src/components/Map/LeftMenuDrawer/Legends/Vector/index.tsx
+++ src/components/Map/LeftMenuDrawer/Legends/Vector/index.tsx
@@ -1,22 +1,27 @@
 import { useLiveQuery } from '@electric-sql/pglite-react'
 
 import { Display } from './Display.tsx'
+import type VectorLayerDisplays from '../../../../../models/public/VectorLayerDisplays.ts'
+import type VectorLayers from '../../../../../models/public/VectorLayers.ts'
+import type WmsLayers from '../../../../../models/public/WmsLayers.ts'
 
-export const VectorLegend = ({ layer }) => {
+interface VectorLegendProps {
+  layer: VectorLayers | WmsLayers
+}
+
+export const VectorLegend = ({ layer }: VectorLegendProps) => {
   // fetch all vector_layer_displays for this layer
   const res = useLiveQuery(
     `SELECT * FROM vector_layer_displays WHERE vector_layer_id = $1`,
     [layer.vector_layer_id],
   )
-  const vectorLayerDisplays = res?.rows ?? []
+  const vectorLayerDisplays: VectorLayerDisplays[] = res?.rows ?? []
 
-  return vectorLayerDisplays.map((display) => {
-    return (
-      <Display
-        key={display.vector_layer_display_id}
-        display={display}
-        layerPresentation={layer?.layer_presentations?.[0]}
-      />
-    )
-  })
+  return vectorLayerDisplays.map((display) => (
+    <Display
+      key={display.vector_layer_display_id}
+      display={display}
+      layerPresentation={layer?.layer_presentations?.[0]}
+    />
+  ))
 }
