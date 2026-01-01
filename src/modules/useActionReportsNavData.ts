03e91c17765bc49ed6a96d02d633ba7135b56cf3
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2026-01-01 18:17:37 +0100

    type query results

--- src/modules/useActionReportsNavData.ts
+++ src/modules/useActionReportsNavData.ts
@@ -6,13 +6,26 @@ import { isEqual } from 'es-toolkit'
 import { treeOpenNodesAtom } from '../store.ts'
 import { buildNavLabel } from './buildNavLabel.ts'
 
+type Props = {
+  projectId: string
+  subprojectId: string
+  placeId: string
+  placeId2?: string
+  actionId: string
+}
+
+type NavData = {
+  id: string
+  label: string
+}[]
+
 export const useActionReportsNavData = ({
   projectId,
   subprojectId,
   placeId,
   placeId2,
   actionId,
-}) => {
+}: Props) => {
   const [openNodes] = useAtom(treeOpenNodesAtom)
   const location = useLocation()
 
@@ -29,7 +42,7 @@ export const useActionReportsNavData = ({
 
   const loading = res === undefined
 
-  const navs = res?.rows ?? []
+  const navs: NavData = res?.rows ?? []
   const parentArray = [
     'data',
     'projects',
