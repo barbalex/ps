4f46928e7a75a5c1e4e9f256ff8b7b10ee60a925
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2026-01-11 10:29:51 +0100

    validate forms

--- src/formsAndLists/list/index.tsx
+++ src/formsAndLists/list/index.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { useParams } from '@tanstack/react-router'
 import { usePGlite, useLiveQuery } from '@electric-sql/pglite-react'
 import { useSetAtom } from 'jotai'
@@ -18,19 +18,33 @@ export const List = ({ from }) => {
   const autoFocusRef = useRef<HTMLInputElement>(null)
   const db = usePGlite()
   const addOperation = useSetAtom(addOperationAtom)
+  const [validations, setValidations] = useState({})
 
   const res = useLiveQuery(`SELECT * FROM lists WHERE list_id = $1`, [listId])
   const row: Lists | undefined = res?.rows?.[0]
 
-  const onChange = (e, data) => {
+  const onChange = async (e, data) => {
     const { name, value } = getValueFromChange(e, data)
     // only change if value has changed: maybe only focus entered and left
     if (row[name] === value) return
 
-    db.query(`UPDATE lists SET ${name} = $1 WHERE list_id = $2`, [
-      value,
-      listId,
-    ])
+    try {
+      await db.query(`UPDATE lists SET ${name} = $1 WHERE list_id = $2`, [
+        value,
+        listId,
+      ])
+    } catch (error) {
+      setValidations((prev) => ({
+        ...prev,
+        [name]: { state: 'error', message: error.message },
+      }))
+      return
+    }
+    setValidations((prev) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { [name]: _, ...rest } = prev
+      return rest
+    })
     addOperation({
       table: 'lists',
       rowIdName: 'list_id',
@@ -51,7 +65,12 @@ export const List = ({ from }) => {
     <div className="form-outer-container">
       <Header autoFocusRef={autoFocusRef} from={from} />
       <div className="form-container">
-        <Form onChange={onChange} row={row} autoFocusRef={autoFocusRef} />
+        <Form
+          onChange={onChange}
+          row={row}
+          autoFocusRef={autoFocusRef}
+          validations={validations}
+        />
       </div>
     </div>
   )
