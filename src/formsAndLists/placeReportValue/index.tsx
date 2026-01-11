116ddb704a9f509f37088b5d6d17b85f474b15b4
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2026-01-11 16:05:51 +0100

    validate forms

--- src/formsAndLists/placeReportValue/index.tsx
+++ src/formsAndLists/placeReportValue/index.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { useParams } from '@tanstack/react-router'
 import { usePGlite, useLiveQuery } from '@electric-sql/pglite-react'
 import { useSetAtom } from 'jotai'
@@ -18,6 +18,8 @@ export const PlaceReportValue = ({ from }) => {
   const { placeReportValueId } = useParams({ from })
   const addOperation = useSetAtom(addOperationAtom)
 
+  const [validations, setValidations] = useState({})
+
   const autoFocusRef = useRef<HTMLInputElement>(null)
 
   const db = usePGlite()
@@ -34,10 +36,23 @@ export const PlaceReportValue = ({ from }) => {
     // only change if value has changed: maybe only focus entered and left
     if (row[name] === value) return
 
-    await db.query(
-      `UPDATE place_report_values SET ${name} = $1 WHERE place_report_value_id = $2`,
-      [value, placeReportValueId],
-    )
+    try {
+      await db.query(
+        `UPDATE place_report_values SET ${name} = $1 WHERE place_report_value_id = $2`,
+        [value, placeReportValueId],
+      )
+    } catch (error) {
+      setValidations((prev) => ({
+        ...prev,
+        [name]: { state: 'error', message: error.message },
+      }))
+      return
+    }
+    setValidations((prev) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { [name]: _, ...rest } = prev
+      return rest
+    })
     addOperation({
       table: 'place_report_values',
       rowIdName: 'place_report_value_id',
@@ -67,6 +82,8 @@ export const PlaceReportValue = ({ from }) => {
           onChange={onChange}
           autoFocus
           ref={autoFocusRef}
+          validationState={validations.unit_id?.state}
+          validationMessage={validations.unit_id?.message}
         />
         <TextField
           label="Value (integer)"
@@ -74,6 +91,8 @@ export const PlaceReportValue = ({ from }) => {
           type="number"
           value={row.value_integer ?? ''}
           onChange={onChange}
+          validationState={validations.value_integer?.state}
+          validationMessage={validations.value_integer?.message}
         />
         <TextField
           label="Value (numeric)"
@@ -81,12 +100,16 @@ export const PlaceReportValue = ({ from }) => {
           type="number"
           value={row.value_numeric ?? ''}
           onChange={onChange}
+          validationState={validations.value_numeric?.state}
+          validationMessage={validations.value_numeric?.message}
         />
         <TextField
           label="Value (text)"
           name="value_text"
           value={row.value_text ?? ''}
           onChange={onChange}
+          validationState={validations.value_text?.state}
+          validationMessage={validations.value_text?.message}
         />
       </div>
     </div>
