25b6d99397e1d0cdcb48871c5dfe02c1ba379b55
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-12 12:21:23 +0100

    add validation

--- src/formsAndLists/placeLevel/Header.tsx
+++ src/formsAndLists/placeLevel/Header.tsx
@@ -30,56 +30,68 @@ export const Header = ({ autoFocusRef }: Props) => {
   }
 
   const deleteRow = async () => {
-    const prevRes = await db.query(
-      `SELECT * FROM place_levels WHERE place_level_id = $1`,
-      [placeLevelId],
-    )
-    const prev = prevRes?.rows?.[0] ?? {}
-    await db.query(`DELETE FROM place_levels WHERE place_level_id = $1`, [
-      placeLevelId,
-    ])
-    addOperation({
-      table: 'place_levels',
-      rowIdName: 'place_level_id',
-      rowId: placeLevelId,
-      operation: 'delete',
-      prev,
-    })
-    navigate({ to: '..' })
+    try {
+      const prevRes = await db.query(
+        `SELECT * FROM place_levels WHERE place_level_id = $1`,
+        [placeLevelId],
+      )
+      const prev = prevRes?.rows?.[0] ?? {}
+      await db.query(`DELETE FROM place_levels WHERE place_level_id = $1`, [
+        placeLevelId,
+      ])
+      addOperation({
+        table: 'place_levels',
+        rowIdName: 'place_level_id',
+        rowId: placeLevelId,
+        operation: 'delete',
+        prev,
+      })
+      navigate({ to: '..' })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   const toNext = async () => {
-    const res = await db.query(
-      `SELECT place_level_id FROM place_levels WHERE project_id = $1 ORDER BY label`,
-      [projectId],
-    )
-    const placeLevels = res?.rows
-    const len = placeLevels.length
-    const index = placeLevels.findIndex(
-      (p) => p.place_level_id === placeLevelId,
-    )
-    const next = placeLevels[(index + 1) % len]
-    navigate({
-      to: `../${next.place_level_id}`,
-      params: (prev) => ({ ...prev, placeLevelId: next.place_level_id }),
-    })
+    try {
+      const res = await db.query(
+        `SELECT place_level_id FROM place_levels WHERE project_id = $1 ORDER BY label`,
+        [projectId],
+      )
+      const placeLevels = res?.rows
+      const len = placeLevels.length
+      const index = placeLevels.findIndex(
+        (p) => p.place_level_id === placeLevelId,
+      )
+      const next = placeLevels[(index + 1) % len]
+      navigate({
+        to: `../${next.place_level_id}`,
+        params: (prev) => ({ ...prev, placeLevelId: next.place_level_id }),
+      })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   const toPrevious = async () => {
-    const res = await db.query(
-      `SELECT place_level_id FROM place_levels WHERE project_id = $1 ORDER BY label`,
-      [projectId],
-    )
-    const placeLevels = res?.rows
-    const len = placeLevels.length
-    const index = placeLevels.findIndex(
-      (p) => p.place_level_id === placeLevelId,
-    )
-    const previous = placeLevels[(index + len - 1) % len]
-    navigate({
-      to: `../${previous.place_level_id}`,
-      params: (prev) => ({ ...prev, placeLevelId: previous.place_level_id }),
-    })
+    try {
+      const res = await db.query(
+        `SELECT place_level_id FROM place_levels WHERE project_id = $1 ORDER BY label`,
+        [projectId],
+      )
+      const placeLevels = res?.rows
+      const len = placeLevels.length
+      const index = placeLevels.findIndex(
+        (p) => p.place_level_id === placeLevelId,
+      )
+      const previous = placeLevels[(index + len - 1) % len]
+      navigate({
+        to: `../${previous.place_level_id}`,
+        params: (prev) => ({ ...prev, placeLevelId: previous.place_level_id }),
+      })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   return (
