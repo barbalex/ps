cdb0993b101189437efd8f1e7887f348feaedf8b
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-12 12:11:11 +0100

    add validation

--- src/formsAndLists/goalReport/Header.tsx
+++ src/formsAndLists/goalReport/Header.tsx
@@ -29,60 +29,72 @@ export const Header = ({ autoFocusRef, from }) => {
   }
 
   const deleteRow = async () => {
-    const prevRes = await db.query(
-      `SELECT * FROM goal_reports WHERE goal_report_id = $1`,
-      [goalReportId],
-    )
-    const prev = prevRes?.rows?.[0] ?? {}
-    db.query(`DELETE FROM goal_reports WHERE goal_report_id = $1`, [
-      goalReportId,
-    ])
-    addOperation({
-      table: 'goal_reports',
-      rowIdName: 'goal_report_id',
-      rowId: goalReportId,
-      operation: 'delete',
-      prev,
-    })
-    navigate({ to: isForm ? `../..` : `..` })
+    try {
+      const prevRes = await db.query(
+        `SELECT * FROM goal_reports WHERE goal_report_id = $1`,
+        [goalReportId],
+      )
+      const prev = prevRes?.rows?.[0] ?? {}
+      db.query(`DELETE FROM goal_reports WHERE goal_report_id = $1`, [
+        goalReportId,
+      ])
+      addOperation({
+        table: 'goal_reports',
+        rowIdName: 'goal_report_id',
+        rowId: goalReportId,
+        operation: 'delete',
+        prev,
+      })
+      navigate({ to: isForm ? `../..` : `..` })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   const toNext = async () => {
-    const res = await db.query(
-      `SELECT goal_report_id FROM goal_reports WHERE goal_id = $1 ORDER BY label`,
-      [goalId],
-    )
-    const goalReports = res?.rows
-    const len = goalReports.length
-    const index = goalReports.findIndex(
-      (p) => p.goal_report_id === goalReportId,
-    )
-    const next = goalReports[(index + 1) % len]
-    navigate({
-      to: isForm
-        ? `../../${next.goal_report_id}/report`
-        : `../${next.goal_report_id}`,
-      params: (prev) => ({ ...prev, goalReportId: next.goal_report_id }),
-    })
+    try {
+      const res = await db.query(
+        `SELECT goal_report_id FROM goal_reports WHERE goal_id = $1 ORDER BY label`,
+        [goalId],
+      )
+      const goalReports = res?.rows
+      const len = goalReports.length
+      const index = goalReports.findIndex(
+        (p) => p.goal_report_id === goalReportId,
+      )
+      const next = goalReports[(index + 1) % len]
+      navigate({
+        to: isForm
+          ? `../../${next.goal_report_id}/report`
+          : `../${next.goal_report_id}`,
+        params: (prev) => ({ ...prev, goalReportId: next.goal_report_id }),
+      })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   const toPrevious = async () => {
-    const res = await db.query(
-      `SELECT goal_report_id FROM goal_reports WHERE goal_id = $1 ORDER BY label`,
-      [goalId],
-    )
-    const goalReports = res?.rows
-    const len = goalReports.length
-    const index = goalReports.findIndex(
-      (p) => p.goal_report_id === goalReportId,
-    )
-    const previous = goalReports[(index + len - 1) % len]
-    navigate({
-      to: isForm
-        ? `../../${previous.goal_report_id}/report`
-        : `../${previous.goal_report_id}`,
-      params: (prev) => ({ ...prev, goalReportId: previous.goal_report_id }),
-    })
+    try {
+      const res = await db.query(
+        `SELECT goal_report_id FROM goal_reports WHERE goal_id = $1 ORDER BY label`,
+        [goalId],
+      )
+      const goalReports = res?.rows
+      const len = goalReports.length
+      const index = goalReports.findIndex(
+        (p) => p.goal_report_id === goalReportId,
+      )
+      const previous = goalReports[(index + len - 1) % len]
+      navigate({
+        to: isForm
+          ? `../../${previous.goal_report_id}/report`
+          : `../${previous.goal_report_id}`,
+        params: (prev) => ({ ...prev, goalReportId: previous.goal_report_id }),
+      })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   return (
