96323fafb0dd77471db839acf6a9d00170346cf6
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-12 12:27:35 +0100

    add validation

--- src/formsAndLists/projectCrs/Header.tsx
+++ src/formsAndLists/projectCrs/Header.tsx
@@ -26,52 +26,64 @@ export const Header = ({ autoFocusRef }) => {
   }
 
   const deleteRow = async () => {
-    const prevRes = await db.query(
-      `SELECT * FROM project_crs WHERE project_crs_id = $1`,
-      [projectId],
-    )
-    const prev = prevRes?.rows?.[0] ?? {}
-    db.query(`DELETE FROM project_crs WHERE project_crs_id = $1`, [
-      projectCrsId,
-    ])
-    addOperation({
-      table: 'project_crs',
-      rowIdName: 'project_crs_id',
-      rowId: projectCrsId,
-      operation: 'delete',
-      prev,
-    })
-    navigate({ to: '..' })
+    try {
+      const prevRes = await db.query(
+        `SELECT * FROM project_crs WHERE project_crs_id = $1`,
+        [projectId],
+      )
+      const prev = prevRes?.rows?.[0] ?? {}
+      db.query(`DELETE FROM project_crs WHERE project_crs_id = $1`, [
+        projectCrsId,
+      ])
+      addOperation({
+        table: 'project_crs',
+        rowIdName: 'project_crs_id',
+        rowId: projectCrsId,
+        operation: 'delete',
+        prev,
+      })
+      navigate({ to: '..' })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   const toNext = async () => {
-    const res = await db.query(
-      `SELECT project_crs_id FROM project_crs WHERE project_id = $1 ORDER BY label`,
-      [projectId],
-    )
-    const projectCrs = res?.rows
-    const len = projectCrs.length
-    const index = projectCrs.findIndex((p) => p.project_crs_id === projectCrsId)
-    const next = projectCrs[(index + 1) % len]
-    navigate({
-      to: `../${next.project_crs_id}`,
-      params: (prev) => ({ ...prev, projectCrsId: next.project_crs_id }),
-    })
+    try {
+      const res = await db.query(
+        `SELECT project_crs_id FROM project_crs WHERE project_id = $1 ORDER BY label`,
+        [projectId],
+      )
+      const projectCrs = res?.rows
+      const len = projectCrs.length
+      const index = projectCrs.findIndex((p) => p.project_crs_id === projectCrsId)
+      const next = projectCrs[(index + 1) % len]
+      navigate({
+        to: `../${next.project_crs_id}`,
+        params: (prev) => ({ ...prev, projectCrsId: next.project_crs_id }),
+      })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   const toPrevious = async () => {
-    const res = await db.query(
-      `SELECT project_crs_id FROM project_crs WHERE project_id = $1 ORDER BY label`,
-      [projectId],
-    )
-    const projectCrs = res?.rows
-    const len = projectCrs.length
-    const index = projectCrs.findIndex((p) => p.project_crs_id === projectCrsId)
-    const previous = projectCrs[(index + len - 1) % len]
-    navigate({
-      to: `../${previous.project_crs_id}`,
-      params: (prev) => ({ ...prev, projectCrsId: previous.project_crs_id }),
-    })
+    try {
+      const res = await db.query(
+        `SELECT project_crs_id FROM project_crs WHERE project_id = $1 ORDER BY label`,
+        [projectId],
+      )
+      const projectCrs = res?.rows
+      const len = projectCrs.length
+      const index = projectCrs.findIndex((p) => p.project_crs_id === projectCrsId)
+      const previous = projectCrs[(index + len - 1) % len]
+      navigate({
+        to: `../${previous.project_crs_id}`,
+        params: (prev) => ({ ...prev, projectCrsId: previous.project_crs_id }),
+      })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   return (
