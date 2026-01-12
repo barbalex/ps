24af45cf2b731d2118ea87c31b9229f5e8aae485
Alexander Gabriel <alex.barbalex@gmail.com>
2026-01-12 12:23:33 +0100

    add validation

--- src/formsAndLists/placeReportValue/Header.tsx
+++ src/formsAndLists/placeReportValue/Header.tsx
@@ -27,63 +27,75 @@ export const Header = ({ autoFocusRef, from }) => {
   }
 
   const deleteRow = async () => {
-    const prevRes = await db.query(
-      `SELECT * FROM place_report_values WHERE place_report_value_id = $1`,
-      [placeReportValueId],
-    )
-    const prev = prevRes?.rows?.[0] ?? {}
-    db.query(
-      `DELETE FROM place_report_values WHERE place_report_value_id = $1`,
-      [placeReportValueId],
-    )
-    addOperation({
-      table: 'place_report_values',
-      rowIdName: 'place_report_value_id',
-      rowId: placeReportValueId,
-      operation: 'delete',
-      prev,
-    })
-    navigate({ to: '..' })
+    try {
+      const prevRes = await db.query(
+        `SELECT * FROM place_report_values WHERE place_report_value_id = $1`,
+        [placeReportValueId],
+      )
+      const prev = prevRes?.rows?.[0] ?? {}
+      db.query(
+        `DELETE FROM place_report_values WHERE place_report_value_id = $1`,
+        [placeReportValueId],
+      )
+      addOperation({
+        table: 'place_report_values',
+        rowIdName: 'place_report_value_id',
+        rowId: placeReportValueId,
+        operation: 'delete',
+        prev,
+      })
+      navigate({ to: '..' })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   const toNext = async () => {
-    const res = await db.query(
-      `SELECT place_report_value_id FROM place_report_values WHERE place_report_id = $1 ORDER BY label`,
-      [placeReportId],
-    )
-    const rows = res?.rows
-    const len = rows.length
-    const index = rows.findIndex(
-      (p) => p.place_report_value_id === placeReportValueId,
-    )
-    const next = rows[(index + 1) % len]
-    navigate({
-      to: `../${next.place_report_value_id}`,
-      params: (prev) => ({
-        ...prev,
-        placeReportValueId: next.place_report_value_id,
-      }),
-    })
+    try {
+      const res = await db.query(
+        `SELECT place_report_value_id FROM place_report_values WHERE place_report_id = $1 ORDER BY label`,
+        [placeReportId],
+      )
+      const rows = res?.rows
+      const len = rows.length
+      const index = rows.findIndex(
+        (p) => p.place_report_value_id === placeReportValueId,
+      )
+      const next = rows[(index + 1) % len]
+      navigate({
+        to: `../${next.place_report_value_id}`,
+        params: (prev) => ({
+          ...prev,
+          placeReportValueId: next.place_report_value_id,
+        }),
+      })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   const toPrevious = async () => {
-    const res = await db.query(
-      `SELECT place_report_value_id FROM place_report_values WHERE place_report_id = $1 ORDER BY label`,
-      [placeReportId],
-    )
-    const rows = res?.rows
-    const len = rows.length
-    const index = rows.findIndex(
-      (p) => p.place_report_value_id === placeReportValueId,
-    )
-    const previous = rows[(index + len - 1) % len]
-    navigate({
-      to: `../${previous.place_report_value_id}`,
-      params: (prev) => ({
-        ...prev,
-        placeReportValueId: previous.place_report_value_id,
-      }),
-    })
+    try {
+      const res = await db.query(
+        `SELECT place_report_value_id FROM place_report_values WHERE place_report_id = $1 ORDER BY label`,
+        [placeReportId],
+      )
+      const rows = res?.rows
+      const len = rows.length
+      const index = rows.findIndex(
+        (p) => p.place_report_value_id === placeReportValueId,
+      )
+      const previous = rows[(index + len - 1) % len]
+      navigate({
+        to: `../${previous.place_report_value_id}`,
+        params: (prev) => ({
+          ...prev,
+          placeReportValueId: previous.place_report_value_id,
+        }),
+      })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   return (
