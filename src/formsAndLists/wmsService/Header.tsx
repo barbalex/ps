2a2a7e4835370991a2f437ffa9012a3a4777b362
Alexander Gabriel <alex.barbalex@gmail.com>
2026-02-04 18:37:53 +0100

    add wms services

--- src/formsAndLists/wmsService/Header.tsx
+++ src/formsAndLists/wmsService/Header.tsx
@@ -1,13 +1,22 @@
 import { useParams, useNavigate } from '@tanstack/react-router'
-import { useLiveQuery } from '@electric-sql/pglite-react'
+import { usePGlite, useLiveQuery } from '@electric-sql/pglite-react'
+import { useSetAtom } from 'jotai'
 
+import { createWmsService } from '../../modules/createRows.ts'
 import { FormHeader } from '../../components/FormHeader/index.tsx'
+import { addOperationAtom } from '../../store.ts'
 
 const from = '/data/projects/$projectId_/wms-services/$wmsServiceId/'
 
-export const Header = () => {
+interface Props {
+  autoFocusRef: React.RefObject<HTMLInputElement>
+}
+
+export const Header = ({ autoFocusRef }: Props) => {
   const { projectId, wmsServiceId } = useParams({ from })
   const navigate = useNavigate()
+  const addOperation = useSetAtom(addOperationAtom)
+  const db = usePGlite()
 
   const countRes = useLiveQuery(
     `SELECT COUNT(*) as count FROM wms_services WHERE project_id = '${projectId}'`,
@@ -25,6 +34,37 @@ export const Header = () => {
   const rows = res?.rows ?? []
   const len = rows.length
   const ownIndex = rows.findIndex((row) => row.wms_service_id === wmsServiceId)
+
+  const addRow = async () => {
+    const id = await createWmsService({ projectId })
+    if (!id) return
+    navigate({
+      to: `../${id}`,
+      params: (prev) => ({ ...prev, wmsServiceId: id }),
+    })
+    autoFocusRef?.current?.focus()
+  }
+
+  const deleteRow = async () => {
+    try {
+      const prevRes = await db.query(
+        `SELECT * FROM wms_services WHERE wms_service_id = $1`,
+        [wmsServiceId],
+      )
+      const prev = prevRes?.rows?.[0] ?? {}
+      await db.query(`DELETE FROM wms_services WHERE wms_service_id = $1`, [wmsServiceId])
+      addOperation({
+        table: 'wms_services',
+        rowIdName: 'wms_service_id',
+        rowId: wmsServiceId,
+        operation: 'delete',
+        prev,
+      })
+      navigate({ to: '..' })
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   const toNext = () => {
     const next = rows[(ownIndex + 1) % len]
@@ -47,6 +87,8 @@ export const Header = () => {
   return (
     <FormHeader
       title="WMS Service"
+      addRow={addRow}
+      deleteRow={deleteRow}
       toNext={toNext}
       toPrevious={toPrevious}
       toNextDisabled={rowCount <= 1}
