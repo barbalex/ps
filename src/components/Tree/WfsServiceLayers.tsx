7074fdb783bb85535fa3c5ca80601da6ec499d79
Alexander Gabriel <alex.barbalex@gmail.com>
2026-02-07 00:21:22 +0100

    working on WFS services

--- src/components/Tree/WfsServiceLayers.tsx
+++ src/components/Tree/WfsServiceLayers.tsx
@@ -1,5 +1,9 @@
+import { useNavigate } from '@tanstack/react-router'
+
 import { Node } from './Node.tsx'
 import { WfsServiceLayerNode } from './WfsServiceLayer.tsx'
+import { removeChildNodes } from '../../modules/tree/removeChildNodes.ts'
+import { addOpenNodes } from '../../modules/tree/addOpenNodes.ts'
 import { useWfsServiceLayersNavData } from '../../modules/useWfsServiceLayersNavData.ts'
 
 interface Props {
@@ -13,8 +17,31 @@ export const WfsServiceLayersNode = ({
   wfsServiceId,
   level = 5,
 }: Props) => {
+  const navigate = useNavigate()
+
   const { navData } = useWfsServiceLayersNavData({ projectId, wfsServiceId })
-  const { label, ownUrl, isOpen, isInActiveNodeArray, isActive, navs } = navData
+  const {
+    label,
+    parentUrl,
+    ownArray,
+    ownUrl,
+    urlPath,
+    isOpen,
+    isInActiveNodeArray,
+    isActive,
+    navs,
+  } = navData
+
+  const onClickButton = () => {
+    if (isOpen) {
+      removeChildNodes({ node: ownArray })
+      if (isInActiveNodeArray && ownArray.length <= urlPath.length) {
+        navigate({ to: parentUrl })
+      }
+      return
+    }
+    addOpenNodes({ nodes: [ownArray] })
+  }
 
   const showNavs = isOpen && navs.length > 0 && navs[0].id
 
@@ -28,6 +55,7 @@ export const WfsServiceLayersNode = ({
         isActive={isActive}
         childrenCount={navs.length}
         to={ownUrl}
+        onClickButton={onClickButton}
       />
       {showNavs &&
         navs.map((nav) => (
