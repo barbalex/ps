a7c7b39cda136e284cc3e7ce99a9c580398c7bd8
Alexander Gabriel <alex.barbalex@gmail.com>
2026-02-07 10:41:48 +0100

    describe better

--- src/formsAndLists/projectCrss/Info.tsx
+++ src/formsAndLists/projectCrss/Info.tsx
@@ -1,10 +1,14 @@
 import styles from './Info.module.css'
 
 export const Info = () => (
-  <div className={styles.container} tabIndex={-1}>
+  <div
+    className={styles.container}
+    tabIndex={-1}
+  >
     <p>You can add multiple CRS.</p>
     <p>One of them can be set as the map presentation crs.</p>
     <p>It will be used to show coordinates in the map.</p>
     <p>Users can choose in the map one of the CRS'es you add here.</p>
+    <p>If no CRS is selected, WGS84 will be used by default.</p>
   </div>
 )
