a16c03341e3a46149bd8d60223cb044d6ecc184a
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2026-02-15 12:51:03 +0100

    validate navigation ids

--- src/routes/data/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/checks/$checkId_/taxa/$checkTaxonId.index.tsx
+++ src/routes/data/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/checks/$checkId_/taxa/$checkTaxonId.index.tsx
@@ -6,9 +6,26 @@ export const Route = createFileRoute(
   '/data/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/checks/$checkId_/taxa/$checkTaxonId/',
 )({
   component: RouteComponent,
-  beforeLoad: () => ({
+  beforeLoad: ({ params }) => {
+    if (!params.projectId_ || params.projectId_ === 'undefined') {
+      throw new Error('Invalid or missing projectId_ in route parameters')
+    }
+    if (!params.subprojectId_ || params.subprojectId_ === 'undefined') {
+      throw new Error('Invalid or missing subprojectId_ in route parameters')
+    }
+    if (!params.placeId_ || params.placeId_ === 'undefined') {
+      throw new Error('Invalid or missing placeId_ in route parameters')
+    }
+    if (!params.checkId_ || params.checkId_ === 'undefined') {
+      throw new Error('Invalid or missing checkId_ in route parameters')
+    }
+    if (!params.checkTaxonId || params.checkTaxonId === 'undefined') {
+      throw new Error('Invalid or missing checkTaxonId in route parameters')
+    }
+    return {
     navDataFetcher: 'useCheckTaxonNavData',
-  }),
+  }
+  },
 })
 
 function RouteComponent() {
