d4a18dc653449f6f69f82c100d0b21780e10b6de
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2026-02-15 12:48:20 +0100

    validate navigation ids

--- src/routes/data/projects/$projectId_/persons/$personId.index.tsx
+++ src/routes/data/projects/$projectId_/persons/$personId.index.tsx
@@ -6,7 +6,15 @@ export const Route = createFileRoute(
   '/data/projects/$projectId_/persons/$personId/',
 )({
   component: Person,
-  beforeLoad: () => ({
+  beforeLoad: ({ params }) => {
+    if (!params.projectId_ || params.projectId_ === 'undefined') {
+      throw new Error('Invalid or missing projectId_ in route parameters')
+    }
+    if (!params.personId || params.personId === 'undefined') {
+      throw new Error('Invalid or missing personId in route parameters')
+    }
+    return {
     navDataFetcher: 'usePersonNavData',
-  }),
+  }
+  },
 })
