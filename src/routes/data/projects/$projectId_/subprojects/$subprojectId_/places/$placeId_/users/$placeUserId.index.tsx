c167ba1585099839ea9d1b8af48b0da4839808f4
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2026-02-15 12:52:08 +0100

    validate navigation ids

--- src/routes/data/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/users/$placeUserId.index.tsx
+++ src/routes/data/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/users/$placeUserId.index.tsx
@@ -6,9 +6,23 @@ export const Route = createFileRoute(
   '/data/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/users/$placeUserId/',
 )({
   component: RouteComponent,
-  beforeLoad: () => ({
+  beforeLoad: ({ params }) => {
+    if (!params.projectId_ || params.projectId_ === 'undefined') {
+      throw new Error('Invalid or missing projectId_ in route parameters')
+    }
+    if (!params.subprojectId_ || params.subprojectId_ === 'undefined') {
+      throw new Error('Invalid or missing subprojectId_ in route parameters')
+    }
+    if (!params.placeId_ || params.placeId_ === 'undefined') {
+      throw new Error('Invalid or missing placeId_ in route parameters')
+    }
+    if (!params.placeUserId || params.placeUserId === 'undefined') {
+      throw new Error('Invalid or missing placeUserId in route parameters')
+    }
+    return {
     navDataFetcher: 'usePlaceUserNavData',
-  }),
+  }
+  },
 })
 
 function RouteComponent() {
