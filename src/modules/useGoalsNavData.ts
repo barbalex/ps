48fda23b9bcf799b0066be6f17c9bb09adc9240e
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2026-02-15 12:34:49 +0100

    validate navigation ids

--- src/modules/useGoalsNavData.ts
+++ src/modules/useGoalsNavData.ts
@@ -6,7 +6,6 @@ import { isEqual } from 'es-toolkit'
 import { filterStringFromFilter } from './filterStringFromFilter.ts'
 import { buildNavLabel } from './buildNavLabel.ts'
 import { goalsFilterAtom, treeOpenNodesAtom } from '../store.ts'
-import { validateIds } from './validateIds.ts'
 
 type Props = {
   projectId: string
--- src/modules/usePlaceNavData.ts
+++ src/modules/usePlaceNavData.ts
@@ -15,7 +15,6 @@ import {
 } from '../store.ts'
 import { buildNavLabel } from './buildNavLabel.ts'
 import { filterStringFromFilter } from './filterStringFromFilter.ts'
-import { validateIds } from './validateIds.ts'
 
 type Props = {
   projectId: string
@@ -77,9 +76,6 @@ export const usePlaceNavData = ({
   const [filesFilter] = useAtom(filesFilterAtom)
   const filesFilterString = filterStringFromFilter(filesFilter)
   const filesIsFiltered = !!filesFilterString
-
-  // Validate after hooks to comply with Rules of Hooks
-  validateIds({ projectId, subprojectId })
 
   const sql = `
       WITH
