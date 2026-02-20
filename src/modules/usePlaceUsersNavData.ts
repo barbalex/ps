e727565be4935067430e4890cbd346a01ab1c647
alex.barbalex@gmail.com <alex.barbalex@gmail.com>
2026-02-20 11:31:49 +0100

    correct

--- src/modules/usePlaceUsersNavData.ts
+++ src/modules/usePlaceUsersNavData.ts
@@ -1,29 +1,29 @@
-import { useLiveQuery } from "@electric-sql/pglite-react";
-import { useAtom } from "jotai";
-import { useLocation } from "@tanstack/react-router";
-import { isEqual } from "es-toolkit";
+import { useLiveQuery } from '@electric-sql/pglite-react'
+import { useAtom } from 'jotai'
+import { useLocation } from '@tanstack/react-router'
+import { isEqual } from 'es-toolkit'
 
-import { filterStringFromFilter } from "./filterStringFromFilter.ts";
-import { buildNavLabel } from "./buildNavLabel.ts";
+import { filterStringFromFilter } from './filterStringFromFilter.ts'
+import { buildNavLabel } from './buildNavLabel.ts'
 import {
   placeUsers1FilterAtom,
   placeUsers2FilterAtom,
   treeOpenNodesAtom,
-} from "../store.ts";
+} from '../store.ts'
 
 type Props = {
-  projectId: string;
-  subprojectId: string;
-  placeId: string;
-  placeId2?: string;
-};
+  projectId: string
+  subprojectId: string
+  placeId: string
+  placeId2?: string
+}
 
 type NavData = {
-  id: string;
-  label: string;
-  count_unfiltered?: number;
-  count_filtered?: number;
-}[];
+  id: string
+  label: string
+  count_unfiltered?: number
+  count_filtered?: number
+}[]
 
 export const usePlaceUsersNavData = ({
   projectId,
@@ -31,20 +31,36 @@ export const usePlaceUsersNavData = ({
   placeId,
   placeId2,
 }: Props) => {
-  const [openNodes] = useAtom(treeOpenNodesAtom);
-  const location = useLocation();
+  const [openNodes] = useAtom(treeOpenNodesAtom)
+  const location = useLocation()
 
   const [filter] = useAtom(
     placeId2 ? placeUsers2FilterAtom : placeUsers1FilterAtom,
-  );
-  const filterString = filterStringFromFilter(filter);
-  const isFiltered = !!filterString;
+  )
+  const filterString = filterStringFromFilter(filter)
+  const isFiltered = !!filterString
+
+  const parentArray = [
+    'data',
+    'projects',
+    projectId,
+    'subprojects',
+    subprojectId,
+    'places',
+    placeId,
+    ...(placeId2 ? ['places', placeId2] : []),
+  ]
+  const parentUrl = `/${parentArray.join('/')}`
+  const ownArray = [...parentArray, 'users']
+  const ownUrl = `/${ownArray.join('/')}`
+  // needs to work not only works for urlPath, for all opened paths!
+  const isOpen = openNodes.some((array) => isEqual(array, ownArray))
 
   const sql = isOpen
     ? `
       WITH
         count_unfiltered AS (SELECT count(*) FROM place_users WHERE place_id = '${placeId2 ?? placeId}'),
-        count_filtered AS (SELECT count(*) FROM place_users WHERE place_id = '${placeId2 ?? placeId}'${isFiltered ? ` AND ${filterString}` : ""})
+        count_filtered AS (SELECT count(*) FROM place_users WHERE place_id = '${placeId2 ?? placeId}'${isFiltered ? ` AND ${filterString}` : ''})
       SELECT
         place_user_id AS id,
         label,
@@ -53,43 +69,28 @@ export const usePlaceUsersNavData = ({
       FROM place_users 
       , count_unfiltered, count_filtered
       WHERE place_id = '${placeId2 ?? placeId}'
-        ${isFiltered ? ` AND ${filterString}` : ""}
+        ${isFiltered ? ` AND ${filterString}` : ''}
       ORDER BY label`
     : `
       WITH
         count_unfiltered AS (SELECT count(*) FROM place_users WHERE place_id = '${placeId2 ?? placeId}'),
-        count_filtered AS (SELECT count(*) FROM place_users WHERE place_id = '${placeId2 ?? placeId}'${isFiltered ? ` AND ${filterString}` : ""})
+        count_filtered AS (SELECT count(*) FROM place_users WHERE place_id = '${placeId2 ?? placeId}'${isFiltered ? ` AND ${filterString}` : ''})
       SELECT
         count_unfiltered.count AS count_unfiltered,
         count_filtered.count AS count_filtered
       FROM count_unfiltered, count_filtered
-    `;
+    `
 
-  const res = useLiveQuery(sql);
+  const res = useLiveQuery(sql)
 
-  const loading = res === undefined;
+  const loading = res === undefined
 
-  const navs: NavData = res?.rows ?? [];
-  const countUnfiltered = navs[0]?.count_unfiltered ?? 0;
-  const countFiltered = navs[0]?.count_filtered ?? 0;
-  const parentArray = [
-    "data",
-    "projects",
-    projectId,
-    "subprojects",
-    subprojectId,
-    "places",
-    placeId,
-    ...(placeId2 ? ["places", placeId2] : []),
-  ];
-  const parentUrl = `/${parentArray.join("/")}`;
-  const ownArray = [...parentArray, "users"];
-  const ownUrl = `/${ownArray.join("/")}`;
-  // needs to work not only works for urlPath, for all opened paths!
-  const isOpen = openNodes.some((array) => isEqual(array, ownArray));
-  const urlPath = location.pathname.split("/").filter((p) => p !== "");
-  const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part);
-  const isActive = isEqual(urlPath, ownArray);
+  const navs: NavData = res?.rows ?? []
+  const countUnfiltered = navs[0]?.count_unfiltered ?? 0
+  const countFiltered = navs[0]?.count_filtered ?? 0
+  const urlPath = location.pathname.split('/').filter((p) => p !== '')
+  const isInActiveNodeArray = ownArray.every((part, i) => urlPath[i] === part)
+  const isActive = isEqual(urlPath, ownArray)
 
   const navData = {
     isInActiveNodeArray,
@@ -103,12 +104,12 @@ export const usePlaceUsersNavData = ({
       countFiltered,
       countUnfiltered,
       isFiltered,
-      namePlural: "Users",
+      namePlural: 'Users',
       loading,
     }),
-    nameSingular: "User",
+    nameSingular: 'User',
     navs,
-  };
+  }
 
-  return { loading, navData, isFiltered };
-};
+  return { loading, navData, isFiltered }
+}
