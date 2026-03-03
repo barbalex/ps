bb62147fbec4ef886c0bf915cec4c84909c7ab5f
Alexander Gabriel <alex.barbalex@gmail.com>
2026-03-03 09:51:31 +0100

    postpone auth

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
 // import { useCorbado } from '@corbado/react'
 import { Loading } from './shared/Loading.tsx'
 
-import { Auth } from './Auth.tsx'
+// import { Auth } from './Auth.tsx'
 import { useSession } from '../modules/authClient.ts'
 
 // https://www.robinwieruch.de/react-router-authentication/
@@ -9,7 +9,7 @@ export const ProtectedRoute = ({ children }) => {
   // const { loading, isAuthenticated } = useCorbado()
   // console.log('hello ProtectedRoute', { loading, isAuthenticated })
   const { data: session, isPending, error } = useSession()
-  console.log('ProtectedRoute, session data:', { session, isPending, error })
+  // console.log('ProtectedRoute, session data:', { session, isPending, error })
 
   //TODO: get this working again
   if (isPending) return <Loading label="Authenticating" />
