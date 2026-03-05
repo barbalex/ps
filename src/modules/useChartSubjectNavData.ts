8e713c1cb7de2496a8dd028285345cb938eaeb4b
Alexander Gabriel <alex.barbalex@gmail.com>
2026-03-05 15:55:48 +0100

    work on translations

--- src/modules/useChartSubjectNavData.ts
+++ src/modules/useChartSubjectNavData.ts
@@ -2,6 +2,7 @@ import { useLiveQuery } from '@electric-sql/pglite-react'
 import { useAtom } from 'jotai'
 import { useLocation } from '@tanstack/react-router'
 import { isEqual } from 'es-toolkit'
+import { useIntl } from 'react-intl'
 
 import { treeOpenNodesAtom } from '../store.ts'
 
@@ -27,6 +28,7 @@ export const useChartSubjectNavData = ({
   chartId,
   chartSubjectId,
 }: Props) => {
+  const { formatMessage } = useIntl()
   const [openNodes] = useAtom(treeOpenNodesAtom)
   const location = useLocation()
 
@@ -63,7 +65,9 @@ export const useChartSubjectNavData = ({
   const isActive = isEqual(urlPath, ownArray)
 
   const notFound = !!res && !nav
-  const label = notFound ? 'Not Found' : (nav?.label ?? nav?.id)
+  const label = notFound
+    ? formatMessage({ id: 'p+ORxp', defaultMessage: 'Nicht gefunden' })
+    : (nav?.label ?? nav?.id)
 
   const navData = {
     isInActiveNodeArray,
@@ -75,7 +79,10 @@ export const useChartSubjectNavData = ({
     ownUrl,
     label,
     notFound,
-    nameSingular: 'Chart Subject',
+    nameSingular: formatMessage({
+      id: 'jmuVHU',
+      defaultMessage: 'Diagramm-Thema',
+    }),
   }
 
   return { loading, navData }
