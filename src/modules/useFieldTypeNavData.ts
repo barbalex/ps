26002d7fd90c33ab1b043560de8df2b08e219637
Alexander Gabriel <alex.barbalex@gmail.com>
2026-03-05 16:15:27 +0100

    work on translations

--- src/modules/useFieldTypeNavData.ts
+++ src/modules/useFieldTypeNavData.ts
@@ -2,6 +2,7 @@ import { useAtom } from 'jotai'
 import { useLiveQuery } from '@electric-sql/pglite-react'
 import { useLocation } from '@tanstack/react-router'
 import { isEqual } from 'es-toolkit'
+import { useIntl } from 'react-intl'
 
 import { treeOpenNodesAtom } from '../store.ts'
 
@@ -20,6 +21,7 @@ type NavData = {
 export const useFieldTypeNavData = ({ fieldTypeId }: Props) => {
   const [openNodes] = useAtom(treeOpenNodesAtom)
   const location = useLocation()
+  const { formatMessage } = useIntl()
 
   const sql = `
       SELECT 
@@ -44,7 +46,9 @@ export const useFieldTypeNavData = ({ fieldTypeId }: Props) => {
   const isActive = isEqual(urlPath, ownArray)
 
   const notFound = !!res && !nav
-  const label = notFound ? 'Not Found' : (nav?.label ?? nav?.id)
+  const label = notFound
+    ? formatMessage({ id: 'p+ORxp', defaultMessage: 'Nicht gefunden' })
+    : (nav?.label ?? nav?.id)
 
   const navData = {
     isInActiveNodeArray,
@@ -57,7 +61,7 @@ export const useFieldTypeNavData = ({ fieldTypeId }: Props) => {
     urlPath,
     label,
     notFound,
-    nameSingular: 'Field Type',
+    nameSingular: formatMessage({ id: 'LTiTmL', defaultMessage: 'Feld-Typ' }),
   }
 
   return { loading, navData }
