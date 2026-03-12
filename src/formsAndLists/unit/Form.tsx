ae8d22141cd8c91f706c47a4fda1065538eb5732
Alexander Gabriel <alex.barbalex@gmail.com>
2026-03-12 18:22:10 +0100

    translate

--- src/formsAndLists/unit/Form.tsx
+++ src/formsAndLists/unit/Form.tsx
@@ -1,101 +1,107 @@
+import { useIntl } from 'react-intl'
+
 import { TextField } from '../../components/shared/TextField.tsx'
 import { SwitchField } from '../../components/shared/SwitchField.tsx'
 
 import '../../form.css'
 
 // this form is rendered from a parent or outlet
-export const UnitForm = ({ onChange, row, autoFocusRef, validations = {} }) => (
-  <>
-    <TextField
-      label="Name"
-      name="name"
-      value={row.name ?? ''}
-      onChange={onChange}
-      autoFocus
-      ref={autoFocusRef}
-      validationState={validations?.name?.state}
-      validationMessage={validations?.name?.message}
-    />
-    <SwitchField
-      label="Use for action values"
-      name="use_for_action_values"
-      value={row.use_for_action_values ?? false}
-      onChange={onChange}
-      validationState={validations?.use_for_action_values?.state}
-      validationMessage={validations?.use_for_action_values?.message}
-    />
-    <SwitchField
-      label="Use for action report values"
-      name="use_for_action_report_values"
-      value={row.use_for_action_report_values ?? false}
-      onChange={onChange}
-      validationState={validations?.use_for_action_report_values?.state}
-      validationMessage={validations?.use_for_action_report_values?.message}
-    />
-    <SwitchField
-      label="Use for check values"
-      name="use_for_check_values"
-      value={row.use_for_check_values ?? false}
-      onChange={onChange}
-      validationState={validations?.use_for_check_values?.state}
-      validationMessage={validations?.use_for_check_values?.message}
-    />
-    <SwitchField
-      label="Use for place report values"
-      name="use_for_place_report_values"
-      value={row.use_for_place_report_values ?? false}
-      onChange={onChange}
-      validationState={validations?.use_for_place_report_values?.state}
-      validationMessage={validations?.use_for_place_report_values?.message}
-    />
-    <SwitchField
-      label="Use for goal report values"
-      name="use_for_goal_report_values"
-      value={row.use_for_goal_report_values ?? false}
-      onChange={onChange}
-      validationState={validations?.use_for_goal_report_values?.state}
-      validationMessage={validations?.use_for_goal_report_values?.message}
-    />
-    <SwitchField
-      label="Use for subproject taxa"
-      name="use_for_subproject_taxa"
-      value={row.use_for_subproject_taxa ?? false}
-      onChange={onChange}
-      validationState={validations?.use_for_subproject_taxa?.state}
-      validationMessage={validations?.use_for_subproject_taxa?.message}
-    />
-    <SwitchField
-      label="Use for check taxa"
-      name="use_for_check_taxa"
-      value={row.use_for_check_taxa ?? false}
-      onChange={onChange}
-      validationState={validations?.use_for_check_taxa?.state}
-      validationMessage={validations?.use_for_check_taxa?.message}
-    />
-    <SwitchField
-      label="Summable"
-      name="summable"
-      value={row.summable ?? false}
-      onChange={onChange}
-      validationState={validations?.summable?.state}
-      validationMessage={validations?.summable?.message}
-    />
-    <TextField
-      label="Sort value"
-      name="sort"
-      type="number"
-      value={row.sort ?? ''}
-      onChange={onChange}
-      validationState={validations?.sort?.state}
-      validationMessage={validations?.sort?.message}
-    />
-    <TextField
-      label="List"
-      name="list_id"
-      value={row.list_id ?? ''}
-      onChange={onChange}
-      validationState={validations?.list_id?.state}
-      validationMessage={validations?.list_id?.message}
-    />
-  </>
-)
+export const UnitForm = ({ onChange, row, autoFocusRef, validations = {} }) => {
+  const { formatMessage } = useIntl()
+
+  return (
+    <>
+      <TextField
+        label={formatMessage({ id: 'XkV5yZ', defaultMessage: 'Name' })}
+        name="name"
+        value={row.name ?? ''}
+        onChange={onChange}
+        autoFocus
+        ref={autoFocusRef}
+        validationState={validations?.name?.state}
+        validationMessage={validations?.name?.message}
+      />
+      <SwitchField
+        label={formatMessage({ id: 'Xa1BcD', defaultMessage: 'Für Massnahmen-Mengen verwenden' })}
+        name="use_for_action_values"
+        value={row.use_for_action_values ?? false}
+        onChange={onChange}
+        validationState={validations?.use_for_action_values?.state}
+        validationMessage={validations?.use_for_action_values?.message}
+      />
+      <SwitchField
+        label={formatMessage({ id: 'Yb2CdE', defaultMessage: 'Für Massnahmen-Berichte-Mengen verwenden' })}
+        name="use_for_action_report_values"
+        value={row.use_for_action_report_values ?? false}
+        onChange={onChange}
+        validationState={validations?.use_for_action_report_values?.state}
+        validationMessage={validations?.use_for_action_report_values?.message}
+      />
+      <SwitchField
+        label={formatMessage({ id: 'Zc3DeF', defaultMessage: 'Für Kontroll-Mengen verwenden' })}
+        name="use_for_check_values"
+        value={row.use_for_check_values ?? false}
+        onChange={onChange}
+        validationState={validations?.use_for_check_values?.state}
+        validationMessage={validations?.use_for_check_values?.message}
+      />
+      <SwitchField
+        label={formatMessage({ id: 'Ad4EfG', defaultMessage: 'Für Ort-Bericht-Mengen verwenden' })}
+        name="use_for_place_report_values"
+        value={row.use_for_place_report_values ?? false}
+        onChange={onChange}
+        validationState={validations?.use_for_place_report_values?.state}
+        validationMessage={validations?.use_for_place_report_values?.message}
+      />
+      <SwitchField
+        label={formatMessage({ id: 'Be5FgH', defaultMessage: 'Für Ziel-Bericht-Mengen verwenden' })}
+        name="use_for_goal_report_values"
+        value={row.use_for_goal_report_values ?? false}
+        onChange={onChange}
+        validationState={validations?.use_for_goal_report_values?.state}
+        validationMessage={validations?.use_for_goal_report_values?.message}
+      />
+      <SwitchField
+        label={formatMessage({ id: 'Cf6GhI', defaultMessage: 'Für Teilprojekt-Taxa verwenden' })}
+        name="use_for_subproject_taxa"
+        value={row.use_for_subproject_taxa ?? false}
+        onChange={onChange}
+        validationState={validations?.use_for_subproject_taxa?.state}
+        validationMessage={validations?.use_for_subproject_taxa?.message}
+      />
+      <SwitchField
+        label={formatMessage({ id: 'Dg7HiJ', defaultMessage: 'Für Kontroll-Taxa verwenden' })}
+        name="use_for_check_taxa"
+        value={row.use_for_check_taxa ?? false}
+        onChange={onChange}
+        validationState={validations?.use_for_check_taxa?.state}
+        validationMessage={validations?.use_for_check_taxa?.message}
+      />
+      <SwitchField
+        label={formatMessage({ id: 'Eh8IjK', defaultMessage: 'Summierbar' })}
+        name="summable"
+        value={row.summable ?? false}
+        onChange={onChange}
+        validationState={validations?.summable?.state}
+        validationMessage={validations?.summable?.message}
+      />
+      <TextField
+        label={formatMessage({ id: 'Pq7nWk', defaultMessage: 'Sortierwert' })}
+        name="sort"
+        type="number"
+        value={row.sort ?? ''}
+        onChange={onChange}
+        validationState={validations?.sort?.state}
+        validationMessage={validations?.sort?.message}
+      />
+      <TextField
+        label={formatMessage({ id: '4+BE1s', defaultMessage: 'Liste' })}
+        name="list_id"
+        value={row.list_id ?? ''}
+        onChange={onChange}
+        validationState={validations?.list_id?.state}
+        validationMessage={validations?.list_id?.message}
+      />
+    </>
+  )
+}
