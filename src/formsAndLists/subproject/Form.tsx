9fb2f0cf863e41234c9c455b21c1851eb5e006af
Alexander Gabriel <alex.barbalex@gmail.com>
2026-03-14 12:29:40 +0100

    translate

--- src/formsAndLists/subproject/Form.tsx
+++ src/formsAndLists/subproject/Form.tsx
@@ -1,3 +1,5 @@
+import { useIntl } from 'react-intl'
+
 import { TextField } from '../../components/shared/TextField.tsx'
 import { Jsonb } from '../../components/shared/Jsonb/index.tsx'
 
@@ -8,34 +10,38 @@ export const SubprojectForm = ({
   from,
   autoFocusRef,
   validations = {},
-}) => (
-  <>
-    <TextField
-      label="Name"
-      name="name"
-      value={row.name ?? ''}
-      onChange={onChange}
-      autoFocus
-      ref={autoFocusRef}
-      validationState={validations?.name?.state}
-      validationMessage={validations?.name?.message}
-    />
-    <TextField
-      label="Start year"
-      name="start_year"
-      value={row.start_year ?? ''}
-      type="number"
-      onChange={onChange}
-      validationState={validations?.start_year?.state}
-      validationMessage={validations?.start_year?.message}
-    />
-    <Jsonb
-      table="subprojects"
-      idField="subproject_id"
-      id={row.subproject_id}
-      data={row.data ?? {}}
-      orIndex={orIndex}
-      from={from}
-    />
-  </>
-)
+}) => {
+  const { formatMessage } = useIntl()
+
+  return (
+    <>
+      <TextField
+        label={formatMessage({ id: 'XkV5yZ', defaultMessage: 'Name' })}
+        name="name"
+        value={row.name ?? ''}
+        onChange={onChange}
+        autoFocus
+        ref={autoFocusRef}
+        validationState={validations?.name?.state}
+        validationMessage={validations?.name?.message}
+      />
+      <TextField
+        label={formatMessage({ id: 'bEkKpP', defaultMessage: 'Startjahr' })}
+        name="start_year"
+        value={row.start_year ?? ''}
+        type="number"
+        onChange={onChange}
+        validationState={validations?.start_year?.state}
+        validationMessage={validations?.start_year?.message}
+      />
+      <Jsonb
+        table="subprojects"
+        idField="subproject_id"
+        id={row.subproject_id}
+        data={row.data ?? {}}
+        orIndex={orIndex}
+        from={from}
+      />
+    </>
+  )
+}
