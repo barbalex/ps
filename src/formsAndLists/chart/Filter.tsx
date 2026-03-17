5c614f819c350498f0ed6c77961e210a1e00e38c
Alexander Gabriel <alex.barbalex@gmail.com>
2026-03-17 22:36:04 +0100

    translate

--- src/formsAndLists/chart/Filter.tsx
+++ src/formsAndLists/chart/Filter.tsx
@@ -1,3 +1,4 @@
+import { useIntl } from 'react-intl'
 import { Filter } from '../../components/shared/Filter/index.tsx'
 import { TextField } from '../../components/shared/TextField.tsx'
 import { RadioGroupField } from '../../components/shared/RadioGroupField.tsx'
@@ -8,22 +9,33 @@ type Props = {
 }
 
 export const ChartFilter = ({ from }: Props) => {
+  const { formatMessage } = useIntl()
   const list = chartTypeOptions.map((o) => o.value)
+  const labelMap = Object.fromEntries(
+    chartTypeOptions.map((o) => [
+      o.value,
+      formatMessage({ id: o.labelId, defaultMessage: o.defaultMessage }),
+    ]),
+  )
 
   return (
     <Filter from={from}>
       {({ row, onChange }) => (
         <>
           <TextField
-            label="Name"
+            label={formatMessage({ id: 'XkV5yZ', defaultMessage: 'Name' })}
             name="name"
             value={row.name ?? ''}
             onChange={onChange}
           />
           <RadioGroupField
-            label="Chart Type"
+            label={formatMessage({
+              id: 'bCHkLm',
+              defaultMessage: 'Diagrammtyp',
+            })}
             name="chart_type"
             list={list}
+            labelMap={labelMap}
             value={row.chart_type ?? ''}
             onChange={onChange}
           />
