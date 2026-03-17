3abaca11f2b8885a958ae93e95581803466bf4ed
Alexander Gabriel <alex.barbalex@gmail.com>
2026-03-17 23:20:55 +0100

    translate

--- src/formsAndLists/subprojectTaxon/Filter.tsx
+++ src/formsAndLists/subprojectTaxon/Filter.tsx
@@ -1,3 +1,4 @@
+import { useIntl } from 'react-intl'
 import { Filter } from '../../components/shared/Filter/index.tsx'
 import { DropdownField } from '../../components/shared/DropdownField.tsx'
 
@@ -5,20 +6,23 @@ type Props = {
   from: string
 }
 
-export const SubprojectTaxonFilter = ({ from }: Props) => (
-  <Filter
-    from={from}
-    tableNameOverride="subproject_taxa"
-    filterAtomNameOverride="subprojectTaxaFilterAtom"
-  >
-    {({ row, onChange }) => (
-      <DropdownField
-        label="Taxon"
-        name="taxon_id"
-        table="taxa"
-        value={row.taxon_id ?? ''}
-        onChange={onChange}
-      />
-    )}
-  </Filter>
-)
+export const SubprojectTaxonFilter = ({ from }: Props) => {
+  const { formatMessage } = useIntl()
+  return (
+    <Filter
+      from={from}
+      tableNameOverride="subproject_taxa"
+      filterAtomNameOverride="subprojectTaxaFilterAtom"
+    >
+      {({ row, onChange }) => (
+        <DropdownField
+          label={formatMessage({ id: 'OSk4zO', defaultMessage: 'Taxon' })}
+          name="taxon_id"
+          table="taxa"
+          value={row.taxon_id ?? ''}
+          onChange={onChange}
+        />
+      )}
+    </Filter>
+  )
+}
