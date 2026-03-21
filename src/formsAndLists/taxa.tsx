c2424ee479c399ff5bc23d147d6804a1edc3ef25
Alexander Gabriel <alex.barbalex@gmail.com>
2026-03-22 00:42:06 +0100

    work on importing taxa

--- src/formsAndLists/taxa.tsx
+++ src/formsAndLists/taxa.tsx
@@ -1,4 +1,9 @@
 import { useParams, useNavigate } from '@tanstack/react-router'
+import { useState } from 'react'
+import * as fluentUiReactComponents from '@fluentui/react-components'
+const { Button, Tooltip } = fluentUiReactComponents
+import { FaFileImport } from 'react-icons/fa'
+import { useIntl } from 'react-intl'
 
 import { createTaxon } from '../modules/createRows.ts'
 import { useTaxaNavData } from '../modules/useTaxaNavData.ts'
@@ -12,6 +17,8 @@ const from = '/data/projects/$projectId_/taxonomies/$taxonomyId_/taxa/'
 export const Taxa = () => {
   const { projectId, taxonomyId } = useParams({ from })
   const navigate = useNavigate()
+  const { formatMessage } = useIntl()
+  const [importDialogOpen, setImportDialogOpen] = useState(false)
 
   const { loading, navData } = useTaxaNavData({
     projectId,
@@ -28,9 +35,30 @@ export const Taxa = () => {
     })
   }
 
+  const importButton = (
+    <Tooltip
+      content={formatMessage({
+        id: 'tImport',
+        defaultMessage: 'Taxa aus Datei importieren',
+      })}
+      relationship="label"
+    >
+      <Button
+        size="medium"
+        icon={<FaFileImport />}
+        onClick={() => setImportDialogOpen(true)}
+      />
+    </Tooltip>
+  )
+
   return (
     <div className="list-view">
-      <ListHeader label={label} nameSingular={nameSingular} addRow={add} />
+      <ListHeader
+        label={label}
+        nameSingular={nameSingular}
+        addRow={add}
+        menus={[importButton]}
+      />
       <div className="list-container">
         {loading ? (
           <Loading />
