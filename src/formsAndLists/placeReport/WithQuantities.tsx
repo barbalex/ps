63daf1092a6cc6d16f11cb3037850dc745986fde
Alexander Gabriel <alex.barbalex@gmail.com>
2026-03-23 12:11:40 +0100

    implement place_report_quantities_in_report

--- src/formsAndLists/placeReport/WithQuantities.tsx
+++ src/formsAndLists/placeReport/WithQuantities.tsx
@@ -125,6 +125,15 @@ export const PlaceReportWithQuantities = ({ from }) => {
                     />
                   </div>
                 ))}
+                {quantities.length > 0 && (
+                  <div
+                    style={{
+                      borderTop: '8px solid rgb(225, 247, 224)',
+                      marginLeft: -10,
+                      marginRight: -10,
+                    }}
+                  />
+                )}
                 <Tooltip
                   content={formatMessage({
                     id: 'V6iUlF',
