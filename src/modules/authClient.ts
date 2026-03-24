7a05a57b4510ee06db8a61ab2c7b0c75a0a9e987
Alexander Gabriel <alex.barbalex@gmail.com>
2026-03-24 11:24:38 +0100

    work on sync

--- src/modules/authClient.ts
+++ src/modules/authClient.ts
@@ -2,5 +2,5 @@ import { createAuthClient } from 'better-auth/react'
 
 export const { signIn, signUp, useSession, getSession } = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: 'http://localhost:3002',
+  baseURL: 'http://localhost:3003',
 })
