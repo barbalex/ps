c25b41baf1ee91b129165850f9a9e05b7a47e654
Alexander Gabriel <alex.barbalex@gmail.com>
2026-03-26 11:25:46 +0100

    translate

--- src/formsAndLists/placeCheckReport/Header.tsx
+++ src/formsAndLists/placeCheckReport/Header.tsx
@@ -1,12 +1,12 @@
 import { useParams, useNavigate } from '@tanstack/react-router'
 import { usePGlite, useLiveQuery } from '@electric-sql/pglite-react'
-import { useSetAtom, useAtom } from 'jotai'
+import { useSetAtom } from 'jotai'
 import { useRef, useEffect } from 'react'
 import { useIntl } from 'react-intl'
 
 import { createPlaceCheckReport } from '../../modules/createRows.ts'
 import { FormHeader } from '../../components/FormHeader/index.tsx'
-import { addOperationAtom, languageAtom } from '../../store.ts'
+import { addOperationAtom } from '../../store.ts'
 
 export const Header = ({ autoFocusRef, from }) => {
   const { projectId, placeId, placeId2, placeCheckReportId } = useParams({
@@ -15,7 +15,6 @@ export const Header = ({ autoFocusRef, from }) => {
   const navigate = useNavigate()
   const addOperation = useSetAtom(addOperationAtom)
   const { formatMessage } = useIntl()
-  const [language] = useAtom(languageAtom)
 
   const db = usePGlite()
 
@@ -27,21 +26,14 @@ export const Header = ({ autoFocusRef, from }) => {
   }, [placeCheckReportId])
 
   const combinedRes = useLiveQuery(
-    `SELECT
-      pl.name_singular_${language} AS name_singular,
-      (SELECT COUNT(*) FROM place_check_reports WHERE place_id = $1) AS count
+    `SELECT (SELECT COUNT(*) FROM place_check_reports WHERE place_id = $1) AS count
     FROM place_levels pl
     WHERE pl.project_id = $2 AND pl.level = $3`,
     [placeId2 ?? placeId, projectId, placeId2 ? 2 : 1],
   )
-  const nameSingular = combinedRes?.rows?.[0]?.name_singular as
-    | string
-    | undefined
   const rowCount = combinedRes?.rows?.[0]?.count ?? 2
 
-  const title = nameSingular
-    ? `${nameSingular}-${formatMessage({ id: 'bCSwTx', defaultMessage: 'Kontroll-Bericht' })}`
-    : formatMessage({ id: 'bCTxUy', defaultMessage: 'Ort-Kontroll-Bericht' })
+  const title = formatMessage({ id: 'bCSwTx', defaultMessage: 'Kontroll-Bericht' })
 
   const addRow = async () => {
     const id = await createPlaceCheckReport({
