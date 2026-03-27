6f6bc210f350041802eb6d8a1d10f6359b3e900f
Alexander Gabriel <alex.barbalex@gmail.com>
2026-03-27 13:30:21 +0100

    optimize rendering

--- src/routes/data/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/action-reports/$placeActionReportId_/quantities/route.tsx
+++ src/routes/data/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/action-reports/$placeActionReportId_/quantities/route.tsx
@@ -12,9 +12,8 @@ const QuantitiesWrapper = () => {
     `SELECT place_action_report_quantities_in_report FROM place_levels WHERE project_id = $1 AND level = 1`,
     [projectId],
   )
-  if (!res) return null
   const quantitiesInReport =
-    res.rows?.[0]?.place_action_report_quantities_in_report !== false
+    res?.rows?.[0]?.place_action_report_quantities_in_report !== false
   if (quantitiesInReport) return <PlaceActionReportWithQuantities from={from} />
   return <Outlet />
 }
