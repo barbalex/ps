bc647d9bb3fc6f88692026adbe7c161acdb6cfb3
Alexander Gabriel <alex.barbalex@gmail.com>
2026-03-28 00:22:17 +0100

    install pg_partman

--- backend/db/init/03_extensions.sql
+++ backend/db/init/03_extensions.sql
@@ -1,2 +1,6 @@
 CREATE EXTENSION temporal_tables;
 CREATE EXTENSION postgis;
+CREATE SCHEMA partman;
+CREATE EXTENSION pg_partman SCHEMA partman;
+-- TODO: add dedicated user for pg_partman
+-- see: https://neon.com/docs/extensions/pg_partman#enable-the-pgpartman-extension
