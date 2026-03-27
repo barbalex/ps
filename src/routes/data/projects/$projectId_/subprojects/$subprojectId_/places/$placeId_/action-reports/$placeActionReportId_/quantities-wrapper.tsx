adb64801b1a6bc9c5f886d96d125e39fceaaf119
Alexander Gabriel <alex.barbalex@gmail.com>
2026-03-27 15:29:17 +0100

    correct

--- src/routes/data/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/action-reports/$placeActionReportId_/quantities-wrapper.tsx
+++ src/routes/data/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/action-reports/$placeActionReportId_/quantities-wrapper.tsx
@@ -16,36 +16,3 @@ export const QuantitiesWrapper = () => {
   if (quantitiesInReport) return <PlaceActionReportWithQuantities from={from} />
   return <Outlet />
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}  return <Outlet />  if (quantitiesInReport) return <PlaceActionReportWithQuantities from={from} />    res?.rows?.[0]?.place_action_report_quantities_in_report !== false  const quantitiesInReport =  )    [projectId],    `SELECT place_action_report_quantities_in_report FROM place_levels WHERE project_id = $1 AND level = 1`,  const res = useLiveQuery(  const { projectId } = useParams({ strict: false })export const QuantitiesWrapper = () => {  '/data/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/action-reports/$placeActionReportId_'const from =import { PlaceActionReportWithQuantities } from '../../../../../../../../../../formsAndLists/placeActionReport/WithQuantities.tsx'import { useLiveQuery } from '@electric-sql/pglite-react'
-export const Route = createFileRoute(
-  '/data/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/action-reports/$placeActionReportId_/quantities-wrapper',
-)({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return (
-    <div>
-      Hello
-      "/data/projects/$projectId_/subprojects/$subprojectId_/places/$placeId_/action-reports/$placeActionReportId_/quantities-wrapper"!
-    </div>
-  )
-}
