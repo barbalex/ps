8ebab627116dc29e75e759855016c68a71fdcddc
Alexander Gabriel <alex.barbalex@gmail.com>
2026-03-28 12:28:10 +0100

    work on pg

--- backend/db/init/03_extensions.sql
+++ backend/db/init/03_extensions.sql
@@ -1,6 +1,8 @@
-CREATE EXTENSION temporal_tables;
-CREATE EXTENSION postgis;
-CREATE SCHEMA partman;
-CREATE EXTENSION pg_partman SCHEMA partman;
+CREATE EXTENSION IF NOT EXISTS temporal_tables;
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS dblink;
+CREATE EXTENSION IF NOT EXISTS pg_jobmon;
+CREATE SCHEMA IF NOT EXISTS partman;
+CREATE EXTENSION IF NOT EXISTS pg_partman SCHEMA partman;
 -- TODO: add dedicated user for pg_partman
 -- see: https://neon.com/docs/extensions/pg_partman#enable-the-pgpartman-extension
