f0a9ba8c0f369d18e267b57cc87bf45aaa23cde5
Alexander Gabriel <alex.barbalex@gmail.com>
2026-03-29 11:47:14 +0200

    update deps and remove tanstack devtools

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,10 +1,8 @@
 import { Outlet, Scripts } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 export const Root = () => (
   <>
     <Outlet />
     <Scripts />
-    <TanStackRouterDevtools />
   </>
 )
