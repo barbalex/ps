52620a96d481e4bd9c08fcafae1d5e92d5144879
Alexander Gabriel <alex.barbalex@gmail.com>
2026-04-07 16:58:06 +0200

    lint

--- src/routes/_layout.index.tsx
+++ src/routes/_layout.index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-import { Home } from '../components'
+import { Home } from '../components/index.tsx'
 
 export const Route = createFileRoute('/_layout/')({
   component: Home,
