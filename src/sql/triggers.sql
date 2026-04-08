da1a4b12873c2c7b787c5b7656eedbd1f521d1c1
Alexander Gabriel <alex.barbalex@gmail.com>
2026-04-08 12:56:06 +0200

    catch

--- src/sql/triggers.sql
+++ src/sql/triggers.sql
@@ -854,6 +854,32 @@ BEFORE INSERT ON taxonomies
 FOR EACH ROW
 EXECUTE PROCEDURE taxonomies_sync_ignore_duplicate_insert_trigger();
 
+CREATE OR REPLACE FUNCTION subproject_taxa_sync_ignore_duplicate_insert_trigger()
+RETURNS TRIGGER AS $$
+DECLARE
+  is_syncing BOOLEAN;
+BEGIN
+  SELECT COALESCE(NULLIF(current_setting('electric.syncing', true), ''), 'false')::boolean INTO is_syncing;
+
+  IF is_syncing AND EXISTS (
+    SELECT 1
+    FROM subproject_taxa
+    WHERE subproject_taxon_id = NEW.subproject_taxon_id
+  ) THEN
+    RETURN NULL;
+  END IF;
+
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS subproject_taxa_sync_ignore_duplicate_insert_trigger ON subproject_taxa;
+
+CREATE TRIGGER subproject_taxa_sync_ignore_duplicate_insert_trigger
+BEFORE INSERT ON subproject_taxa
+FOR EACH ROW
+EXECUTE PROCEDURE subproject_taxa_sync_ignore_duplicate_insert_trigger();
+
 -- taxa.label
 CREATE OR REPLACE FUNCTION taxa_label_trigger()
 RETURNS TRIGGER AS $$
