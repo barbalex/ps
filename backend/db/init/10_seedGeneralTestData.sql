0337b9fba3b5d0d19afb3301d5e2f7c93cdfb598
Alexander Gabriel <alex.barbalex@gmail.com>
2026-04-10 20:19:13 +0200

    see test user

--- backend/db/init/10_seedGeneralTestData.sql
+++ backend/db/init/10_seedGeneralTestData.sql
@@ -1,5 +1,8 @@
 -- users
 INSERT INTO users(user_id, email) values ('018cf95a-d817-7000-92fa-bb3b2ad59dda', 'alex.barbalex@gmail.com');
+INSERT INTO users(email, name, label)
+VALUES ('test@test.ch', 'Preview Test User', 'test@test.ch')
+ON CONFLICT (email) DO NOTHING;
 -- accounts
 INSERT INTO accounts(account_id, user_id, type) values ('018cf958-27e2-7000-90d3-59f024d467be', '018cf95a-d817-7000-92fa-bb3b2ad59dda', 'premium');
 -- fieldTypes
