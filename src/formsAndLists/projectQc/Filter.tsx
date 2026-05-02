0b5582d1d49eb58d0a638be1638c42554248084d
Alexander Gabriel <alex.barbalex@gmail.com>
2026-05-02 23:10:12 +0200

    work on project qcs

--- src/formsAndLists/projectQc/Filter.tsx
+++ src/formsAndLists/projectQc/Filter.tsx
@@ -1,28 +1,17 @@
-import { useIntl } from 'react-intl'
 import { Filter } from '../../components/shared/Filter/index.tsx'
-import { TextField } from '../../components/shared/TextField.tsx'
+import { ProjectQcForm } from './Form.tsx'
 
 type Props = {
   from: string
 }
 
-export const ProjectQcFilter = ({ from }: Props) => {
-  const { formatMessage } = useIntl()
-  return (
-    <Filter
-      from={from}
-      tableNameOverride="project_qcs"
-    >
-      {({ row, onChange }) => (
-        <>
-          <TextField
-            label={formatMessage({ id: 'XkV5yZ', defaultMessage: 'Name' })}
-            name="name_de"
-            value={row.name_de ?? ''}
-            onChange={onChange}
-          />
-        </>
-      )}
-    </Filter>
-  )
-}
+export const ProjectQcFilter = ({ from }: Props) => (
+  <Filter
+    from={from}
+    tableNameOverride="project_qcs"
+  >
+    {({ row, onChange }) => (
+      <ProjectQcForm row={row} onChange={onChange} from={from} />
+    )}
+  </Filter>
+)
