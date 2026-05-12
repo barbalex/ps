a18d6f95ca96df49e14c2c10141b0223d2ef11ff
Alexander Gabriel <alex.barbalex@gmail.com>
2026-05-12 12:52:57 +0200

    add manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import svgr from 'vite-plugin-svgr'
 import formatjs from '@formatjs/vite-plugin'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
@@ -33,6 +34,59 @@ export default defineConfig({
       ast: true,
     }),
     svgr(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['robots.txt'],
+      manifest: {
+        scope: '.',
+        name: 'Arten fördern App',
+        short_name: 'Arten fördern',
+        start_url: '/data',
+        // https://web.dev/add-manifest/:
+        // Your start_url should direct the user straight into your app,
+        // rather than a product landing page.
+        // Think about what the user will want to do once they open your app,
+        // and place them there
+        display: 'standalone',
+        background_color: '#265225e6',
+        theme_color: '#265225e6',
+        icons: [
+          {
+            src: '/icon_072.png',
+            sizes: '72x72',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/icon_144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/icon_192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/icon_512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/icon_1024.png',
+            sizes: '1024x1024',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        orientation: 'portrait',
+        description:
+          'Naturschutzfachleute fördern bedrohte Arten und Lebensräume',
+      },
+    }),
   ],
   envPrefix: ['VITE_', 'ELECTRIC_'],
   optimizeDeps: {
