dbe46134ff4c9110d12d49ae6fe24bb60c5e8040
Alexander Gabriel <alex.barbalex@gmail.com>
2026-06-23 15:48:07 +0200

    remove no more used account_id

--- src/components/Map/layers/TableLayers/Places2.tsx
+++ src/components/Map/layers/TableLayers/Places2.tsx
@@ -13,7 +13,7 @@ export const Places2 = ({ layerPresentation }) => {
   const placeId2 = secondPlacesIdx !== -1 ? pathParts[secondPlacesIdx + 1] : undefined
   // TODO: query only inside current map bounds using places.bbox
   const res = useLiveQuery(
-    `SELECT place_id, account_id, subproject_id, parent_id, level, since, until, data,
+    `SELECT place_id, subproject_id, parent_id, level, since, until, data,
       ST_AsGeoJSON(geometry)::json as geometry, bbox, relevant_for_reports, label, created_at, updated_at, updated_by
     FROM places WHERE parent_id IS NOT NULL AND geometry IS NOT NULL`,
 
