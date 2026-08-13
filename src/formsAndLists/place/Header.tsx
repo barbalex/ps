cf7835509ea277a5573def86dc27ca1654d74eca
Alexander Gabriel <alex.barbalex@gmail.com>
2026-08-13 19:02:40 +0200

    do

--- src/formsAndLists/place/Header.tsx
+++ src/formsAndLists/place/Header.tsx
@@ -85,7 +85,7 @@ export const Header = ({
       type: 'own',
       ownTable: 'places',
       ownTableLevel: placeId2 ? 2 : 1,
-      label: namePlural,
+      name: placeId2 ? 'places_2' : 'places_1',
     })
 
     const idName = placeId2 ? 'placeId2' : 'placeId'
