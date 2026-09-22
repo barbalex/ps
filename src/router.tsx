619cce1ae1c7e77af935cf016902c038c4f8962b
Alexander Gabriel <alex.barbalex@gmail.com>
2026-09-22 10:50:41 +0200

    type

--- src/router.tsx
+++ src/router.tsx
@@ -4,13 +4,13 @@ import { routeTree } from './routeTree.gen'
 import { RouterErrorBoundary } from './components/shared/RouterErrorBoundary.tsx'
 import { DefaultPending } from './components/shared/DefaultPending.tsx'
 
-// TODO: add defaultErrorComponent
-// https://tanstack.com/start/latest/docs/framework/react/guide/error-boundaries
 export const router = createRouter({
   routeTree,
   scrollRestoration: true,
   defaultPendingComponent: DefaultPending,
-  defaultErrorComponent: ({ error }) => <RouterErrorBoundary error={error} />,
+  defaultErrorComponent: ({ error }) => (
+    <RouterErrorBoundary error={error as Error} />
+  ),
 })
 
 declare module '@tanstack/react-router' {
